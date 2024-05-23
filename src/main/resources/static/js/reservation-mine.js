9905a805a8f2fb05c95464e11bbcfeb516aaa697
solarsalt <solar@woowahan.com>
2024-05-23 18:33:33 +0900

    feat: 2단계 진행을 위한 클라이언트 코드

--- src/main/resources/static/js/reservation-mine.js
+++ src/main/resources/static/js/reservation-mine.js
@@ -19,17 +19,16 @@ function render(data) {
   data.forEach(item => {
     const row = tableBody.insertRow();
 
-    /*
-    TODO: [2단계] 내 예약 목록 조회 기능
-          response 명세에 맞춰 값 설정
-     */
     row.insertCell(0).textContent = item.theme;
     row.insertCell(1).textContent = item.date;
     row.insertCell(2).textContent = item.time;
     row.insertCell(3).textContent = item.status;
 
+
     /*
-    TODO: [2단계] 예약 대기 기능 - 예약 대기 취소 기능 구현 후 활성화
+    TODO: [2단계] 내 예약 목록 조회 시,
+     예약 완료 상태일 때 결제 정보를 함께 보여주기
+     결제 정보 필드명은 자신의 response 에 맞게 변경하기
      */
     if (item.status !== '예약') { // 예약 대기 상태일 때 예약 대기 취소 버튼 추가
       const cancelCell = row.insertCell(4);
@@ -42,6 +41,8 @@ function render(data) {
       cancelCell.appendChild(cancelButton);
     } else { // 예약 완료 상태일 때
       row.insertCell(4).textContent = '';
+      row.insertCell(5).textContent = item.paymentKey;
+      row.insertCell(6).textContent = item.amount;
     }
   });
 }
