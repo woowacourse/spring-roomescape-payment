0f72314ad1686260837dc3e81ca4bfa22a3dbb09
solarsalt <solar@woowahan.com>
2024-05-23 21:19:37 +0900

    feat:[2단계] 내 예약 수정 클라이언트 코드
    - 내 예약 목록 조회 시, 결제 정보 포함

--- src/main/resources/static/js/reservation-mine.js
+++ src/main/resources/static/js/reservation-mine.js
@@ -24,20 +24,28 @@ function render(data) {
         row.insertCell(1).textContent = date;
         row.insertCell(2).textContent = time;
         row.insertCell(3).textContent = status;
+        
+    if (status !== '예약') { // 예약 대기 상태일 때 예약 대기 취소 버튼 추가하는 코드, 상태 값은 변경 가능
+      const cancelCell = row.insertCell(4);
+      const cancelButton = document.createElement('button');
+      cancelButton.textContent = '취소';
+      cancelButton.className = 'btn btn-danger';
+      cancelButton.onclick = function () {
+        requestDeleteWaiting(item.id).then(() => window.location.reload());
+      };
+      cancelCell.appendChild(cancelButton);
+    } else { // 예약 완료 상태일 때
+      /*
+    TODO: [미션4 - 2단계] 내 예약 목록 조회 시,
+     예약 완료 상태일 때 결제 정보를 함께 보여주기
+     결제 정보 필드명은 자신의 response 에 맞게 변경하기
+     */
+      row.insertCell(4).textContent = '';
+      row.insertCell(5).textContent = item.paymentKey;
+      row.insertCell(6).textContent = item.amount;
+    }
+  });
 
-        if (status !== '예약') { // 예약 대기 상태일 때 예약 대기 취소 버튼 추가하는 코드, 상태 값은 변경 가능
-            const cancelCell = row.insertCell(4);
-            const cancelButton = document.createElement('button');
-            cancelButton.textContent = '취소';
-            cancelButton.className = 'btn btn-danger';
-            cancelButton.onclick = function () {
-                requestDeleteWaiting(item.id).then(() => window.location.reload());
-            };
-            cancelCell.appendChild(cancelButton);
-        } else { // 예약 완료 상태일 때
-            row.insertCell(4).textContent = '';
-        }
-    });
 }
 
 function requestDeleteWaiting(id) {
