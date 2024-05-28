78ba491f59a9eec9810f6867aca91fae2bb4228c
Choo <choo000407@naver.com>
2024-05-28 21:17:43 +0900

    feat: 초기 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,9 @@ INSERT INTO member (name, email, password, role)
 VALUES ('관리자', 'admin@gmail.com', '1234567890', 'ADMIN');
 INSERT INTO member (name, email, password, role)
 VALUES ('사용자', 'user@gmail.com', '1234567890', 'USER');
+
+INSERT INTO theme (name, description, thumbnail)
+VALUES ('레벨2', '설명이야', '썸네일이야');
+
+INSERT INTO reservation_time (start_at)
+VALUES ('10:00');
