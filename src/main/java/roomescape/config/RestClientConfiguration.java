a103a047e129c1b29d21abc5756427e170138580
leetaehoon <xogns1514@gmail.com>
2024-05-28 20:58:19 +0900

    refactor(RestClientConfiguration): HttpRequestFactory 주입 객체 변경

--- src/main/java/roomescape/config/RestClientConfiguration.java
+++ src/main/java/roomescape/config/RestClientConfiguration.java
@@ -2,17 +2,20 @@ package roomescape.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.JdkClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 
 @Configuration
 public class RestClientConfiguration {
-
-    private static final int CONNECT_TIMEOUT = 10_000;
+    private static final int CONNECT_TIMEOUT = 20_000;
+    private static final int CONNECT_REQUEST_TIMEOUT = 10_000;
 
     @Bean
-    public JdkClientHttpRequestFactory httpRequestFactoryConfig() {
-        JdkClientHttpRequestFactory factory = new JdkClientHttpRequestFactory();
-        factory.setReadTimeout(CONNECT_TIMEOUT);
+    HttpComponentsClientHttpRequestFactory factory() {
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+        factory.setConnectTimeout(3000);
+        factory.setConnectTimeout(CONNECT_TIMEOUT);
+        factory.setConnectionRequestTimeout(CONNECT_REQUEST_TIMEOUT);
+
         return factory;
     }
 }
