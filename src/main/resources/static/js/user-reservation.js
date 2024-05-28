7364005d8e1591a3d90ff6bb348c9f4b8834f2f1
hyxrxn <gpfls981220@gmail.com>
2024-05-28 17:30:00 +0900

    refactor(client): orderIdPrefix 변경
    Co-authored-by: ashsty <qkralsdk991@naver.com>

--- src/main/resources/static/js/user-reservation.js
+++ src/main/resources/static/js/user-reservation.js
@@ -171,13 +171,9 @@ function onReservationButtonClick(event, paymentWidget) {
 
     const generateRandomString = () =>
         window.btoa(Math.random()).slice(0, 20);
-    /*
-    TODO: [1단계]
-          - orderIdPrefix 를 자신만의 prefix로 변경
-    */
     // TOSS 결제 위젯 Javascript SDK 연동 방식 중 'Promise로 처리하기'를 적용함
     // https://docs.tosspayments.com/reference/widget-sdk#promise%EB%A1%9C-%EC%B2%98%EB%A6%AC%ED%95%98%EA%B8%B0
-    const orderIdPrefix = "WTEST";
+    const orderIdPrefix = "ASHANDATTO";
     paymentWidget.requestPayment({
       orderId: orderIdPrefix + generateRandomString(),
       orderName: "테스트 방탈출 예약 결제 1건",
