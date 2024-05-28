7331798277eb3629aaf35fd21f71821d8540bcf2
eunjungL <dldmswjd322@gmail.com>
2024-05-28 17:17:41 +0900

    feat: 결제 승인 성공 시 Payment 저장 구현

--- src/main/java/roomescape/payment/domain/Payment.java
+++ src/main/java/roomescape/payment/domain/Payment.java
@@ -5,6 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import org.springframework.data.annotation.CreatedDate;
+import roomescape.payment.dto.PaymentResponse;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -22,5 +23,22 @@ public class Payment {
     private LocalDateTime createdAt;
 
     protected Payment() {
+    }
+
+    public Payment(Long id, String paymentKey, String orderId, BigDecimal amount, LocalDateTime createdAt) {
+        this.id = id;
+        this.paymentKey = paymentKey;
+        this.orderId = orderId;
+        this.amount = amount;
+        this.createdAt = createdAt;
+    }
+
+    public Payment(String paymentKey, String orderId, BigDecimal amount) {
+        this(null, paymentKey, orderId, amount, null);
+    }
+
+    public static Payment from(PaymentResponse paymentResponse) {
+        BigDecimal totalAmount = BigDecimal.valueOf(paymentResponse.totalAmount());
+        return new Payment(paymentResponse.paymentKey(), paymentResponse.orderId(), totalAmount);
     }
 }
--- src/main/java/roomescape/payment/service/PaymentService.java
+++ src/main/java/roomescape/payment/service/PaymentService.java
@@ -7,23 +7,28 @@ import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 import roomescape.exception.BadRequestException;
+import roomescape.exception.PaymentFailureException;
+import roomescape.payment.domain.Payment;
 import roomescape.payment.dto.PaymentFailure;
 import roomescape.payment.dto.PaymentRequest;
 import roomescape.payment.dto.PaymentResponse;
+import roomescape.payment.repository.PaymentRepository;
 
 import java.util.Base64;
+import java.util.Optional;
 
 @Service
 public class PaymentService {
 
     private final ObjectMapper objectMapper;
-
     private final RestClient restClient;
+    private final PaymentRepository paymentRepository;
     @Value("${toss.secret}")
     private String tossSecretKey;
 
-    public PaymentService(ObjectMapper objectMapper) {
+    public PaymentService(ObjectMapper objectMapper, PaymentRepository paymentRepository) {
         this.objectMapper = objectMapper;
+        this.paymentRepository = paymentRepository;
         this.restClient = RestClient.builder()
                 .baseUrl("https://api.tosspayments.com/v1/payments")
                 .build();
@@ -32,7 +37,15 @@ public class PaymentService {
     public void confirmPayment(PaymentRequest request) {
         String token = Base64.getEncoder().encodeToString((tossSecretKey + ":").getBytes());
 
-        PaymentResponse response = restClient.post()
+        PaymentResponse response = getPaymentResponse(request, token)
+                .orElseThrow(() -> new PaymentFailureException("결제를 승인하던 중 오류가 발생했습니다."));
+
+        Payment payment = Payment.from(response);
+        paymentRepository.save(payment);
+    }
+
+    private Optional<PaymentResponse> getPaymentResponse(PaymentRequest request, String token) {
+        return Optional.ofNullable(restClient.post()
                 .uri("/confirm")
                 .header(HttpHeaders.AUTHORIZATION, "Basic " + token)
                 .body(request)
@@ -41,6 +54,6 @@ public class PaymentService {
                     PaymentFailure paymentFailure = objectMapper.readValue(res.getBody(), PaymentFailure.class);
                     throw new BadRequestException(paymentFailure.message());
                 })
-                .body(PaymentResponse.class);
+                .body(PaymentResponse.class));
     }
 }
