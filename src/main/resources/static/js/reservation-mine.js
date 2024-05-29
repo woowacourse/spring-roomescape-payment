b8305325d79f97f9772e54b0bd3455fd6ae74a7d
HyungHoKim00 <rlagudgh1109@gmail.com>
2024-05-29 17:42:49 +0900

    style: 해결된 TODO 제거

--- src/main/resources/static/js/reservation-mine.js
+++ src/main/resources/static/js/reservation-mine.js
@@ -25,9 +25,6 @@ function render(data) {
     row.insertCell(2).textContent = time;
     row.insertCell(3).textContent = status;
 
-    /*
-    TODO: [3단계] 예약 대기 기능 - 예약 대기 취소 기능 구현 후 활성화
-     */
     if (status !== '예약') { // 예약 대기 상태일 때 예약 대기 취소 버튼 추가하는 코드, 상태 값은 변경 가능
       const cancelCell = row.insertCell(4);
       const cancelButton = document.createElement('button');
@@ -44,9 +41,6 @@ function render(data) {
 }
 
 function requestDeleteWaiting(id) {
-  /*
-  TODO: [3단계] 예약 대기 기능 - 예약 대기 취소 API 호출
-   */
   const endpoint = `/waitings/${id}`;
   return fetch(endpoint, {
     method: 'DELETE'
