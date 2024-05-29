dbc302034ac4fb101b51141eaa908ed128cdac1d
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-05-29 13:19:29 +0900

    refactor: 불필요 TODO 제거
    Co-Authored-By: youngsu5582 <98307410+youngsu5582@users.noreply.github.com>

--- src/main/resources/static/js/user-reservation.js
+++ src/main/resources/static/js/user-reservation.js
@@ -170,10 +170,7 @@ function onReservationButtonClick(event, paymentWidget) {
 
         const generateRandomString = () =>
             window.btoa(Math.random()).slice(0, 20);
-        /*
-        TODO: [1단계]
-              - orderIdPrefix 를 자신만의 prefix로 변경
-        */
+
         // TOSS 결제 위젯 Javascript SDK 연동 방식 중 'Promise로 처리하기'를 적용함
         // https://docs.tosspayments.com/reference/widget-sdk#promise%EB%A1%9C-%EC%B2%98%EB%A6%AC%ED%95%98%EA%B8%B0
         const orderIdPrefix = "WTEST";
@@ -195,12 +192,7 @@ function onReservationButtonClick(event, paymentWidget) {
 }
 
 async function fetchReservationPayment(paymentData, reservationData) {
-    /*
-    TODO: [1단계]
-        - 자신의 예약 API request에 맞게 reservationPaymentRequest 필드명 수정
-        - 내 서버 URL에 맞게 reservationURL 변경
-        - 예약 결제 실패 시, 사용자가 실패 사유를 알 수 있도록 alert 에서 에러 메시지 수정
-    */
+    
     const reservationPaymentRequest = {
         date: reservationData.date,
         themeId: reservationData.themeId,
