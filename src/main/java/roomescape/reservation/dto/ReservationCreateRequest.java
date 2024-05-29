7e8aa9c1ab4dbafff43001f3b9d6c54ece71ab85
Eugene Jang <kyunellroll@gmail.com>
2024-05-29 19:29:46 +0900

    feat(PaymentType): 결제 타입 enum 추가
    - 예약 요청 Dto 생성
    
    Co-authored-by: nak-honest <e5slnh9876@gmail.com>

--- src/main/java/roomescape/reservation/dto/ReservationCreateRequest.java
+++ src/main/java/roomescape/reservation/dto/ReservationCreateRequest.java
@@ -3,16 +3,28 @@ package roomescape.reservation.dto;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDate;
 import roomescape.member.domain.Member;
+import roomescape.paymenthistory.PaymentType;
+import roomescape.paymenthistory.dto.PaymentCreateRequest;
 import roomescape.reservation.domain.Reservation;
 import roomescape.theme.domain.Theme;
 import roomescape.time.domain.ReservationTime;
 
+// TODO: 내부 필드들 record로 감싸보기
 public record ReservationCreateRequest(
-        Long memberId,
-        @JsonFormat(pattern = "yyyy-MM-dd") LocalDate date,
+        @JsonFormat(pattern = "yyyy-MM-dd")
+        LocalDate date,
         Long timeId,
-        Long themeId) {
+        Long themeId,
+        String paymentKey,
+        String orderId,
+        int amount,
+        PaymentType paymentType
+) {
     public Reservation createReservation(Member member, ReservationTime time, Theme theme) {
         return new Reservation(member, date, time, theme);
+    }
+
+    public PaymentCreateRequest createPaymentRequest(Reservation reservation) {
+        return new PaymentCreateRequest(paymentKey, orderId, amount, reservation);
     }
 }
