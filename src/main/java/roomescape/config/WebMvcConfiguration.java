2d10b99bc5d9b0d9e1cd95e4f4ab53b3abc495b3
hangillee <skfcb10@naver.com>
2024-05-29 17:04:29 +0900

    refactor: RestClient 생성 로직 개선

--- src/main/java/roomescape/config/WebMvcConfiguration.java
+++ src/main/java/roomescape/config/WebMvcConfiguration.java
@@ -2,8 +2,11 @@ package roomescape.config;
 
 import java.time.Duration;
 import java.util.List;
+import org.springframework.boot.web.client.ClientHttpRequestFactories;
+import org.springframework.boot.web.client.ClientHttpRequestFactorySettings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.JdkClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
@@ -36,12 +39,19 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
 
     @Bean
     public RestClient restClient() {
-        final JdkClientHttpRequestFactory factory = new JdkClientHttpRequestFactory();
-        factory.setReadTimeout(Duration.ofSeconds(5));
+        final ClientHttpRequestFactory factory = getClientHttpRequestFactory();
 
         return RestClient.builder()
                 .requestFactory(factory)
                 .baseUrl("https://api.tosspayments.com/v1/payments/confirm")
                 .build();
+    }
+
+    private ClientHttpRequestFactory getClientHttpRequestFactory() {
+        final ClientHttpRequestFactorySettings settings = ClientHttpRequestFactorySettings.DEFAULTS
+                .withConnectTimeout(Duration.ofSeconds(30))
+                .withReadTimeout(Duration.ofSeconds(30));
+
+        return ClientHttpRequestFactories.get(JdkClientHttpRequestFactory.class, settings);
     }
 }
