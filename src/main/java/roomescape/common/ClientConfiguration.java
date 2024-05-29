f14d05e89e73a9743dc38501820b3f5a003e7479
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-05-29 21:21:13 +0900

    feat(ClientConfiguration): 외부 결제 Client 설정 추가
    Co-authored-by: tkdgur0906 <tkdgur0906@naver.com>

---
+++ src/main/java/roomescape/common/ClientConfiguration.java
@@ -0,0 +1,34 @@
+package roomescape.common;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+import roomescape.payment.client.PaymentProperties;
+
+@Configuration
+public class ClientConfiguration {
+
+    private static final String BASIC_PREFIX = "Basic ";
+
+    private final PaymentProperties paymentProperties;
+
+    public ClientConfiguration(final PaymentProperties paymentProperties) {
+        this.paymentProperties = paymentProperties;
+    }
+
+    @Bean
+    public RestClient tossPaymentClient() {
+        byte[] encodedBytes = Base64.getEncoder()
+                .encode((paymentProperties.getSecretKey() + ":")
+                        .getBytes(StandardCharsets.UTF_8));
+
+        String authorizations = BASIC_PREFIX + new String(encodedBytes);
+
+        return RestClient.builder()
+                .baseUrl("https://api.tosspayments.com/v1/payments")
+                .defaultHeader("Authorization", authorizations)
+                .build();
+    }
+}
