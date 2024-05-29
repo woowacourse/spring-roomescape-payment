af7ae3e6570f00aba8cc9cb3fc93b2b6e5fb835a
HyungHoKim00 <rlagudgh1109@gmail.com>
2024-05-29 17:43:38 +0900

    style: 사용하지 않는 변수 제거

--- src/test/java/roomescape/core/controller/ReservationControllerTest.java
+++ src/test/java/roomescape/core/controller/ReservationControllerTest.java
@@ -14,7 +14,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import roomescape.core.dto.reservation.ReservationPaymentRequest;
 import roomescape.core.dto.waiting.MemberWaitingRequest;
-import roomescape.infrastructure.PaymentSecretKeyEncoder;
 import roomescape.utils.AccessTokenGenerator;
 import roomescape.utils.DatabaseCleaner;
 import roomescape.utils.TestFixture;
@@ -33,9 +32,6 @@ class ReservationControllerTest {
 
     @Autowired
     private TestFixture testFixture;
-
-    @Autowired
-    private PaymentSecretKeyEncoder paymentSecretKeyEncoder;
 
     private String accessToken;
 
