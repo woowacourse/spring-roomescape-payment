1407e66f5a924f32f177d40f0c5ff2d9da81c580
takoyakimchi <c4nyou@gmail.com>
2024-05-29 15:19:45 +0900

    refactor: 복잡한 파싱 로직 개선
    Co-authored-by: kyum-q <kyungmi.dev@gmail.com>

---
+++ src/main/java/roomescape/controller/dto/PaymentErrorMessageResponse.java
@@ -0,0 +1,5 @@
+package roomescape.controller.dto;
+
+public record PaymentErrorMessageResponse(String code, String message) {
+
+}
--- src/main/java/roomescape/service/PaymentService.java
+++ src/main/java/roomescape/service/PaymentService.java
@@ -3,12 +3,13 @@ package roomescape.service;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestClient;
+import roomescape.controller.dto.PaymentErrorMessageResponse;
+import roomescape.global.exception.RoomescapeException;
 import roomescape.service.dto.PaymentRequestDto;
-import roomescape.service.errorhandler.PaymentErrorHandler;
 
 @Service
 public class PaymentService {
@@ -31,14 +32,17 @@ public class PaymentService {
         byte[] encodedBytes = encoder.encode(tossPaymentTestKey.getBytes(StandardCharsets.UTF_8));
         String authorizations = AUTHORIZATION_PREFIX + new String(encodedBytes);
 
-        restClient.post()
-            .uri(TOSS_PAYMENTS_URL)
-            .body(new PaymentRequestDto(orderId, amount, paymentKey))
-            .contentType(MediaType.APPLICATION_JSON)
-            .header("Authorization", authorizations)
-            .retrieve()
-            .onStatus(HttpStatusCode::is4xxClientError, new PaymentErrorHandler())
-            .onStatus(HttpStatusCode::is5xxServerError, new PaymentErrorHandler())
-            .toBodilessEntity();
+        try {
+            restClient.post()
+                .uri(TOSS_PAYMENTS_URL)
+                .body(new PaymentRequestDto(orderId, amount, paymentKey))
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("Authorization", authorizations)
+                .retrieve()
+                .toBodilessEntity();
+        } catch (HttpClientErrorException e) {
+            PaymentErrorMessageResponse response = e.getResponseBodyAs(PaymentErrorMessageResponse.class);
+            throw new RoomescapeException(response.message());
+        }
     }
 }
