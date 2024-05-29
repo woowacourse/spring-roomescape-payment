77e2231f25b07d816d975780f83456fa30a0b3e7
SungKyum Kim <sungkyum1@naver.com>
2024-05-29 13:32:38 +0900

    refactor: MemberRepository 추상화

--- src/main/java/roomescape/domain/member/MemberRepository.java
+++ src/main/java/roomescape/domain/member/MemberRepository.java
@@ -1,11 +1,17 @@
 package roomescape.domain.member;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
+import java.util.List;
 import java.util.Optional;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository {
+
     Optional<Member> findByEmail(Email email);
 
     boolean existsByEmail(Email email);
+
+    Optional<Member> findById(long id);
+
+    List<Member> findAll();
+
+    Member save(Member member);
 }
