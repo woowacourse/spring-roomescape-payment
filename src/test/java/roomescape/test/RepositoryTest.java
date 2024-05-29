faacb875cf838bc33fa2d6da9d7379109710313d
Eugene Jang <kyunellroll@gmail.com>
2024-05-29 19:20:25 +0900

    test(RepositoryTest): 테스트 어노테이션 변경
    Co-authored-by: nak-honest <e5slnh9876@gmail.com>

--- src/test/java/roomescape/test/RepositoryTest.java
+++ src/test/java/roomescape/test/RepositoryTest.java
@@ -1,15 +1,15 @@
 package roomescape.test;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.junit.jupiter.api.AfterEach;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.jdbc.Sql;
 
 @DataJpaTest
 @Sql(scripts = "/init.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 public abstract class RepositoryTest {
-    @Autowired
+    @PersistenceContext
     private EntityManager entityManager;
 
     @AfterEach
