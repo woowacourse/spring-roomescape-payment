3f7dd91f44e2363a7a38df62a7cc912c5c8e605e
SungKyum Kim <sungkyum1@naver.com>
2024-05-29 13:49:33 +0900

    refactor: ReservationTimeRepository 추상화하여 도메인의 jpa 의존성 제거

--- src/main/java/roomescape/domain/schedule/ReservationTimeRepository.java
+++ src/main/java/roomescape/domain/schedule/ReservationTimeRepository.java
@@ -1,9 +1,21 @@
 package roomescape.domain.schedule;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalTime;
+import roomescape.infrastructure.reservationtime.ReservationTimeJpaRepository;
 
-public interface ReservationTimeRepository extends JpaRepository<ReservationTime, Long> {
+public interface ReservationTimeRepository {
     boolean existsByStartAt(LocalTime startAt);
+
+    Optional<ReservationTime> findById(long id);
+
+    ReservationTime save(ReservationTime reservationTime);
+
+    List<ReservationTime> findAll();
+
+    void deleteById(long id);
 }
