1ab7b42ba2ef0958daadf80af41a2d5c1c6070f0
Arachneee <jhg2819@naver.com>
2024-05-29 16:30:21 +0900

    feat(PaymentExceptionHandler): 예약 실패 시 결제 실패 사유 출력
    Co-authored-by: helenason <helenason@naver.com>

--- src/main/java/roomescape/exception/PaymentException.java
+++ src/main/java/roomescape/exception/PaymentException.java
@@ -1,18 +1,10 @@
 package roomescape.exception;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
 import org.springframework.http.HttpStatusCode;
 
 public class PaymentException extends RuntimeException {
 
     private final HttpStatusCode statusCode;
-
-    public PaymentException(HttpStatusCode statusCode, InputStream inputStream) throws IOException {
-        super(new String(inputStream.readAllBytes(), StandardCharsets.UTF_8));
-        this.statusCode = statusCode;
-    }
 
     public PaymentException(HttpStatusCode statusCode, String message) {
         super(message);
--- src/main/java/roomescape/exception/PaymentExceptionHandler.java
+++ src/main/java/roomescape/exception/PaymentExceptionHandler.java
@@ -1,6 +1,10 @@
 package roomescape.exception;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.web.client.ResponseErrorHandler;
 
@@ -13,6 +17,9 @@ public class PaymentExceptionHandler implements ResponseErrorHandler {
 
     @Override
     public void handleError(ClientHttpResponse response) throws IOException {
-        throw new PaymentException(response.getStatusCode(), response.getBody());
+        String body = new String(response.getBody().readAllBytes(), StandardCharsets.UTF_8);
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, Object> properties = mapper.readValue(body, new TypeReference<>() {});
+        throw new PaymentException(response.getStatusCode(), properties.get("message").toString());
     }
 }
