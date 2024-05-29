0de61fb1349d8f68b88b5324c3721ea9ddef928f
SungKyum Kim <sungkyum1@naver.com>
2024-05-29 13:34:46 +0900

    refactor: PaymentRepositoryImpl 제거하여 추상화 수준을 낮춤

--- src/main/java/roomescape/infrastructure/payment/PaymentJpaRepository.java
+++ src/main/java/roomescape/infrastructure/payment/PaymentJpaRepository.java
@@ -2,7 +2,8 @@ package roomescape.infrastructure.payment;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import roomescape.domain.payment.Payment;
+import roomescape.domain.payment.PaymentRepository;
 
-public interface PaymentJpaRepository extends JpaRepository<Payment, Long> {
+public interface PaymentJpaRepository extends JpaRepository<Payment, Long>, PaymentRepository {
 
 }
