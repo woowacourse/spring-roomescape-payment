66ce29952e59c14e05e27a920f27915e08eb73b8
SungKyum Kim <sungkyum1@naver.com>
2024-05-29 13:52:40 +0900

    refactor: ReservationThemeRepository 추상화하여 도메인의 jpa 의존성 제거

--- src/main/java/roomescape/domain/theme/ThemeRepository.java
+++ src/main/java/roomescape/domain/theme/ThemeRepository.java
@@ -1,26 +1,21 @@
 package roomescape.domain.theme;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
+import org.springframework.data.repository.query.Param;
 
-public interface ThemeRepository extends JpaRepository<Theme, Long> {
+public interface ThemeRepository {
+
     boolean existsByName(ThemeName name);
 
-    //TODO: 테스트 추가
-    @Query(value = """
-            SELECT t.id, t.name, t.description, t.thumbnail, COUNT(r.id) AS reservation_count
-            FROM theme t
-            INNER JOIN reservation_detail rd ON rd.theme_id = t.id
-            INNER JOIN reservation r ON r.detail_id = rd.id
-            WHERE rd.date BETWEEN :startDate AND :endDate
-            AND r.status = 'RESERVED'
-            GROUP BY t.id
-            ORDER BY reservation_count DESC
-            LIMIT :limit
-            """, nativeQuery = true)
     List<Theme> findByReservationTermAndLimit(@Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate, @Param("limit") long limit);
+
+    Optional<Theme> findById(long id);
+
+    void deleteById(long id);
+
+    List<Theme> findAll();
+
+    Theme save(Theme theme);
 }
