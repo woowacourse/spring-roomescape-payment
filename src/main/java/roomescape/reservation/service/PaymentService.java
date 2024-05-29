6628cbfa3a5534ebfaa5fbfdba24f7af2b1fa74a
tackyu <tack970913@gmail.com>
2024-05-29 17:39:50 +0900

    feat: restClient timeout 설정

--- src/main/java/roomescape/reservation/service/PaymentService.java
+++ src/main/java/roomescape/reservation/service/PaymentService.java
@@ -1,5 +1,6 @@
 package roomescape.reservation.service;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
@@ -9,6 +10,9 @@ import java.util.Base64;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
@@ -26,25 +30,38 @@ public class PaymentService {
     private String tossSecretKey;
 
     public PaymentResponse requestTossPayment(PaymentRequest paymentRequest) {
-        byte[] encoded = Base64.getEncoder().encode((tossSecretKey + "moaKey:").getBytes(StandardCharsets.UTF_8));
+        byte[] encoded = Base64.getEncoder().encode((tossSecretKey + ":").getBytes(StandardCharsets.UTF_8));
         String authorization = "Basic " + new String(encoded);
 
-        RestClient tossRestClient = RestClient.builder().baseUrl("https://api.tosspayments.com/v1/payments").build();
+        RestClient tossRestClient = RestClient.builder()
+                .baseUrl("https://api.tosspayments.com/v1/payments")
+                .requestFactory(getClientHttpRequestFactory())
+                .build();
+
         return tossRestClient.post()
                 .uri("/confirm")
                 .header("Authorization", authorization)
-                .body(paymentRequest, new ParameterizedTypeReference<>() {
-                })
+                .body(paymentRequest, new ParameterizedTypeReference<>() {})
                 .retrieve()
                 .onStatus(r -> r.is4xxClientError() || r.is5xxServerError(), (request, response) -> {
-                    InputStream body = response.getBody();
-                    Reader inputStreamReader = new InputStreamReader(body, StandardCharsets.UTF_8);
-                    JsonObject jsonObject = (JsonObject) JsonParser.parseReader(inputStreamReader);
-                    String errorMessage = jsonObject.get("message").toString().replace("\"", "");
-
+                    String errorMessage = parseErrorMessage(response);
                     throw new PaymentException("결제 오류가 발생했습니다. " + errorMessage, HttpStatus.valueOf(response.getStatusCode().value()));
                 })
                 .toEntity(PaymentResponse.class)
                 .getBody();
+    }
+
+    private String parseErrorMessage(ClientHttpResponse response) throws IOException {
+        InputStream body = response.getBody();
+        Reader inputStreamReader = new InputStreamReader(body, StandardCharsets.UTF_8);
+        JsonObject jsonObject = (JsonObject) JsonParser.parseReader(inputStreamReader);
+        return jsonObject.get("message").toString().replace("\"", "");
+    }
+
+    private ClientHttpRequestFactory getClientHttpRequestFactory() {
+        HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+        clientHttpRequestFactory.setConnectTimeout(3000);
+        clientHttpRequestFactory.setConnectionRequestTimeout(1000);
+        return clientHttpRequestFactory;
     }
 }
