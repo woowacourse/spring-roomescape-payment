40b81f80fadbf367017778e8f662a17d6da457da
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-05-29 21:26:04 +0900

    refactor(PaymentClient): 결제 관련 외부 Client 객체 추상화
    Co-authored-by: tkdgur0906 <tkdgur0906@naver.com>

--- src/main/java/roomescape/payment/client/PaymentClient.java
+++ src/main/java/roomescape/payment/client/PaymentClient.java
@@ -1,26 +1,10 @@
 package roomescape.payment.client;
 
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.client.RestClient;
 import roomescape.payment.dto.request.ConfirmPaymentRequest;
 import roomescape.payment.model.Payment;
 
-@Component
-public class PaymentClient {
+public interface PaymentClient {
 
-    private final RestClient restClient;
-
-    public PaymentClient(final RestClient restClient) {
-        this.restClient = restClient;
-    }
-
-    public Payment confirm(@RequestBody ConfirmPaymentRequest confirmPaymentRequest) {
-        return restClient.post()
-                .uri("/confirm")
-                .body(confirmPaymentRequest)
-                .retrieve()
-                .toEntity(Payment.class)
-                .getBody();
-    }
+    Payment confirm(@RequestBody ConfirmPaymentRequest confirmPaymentRequest);
 }
---
+++ src/main/java/roomescape/payment/client/TossPaymentClient.java
@@ -0,0 +1,27 @@
+package roomescape.payment.client;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.client.RestClient;
+import roomescape.payment.dto.request.ConfirmPaymentRequest;
+import roomescape.payment.model.Payment;
+
+@Component
+public class TossPaymentClient implements PaymentClient {
+
+    private final RestClient restClient;
+
+    public TossPaymentClient(final RestClient restClient) {
+        this.restClient = restClient;
+    }
+
+    @Override
+    public Payment confirm(@RequestBody ConfirmPaymentRequest confirmPaymentRequest) {
+        return restClient.post()
+                .uri("/confirm")
+                .body(confirmPaymentRequest)
+                .retrieve()
+                .toEntity(Payment.class)
+                .getBody();
+    }
+}
