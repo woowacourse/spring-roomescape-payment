27cf5450d683d6ec93b08dc5dae327a1d4a599c4
lemone <lejinhy@gmail.com>
2024-05-29 15:26:24 +0900

    remove(PaymentRequest): 사용하지 않는 dto 삭제
    Co-authored-by: lemone <jihyeonl@gmail.com>
    Co-authored-by: nayonsoso <panda0329@naver.com>

--- src/main/java/roomescape/payment/dto/PaymentRequest.java
+++
@@ -1,7 +0,0 @@
-package roomescape.payment.dto;
-
-import java.time.LocalDate;
-
-public record PaymentRequest(LocalDate date, Long themeId, Long timeId, String paymentType,
-                             String paymentKey, String orderId, Integer amount) {
-}
