5f4ed5506b2c692daa682fd3eeb70d31d3237aca
MingyeomKim <mingyum119@naver.com>
2024-05-29 14:26:10 +0900

    test: 예약 생성 로직을 다른 테스트 클래스로 분리

--- src/test/java/roomescape/reservation/controller/ReservationControllerTest.java
+++ src/test/java/roomescape/reservation/controller/ReservationControllerTest.java
@@ -2,23 +2,16 @@ package roomescape.reservation.controller;
 
 import static org.hamcrest.Matchers.hasSize;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import java.time.LocalDate;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import roomescape.common.config.IntegrationTest;
 import roomescape.common.util.CookieUtils;
-import roomescape.reservation.dto.request.ReservationDetailRequest;
 
 class ReservationControllerTest extends IntegrationTest {
-
-    @Autowired
-    private ObjectMapper objectMapper;
 
     @DisplayName("예약 목록 조회에 성공하면 200 응답을 받는다.")
     @Test
@@ -67,27 +60,6 @@ class ReservationControllerTest extends IntegrationTest {
                 .statusCode(HttpStatus.OK.value())
                 .contentType(ContentType.JSON)
                 .body("resources.$", hasSize(1));
-    }
-
-    @DisplayName("회원이 예약을 성공적으로 추가하면 201 응답과 Location 헤더에 리소스 저장 경로를 받는다.")
-    @Test
-    void saveMemberReservation() throws JsonProcessingException {
-        saveMemberAsKaki();
-        saveThemeAsHorror();
-        saveReservationTimeAsTen();
-
-        ReservationDetailRequest reservationDetailRequest = new ReservationDetailRequest(LocalDate.now(), 1L, 1L);
-
-        RestAssured.given().log().all()
-                .cookie(CookieUtils.TOKEN_KEY, getMemberToken())
-                .contentType(ContentType.JSON)
-                .body(objectMapper.writeValueAsString(reservationDetailRequest))
-                .accept(ContentType.JSON)
-                .when()
-                .post("/reservations")
-                .then().log().all()
-                .statusCode(HttpStatus.CREATED.value())
-                .header("Location", "/reservations/1");
     }
 
     @DisplayName("예약을 성공적으로 제거하면 204 응답을 받는다.")
