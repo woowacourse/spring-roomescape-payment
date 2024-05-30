09d7a833041640d7a4454625373e2e873ca4520f
Eugene Jang <kyunellroll@gmail.com>
2024-05-30 11:10:56 +0900

    refactor(ReservationStatus): 예약 상태 수정
    Co-authored-by: nak-honest <e5slnh9876@gmail.com>

--- src/main/java/roomescape/reservation/domain/ReservationStatus.java
+++ src/main/java/roomescape/reservation/domain/ReservationStatus.java
@@ -3,7 +3,6 @@ package roomescape.reservation.domain;
 public enum ReservationStatus {
 
     RESERVED("예약 확정"),
-    CANCELED_RESERVE("예약 취소"),
     PAYMENT_PENDING("결제 대기");
 
     private final String name;
