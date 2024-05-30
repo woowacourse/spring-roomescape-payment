3cf0857d130a4551ff80bd5ac0d7d9de1828c128
haiseong <wjdgotjd9908@gmail.com>
2024-05-30 17:09:41 +0900

    test: 결제 실패시 예약의 결제정보를 갱신하지 않는지 여부 테스트
    Co-authored-by: Haiseong <wjdgotjd9908@gmail.com>
    Co-authored-by: J-I-H-O <jeongjiho0731@gmail.com>

--- src/test/java/roomescape/service/PaymentServiceTest.java
+++ src/test/java/roomescape/service/PaymentServiceTest.java
@@ -1,6 +1,8 @@
 package roomescape.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 
@@ -66,7 +68,7 @@ class PaymentServiceTest {
 
     @Test
     @DisplayName("결제가 정상 처리되면, 결제 정보를 저장한다.")
-    void saveOnPurchaseSuccess() {
+    void savePayment() {
         // given
         Member member = memberRepository.save(MemberFixture.DEFAULT_MEMBER);
         Theme theme = themeRepository.save(ThemeFixture.DEFAULT_THEME);
@@ -89,5 +91,36 @@ class PaymentServiceTest {
         // when & then
         assertThatCode(() -> paymentService.savePaymentAndUpdateReservationStatus(request))
                 .doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("결제가 실패하면, 예약의 결제 정보를 갱신하지 않는다.")
+    void failPaymentThenReservationPaymentInfoNotUpdated() {
+        // given
+        Member member = memberRepository.save(MemberFixture.DEFAULT_MEMBER);
+        Theme theme = themeRepository.save(ThemeFixture.DEFAULT_THEME);
+        ReservationTime time = reservationTimeRepository.save(ReservationTimeFixture.DEFAULT_TIME);
+        LocalDate date = LocalDate.now().plusDays(1);
+        ReservationStatus status = ReservationStatus.RESERVED_UNPAID;
+
+        Reservation reservation = reservationRepository.save(Reservation.builder()
+                .member(member)
+                .date(date)
+                .time(time)
+                .theme(theme)
+                .status(status)
+                .build());
+
+        given(paymentRestClient.requestPaymentApproval(any(PaymentRequest.class)))
+                .willThrow(new RuntimeException());
+
+        // when
+        Reservation foundReservation = reservationRepository.findById(reservation.getId()).get();
+
+        // then
+        assertAll(
+                () -> assertThat(foundReservation.getStatus()).isEqualTo(ReservationStatus.RESERVED_UNPAID),
+                () -> assertThat(foundReservation.getPayment()).isNull()
+        );
     }
 }
