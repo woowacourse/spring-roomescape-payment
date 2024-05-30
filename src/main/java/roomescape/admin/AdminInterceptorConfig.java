34263602b4f8a5ab0bcb0ab600da69394251cc02
libienz <leekh10195@gmail.com>
2024-05-30 13:33:46 +0900

    fix: 인터셉터 미등록 오류 수정

--- src/main/java/roomescape/admin/AdminInterceptorConfig.java
+++ src/main/java/roomescape/admin/AdminInterceptorConfig.java
@@ -1,8 +1,10 @@
 package roomescape.admin;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class AdminInterceptorConfig implements WebMvcConfigurer {
 
     private final AdminHandlerInterceptor adminHandlerInterceptor;
