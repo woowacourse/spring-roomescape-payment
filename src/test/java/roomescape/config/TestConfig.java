bd4af8d0a1db03704dcec2075c1c4a9af09fc32d
Nam Gi Beom <rlqja0523@naver.com>
2024-05-30 23:13:34 +0900

    refactor: allow-bean-definition-overriding 제거 및 @Primary 추가

--- src/test/java/roomescape/config/TestConfig.java
+++ src/test/java/roomescape/config/TestConfig.java
@@ -3,7 +3,7 @@ package roomescape.config;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
-import roomescape.payment.PaymentClient;
+import roomescape.service.PaymentClient;
 import roomescape.support.FakePaymentClient;
 
 import java.time.Clock;
@@ -15,16 +15,17 @@ import java.time.ZoneOffset;
 public class TestConfig {
 
     @Bean
-    public Clock clock() {
+    @Primary
+    public Clock testClock() {
         Instant fixedInstant = Instant.parse("2024-04-08T00:00:00Z");
         ZoneId zone = ZoneOffset.UTC;
 
         return Clock.fixed(fixedInstant, zone);
     }
 
-    @Primary
     @Bean
-    public PaymentClient paymentClient() {
+    @Primary
+    public PaymentClient fakePaymentClient() {
         return new FakePaymentClient();
     }
 }
