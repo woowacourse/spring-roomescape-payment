4a8cfaa6ffbfeaf2709afebbf30c5b55d8ae57be
Nam Gi Beom <rlqja0523@naver.com>
2024-05-31 01:48:08 +0900

    refactor: 토스 승인 요청 uri @Value로 추출

--- src/main/java/roomescape/infrastructure/payment/TossPaymentClient.java
+++ src/main/java/roomescape/infrastructure/payment/TossPaymentClient.java
@@ -12,26 +12,30 @@ import java.util.Base64;
 
 @Component
 public class TossPaymentClient implements PaymentClient {
-    private static final String URI = "https://api.tosspayments.com/v1/payments/confirm";
+
     private static final String ENCODING_FORMAT = "%s:";
     private static final String SECRET_KEY_PREFIX = "Basic ";
 
     private final RestClient restClient;
     private final TossPaymentClientErrorHandler tossPaymentClientErrorHandler;
+    private final String uri;
     private final String encodedSecretKey;
 
     public TossPaymentClient(RestClient restClient,
                              TossPaymentClientErrorHandler tossPaymentClientErrorHandler,
+                             @Value("${toss.payment-uri}") String uri,
+                             @Value("${toss.confirm-path}") String confirmPath,
                              @Value("${toss.secret-key}") String secretKey) {
         this.restClient = restClient;
         this.tossPaymentClientErrorHandler = tossPaymentClientErrorHandler;
+        this.uri = uri + confirmPath;
         this.encodedSecretKey = Base64.getEncoder().encodeToString(String.format(ENCODING_FORMAT, secretKey).getBytes());
     }
 
     @Override
     public void confirm(PaymentRequest paymentRequest) {
         restClient.post()
-                .uri(URI)
+                .uri(uri)
                 .header(HttpHeaders.AUTHORIZATION, SECRET_KEY_PREFIX + encodedSecretKey)
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(paymentRequest)
