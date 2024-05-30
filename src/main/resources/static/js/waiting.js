5bf4b5b20e2504f21dcd1dd89c2308a25e68fed4
Hyeon0208 <dmsgktn0208@gmail.com>
2024-05-30 12:27:59 +0900

    refactor: 클라이언트 코드 TODO 제거

--- src/main/resources/static/js/waiting.js
+++ src/main/resources/static/js/waiting.js
@@ -31,10 +31,6 @@ function render(data) {
 
         const actionCell = row.insertCell(row.cells.length);
 
-        /*
-        TODO: [4단계] 예약 대기 관리 기능
-              예약 대기 승인/거절 버튼이 필요한 경우 활성화하여 사용
-         */
         // actionCell.appendChild(createActionButton('승인', 'btn-primary', approve));
         actionCell.appendChild(createActionButton('거절', 'btn-danger', deny));
     });
@@ -43,12 +39,8 @@ function render(data) {
 function approve(event) {
     const row = event.target.closest('tr');
     const id = row.cells[0].textContent;
-
-    /*
-    TODO: [4단계] 예약 대기 목록 관리 기능
-          예약 대기 승인 API 호출
-     */
     const endpoint = '' + id;
+
     return fetch(endpoint, {
         method: ''
     }).then(response => {
@@ -60,12 +52,8 @@ function approve(event) {
 function deny(event) {
     const row = event.target.closest('tr');
     const id = row.cells[0].textContent;
-
-    /*
-    TODO: [4단계] 예약 대기 목록 관리 기능
-          예약 대기 거절 API 호출
-     */
     const endpoint = `${RESERVATION_API_ENDPOINT}/${id}`;
+
     return fetch(endpoint, {
         method: 'DELETE'
     }).then(response => {
