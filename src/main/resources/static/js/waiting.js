be3a82c58a22aec7619fc0711fc3e7d5b1bddaa4
3juhwan <13selfesteem91@naver.com>
2024-05-30 17:10:25 +0900

    fix: 에약 대기 승인 버튼 삭제, 엔드포인트 수정

--- src/main/resources/static/js/waiting.js
+++ src/main/resources/static/js/waiting.js
@@ -41,7 +41,7 @@ function render(data) {
         TODO: [4단계] 예약 대기 관리 기능
               예약 대기 승인/거절 버튼이 필요한 경우 활성화하여 사용
          */
-        actionCell.appendChild(createActionButton('승인', 'btn-primary', approve));
+        // actionCell.appendChild(createActionButton('승인', 'btn-primary', approve));
         actionCell.appendChild(createActionButton('거절', 'btn-danger', deny));
     });
 }
@@ -71,7 +71,7 @@ function deny(event) {
     TODO: [4단계] 예약 대기 목록 관리 기능
           예약 대기 거절 API 호출
      */
-    const endpoint = '/admin/waiting/' + id;
+    const endpoint = '/admin/waitings/' + id;
     return fetch(endpoint, {
         method: 'DELETE'
     }).then(response => {
