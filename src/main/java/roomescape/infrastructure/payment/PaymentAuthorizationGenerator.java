aa2cd1b3aef96ab743ec61714ba1454dd86cbaa7
juha <khabh@naver.com>
2024-05-30 16:23:18 +0900

    refactor: Encoder 상수로 변경

--- src/main/java/roomescape/infrastructure/payment/PaymentAuthorizationGenerator.java
+++ src/main/java/roomescape/infrastructure/payment/PaymentAuthorizationGenerator.java
@@ -2,11 +2,15 @@ package roomescape.infrastructure.payment;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
+import java.util.Base64.Encoder;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PaymentAuthorizationGenerator {
+
+    private static final Encoder ENCODER = Base64.getEncoder();
 
     private final String secretKey;
 
@@ -15,8 +19,7 @@ public class PaymentAuthorizationGenerator {
     }
 
     public String createAuthorizations() {
-        Base64.Encoder encoder = Base64.getEncoder();
-        byte[] encodedBytes = encoder.encode((secretKey + ":").getBytes(StandardCharsets.UTF_8));
+        byte[] encodedBytes = ENCODER.encode((secretKey + ":").getBytes(StandardCharsets.UTF_8));
         return "Basic " + new String(encodedBytes);
     }
 }
