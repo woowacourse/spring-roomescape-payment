bd3b43ca4f58e6e21c4ab2431d720c84721bf393
김민수 MINSOO KIM <alstn113@gmail.com>
2024-05-30 15:47:47 +0900

    refactor: rest template builder로 변경

--- src/main/java/roomescape/config/ClientConfig.java
+++ src/main/java/roomescape/config/ClientConfig.java
@@ -1,16 +1,10 @@
 package roomescape.config;
 
-import org.apache.hc.client5.http.classic.HttpClient;
-import org.apache.hc.client5.http.config.ConnectionConfig;
-import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
-import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
-import org.apache.hc.core5.http.io.SocketConfig;
-import org.apache.hc.core5.util.Timeout;
+import java.time.Duration;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.ClientHttpRequestFactory;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
 
@@ -19,41 +13,20 @@ public class ClientConfig {
 
     @Bean
     public RestClient restClient(
-            @Value("${client.connection-timeout-ms}") int connectionTimeoutMs,
-            @Value("${client.read-timeout-ms}") int readTimeoutMs
+            @Value("${client.connection-timeout-second}") int connectionTimeoutSecond,
+            @Value("${client.read-timeout-second}") int readTimeoutSecond
     ) {
-        RestTemplate template = new RestTemplate(clientHttpRequestFactory(connectionTimeoutMs, readTimeoutMs));
+        RestTemplate restTemplate = createRestTemplate(connectionTimeoutSecond, readTimeoutSecond);
 
-        return RestClient.builder(template)
+        return RestClient.builder(restTemplate)
                 .build();
     }
 
-    private ClientHttpRequestFactory clientHttpRequestFactory(int connectionTimeoutMs, int readTimeoutMs) {
-        ConnectionConfig connectionConfig = ConnectionConfig.custom()
-                .setConnectTimeout(Timeout.ofMilliseconds(connectionTimeoutMs))
+    private RestTemplate createRestTemplate(int connectionTimeoutSecond, int readTimeoutSecond) {
+        return new RestTemplateBuilder()
+                .setConnectTimeout(Duration.ofSeconds(connectionTimeoutSecond))
+                .setReadTimeout(Duration.ofSeconds(readTimeoutSecond))
                 .build();
-
-        SocketConfig socketConfig = SocketConfig.custom()
-                .setSoTimeout(Timeout.ofMilliseconds(readTimeoutMs))
-                .build();
-
-        PoolingHttpClientConnectionManager connectionManager = createConnectionManager(socketConfig, connectionConfig);
-
-        HttpClient httpClient = HttpClientBuilder.create()
-                .setConnectionManager(connectionManager)
-                .build();
-
-        return new HttpComponentsClientHttpRequestFactory(httpClient);
-    }
-
-    private PoolingHttpClientConnectionManager createConnectionManager(
-            SocketConfig socketConfig,
-            ConnectionConfig connectionConfig
-    ) {
-        PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
-        connectionManager.setDefaultSocketConfig(socketConfig);
-        connectionManager.setDefaultConnectionConfig(connectionConfig);
-
-        return connectionManager;
     }
 }
+
