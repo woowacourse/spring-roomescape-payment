d13d315cd9fa2b3a8c1ae391ad0e978925f59711
jhon3242 <rldotnr2@gmail.com>
2024-05-30 16:50:53 +0900

    refactor: PaymentWithRestClient 리팩터링

--- src/main/java/roomescape/payment/PaymentWithRestClient.java
+++ src/main/java/roomescape/payment/PaymentWithRestClient.java
@@ -1,5 +1,7 @@
 package roomescape.payment;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.web.client.RestClient;
 import roomescape.payment.dto.PaymentRequest;
@@ -8,6 +10,9 @@ import roomescape.payment.dto.PaymentResponse;
 public class PaymentWithRestClient implements PaymentClient {
 
     private final RestClient restClient;
+
+    @Value("${security.payment.api.secret-key}")
+    private String secretKey;
 
     public PaymentWithRestClient(RestClient restClient) {
         this.restClient = restClient;
@@ -18,7 +23,7 @@ public class PaymentWithRestClient implements PaymentClient {
         return restClient.post()
                 .uri("/confirm")
                 .contentType(MediaType.APPLICATION_JSON)
-                .header("Authorization", "Basic dGVzdF9nc2tfZG9jc19PYVB6OEw1S2RtUVhrelJ6M3k0N0JNdzY6")
+                .header(HttpHeaders.AUTHORIZATION, "Basic " + secretKey)
                 .body(paymentRequest)
                 .retrieve()
                 .body(PaymentResponse.class);
