bedd000616f291235b40b90fd0dfb46d2a7dd377
yoonseo choi <choiyoonseo02@gmail.com>
2024-05-30 10:45:52 +0900

    refactor: 불필요한 쿼리 제거
    Co-Authored-By: devhoya97 <146502065+devhoya97@users.noreply.github.com>

--- src/main/java/roomescape/core/repository/PaymentRepository.java
+++ src/main/java/roomescape/core/repository/PaymentRepository.java
@@ -4,6 +4,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import roomescape.core.domain.Payment;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
-
-    boolean existsByPaymentKey(String paymentKey);
 }
