9ba40a6c408ece65c3d43c5768f76d5ff58faea1
SungKyum Kim <sungkyum1@naver.com>
2024-05-30 10:43:53 +0900

    refactor: 요청 DTO 검증 메시지 추가

--- src/main/java/roomescape/service/reservation/dto/ReservationRequest.java
+++ src/main/java/roomescape/service/reservation/dto/ReservationRequest.java
@@ -8,9 +8,9 @@ public record ReservationRequest(
     @NotNull(message = "날짜를 입력해주세요.") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate date,
     @NotNull(message = "시간을 입력해주세요.") Long timeId,
     @NotNull(message = "테마를 입력해주세요.") Long themeId,
-    @NotNull String paymentKey,
-    @NotNull String orderId,
-    @NotNull Long amount
+    @NotNull(message = "결제 정보를 입력해주세요.") String paymentKey,
+    @NotNull(message = "결제 정보를 입력해주세요.") String orderId,
+    @NotNull(message = "결제 정보를 입력해주세요.") Long amount
 ) {
 
 }
