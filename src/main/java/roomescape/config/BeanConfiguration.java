2cc7b03e14b44f0c512d4fa8a8f2ad03c040ac57
Eugene Jang <kyunellroll@gmail.com>
2024-05-30 13:31:34 +0900

    test(BeanConfiguration): 타임아웃 설정 추가
    Co-authored-by: nak-honest <e5slnh9876@gmail.com>

--- src/main/java/roomescape/config/BeanConfiguration.java
+++ src/main/java/roomescape/config/BeanConfiguration.java
@@ -3,6 +3,7 @@ package roomescape.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import roomescape.paymenthistory.domain.PaymentRestClient;
 
@@ -18,6 +19,14 @@ public class BeanConfiguration {
     public PaymentRestClient paymentRestClient() {
         return new PaymentRestClient(RestClient.builder()
                 .baseUrl(TOSS_PAYMENT_URL)
+                .requestFactory(getClientHttpRequestFactory())
                 .build(), secretKey);
+    }
+
+    private SimpleClientHttpRequestFactory getClientHttpRequestFactory() {
+        SimpleClientHttpRequestFactory simpleClientHttpRequestFactory = new SimpleClientHttpRequestFactory();
+        simpleClientHttpRequestFactory.setConnectTimeout(5000);
+        simpleClientHttpRequestFactory.setReadTimeout(30000);
+        return simpleClientHttpRequestFactory;
     }
 }
