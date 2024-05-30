eecc7e521a4d2c74a807a92f6b32f39dc6eeccc0
hangillee <skfcb10@naver.com>
2024-05-30 12:56:21 +0900

    refactor: 테스트 더블 방식 Fake에서 Mock으로 전환

--- src/test/java/roomescape/core/controller/WaitingControllerTest.java
+++ src/test/java/roomescape/core/controller/WaitingControllerTest.java
@@ -1,25 +1,29 @@
 package roomescape.core.controller;
 
 import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import jakarta.annotation.PostConstruct;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import roomescape.core.dto.payment.PaymentConfirmResponse;
 import roomescape.core.dto.reservation.ReservationPaymentRequest;
 import roomescape.core.dto.waiting.MemberWaitingRequest;
+import roomescape.infrastructure.PaymentProvider;
 import roomescape.utils.AccessTokenGenerator;
-import roomescape.utils.AdminGenerator;
 import roomescape.utils.DatabaseCleaner;
 import roomescape.utils.TestFixture;
 
 @AcceptanceTest
 class WaitingControllerTest {
     public static final String WAITING_IS_NOT_YOURS_EXCEPTION_MESSAGE = "본인의 예약 대기만 취소할 수 있습니다.";
-    private static final String TODAY = TestFixture.getTodayDate();
     private static final String TOMORROW = TestFixture.getTomorrowDate();
 
     private String accessToken;
@@ -31,10 +35,16 @@ class WaitingControllerTest {
     private DatabaseCleaner databaseCleaner;
 
     @Autowired
-    private AdminGenerator adminGenerator;
-
-    @Autowired
     private TestFixture testFixture;
+
+    @MockBean
+    private PaymentProvider paymentProvider;
+
+    @PostConstruct
+    public void init() {
+        when(paymentProvider.getPaymentConfirmResponse(any()))
+                .thenReturn(new PaymentConfirmResponse(1000, "orderId", "paymentKey"));
+    }
 
     @BeforeEach
     void setUp() {
