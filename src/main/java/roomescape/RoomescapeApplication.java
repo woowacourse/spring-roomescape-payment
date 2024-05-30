ff48d1ccf06e6b07f89b25e727807ee70b895f84
Nam Gi Beom <rlqja0523@naver.com>
2024-05-31 02:31:24 +0900

    refactor: @ConfigurationProperties로 바인딩

--- src/main/java/roomescape/RoomescapeApplication.java
+++ src/main/java/roomescape/RoomescapeApplication.java
@@ -2,8 +2,11 @@ package roomescape;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import roomescape.infrastructure.payment.TossPaymentProperties;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan(basePackageClasses = TossPaymentProperties.class)
 public class RoomescapeApplication {
     public static void main(String[] args) {
         SpringApplication.run(RoomescapeApplication.class, args);
--- src/main/java/roomescape/infrastructure/payment/TossPaymentClient.java
+++ src/main/java/roomescape/infrastructure/payment/TossPaymentClient.java
@@ -1,6 +1,5 @@
 package roomescape.infrastructure.payment;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
@@ -18,24 +17,23 @@ public class TossPaymentClient implements PaymentClient {
 
     private final RestClient restClient;
     private final TossPaymentClientErrorHandler tossPaymentClientErrorHandler;
-    private final String uri;
+    private final TossPaymentProperties properties;
     private final String encodedSecretKey;
 
     public TossPaymentClient(RestClient restClient,
                              TossPaymentClientErrorHandler tossPaymentClientErrorHandler,
-                             @Value("${toss.payment-uri}") String uri,
-                             @Value("${toss.confirm-path}") String confirmPath,
-                             @Value("${toss.secret-key}") String secretKey) {
+                             TossPaymentProperties properties) {
         this.restClient = restClient;
         this.tossPaymentClientErrorHandler = tossPaymentClientErrorHandler;
-        this.uri = uri + confirmPath;
-        this.encodedSecretKey = Base64.getEncoder().encodeToString(String.format(ENCODING_FORMAT, secretKey).getBytes());
+        this.properties = properties;
+        this.encodedSecretKey = Base64.getEncoder()
+                .encodeToString(String.format(ENCODING_FORMAT, properties.getSecretKey()).getBytes());
     }
 
     @Override
     public void confirm(PaymentRequest paymentRequest) {
         restClient.post()
-                .uri(uri)
+                .uri(properties.getPaymentUri())
                 .header(HttpHeaders.AUTHORIZATION, SECRET_KEY_PREFIX + encodedSecretKey)
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(paymentRequest)
