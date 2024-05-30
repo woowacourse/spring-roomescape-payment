1b1b0cd973e8351a6ef7232604c45e2d73e717c8
hyeonjilee <skylar12200@gmail.com>
2024-05-30 21:25:39 +0900

    refactor(ClientConfig): infrastructure로 패키지 이동

--- src/main/java/roomescape/infrastructure/config/ClientConfig.java
+++ src/main/java/roomescape/infrastructure/config/ClientConfig.java
@@ -1,4 +1,4 @@
-package roomescape.web.config;
+package roomescape.infrastructure.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
