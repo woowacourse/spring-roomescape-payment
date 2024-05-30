944d62d4dc10a22b51605e3f52d4a42138c2c07b
Hyeon0208 <dmsgktn0208@gmail.com>
2024-05-30 13:38:42 +0900

    refactor: RestClient 타임아웃 설정

--- src/main/java/roomescape/config/PaymentConfig.java
+++ src/main/java/roomescape/config/PaymentConfig.java
@@ -1,5 +1,8 @@
 package roomescape.config;
 
+import java.time.Duration;
+import org.springframework.boot.web.client.ClientHttpRequestFactories;
+import org.springframework.boot.web.client.ClientHttpRequestFactorySettings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
@@ -9,8 +12,13 @@ public class PaymentConfig {
 
     @Bean
     public RestClient restClient() {
+        ClientHttpRequestFactorySettings httpRequestFactorySettings = ClientHttpRequestFactorySettings.DEFAULTS
+                .withConnectTimeout(Duration.ofSeconds(5))
+                .withReadTimeout(Duration.ofSeconds(5));
+
         return RestClient.builder()
                 .baseUrl("https://api.tosspayments.com")
+                .requestFactory(ClientHttpRequestFactories.get(httpRequestFactorySettings))
                 .build();
     }
 }
