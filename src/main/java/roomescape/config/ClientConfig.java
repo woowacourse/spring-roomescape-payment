9e8d607c2e6978efffdbf762a8d624bd66db3cbd
김민수 MINSOO KIM <alstn113@gmail.com>
2024-05-30 17:01:48 +0900

    fix: Read Timeout 안들어가는 문제 수정

--- src/main/java/roomescape/config/ClientConfig.java
+++ src/main/java/roomescape/config/ClientConfig.java
@@ -2,11 +2,11 @@ package roomescape.config;
 
 import java.time.Duration;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
-import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class ClientConfig {
@@ -16,11 +16,18 @@ public class ClientConfig {
             @Value("${client.connection-timeout-second}") int connectionTimeoutSecond,
             @Value("${client.read-timeout-second}") int readTimeoutSecond
     ) {
-        RestTemplate restTemplate = new RestTemplateBuilder()
-                .setConnectTimeout(Duration.ofSeconds(connectionTimeoutSecond))
-                .setReadTimeout(Duration.ofSeconds(readTimeoutSecond))
-                .build();
+        ClientHttpRequestFactory requestFactory = createRequestFactory(connectionTimeoutSecond, readTimeoutSecond);
 
-        return RestClient.create(restTemplate);
+        return RestClient.builder()
+                .requestFactory(requestFactory)
+                .build();
+    }
+
+    private ClientHttpRequestFactory createRequestFactory(int connectionTimeoutSecond, int readTimeoutSecond) {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(Duration.ofSeconds(connectionTimeoutSecond));
+        factory.setReadTimeout(Duration.ofSeconds(readTimeoutSecond));
+
+        return factory;
     }
 }
