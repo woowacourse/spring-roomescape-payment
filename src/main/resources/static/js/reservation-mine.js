1bfa18aead7ba46157fa3ba12bc00512a2def1ab
haiseong <wjdgotjd9908@gmail.com>
2024-05-30 10:48:47 +0900

    chore: 프론트 ui 개선
    Co-authored-by: Haiseong <wjdgotjd9908@gmail.com>
    Co-authored-by: J-I-H-O <jeongjiho0731@gmail.com>

--- src/main/resources/static/js/reservation-mine.js
+++ src/main/resources/static/js/reservation-mine.js
@@ -23,31 +23,26 @@ function render(data) {
     row.insertCell(1).textContent = date;
     row.insertCell(2).textContent = time;
     row.insertCell(3).textContent = status;
+    const buttonCell = row.insertCell(4);
 
     if (status == '결제대기') {
-      const paymentCell = row.insertCell(4);
       const paymentButton = document.createElement('button');
       paymentButton.textContent = '결제';
       paymentButton.className = 'btn btn-primary';
       paymentButton.onclick = function () {
         requestPayment(item.id);
       };
-      paymentCell.appendChild(paymentButton);
-    } else {
-      row.insertCell(4).textContent = '';
+      buttonCell.appendChild(paymentButton);
     }
 
     if (status !== '예약') { // 예약 대기 상태일 때 예약 대기 취소 버튼 추가하는 코드, 상태 값은 변경 가능
-      const cancelCell = row.insertCell(5);
       const cancelButton = document.createElement('button');
       cancelButton.textContent = '취소';
       cancelButton.className = 'btn btn-danger';
       cancelButton.onclick = function () {
         requestDeleteWaiting(item.id).then(() => window.location.reload());
       };
-      cancelCell.appendChild(cancelButton);
-    } else { // 예약 완료 상태일 때
-      row.insertCell(5).textContent = '';
+      buttonCell.appendChild(cancelButton);
     }
   });
 }
