9e608964cf3ee990f72221c86705dac8ebe124ce
MingyeomKim <mingyum119@naver.com>
2024-05-31 16:51:46 +0900

    feat: 에러 로그 출력 및 RuntimeException 핸들러 생성

--- src/main/java/roomescape/common/exception/ExceptionApiController.java
+++ src/main/java/roomescape/common/exception/ExceptionApiController.java
@@ -2,7 +2,10 @@ package roomescape.common.exception;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,8 +15,15 @@ import roomescape.reservation.controller.dto.response.PaymentErrorResponse;
 @RestControllerAdvice(annotations = RestController.class)
 public class ExceptionApiController {
 
+    private static final Logger logger = LoggerFactory.getLogger(ExceptionApiController.class);
+
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> IllegalArgExHandler(IllegalArgumentException exception) {
+        return ResponseEntity.badRequest().body(exception.getMessage());
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<String> httpMessageNotReadableExceptionHandler(HttpMessageNotReadableException exception) {
         return ResponseEntity.badRequest().body(exception.getMessage());
     }
 
@@ -31,7 +41,14 @@ public class ExceptionApiController {
 
     @ExceptionHandler(PaymentException.class)
     public ResponseEntity<PaymentErrorResponse> paymentExHandler(PaymentException exception) {
+        logger.error(exception.getMessage(), exception);
         return ResponseEntity.status(exception.getStatusCode())
                 .body(new PaymentErrorResponse(exception.getMessage()));
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> exceptionHandler(RuntimeException exception) {
+        logger.error(exception.getMessage(), exception);
+        return ResponseEntity.status(500).body("내부 서버 오류입니다.");
     }
 }
