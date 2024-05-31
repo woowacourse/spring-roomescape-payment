3480d765d8efc3143e6141c23e1829f4e443517b
eun-byeol <joeunbyeol98@gmail.com>
2024-05-31 11:52:22 +0900

    feat: 예외 처리 시 log 출력

--- src/main/java/roomescape/common/exception/ExceptionApiController.java
+++ src/main/java/roomescape/common/exception/ExceptionApiController.java
@@ -2,6 +2,8 @@ package roomescape.common.exception;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,8 +14,11 @@ import roomescape.reservation.controller.dto.response.PaymentErrorResponse;
 @RestControllerAdvice(annotations = RestController.class)
 public class ExceptionApiController {
 
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> IllegalArgExHandler(IllegalArgumentException exception) {
+        log.error("[IllegalArgumentException] ", exception);
         return ResponseEntity.badRequest().body(exception.getMessage());
     }
 
@@ -25,12 +30,14 @@ public class ExceptionApiController {
             String errorMessage = error.getDefaultMessage();
             errors.put(fieldName, errorMessage);
         });
+        log.error("[MethodArgumentNotValidException]", exception);
 
         return ResponseEntity.badRequest().body(errors);
     }
 
     @ExceptionHandler(PaymentException.class)
     public ResponseEntity<PaymentErrorResponse> paymentExHandler(PaymentException exception) {
+        log.error("[PaymentException]", exception);
         return ResponseEntity.status(exception.getStatusCode())
                 .body(new PaymentErrorResponse(exception.getMessage()));
     }
