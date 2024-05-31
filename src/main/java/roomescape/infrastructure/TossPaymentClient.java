c2a443c229b9897a810f51f456f36ed3df6d8c40
Arachneee <jhg2819@naver.com>
2024-05-31 13:28:54 +0900

    refactor(TossPaymentClient): 결제 예외 처리 추가

--- src/main/java/roomescape/infrastructure/TossPaymentClient.java
+++ src/main/java/roomescape/infrastructure/TossPaymentClient.java
@@ -1,5 +1,7 @@
 package roomescape.infrastructure;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.ResourceAccessException;
@@ -11,6 +13,7 @@ import roomescape.service.dto.PaymentRequest;
 @Component
 public class TossPaymentClient implements PaymentClient {
 
+    private final Logger logger = LoggerFactory.getLogger(getClass());
     private final RestClient restClient;
 
     public TossPaymentClient(RestClient restClient) {
@@ -25,8 +28,14 @@ public class TossPaymentClient implements PaymentClient {
                     .body(request)
                     .retrieve()
                     .toBodilessEntity();
+        } catch (PaymentException e) {
+            throw e;
         } catch (ResourceAccessException e) {
+            logger.error(e.getMessage(), e);
             throw new PaymentException(HttpStatus.INTERNAL_SERVER_ERROR, "결제 서버의 연결 가능 시간이 초과되었습니다.");
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+            throw new PaymentException(HttpStatus.INTERNAL_SERVER_ERROR, "결제를 할 수 없습니다.");
         }
     }
 }
