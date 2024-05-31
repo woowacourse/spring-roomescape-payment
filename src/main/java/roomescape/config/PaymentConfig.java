24728cfae55e15731ee40090270fcced59d25f5f
cutehumanS2 <oddpinkjadeite@gmail.com>
2024-05-31 21:48:14 +0900

    feat: 결제 승인 API에 timeout 설정

--- src/main/java/roomescape/config/PaymentConfig.java
+++ src/main/java/roomescape/config/PaymentConfig.java
@@ -3,7 +3,11 @@ package roomescape.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
+
+import java.time.Duration;
 
 @Configuration
 public class PaymentConfig {
@@ -15,6 +19,14 @@ public class PaymentConfig {
     public RestClient paymentRestClient() {
         return RestClient.builder()
                         .baseUrl(baseUrl)
+                        .requestFactory(clientHttpRequestFactory())
                         .build();
+    }
+
+    private ClientHttpRequestFactory clientHttpRequestFactory() {
+        final SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        requestFactory.setConnectTimeout(Duration.ofSeconds(30L));
+        requestFactory.setReadTimeout(Duration.ofSeconds(30L));
+        return requestFactory;
     }
 }
