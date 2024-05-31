4460aa2538941b3774d4ba3345c985fc988892e4
hyun <osh123chm@gmail.com>
2024-05-31 15:09:56 +0900

    refactor: 통신 담당 객체 설정 추가

--- src/main/java/roomescape/config/PaymentConfig.java
+++ src/main/java/roomescape/config/PaymentConfig.java
@@ -1,11 +1,16 @@
 package roomescape.config;
 
 import java.nio.charset.StandardCharsets;
+import java.time.Duration;
 import java.util.Base64;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.client.ClientHttpRequestFactories;
+import org.springframework.boot.web.client.ClientHttpRequestFactorySettings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.JdkClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import roomescape.payment.client.TossPayRestClient;
 
@@ -14,6 +19,8 @@ import roomescape.payment.client.TossPayRestClient;
 public class PaymentConfig {
 
     private static final String TOSS_KEY_PREFIX = "Basic ";
+    private static final long CONNECTION_TIMEOUT = 5L;
+    private static final long READ_TIMEOUT = 30L;
 
     private final PaymentProperties paymentProperties;
 
@@ -25,10 +32,19 @@ public class PaymentConfig {
     public TossPayRestClient tossPayRestClient() {
         return new TossPayRestClient(
                 RestClient.builder()
+                        .requestFactory(createHttpRequestFactory())
                         .defaultHeader(HttpHeaders.AUTHORIZATION, createTossAuthorizations())
                         .baseUrl("https://api.tosspayments.com")
                         .build()
         );
+    }
+
+    private ClientHttpRequestFactory createHttpRequestFactory() {
+        ClientHttpRequestFactorySettings settings = ClientHttpRequestFactorySettings.DEFAULTS
+                .withConnectTimeout(Duration.ofSeconds(CONNECTION_TIMEOUT))
+                .withReadTimeout(Duration.ofSeconds(READ_TIMEOUT));
+
+        return ClientHttpRequestFactories.get(JdkClientHttpRequestFactory.class, settings);
     }
 
     private String createTossAuthorizations() {
