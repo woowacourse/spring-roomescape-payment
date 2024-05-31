71f5736bc03a73495d765151a257758c039fa8f2
SCY <helenason@naver.com>
2024-06-01 02:27:45 +0900

    feat(TossPaymentClient): 예외 발생 시 로그 출력

--- src/main/java/roomescape/infrastructure/TossPaymentClient.java
+++ src/main/java/roomescape/infrastructure/TossPaymentClient.java
@@ -1,6 +1,9 @@
 package roomescape.infrastructure;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestClient;
 import roomescape.exception.PaymentException;
@@ -8,6 +11,7 @@ import roomescape.service.dto.PaymentRequest;
 
 public class TossPaymentClient implements PaymentClient {
 
+    private final Logger logger = LoggerFactory.getLogger(getClass());
     private final RestClient restClient;
 
     public TossPaymentClient(RestClient restClient) {
@@ -18,10 +22,12 @@ public class TossPaymentClient implements PaymentClient {
         try {
             restClient.post()
                     .uri("/v1/payments/confirm")
+                    .contentType(MediaType.APPLICATION_JSON)
                     .body(request)
                     .retrieve()
                     .toBodilessEntity();
         } catch (ResourceAccessException e) {
+            logger.error("payment request body = {}" , request, e);
             throw new PaymentException(HttpStatus.INTERNAL_SERVER_ERROR, "결제 서버의 연결 가능 시간이 초과되었습니다.");
         }
     }
