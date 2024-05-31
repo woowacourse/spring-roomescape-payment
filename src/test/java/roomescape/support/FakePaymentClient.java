9ab6b5859d210bc3a8b0d7342a68d751a208cda3
3juhwan <13selfesteem91@naver.com>
2024-06-01 02:13:05 +0900

    test: 테스트에 fake 객체 추가

---
+++ src/test/java/roomescape/support/FakePaymentClient.java
@@ -0,0 +1,26 @@
+package roomescape.support;
+
+import roomescape.application.dto.request.payment.PaymentRequest;
+import roomescape.application.dto.response.payment.PaymentResponse;
+import roomescape.domain.payment.CancelReason;
+import roomescape.domain.payment.Payment;
+import roomescape.domain.payment.PaymentClient;
+
+public class FakePaymentClient implements PaymentClient {
+
+    @Override
+    public PaymentResponse confirm(PaymentRequest paymentRequest) {
+        return new PaymentResponse(
+                1000L,
+                "paymentKey",
+                "orderId",
+                "status",
+                "requestedAt",
+                "approvedAt"
+        );
+    }
+
+    @Override
+    public void cancel(Payment payment, CancelReason request) {
+    }
+}
