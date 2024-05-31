ae060e7cb552035d5c9861c1b9fd7d96bc179b67
HyungHoKim00 <rlagudgh1109@gmail.com>
2024-05-31 16:16:36 +0900

    refactor: 단순 널 체킹에 사용되는 optional 제거

--- src/main/java/roomescape/exception/PaymentException.java
+++ src/main/java/roomescape/exception/PaymentException.java
@@ -1,6 +1,5 @@
 package roomescape.exception;
 
-import java.util.Optional;
 import org.springframework.web.client.HttpClientErrorException;
 import roomescape.core.dto.exception.HttpExceptionResponse;
 
@@ -10,10 +9,10 @@ public class PaymentException extends RuntimeException {
     }
 
     public static PaymentException from(HttpClientErrorException e) {
-        final String message = Optional.ofNullable(e.getResponseBodyAs(HttpExceptionResponse.class))
-                .map(HttpExceptionResponse::getMessage)
-                .orElse(e.getMessage());
-
-        return new PaymentException(message);
+        HttpExceptionResponse responseBody = e.getResponseBodyAs(HttpExceptionResponse.class);
+        if (responseBody == null) {
+            return new PaymentException(e.getMessage());
+        }
+        return new PaymentException(responseBody.getMessage());
     }
 }
