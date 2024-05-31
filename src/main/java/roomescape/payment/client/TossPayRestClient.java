156831c3c6358d4c11629efd990d70c278d2d4ef
hyun <osh123chm@gmail.com>
2024-06-01 03:22:44 +0900

    refactor: timeout 예외 처리

--- src/main/java/roomescape/payment/client/TossPayRestClient.java
+++ src/main/java/roomescape/payment/client/TossPayRestClient.java
@@ -1,10 +1,17 @@
 package roomescape.payment.client;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestClient;
+import roomescape.exception.PaymentFailException;
 import roomescape.payment.dto.PaymentRequest;
 
 public class TossPayRestClient {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TossPayRestClient.class);
 
     private final RestClient restClient;
 
@@ -13,11 +20,19 @@ public class TossPayRestClient {
     }
 
     public void pay(PaymentRequest paymentRequest) {
-        restClient.post()
-                .uri("/v1/payments/confirm")
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(paymentRequest)
-                .retrieve()
-                .toBodilessEntity();
+        try {
+            restClient.post()
+                    .uri("/v1/payments/confirm")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(paymentRequest)
+                    .retrieve()
+                    .toBodilessEntity();
+        } catch (ResourceAccessException exception) {
+            LOGGER.error(exception.getMessage(), exception);
+            throw new PaymentFailException(
+                    HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(),
+                    "요청 시간이 초과되었습니다. 잠시 후 다시 시도해주세요."
+            );
+        }
     }
 }
