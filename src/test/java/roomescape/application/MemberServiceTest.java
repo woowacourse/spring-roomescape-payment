f17e9b66fddfbbb631da970611e1465e5f863314
3juhwan <13selfesteem91@naver.com>
2024-05-31 22:24:49 +0900

    test: 멤버 서비스 테스트 추가

---
+++ src/test/java/roomescape/application/MemberServiceTest.java
@@ -0,0 +1,61 @@
+package roomescape.application;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import roomescape.application.dto.request.member.LoginRequest;
+import roomescape.application.dto.request.member.SignupRequest;
+import roomescape.exception.member.AuthenticationFailureException;
+import roomescape.fixture.CommonFixture;
+
+class MemberServiceTest extends BaseServiceTest {
+
+    @Autowired
+    private MemberService memberService;
+
+    @BeforeEach
+    void setUp() {
+        SignupRequest request = new SignupRequest(CommonFixture.NAME, CommonFixture.EMAIL, CommonFixture.PASSWORD);
+        memberService.signup(request);
+    }
+
+    @DisplayName("로그인 시, 토큰을 반환한다")
+    @Test
+    void when_loginSuccess_then_getToken() {
+        // given
+        LoginRequest request = new LoginRequest(CommonFixture.EMAIL, CommonFixture.PASSWORD);
+
+        // when, then
+        Assertions.assertThatCode(() -> memberService.login(request))
+                .doesNotThrowAnyException();
+    }
+
+    @DisplayName("로그인 실패 시, 예외를 반환한다")
+    @Test
+    void when_loginFail_then_throwException() {
+        // given
+        LoginRequest request = new LoginRequest(CommonFixture.EMAIL, "wrong_password");
+
+        // when, then
+        Assertions.assertThatThrownBy(() -> memberService.login(request))
+                .isExactlyInstanceOf(AuthenticationFailureException.class);
+    }
+
+    @DisplayName("회원 탈퇴 시, 로그인할 수 없다")
+    @Test
+    void when_withdrawal_then_cannotLogin() {
+        // given
+        LoginRequest request = new LoginRequest(CommonFixture.EMAIL, CommonFixture.PASSWORD);
+
+        // when
+        memberService.findAllMember().stream()
+                .findFirst()
+                .ifPresent(member -> memberService.withdrawal(member.id()));
+
+        // then
+        Assertions.assertThatThrownBy(() -> memberService.login(request))
+                .isExactlyInstanceOf(AuthenticationFailureException.class);
+    }
+}
