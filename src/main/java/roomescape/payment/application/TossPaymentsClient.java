94bccbfab90040ede122fef984b946718fa11409
le2sky <leehaneul990623@gmail.com>
2024-06-01 11:00:09 +0900

    feat: rest client 타임아웃 설정

--- src/main/java/roomescape/payment/application/TossPaymentsClient.java
+++ src/main/java/roomescape/payment/application/TossPaymentsClient.java
@@ -2,7 +2,10 @@ package roomescape.payment.application;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.ClientHttpRequestFactories;
+import org.springframework.boot.web.client.ClientHttpRequestFactorySettings;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
@@ -12,12 +15,16 @@ import roomescape.payment.dto.TossPaymentsErrorResponse;
 import roomescape.payment.exception.PaymentServerException;
 
 import java.io.IOException;
+import java.time.Duration;
 import java.util.Base64;
 
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
 @Component
 public class TossPaymentsClient {
+    private static final int CONNECTION_TIMEOUT_SECOND = 3;
+    private static final int READ_TIMEOUT_SECOND = 30;
+
     private final RestClient restClient;
     private final ObjectMapper objectMapper;
 
@@ -25,12 +32,21 @@ public class TossPaymentsClient {
         this.restClient = RestClient.builder()
                 .baseUrl("https://api.tosspayments.com/v1/payments")
                 .defaultHeader(AUTHORIZATION, encodeSecretKey(secretKey))
+                .requestFactory(getRequestFactory())
                 .build();
         this.objectMapper = objectMapper;
     }
 
     private String encodeSecretKey(String secretKey) {
         return "Basic " + Base64.getEncoder().encodeToString((secretKey + ":").getBytes());
+    }
+
+    private ClientHttpRequestFactory getRequestFactory() {
+        ClientHttpRequestFactorySettings settings = ClientHttpRequestFactorySettings.DEFAULTS
+                .withConnectTimeout(Duration.ofSeconds(CONNECTION_TIMEOUT_SECOND))
+                .withReadTimeout(Duration.ofSeconds(READ_TIMEOUT_SECOND));
+
+        return ClientHttpRequestFactories.get(settings);
     }
 
     public void confirm(PaymentConfirmRequest request) {
