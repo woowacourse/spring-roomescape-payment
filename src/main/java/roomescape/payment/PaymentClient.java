7319dab975380fd8a13c75e100a8d0ca97ec5122
libienz <leekh10195@gmail.com>
2024-06-01 18:03:44 +0900

    refactor: 외부 api 요청 defaultHeader 설정

--- src/main/java/roomescape/payment/PaymentClient.java
+++ src/main/java/roomescape/payment/PaymentClient.java
@@ -19,32 +19,35 @@ public class PaymentClient {
     private static final String BASE_URL = "https://api.tosspayments.com";
     private static final String CONFIRM_URI = "/v1/payments/confirm";
     private static final String AUTH_TYPE = "Basic ";
+    public static final String AUTHORIZATION = "Authorization";
 
-    @Value("${third-party-api.payment.secret-key}")
-    private String secretKey;
     private final RestClient restClient;
 
-    public PaymentClient() {
+    public PaymentClient(@Value("${third-party-api.payment.secret-key}") String secretKey) {
         ClientHttpRequestFactorySettings settings = ClientHttpRequestFactorySettings.DEFAULTS
                 .withConnectTimeout(Duration.ofMinutes(1))
                 .withReadTimeout(Duration.ofMinutes(3));
+
         ClientHttpRequestFactory requestFactory = ClientHttpRequestFactories.get(settings);
 
-        this.restClient = RestClient.builder().baseUrl(BASE_URL).requestFactory(requestFactory).build();
+        this.restClient = RestClient.builder()
+                .baseUrl(BASE_URL)
+                .defaultHeader(AUTHORIZATION, createAuthorizationValue(secretKey))
+                .requestFactory(requestFactory)
+                .build();
     }
 
     public void requestConfirmPayment(PaymentConfirmRequest paymentConfirmRequest) {
         restClient.post()
                 .uri(CONFIRM_URI)
                 .contentType(MediaType.APPLICATION_JSON)
-                .header("Authorization", createAuthorizationValue())
                 .body(paymentConfirmRequest)
                 .retrieve()
                 .onStatus(HttpStatusCode::isError, TossPaymentErrorHandler::handle)
                 .toBodilessEntity();
     }
 
-    private String createAuthorizationValue() {
+    private String createAuthorizationValue(String secretKey) {
         byte[] encodedBytes = Base64.getEncoder().encode((secretKey + ":").getBytes(StandardCharsets.UTF_8));
         String encodedSecretKey = new String(encodedBytes);
 
