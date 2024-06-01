ccac591a5eedef1770762aa26cd9ea722c7208f5
devhoya97 <diajbh123@gmail.com>
2024-06-01 16:18:17 +0900

    refactor: 정보의 은닉화를 고려하여 예외 메시지 설정

--- src/main/java/roomescape/core/controller/PaymentErrorHandler.java
+++ src/main/java/roomescape/core/controller/PaymentErrorHandler.java
@@ -1,12 +1,6 @@
 package roomescape.core.controller;
 
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.nio.charset.StandardCharsets;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.web.client.ResponseErrorHandler;
 import roomescape.core.exception.PaymentException;
@@ -18,28 +12,11 @@ public class PaymentErrorHandler implements ResponseErrorHandler {
         return response.getStatusCode().is4xxClientError() || response.getStatusCode().is5xxServerError();
     }
 
-    // TODO: 토스에서 제공한 메시지를 그대로 넘겨주지 말고, 클라이언트에 필요한 메시지로 변환하여 제공하기
+    // TODO: 로그 남기기
     @Override
     public void handleError(final ClientHttpResponse response) throws IOException {
-        if (response.getStatusCode().is5xxServerError()) {
-            throw new PaymentException(response.getStatusCode(), response.getStatusText());
+        if (response.getStatusCode().is5xxServerError() || response.getStatusCode().is4xxClientError()) {
+            throw new PaymentException(response.getStatusCode(), "결제 승인 과정에서 문제가 발생했습니다.");
         }
-        if (response.getStatusCode().is4xxClientError()) {
-            Reader reader = new InputStreamReader(response.getBody(), StandardCharsets.UTF_8);
-            JSONParser parser = new JSONParser();
-            JSONObject jsonObject = getJsonObject(parser, reader);
-            response.getBody().close();
-            throw new PaymentException(response.getStatusCode(), jsonObject.get("message").toString());
-        }
-    }
-
-    private JSONObject getJsonObject(final JSONParser parser, final Reader reader) throws IOException {
-        JSONObject jsonObject;
-        try {
-            jsonObject = (JSONObject) parser.parse(reader);
-        } catch (ParseException e) {
-            throw new RuntimeException(e.getMessage());
-        }
-        return jsonObject;
     }
 }
