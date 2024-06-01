41dad25428c8f4d86f060a169ac437bb6ac41f27
gyungchan Jo <namejgc@naver.com>
2024-06-02 03:08:03 +0900

    refactor: 에러 로그 레벨 변경

--- src/main/java/roomescape/exception/RoomescapeExceptionHandler.java
+++ src/main/java/roomescape/exception/RoomescapeExceptionHandler.java
@@ -17,28 +17,28 @@ public class RoomescapeExceptionHandler {
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ErrorResponse handle(HttpMessageNotReadableException e) {
-        logger.error("{}{}{}", INVALID_DATE_TIME_FORMAT.getLogMessage(), System.lineSeparator(), e.getMessage());
+        logger.warn("{}{}{}", INVALID_DATE_TIME_FORMAT.getLogMessage(), System.lineSeparator(), e.getMessage());
         return ErrorResponse.builder(e, INVALID_DATE_TIME_FORMAT.getStatus(), INVALID_DATE_TIME_FORMAT.getMessage())
                 .build();
     }
 
     @ExceptionHandler(RoomescapeException.class)
     public ErrorResponse handle(RoomescapeException e) {
-        logger.error(e.getLogMessage());
+        logger.warn(e.getLogMessage());
         return ErrorResponse.builder(e, e.getHttpStatus(), e.getMessage())
                 .build();
     }
 
     @ExceptionHandler(PaymentException.class)
     public ErrorResponse handle(PaymentException e) {
-        logger.error(e.getMessage());
+        logger.warn(e.getMessage());
         return ErrorResponse.builder(e, e.getHttpStatus(), e.getMessage())
                 .build();
     }
 
     @ExceptionHandler(MissingServletRequestParameterException.class)
     public ErrorResponse handle(MissingServletRequestParameterException e) {
-        logger.error("{}{}{}", NO_QUERY_PARAMETER.getLogMessage(), System.lineSeparator(), e.getMessage());
+        logger.warn("{}{}{}", NO_QUERY_PARAMETER.getLogMessage(), System.lineSeparator(), e.getMessage());
         return ErrorResponse.builder(e, NO_QUERY_PARAMETER.getStatus(), NO_QUERY_PARAMETER.getMessage())
                 .build();
     }
