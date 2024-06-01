0791df0126c405064fe83669a5ada7009254e739
hyeonjilee <skylar12200@gmail.com>
2024-06-01 19:34:06 +0900

    refactor(GlobalExceptionHandler): log 라이브러리 및 패키지 변경

--- src/main/java/roomescape/exception/GlobalExceptionHandler.java
+++ src/main/java/roomescape/exception/GlobalExceptionHandler.java
@@ -1,22 +1,21 @@
-package roomescape.web.controller;
+package roomescape.exception;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import roomescape.exception.PaymentException;
 
 @ControllerAdvice
 class GlobalExceptionHandler {
 
-    private final Logger logger = Logger.getLogger(getClass().getName());
+    private final Logger log = LoggerFactory.getLogger(getClass());
 
     @ExceptionHandler(value = HttpMessageNotReadableException.class)
     private ProblemDetail handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
-        logger.log(Level.SEVERE, e.getMessage());
+        log.error("[{}] {}", e.getClass().getName(), e.getMessage());
         String errorMessage = "요청 본문을 읽을 수 없습니다. 요청 형식을 확인해 주세요.";
         return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, errorMessage);
     }
@@ -25,7 +24,8 @@ class GlobalExceptionHandler {
     private ProblemDetail handleIllegalArgumentException(IllegalArgumentException e) {
         Throwable cause = e.getCause();
         if (cause != null) {
-            logger.log(Level.SEVERE, "[IllegalArgumentException] " + cause.getMessage());
+            log.error("[{}] {}", e.getClass().getName(), cause.getMessage()
+            );
         }
         return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, e.getMessage());
     }
@@ -37,7 +37,7 @@ class GlobalExceptionHandler {
 
     @ExceptionHandler(value = Exception.class)
     private ProblemDetail handleGeneralException(Exception e) {
-        logger.log(Level.SEVERE, e.getMessage());
+        log.error("[{}] {}", e.getClass().getName(), e.getMessage());
         String errorMessage = "시스템에서 오류가 발생했습니다. 관리자에게 문의해주세요.";
         return ProblemDetail.forStatusAndDetail(
                 HttpStatus.INTERNAL_SERVER_ERROR, errorMessage);
