441de7a3a9cefa0bfad9b8df806896e745a327ab
JUHA <84626225+khabh@users.noreply.github.com>
2024-06-01 16:09:52 +0900

    test: PaymentManager 테스트 수정

--- src/test/java/roomescape/infrastructure/payment/PaymentManagerTest.java
+++ src/test/java/roomescape/infrastructure/payment/PaymentManagerTest.java
@@ -8,6 +8,8 @@ import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.client.MockRestServiceServer;
+import roomescape.infrastructure.payment.response.PaymentErrorResponse;
+import roomescape.infrastructure.payment.response.PaymentServerErrorCode;
 import roomescape.service.exception.PaymentException;
 import roomescape.service.request.PaymentApproveDto;
 
@@ -19,6 +21,7 @@ import static org.springframework.test.web.client.match.MockRestRequestMatchers.
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withStatus;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
+import static roomescape.infrastructure.payment.response.PaymentServerErrorCode.INVALID_API_KEY;
 
 @RestClientTest(PaymentManager.class)
 class PaymentManagerTest {
@@ -52,14 +55,31 @@ class PaymentManagerTest {
     void invalidPaymentApproveRequest() throws IOException {
         PaymentApproveDto paymentApproveDto = new PaymentApproveDto("invalidKey", "orderId", 1000L);
         String paymentApproveJson = objectMapper.writeValueAsString(paymentApproveDto);
-        String responseJson = objectMapper.writeValueAsString(new RuntimeException("올바르지 않은 결제 요청입니다."));
+        String responseJson = objectMapper.writeValueAsString(new PaymentErrorResponse("ALREADY_PROCESSED_PAYMENT", "이미 처리된 결제 입니다."));
         this.server.expect(requestTo("https://api.tosspayments.com/v1/payments/confirm"))
                 .andExpect(content().json(paymentApproveJson))
                 .andRespond(withStatus(HttpStatus.BAD_REQUEST).body(responseJson));
 
         assertThatThrownBy(() -> paymentManager.approve(paymentApproveDto))
                 .isInstanceOf(PaymentException.class)
-                .hasMessage("올바르지 않은 결제 요청입니다.");
+                .hasMessage("이미 처리된 결제 입니다.");
+        this.server.verify();
+    }
+
+    @DisplayName("서버 오류로 지정된 에러 객체 받을 시 메시지를 올바르게 변환한다.")
+    @Test
+    void convertErrorMessageFromServerCode() throws IOException {
+        PaymentServerErrorCode serverErrorCode = INVALID_API_KEY;
+        PaymentApproveDto paymentApproveDto = new PaymentApproveDto("invalidKey", "orderId", 1000L);
+        String paymentApproveJson = objectMapper.writeValueAsString(paymentApproveDto);
+        String responseJson = objectMapper.writeValueAsString(new PaymentErrorResponse(serverErrorCode.name(), serverErrorCode.getMessage()));
+        this.server.expect(requestTo("https://api.tosspayments.com/v1/payments/confirm"))
+                .andExpect(content().json(paymentApproveJson))
+                .andRespond(withStatus(HttpStatus.BAD_REQUEST).body(responseJson));
+
+        assertThatThrownBy(() -> paymentManager.approve(paymentApproveDto))
+                .isInstanceOf(PaymentException.class)
+                .hasMessage("결제에 실패했습니다.");
         this.server.verify();
     }
 }
