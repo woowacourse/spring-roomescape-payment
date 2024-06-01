ba244fcd2019447e04da37a2ab1838de9e5e3dc4
slimsha2dy <chy2581@naver.com>
2024-06-01 20:41:41 +0900

    feat: 외부 API 요청 timeout 시 예외로 처리하는 기능 구현

--- src/main/java/roomescape/domain/payment/PaymentClient.java
+++ src/main/java/roomescape/domain/payment/PaymentClient.java
@@ -1,21 +1,26 @@
 package roomescape.domain.payment;
 
+import java.time.Duration;
 import java.util.Base64;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.ClientHttpRequestFactories;
+import org.springframework.boot.web.client.ClientHttpRequestFactorySettings;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestClient;
 import roomescape.domain.Member;
 import roomescape.dto.PaymentApproveRequest;
 
 @Component
 public class PaymentClient {
+    private final static String UNKNOWN_ERROR = "알 수 없는 오류가 발생했습니다.";
     private final String apiUri;
     private final String approveSecretKey;
     private final RestClient restClient;
     private final PaymentApiResponseErrorHandler errorHandler;
-
 
     public PaymentClient(
             @Value("${payment.approve.base-url}") String baseUrl,
@@ -24,13 +29,25 @@ public class PaymentClient {
             PaymentApiResponseErrorHandler errorHandler) {
         this.restClient = RestClient.builder()
                 .baseUrl(baseUrl)
-                .build();
+                .requestFactory(ClientHttpRequestFactories.get(
+                        ClientHttpRequestFactorySettings.DEFAULTS
+                                .withConnectTimeout(Duration.ofMillis(3))
+                                .withReadTimeout(Duration.ofMillis(3)))
+                ).build();
         this.apiUri = apiUri;
         this.approveSecretKey = approveSecretKey;
         this.errorHandler = errorHandler;
     }
 
     public Payment approve(PaymentApproveRequest paymentApproveRequest, Member member) {
+        try {
+            return callApiRequest(paymentApproveRequest, member);
+        } catch (ResourceAccessException e) {
+            throw new ApiCallException(UNKNOWN_ERROR);
+        }
+    }
+
+    private Payment callApiRequest(PaymentApproveRequest paymentApproveRequest, Member member) {
         String encryptedKey = Base64.getEncoder().encodeToString(approveSecretKey.getBytes());
         ApproveApiResponse response = Optional.ofNullable(restClient.post()
                         .uri(apiUri)
@@ -39,7 +56,7 @@ public class PaymentClient {
                         .retrieve()
                         .onStatus(errorHandler)
                         .body(ApproveApiResponse.class))
-                .orElseThrow(() -> new ApiCallException("알 수 없는 오류가 발생했습니다."));
+                .orElseThrow(() -> new ApiCallException(UNKNOWN_ERROR));
         return new Payment(response.orderId(), response.paymentKey(), response.amount(), member);
     }
 }
