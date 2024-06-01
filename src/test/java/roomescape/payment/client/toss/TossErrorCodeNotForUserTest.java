e8ca752a761033ffa6a5641a04bcdbee232ca51a
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-06-01 19:50:38 +0900

    refactor(TossErrorCodeNotForUser): 유저에게 보여주지 않을 토스 에러 코드 이넘으로 관리

---
+++ src/test/java/roomescape/payment/client/toss/TossErrorCodeNotForUserTest.java
@@ -0,0 +1,21 @@
+package roomescape.payment.client.toss;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class TossErrorCodeNotForUserTest {
+
+    @Test
+    @DisplayName("문자열이 enum에 존재하는 값: 참")
+    void hasContains() {
+        assertTrue(TossErrorCodeNotForUser.hasContains("INVALID_UNREGISTERED_SUBMALL"));
+    }
+
+    @Test
+    @DisplayName("문자열이 enum에 존재하는 값: 거짓")
+    void hasNotContains() {
+        assertFalse(TossErrorCodeNotForUser.hasContains("EXCEED_MAX_ONE_DAY_AMOUNT"));
+    }
+}
