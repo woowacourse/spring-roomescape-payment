8dbf4a32e9bae776eea68b0af107843c66ba9d4b
gyungchan Jo <namejgc@naver.com>
2024-06-01 18:59:20 +0900

    refactor: 일반 사용자와 관리자의 예약 기능 클래스 분리

--- src/main/java/roomescape/controller/LoginController.java
+++ src/main/java/roomescape/controller/LoginController.java
@@ -3,20 +3,18 @@ package roomescape.controller;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
+import org.springframework.web.bind.annotation.RestController;
 import roomescape.config.CookieExtractor;
 import roomescape.domain.LoginMember;
 import roomescape.dto.LoginRequest;
 import roomescape.dto.LoginResponse;
 import roomescape.service.LoginService;
 
-@Controller
+@RestController
 public class LoginController {
     private final LoginService loginService;
 
--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -1,30 +1,15 @@
 package roomescape.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import roomescape.domain.LoginMember;
+import roomescape.dto.*;
+import roomescape.service.PaymentService;
+import roomescape.service.ReservationService;
+
 import java.net.URI;
 import java.time.LocalDate;
 import java.util.List;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import roomescape.domain.LoginMember;
-import roomescape.dto.AdminReservationDetailResponse;
-import roomescape.dto.AdminReservationRequest;
-import roomescape.dto.PaymentRequest;
-import roomescape.dto.PaymentResponse;
-import roomescape.dto.ReservationDetailResponse;
-import roomescape.dto.ReservationPaymentRequest;
-import roomescape.dto.ReservationPaymentResponse;
-import roomescape.dto.ReservationRequest;
-import roomescape.dto.ReservationResponse;
-import roomescape.service.PaymentService;
-import roomescape.service.ReservationService;
 
 @RestController
 public class ReservationController {
@@ -53,14 +38,6 @@ public class ReservationController {
                 .body(savedReservationResponse);
     }
 
-    @PostMapping("/admin/reservations")
-    public ResponseEntity<ReservationResponse> saveReservationByAdmin(
-            @RequestBody AdminReservationRequest reservationRequest) {
-        ReservationResponse reservationResponse = reservationService.saveByAdmin(reservationRequest);
-        return ResponseEntity.created(URI.create("/reservations/" + reservationResponse.id()))
-                .body(reservationResponse);
-    }
-
     @GetMapping("/reservations")
     public List<ReservationResponse> findAllReservations() {
         return reservationService.findAllReservations();
@@ -81,11 +58,6 @@ public class ReservationController {
         return reservationService.searchReservation(themeId, memberId, dateFrom, dateTo);
     }
 
-    @GetMapping("/admin/reservations-waiting")
-    public List<AdminReservationDetailResponse> findAllWaitingReservations() {
-        return reservationService.findAllWaitingReservations();
-    }
-
     @DeleteMapping("/reservations/{id}")
     public ResponseEntity<Void> delete(@PathVariable long id) {
         reservationService.deleteById(id);
@@ -95,12 +67,6 @@ public class ReservationController {
     @DeleteMapping("/reservations-waiting/{id}")
     public ResponseEntity<Void> deleteReservationWaiting(@Authenticated LoginMember loginMember, @PathVariable long id) {
         reservationService.deleteByMemberIdAndId(loginMember, id);
-        return ResponseEntity.noContent().build();
-    }
-
-    @DeleteMapping("/admin/reservations-waiting/{id}")
-    public ResponseEntity<Void> deleteReservationWaitingByAdmin(@PathVariable long id) {
-        reservationService.deleteById(id);
         return ResponseEntity.noContent().build();
     }
 }
