3e8d1beeaa0b4ace6d5cf94e0f5e93685f32ec11
devhoya97 <diajbh123@gmail.com>
2024-06-01 23:23:21 +0900

    refactor: 필요없는 if문을 지우고 결제 관련 예외에 대해 로그처리

--- src/main/java/roomescape/infrastructure/PaymentApproveErrorHandler.java
+++ src/main/java/roomescape/infrastructure/PaymentApproveErrorHandler.java
@@ -1,22 +1,25 @@
 package roomescape.infrastructure;
 
 import java.io.IOException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.web.client.ResponseErrorHandler;
 import roomescape.core.exception.PaymentException;
 
 public class PaymentApproveErrorHandler implements ResponseErrorHandler {
 
+    private static final Logger logger = LoggerFactory.getLogger(PaymentApproveErrorHandler.class);
+
     @Override
     public boolean hasError(final ClientHttpResponse response) throws IOException {
         return response.getStatusCode().is4xxClientError() || response.getStatusCode().is5xxServerError();
     }
 
-    // TODO: 로그 남기기
     @Override
     public void handleError(final ClientHttpResponse response) throws IOException {
-        if (response.getStatusCode().is5xxServerError() || response.getStatusCode().is4xxClientError()) {
-            throw new PaymentException(response.getStatusCode(), "결제 승인 과정에서 문제가 발생했습니다.");
-        }
+        logger.error("결제 승인 과정에서 문제가 발생했습니다. 상태 코드: {}, 상태 텍스트: {}",
+                response.getStatusCode(), response.getStatusText());
+        throw new PaymentException(response.getStatusCode(), "결제 승인 과정에서 문제가 발생했습니다.");
     }
 }
