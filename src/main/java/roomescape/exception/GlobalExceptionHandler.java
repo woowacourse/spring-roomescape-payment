df966f7a19f8dd731c09534c28a34db77dd59b02
haiseong <wjdgotjd9908@gmail.com>
2024-06-02 14:10:27 +0900

    refactor: GlobalExceptionHandler에서 상태코드를 정하도록 변경

--- src/main/java/roomescape/exception/GlobalExceptionHandler.java
+++ src/main/java/roomescape/exception/GlobalExceptionHandler.java
@@ -5,8 +5,10 @@ import static roomescape.exception.ExceptionType.UNEXPECTED_ERROR;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.Set;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -15,6 +17,20 @@ import roomescape.dto.ErrorResponse;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    private static final Set<String> SERVER_ERROR_CODES = Set.of(
+            "INVALID_API_KEY",
+            "NOT_FOUND_TERMINAL_ID",
+            "INVALID_AUTHORIZE_AUTH",
+            "INVALID_UNREGISTERED_SUBMALL",
+            "UNAPPROVED_ORDER_ID",
+            "UNAUTHORIZED_KEY",
+            "FORBIDDEN_REQUEST",
+            "INCORRECT_BASIC_AUTH_FORMAT",
+            "FAILED_PAYMENT_INTERNAL_SYSTEM_PROCESSING",
+            "FAILED_INTERNAL_SYSTEM_PROCESSING",
+            "UNKNOWN_PAYMENT_ERROR"
+    );
 
     private final Logger logger = LoggerFactory.getLogger(getClass());
     private final StringWriter stringWriter = new StringWriter();
@@ -38,8 +54,13 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(PaymentException.class)
     public ResponseEntity<ErrorResponse> handle(PaymentException e) {
         logger.error(getStackTrace(e));
+        if (SERVER_ERROR_CODES.contains(e.getCode())) {
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ErrorResponse(e.getMessage()));
+        }
         return ResponseEntity
-                .status(e.getStatus())
+                .status(HttpStatus.BAD_REQUEST)
                 .body(new ErrorResponse(e.getMessage()));
     }
 
--- src/main/java/roomescape/exception/PaymentException.java
+++ src/main/java/roomescape/exception/PaymentException.java
@@ -2,46 +2,26 @@ package roomescape.exception;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
-import java.util.Set;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.client.ClientHttpResponse;
 
 public class PaymentException extends RuntimeException {
 
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
-    private static final Set<String> SERVER_ERROR_CODES = Set.of(
-            "INVALID_API_KEY",
-            "NOT_FOUND_TERMINAL_ID",
-            "INVALID_AUTHORIZE_AUTH",
-            "INVALID_UNREGISTERED_SUBMALL",
-            "UNAPPROVED_ORDER_ID",
-            "UNAUTHORIZED_KEY",
-            "FORBIDDEN_REQUEST",
-            "INCORRECT_BASIC_AUTH_FORMAT",
-            "FAILED_PAYMENT_INTERNAL_SYSTEM_PROCESSING",
-            "FAILED_INTERNAL_SYSTEM_PROCESSING",
-            "UNKNOWN_PAYMENT_ERROR"
-    );
 
-    private final HttpStatus status;
     private final String code;
 
-    private PaymentException(HttpStatus status, String code, String message) {
+    private PaymentException(String code, String message) {
         super(message);
-        this.status = status;
         this.code = code;
     }
 
     public static PaymentException from(ClientHttpResponse response) throws IOException {
         PaymentErrorResponse paymentErrorResponse = OBJECT_MAPPER.readValue(response.getBody(), PaymentErrorResponse.class);
 
-        if (SERVER_ERROR_CODES.contains(paymentErrorResponse.code())) {
-            return new PaymentException(HttpStatus.INTERNAL_SERVER_ERROR, paymentErrorResponse.code(), "결제 서버에 문제가 발생했습니다.");
-        }
-        return new PaymentException(HttpStatus.BAD_REQUEST, paymentErrorResponse.code(), paymentErrorResponse.message());
+        return new PaymentException(paymentErrorResponse.code(), paymentErrorResponse.message());
     }
 
-    public HttpStatus getStatus() {
-        return status;
+    public String getCode() {
+        return code;
     }
 }
