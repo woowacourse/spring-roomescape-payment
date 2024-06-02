44c4da514085705f2af0946f16060886256383a5
lemone <lejinhy@gmail.com>
2024-06-02 12:04:48 +0900

    feat(InternalExceptionCode): 서버에서만 사용할 exception code 추가
    Co-authored-by: lemone <lejinhy@gmail.com>

--- src/main/java/roomescape/exception/model/InternalExceptionCode.java
+++ src/main/java/roomescape/exception/model/InternalExceptionCode.java
@@ -3,14 +3,14 @@ package roomescape.exception.model;
 import org.springframework.http.HttpStatus;
 import roomescape.exception.ExceptionCode;
 
-public enum ServerExceptionCode implements ExceptionCode {
-    RESERVATION_FAIL(HttpStatus.INTERNAL_SERVER_ERROR, "서버 문제로 예약에 실패하였습니다."),
-    RESERVATION_URI_MOVE(HttpStatus.FOUND, "예약 주소가 바뀌었습니다. 새로운 예약 주소로 다시 시도해 주세요.");
+public enum InternalExceptionCode implements ExceptionCode {
+
+    INVALID_JSON_DATA(HttpStatus.INTERNAL_SERVER_ERROR, "Json 변환에 실패하였습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
 
-    ServerExceptionCode(HttpStatus httpStatus, String message) {
+    InternalExceptionCode(HttpStatus httpStatus, String message) {
         this.httpStatus = httpStatus;
         this.message = message;
     }
