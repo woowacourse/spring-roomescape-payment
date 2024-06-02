fdc3c8ab37df505aa30944d114d505c2d9003f00
PgmJun <chltmdwns96@gmail.com>
2024-06-02 14:56:15 +0900

    refactor: 클라이언트 측으로 전달하는 payment 에러 메세지에서 전달하지 않아도 되는 정보 제거

--- src/main/java/roomescape/exception/payment/PaymentConfirmErrorCode.java
+++ src/main/java/roomescape/exception/payment/PaymentConfirmErrorCode.java
@@ -1,7 +1,8 @@
 package roomescape.exception.payment;
 
-import java.util.Arrays;
 import org.springframework.http.HttpStatus;
+
+import java.util.Arrays;
 
 public enum PaymentConfirmErrorCode {
     ALREADY_PROCESSED_PAYMENT(HttpStatus.BAD_REQUEST, "이미 처리된 결제 입니다."),
@@ -10,7 +11,7 @@ public enum PaymentConfirmErrorCode {
     INVALID_API_KEY(HttpStatus.BAD_REQUEST, "잘못된 시크릿키 연동 정보 입니다."),
     INVALID_EXCEED_MAX_DAILY_PAYMENT_COUNT(HttpStatus.BAD_REQUEST, "하루 결제 가능 횟수를 초과했습니다."),
     EXCEED_MAX_PAYMENT_AMOUNT(HttpStatus.BAD_REQUEST, "하루 결제 가능 금액을 초과했습니다."),
-    NOT_FOUND_TERMINAL_ID(HttpStatus.BAD_REQUEST, "단말기번호(Terminal Id)가 없습니다. 토스페이먼츠로 문의 바랍니다."),
+    NOT_FOUND_TERMINAL_ID(HttpStatus.BAD_REQUEST, "단말기번호(Terminal Id)가 없습니다. 관리자에게 문의해주세요."),
     UNAPPROVED_ORDER_ID(HttpStatus.BAD_REQUEST, "아직 승인되지 않은 주문번호입니다."),
     UNAUTHORIZED_KEY(HttpStatus.BAD_REQUEST, "인증되지 않은 시크릿 키 혹은 클라이언트 키 입니다."),
     INVALID_PASSWORD(HttpStatus.BAD_REQUEST, "결제 비밀번호가 일치하지 않습니다."),
@@ -19,12 +20,12 @@ public enum PaymentConfirmErrorCode {
 
     INVALID_AUTHORIZE_AUTH(HttpStatus.INTERNAL_SERVER_ERROR, "유효하지 않은 인증 방식입니다."),
     NOT_AVAILABLE_PAYMENT(HttpStatus.INTERNAL_SERVER_ERROR, "결제가 불가능한 시간대입니다"),
-    INCORRECT_BASIC_AUTH_FORMAT(HttpStatus.INTERNAL_SERVER_ERROR, "잘못된 요청입니다. ':' 를 포함해 인코딩해주세요."),
+    INCORRECT_BASIC_AUTH_FORMAT(HttpStatus.INTERNAL_SERVER_ERROR, "서버 측 오류로 결제에 실패했습니다. 관리자에게 문의해주세요."),
     FAILED_PAYMENT_INTERNAL_SYSTEM_PROCESSING(HttpStatus.INTERNAL_SERVER_ERROR, "결제가 완료되지 않았어요. 다시 시도해주세요."),
     FAILED_INTERNAL_SYSTEM_PROCESSING(HttpStatus.INTERNAL_SERVER_ERROR, "내부 시스템 처리 작업이 실패했습니다. 잠시 후 다시 시도해주세요."),
     UNKNOWN_PAYMENT_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "결제에 실패했어요. 같은 문제가 반복된다면 은행이나 카드사로 문의해주세요."),
 
-    PAYMENT_CONFIRM_ERROR_MISMATCH(HttpStatus.INTERNAL_SERVER_ERROR, "결제 과정에서 서버 에러가 발생했습니다."),
+    PAYMENT_CONFIRM_ERROR_MISMATCH_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "결제 과정에서 서버 에러가 발생했습니다. 관리자에게 문의해주세요."),
     ;
 
     private final HttpStatus httpStatus;
@@ -34,7 +35,7 @@ public enum PaymentConfirmErrorCode {
         return Arrays.stream(values())
                 .filter(v -> v.name().equals(name))
                 .findAny()
-                .orElse(PAYMENT_CONFIRM_ERROR_MISMATCH);
+                .orElse(PAYMENT_CONFIRM_ERROR_MISMATCH_ERROR);
     }
 
     PaymentConfirmErrorCode(final HttpStatus httpStatus, final String message) {
