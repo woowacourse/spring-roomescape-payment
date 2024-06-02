2f0d7c7c2175db9862249c11b2de657049c6d3f7
ehtjsv2 <ehtjsv2@naver.com>
2024-06-02 15:59:43 +0900

    refactor: GlobalExceptionHandler에 logger 적용

--- src/main/java/roomescape/exception/GlobalExceptionHandler.java
+++ src/main/java/roomescape/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package roomescape.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,49 +13,52 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException e) {
-        e.printStackTrace();
+        log.info("[Request Error] " + e.getMessage());
         return ResponseEntity.badRequest()
                 .body(new ErrorResponse("[Request Error] " + e.getMessage()));
     }
 
     @ExceptionHandler(HttpClientErrorException.class)
     public ResponseEntity<ErrorResponse> httpClientErrorException(HttpClientErrorException e) {
-        e.printStackTrace();
         ErrorResponse errorResponse = e.getResponseBodyAs(ErrorResponse.class);
         if (errorResponse == null) {
+            log.warn("[Client Error]", e);
             return ResponseEntity.status(e.getStatusCode())
                     .body(new ErrorResponse("[Client Error] 관리자에게 문의하세요"));
         }
+        log.info("[Client Error] " + errorResponse.message());
         return ResponseEntity.status(e.getStatusCode())
                 .body(new ErrorResponse("[Client Error] " + errorResponse.message()));
     }
 
     @ExceptionHandler(ExternalApiException.class)
     public ResponseEntity<ErrorResponse> externalApiException(ExternalApiException e) {
-        e.printStackTrace();
+        log.info("[External Error] " + e.getMessage());
         return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
                 .body(new ErrorResponse("[External Error] " + e.getMessage()));
     }
 
     @ExceptionHandler(AuthorizationException.class)
     public ResponseEntity<ErrorResponse> handleAuthorizationException(AuthorizationException e) {
-        e.printStackTrace();
+        log.info("[Authorization Error] " + e.getMessage());
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
                 .body(new ErrorResponse("[Authorization Error] " + e.getMessage()));
     }
 
     @ExceptionHandler(DataAccessException.class)
     public ResponseEntity<ErrorResponse> handleDataAccessException(DataAccessException e) {
-        e.printStackTrace();
+        log.error("[Data Access Error] " + e.getMessage(),e);
         return ResponseEntity.internalServerError()
                 .body(new ErrorResponse("[Data Access Error] " + e.getMessage()));
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleException(Exception e) {
-        e.printStackTrace();
+        log.error("[Server Error] " + e.getMessage(),e);
         return ResponseEntity.internalServerError()
                 .body(new ErrorResponse("[Server Error] " + e.getMessage()));
     }
