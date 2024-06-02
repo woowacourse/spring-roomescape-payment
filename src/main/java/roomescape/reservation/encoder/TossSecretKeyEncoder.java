0b9f534315285610f01cc63d58d3274896615cd2
tackyu <tack970913@gmail.com>
2024-06-03 04:13:55 +0900

    refactor: AUTH_PREFIX 상수 추가

--- src/main/java/roomescape/reservation/encoder/TossSecretKeyEncoder.java
+++ src/main/java/roomescape/reservation/encoder/TossSecretKeyEncoder.java
@@ -4,9 +4,10 @@ import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
 public class TossSecretKeyEncoder {
+    private static final String AUTH_PREFIX = "Basic ";
 
     public static String encode(String secretKey) {
         byte[] encoded = Base64.getEncoder().encode((secretKey + ":").getBytes(StandardCharsets.UTF_8));
-        return "Basic " + new String(encoded);
+        return AUTH_PREFIX + new String(encoded);
     }
 }
