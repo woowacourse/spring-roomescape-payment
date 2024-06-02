8a01b99b906a68c0ce4af8e6b1e70a3a39170e52
jimi567 <28584160+jimi567@users.noreply.github.com>
2024-06-03 03:46:24 +0900

    style: 개행 추가

--- src/main/java/roomescape/application/reservation/dto/request/ReservationRequest.java
+++ src/main/java/roomescape/application/reservation/dto/request/ReservationRequest.java
@@ -6,7 +6,8 @@ import java.time.LocalDate;
 public record ReservationRequest(
         Long memberId,
         @NotNull(message = "테마 ID를 입력해주세요.")
-        Long themeId, @NotNull(message = "날짜를 입력해주세요.")
+        Long themeId,
+        @NotNull(message = "날짜를 입력해주세요.")
         LocalDate date,
         @NotNull(message = "시간 ID를 입력해주세요.")
         Long timeId
