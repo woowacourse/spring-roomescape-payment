6b8ffd1c2dc6a4d9e794034020a1f997488e3800
haiseong <wjdgotjd9908@gmail.com>
2024-06-02 20:43:45 +0900

    refactor: isError 메서드 사용

--- src/main/java/roomescape/exception/PaymentErrorHandler.java
+++ src/main/java/roomescape/exception/PaymentErrorHandler.java
@@ -8,7 +8,7 @@ public class PaymentErrorHandler implements ResponseErrorHandler {
 
     @Override
     public boolean hasError(ClientHttpResponse response) throws IOException {
-        return response.getStatusCode().is4xxClientError() || response.getStatusCode().is5xxServerError();
+        return response.getStatusCode().isError();
     }
 
     @Override
