67c847861e6df026892bd4f84882e241b240701d
masonkimseoul <masonkimseoul@gmail.com>
2024-06-02 14:37:41 +0900

    refactor: defaultStatusHandler, defaultHeader 적용

--- src/main/java/roomescape/global/config/RestClientConfiguration.java
+++ src/main/java/roomescape/global/config/RestClientConfiguration.java
@@ -1,6 +1,7 @@
 package roomescape.global.config;
 
 import java.time.Duration;
+import java.util.Base64;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.ClientHttpRequestFactories;
 import org.springframework.boot.web.client.ClientHttpRequestFactorySettings;
@@ -8,7 +9,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.JdkClientHttpRequestFactory;
+import org.springframework.web.client.ResponseErrorHandler;
 import org.springframework.web.client.RestClient;
+import roomescape.global.exception.ClientExceptionHandler;
 import roomescape.payment.TossPaymentClient;
 
 @Configuration
@@ -16,6 +19,13 @@ public class RestClientConfiguration {
 
     private static final int CONNECTION_TIMEOUT_DURATION = 5;
     private static final int READ_TIMEOUT_DURATION = 45;
+    private static final String HEADER_NAME = "Authorization";
+    private static final String AUTHENTICATION_TYPE = "Basic";
+    private static final String KEY_DELIMITER = ":";
+    private static final ResponseErrorHandler HANDLER = new ClientExceptionHandler();
+
+    @Value("${payment.secret-key}")
+    private String widgetSecretKey;
 
     @Value("${payment.api.base}")
     private String apiBaseUrl;
@@ -26,8 +36,10 @@ public class RestClientConfiguration {
 
         return new TossPaymentClient(
                 RestClient.builder()
-                        .requestFactory(factory)
                         .baseUrl(apiBaseUrl)
+                        .defaultHeader(HEADER_NAME, provideHeaderValue())
+                        .defaultStatusHandler(HANDLER)
+                        .requestFactory(factory)
                         .build()
         );
     }
@@ -38,5 +50,10 @@ public class RestClientConfiguration {
                 .withReadTimeout(Duration.ofSeconds(READ_TIMEOUT_DURATION));
 
         return ClientHttpRequestFactories.get(JdkClientHttpRequestFactory.class, settings);
+    }
+
+    private String provideHeaderValue() {
+        String secretKey = widgetSecretKey + KEY_DELIMITER;
+        return AUTHENTICATION_TYPE + " " + Base64.getEncoder().encodeToString(secretKey.getBytes());
     }
 }
--- src/main/java/roomescape/payment/TossPaymentClient.java
+++ src/main/java/roomescape/payment/TossPaymentClient.java
@@ -1,21 +1,13 @@
 package roomescape.payment;
 
-import java.util.Base64;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.web.client.RestClient;
-import roomescape.global.exception.PaymentFailException;
-import roomescape.global.util.BodyExtractor;
 import roomescape.payment.dto.PaymentConfirmRequest;
 import roomescape.payment.dto.PaymentConfirmResponse;
 
 public class TossPaymentClient {
 
-    private static final String KEY_DELIMITER = ":";
     private final RestClient restClient;
-
-    @Value("${payment.secret-key}")
-    private String widgetSecretKey;
 
     @Value("${payment.api.confirm}")
     private String confirmApi;
@@ -25,16 +17,10 @@ public class TossPaymentClient {
     }
 
     public PaymentConfirmResponse confirmPayments(PaymentConfirmRequest request) {
-        String secretKey = widgetSecretKey + KEY_DELIMITER;
-
         return restClient.post()
                 .uri(confirmApi)
-                .header("Authorization", "Basic " + Base64.getEncoder().encodeToString(secretKey.getBytes()))
                 .body(request)
                 .retrieve()
-                .onStatus(HttpStatusCode::is4xxClientError, (req, res) -> {
-                    throw new PaymentFailException(BodyExtractor.getMessage(res));
-                })
                 .toEntity(PaymentConfirmResponse.class)
                 .getBody();
     }
