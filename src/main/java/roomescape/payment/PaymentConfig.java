dab9d5e2dd8ef4a62676175bfe5328f72e352a1a
pricelees <priceelees@gmail.com>
2024-06-02 23:07:58 +0900

    feat: RestClient timeout 추가

--- src/main/java/roomescape/payment/PaymentConfig.java
+++ src/main/java/roomescape/payment/PaymentConfig.java
@@ -1,8 +1,12 @@
 package roomescape.payment;
 
+import java.time.Duration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.client.ClientHttpRequestFactories;
+import org.springframework.boot.web.client.ClientHttpRequestFactorySettings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
 @Configuration
@@ -15,6 +19,13 @@ public class PaymentConfig {
 
     @Bean
     public RestClient restClient() {
-        return RestClient.builder().baseUrl("https://api.tosspayments.com").build();
+        ClientHttpRequestFactorySettings settings = ClientHttpRequestFactorySettings.DEFAULTS
+                .withReadTimeout(Duration.ofSeconds(3L))
+                .withConnectTimeout(Duration.ofSeconds(3L));
+        ClientHttpRequestFactory requestFactory = ClientHttpRequestFactories.get(settings);
+
+        return RestClient.builder().baseUrl("https://api.tosspayments.com")
+                .requestFactory(requestFactory)
+                .build();
     }
 }
