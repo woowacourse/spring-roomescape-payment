bf4c87f15d0fd59671aa2e497735d5a7be38bace
PgmJun <chltmdwns96@gmail.com>
2024-06-03 17:26:23 +0900

    refactor: PaymentProperties 설정 Configuration -> Component로 변경

--- src/main/java/roomescape/service/payment/PaymentProperties.java
+++ src/main/java/roomescape/service/payment/PaymentProperties.java
@@ -1,9 +1,9 @@
 package roomescape.service.payment;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
-@Configuration
+@Component
 @ConfigurationProperties(prefix = "payment")
 public class PaymentProperties {
     private String secretKey;
