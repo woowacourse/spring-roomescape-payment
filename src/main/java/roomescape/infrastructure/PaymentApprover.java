813b1ee7b24509652a69c4cbc3eb2c36f964be49
HyungHoKim00 <rlagudgh1109@gmail.com>
2024-06-03 17:37:55 +0900

    refactor: 로깅 삭제

--- src/main/java/roomescape/infrastructure/PaymentApprover.java
+++ src/main/java/roomescape/infrastructure/PaymentApprover.java
@@ -1,7 +1,5 @@
 package roomescape.infrastructure;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
@@ -14,7 +12,6 @@ import roomescape.exception.PaymentException;
 
 @Component
 public class PaymentApprover {
-    private static final Logger log = LoggerFactory.getLogger(PaymentApprover.class);
     private final RestClient restClient;
     private final PaymentSecretKeyEncoder paymentSecretKeyEncoder;
 
@@ -34,15 +31,11 @@ public class PaymentApprover {
                     .retrieve()
                     .body(PaymentConfirmResponse.class);
         } catch (HttpClientErrorException e) {
-            log.error("토스 결제 에러 paymentKey: {}, statusCode : {}, message : {}",
-                    paymentRequest.getPaymentKey(), e.getStatusCode().value(), e.getMessage(), e);
             throw PaymentException.from(e);
         } catch (HttpServerErrorException e) {
-            log.error("토스 결제 에러 message : {}", e.getMessage(), e);
             throw new PaymentException("결제 서버와의 연결이 원활하지 않습니다. 잠시 후 다시 시도해 주세요.",
                     HttpStatus.BAD_GATEWAY);
         } catch (Exception e) {
-            log.error("토스 결제 에러 message : {}", e.getMessage(), e);
             throw new PaymentException("알 수 없는 오류가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
