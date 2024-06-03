6cc07d92aa036f2a243beb675341007a05f7e59e
HoeSeong123 <ghltjd3@gmail.com>
2024-06-03 17:03:39 +0900

    refactor : timeout 설정

--- src/main/java/roomescape/global/config/RestClientConfig.java
+++ src/main/java/roomescape/global/config/RestClientConfig.java
@@ -1,7 +1,9 @@
 package roomescape.global.config;
 
+import java.time.Duration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import roomescape.global.restclient.PaymentWithRestClient;
 
@@ -10,8 +12,13 @@ public class RestClientConfig {
 
     @Bean
     public PaymentWithRestClient paymentWithRestClient() {
+        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        requestFactory.setConnectTimeout(Duration.ofMillis(3000));
+        requestFactory.setReadTimeout(Duration.ofMillis(3000));
         return new PaymentWithRestClient(
-                RestClient.builder().baseUrl("https://api.tosspayments.com/v1/payments")
+                RestClient.builder()
+                        .baseUrl("https://api.tosspayments.com/v1/payments")
+                        .requestFactory(requestFactory)
                         .build()
         );
     }
