fc21c18695fed8d0bc052d827436632a1d8ba21a
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-06-03 16:39:35 +0900

    refactor(ReservationFacadeService): 불필요한 Transactional 처리 제거

--- src/main/java/roomescape/service/ReservationFacadeService.java
+++ src/main/java/roomescape/service/ReservationFacadeService.java
@@ -4,7 +4,6 @@ import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import roomescape.controller.dto.CreateReservationResponse;
 import roomescape.controller.dto.FindMyReservationResponse;
@@ -21,24 +20,20 @@ public class ReservationFacadeService {
         this.paymentService = paymentService;
     }
 
-    @Transactional
     public CreateReservationResponse reserve(String orderId, long amount, String paymentKey,
                                              Long memberId, LocalDate date, Long timeId, Long themeId) {
         paymentService.pay(orderId, amount, paymentKey);
         return userReservationService.reserve(memberId, date, timeId, themeId);
     }
 
-    @Transactional
     public CreateReservationResponse standby(Long memberId, LocalDate date, Long timeId, Long themeId) {
         return userReservationService.standby(memberId, date, timeId, themeId);
     }
 
-    @Transactional
     public void deleteStandby(Long id, Member member) {
         userReservationService.deleteStandby(id, member);
     }
 
-    @Transactional(readOnly = true)
     public List<FindMyReservationResponse> findMyReservationsWithRank(Long memberId) {
         return userReservationService.findMyReservationsWithRank(memberId);
     }
