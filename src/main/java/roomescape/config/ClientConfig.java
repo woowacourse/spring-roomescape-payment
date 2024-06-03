4a08afbab061160eab3d35185a65a0ff83bf2b97
lemone <lejinhy@gmail.com>
2024-06-03 18:12:44 +0900

    feat(ClientConfig): RestClient에 MockServer 주입을 위한 빈 추가
    Co-authored-by: lemone <lejinhy@gmail.com>

--- src/main/java/roomescape/config/ClientConfig.java
+++ src/main/java/roomescape/config/ClientConfig.java
@@ -2,17 +2,37 @@ package roomescape.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestClientCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import roomescape.client.payment.PaymentClient;
 
 @Configuration
 public class ClientConfig {
 
+    private static final String TOSS_PAYMENT_CONFIRM_URL = "https://api.tosspayments.com";
+
     @Bean
-    public PaymentClient paymentClient(@Value("${payment.widget-secret-key}") String widgetSecretKey) {
-        RestClient restClient = RestClient.builder().build();
+    public RestClient restClient(RestClient.Builder builder) {
+        return builder.build();
+    }
+
+    @Bean
+    public PaymentClient paymentClient(@Value("${payment.widget-secret-key}") String widgetSecretKey,
+                                       RestClient restClient) {
         return new PaymentClient(widgetSecretKey, restClient, new ObjectMapper());
+    }
+
+    @Bean
+    public RestClientCustomizer restClientCustomizer() {
+        return restClientBuilder -> restClientBuilder
+                .requestFactory(getClientHttpRequestFactory())
+                .baseUrl(TOSS_PAYMENT_CONFIRM_URL);
+    }
+
+    private SimpleClientHttpRequestFactory getClientHttpRequestFactory() {
+        return new SimpleClientHttpRequestFactory();
     }
 }
