5965fa9a300277126b28ab3eebae9d99a217e1ac
lemone <lejinhy@gmail.com>
2024-06-03 18:18:46 +0900

    fix(PaymentClient): api 제공 예외를 커스텀 예외로 변경하는 과정에서 에러 발생하는 문제 해결
    Co-authored-by: lemone <lejinhy@gmail.com>

--- src/main/java/roomescape/client/payment/PaymentClient.java
+++ src/main/java/roomescape/client/payment/PaymentClient.java
@@ -1,86 +1,79 @@
 package roomescape.client.payment;
 
+import static roomescape.exception.model.PaymentConfirmExceptionCode.FAILED_PAYMENT_INTERNAL_SYSTEM_PROCESSING;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.util.Base64;
 import org.slf4j.Logger;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
-import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.web.client.RestClient;
-import org.springframework.web.client.RestClient.RequestHeadersSpec.ConvertibleClientHttpResponse;
 import org.springframework.web.client.RestClientResponseException;
-import roomescape.client.payment.dto.PaymentConfirmFromTossDto;
 import roomescape.client.payment.dto.PaymentConfirmToTossDto;
+import roomescape.exception.ExceptionResponse;
 import roomescape.exception.PaymentConfirmException;
-import roomescape.exception.RoomEscapeException;
 import roomescape.exception.global.GlobalExceptionCode;
-import roomescape.exception.model.PaymentConfirmExceptionCode;
-import roomescape.exception.model.ReservationExceptionCode;
 import roomescape.util.LoggerUtil;
 
 public class PaymentClient {
 
     private static final Logger log = LoggerUtil.getLogger(PaymentClient.class);
 
-    private final String widgetSecretKey;
+    private final String encodedSecretKey;
     private final RestClient restClient;
     private final ObjectMapper objectMapper;
 
     public PaymentClient(String widgetSecretKey, RestClient restClient, ObjectMapper objectMapper) {
-        this.widgetSecretKey = widgetSecretKey;
+        this.encodedSecretKey = Base64.getEncoder().encodeToString((widgetSecretKey + ":").getBytes());
         this.restClient = restClient;
         this.objectMapper = objectMapper;
     }
 
     public void sendPaymentConfirmToToss(PaymentConfirmToTossDto paymentConfirmToTossDto) {
-        Base64.Encoder encoder = Base64.getEncoder();
-        String encodedBytes = encoder.encodeToString((this.widgetSecretKey + ":").getBytes());
-        String authorizations = "Basic " + encodedBytes;
+        String authorizations = "Basic " + encodedSecretKey;
 
         try {
             restClient.post()
                     .uri("https://api.tosspayments.com/v1/payments/confirm")
-                    .contentType(MediaType.APPLICATION_JSON)
                     .header("Authorization", authorizations)
+                    .contentType(MediaType.APPLICATION_JSON)
                     .body(paymentConfirmToTossDto)
-                    .exchange((request, response) -> {
-                        handlePaymentConfirmationException(response);
-                        return new PaymentConfirmFromTossDto("200", "결제가 성공하였습니다.");
-                    });
+                    .retrieve()
+                    .onStatus(HttpStatusCode::is4xxClientError, (request, response) ->
+                        throwConvertedCustomException(response)
+                    )
+                    .onStatus(HttpStatusCode::is5xxServerError, (request, response) -> {
+                        throw new PaymentConfirmException(GlobalExceptionCode.INTERNAL_SERVER_ERROR);
+                    })
+                    .toBodilessEntity();
         } catch (RestClientResponseException e) {
-            log.error("토스 결제 에러 message: {}, body: {}, cause: {}", e.getMessage(), e.getResponseBodyAsString(), e.getCause());
+            log.error("토스 결제 응답 에러 message: {}, body: {}, cause: {}", e.getMessage(), e.getResponseBodyAsString(),
+                    e.getCause());
+            throw new PaymentConfirmException(GlobalExceptionCode.HTTP_RESPONSE_DATA_INVALID);
         } catch (Exception e) {
-            log.error("토스 에러 message: {}, body: {}, cause: {}", e.getMessage(), e.getCause(), e.getCause());
+            log.error("토스 결제 에러 message: {}, body: {}, cause: {}", e.getMessage(), e.getCause(),
+                    e.getCause());
+            throw new PaymentConfirmException(GlobalExceptionCode.INTERNAL_SERVER_ERROR);
         }
     }
 
-    private void handlePaymentConfirmationException(ConvertibleClientHttpResponse response)
-            throws IOException, RoomEscapeException {
-        if (response.getStatusCode().is4xxClientError()) {
-            try {
-                PaymentConfirmExceptionCode paymentConfirmExceptionCode =  convertApiExceptionToCustomException(response);
-                throw new PaymentConfirmException(paymentConfirmExceptionCode);
-            } catch (HttpMessageNotReadableException e) {
-                log.error("토스 결제 불러오기 에러 message: {}, body: {}, cause: {}", e.getMessage(), e.getCause(), e.getCause());
-            } catch (IOException e) {
-                log.error("토스 결제 입출력 에러 message: {}, body: {}, cause: {}", e.getMessage(), e.getCause(), e.getCause());
-            }
-        }
-        if (response.getStatusCode().is5xxServerError()) {
-            throw new RoomEscapeException(ReservationExceptionCode.RESERVATION_FAIL);
-        }
-        if (!response.getStatusCode().is2xxSuccessful()) {
-            throw new RoomEscapeException(ReservationExceptionCode.RESERVATION_URI_MOVE);
-        }
-    }
-
-    private PaymentConfirmExceptionCode convertApiExceptionToCustomException(ConvertibleClientHttpResponse response) throws IOException{
+    private void throwConvertedCustomException(ClientHttpResponse response) throws IOException {
         try {
-            return objectMapper.readValue(response.getBody(), PaymentConfirmExceptionCode.class);
+            ExceptionResponse paymentException = objectMapper.readValue(response.getBody(), ExceptionResponse.class);
+            throw new PaymentConfirmException(
+                    paymentException.code() + paymentException.message(),
+                    FAILED_PAYMENT_INTERNAL_SYSTEM_PROCESSING
+            );
         } catch (JsonProcessingException e) {
-            throw new PaymentConfirmException(GlobalExceptionCode.INVALID_JSON_DATA);
+            throw new PaymentConfirmException(GlobalExceptionCode.JSON_DATA_INVALID);
         }
+    }
+
+    public String getEncodedSecretKey() {
+        return encodedSecretKey;
     }
 }
 
