ee6e2abf62ed4824cb8525b488657bf22c59aad6
fram1998 <koust6u@pusan.ac.kr>
2024-06-03 22:54:43 +0900

    feat: ObjectMapper 인스턴스 변수로 선언

--- src/main/java/roomescape/client/PaymentErrorDecoder.java
+++ src/main/java/roomescape/client/PaymentErrorDecoder.java
@@ -8,13 +8,12 @@ import feign.Response;
 import feign.codec.ErrorDecoder;
 
 public class PaymentErrorDecoder implements ErrorDecoder {
-
     private final ErrorDecoder defaultErrorDecoder = new Default();
+    private final ObjectMapper mapper = new ObjectMapper();
 
     @Override
     public Exception decode(String methodKey, Response response) {
         try {
-            ObjectMapper mapper = new ObjectMapper();
             TossErrorResponse tossErrorResponse = mapper.readValue(response.body().asInputStream(),
                     TossErrorResponse.class);
             return new PaymentException(tossErrorResponse);
