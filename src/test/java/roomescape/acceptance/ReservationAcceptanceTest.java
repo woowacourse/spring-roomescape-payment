242c3b2d14da53d9acaa345b223afccaf45a4b56
jimi567 <28584160+jimi567@users.noreply.github.com>
2024-06-03 20:24:49 +0900

    test: 실제 결제 api 호출하는 예약 테스트 작성

--- src/test/java/roomescape/acceptance/ReservationAcceptanceTest.java
+++ src/test/java/roomescape/acceptance/ReservationAcceptanceTest.java
@@ -3,14 +3,20 @@ package roomescape.acceptance;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import java.time.LocalDate;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import roomescape.application.member.dto.request.MemberRegisterRequest;
+import roomescape.application.member.dto.response.MemberResponse;
+import roomescape.application.reservation.dto.request.ReservationPaymentRequest;
 import roomescape.application.reservation.dto.request.ReservationRequest;
 import roomescape.application.reservation.dto.request.ThemeRequest;
 import roomescape.application.reservation.dto.response.ReservationResponse;
 import roomescape.application.reservation.dto.response.ReservationStatusResponse;
+import roomescape.application.reservation.dto.response.ReservationTimeResponse;
+import roomescape.application.reservation.dto.response.ThemeResponse;
 
 class ReservationAcceptanceTest extends AcceptanceTest {
 
@@ -55,5 +61,33 @@ class ReservationAcceptanceTest extends AcceptanceTest {
                 .as(ReservationStatusResponse[].class);
 
         assertThat(responses).hasSize(1);
+    }
+
+    @DisplayName("사용자가 예약과 함께 결제를 한다.")
+    @Disabled
+    @Test
+    void reserveWithPurchase() {
+        MemberResponse memberResponse = fixture.registerMember(
+                new MemberRegisterRequest("name", "email@mail.com", "12341234")
+        ).as(MemberResponse.class);
+        String token = fixture.loginAndGetToken("email@mail.com", "12341234");
+        ReservationTimeResponse timeResponse = fixture.createReservationTime(15, 20);
+        ThemeResponse themeResponse = fixture.createTheme(new ThemeRequest("테마", "디스크립션1", "https://theme", 1000L));
+
+        ReservationPaymentRequest reservationPaymentRequest = new ReservationPaymentRequest(
+                memberResponse.id(),
+                themeResponse.id(),
+                LocalDate.of(2024, 12, 25),
+                timeResponse.id(),
+                "paymentKet",
+                "orderId");
+
+        RestAssured.given().log().all()
+                .cookie("token", token)
+                .contentType(ContentType.JSON)
+                .body(reservationPaymentRequest)
+                .when().post("/reservations")
+                .then().log().all()
+                .statusCode(201);
     }
 }
