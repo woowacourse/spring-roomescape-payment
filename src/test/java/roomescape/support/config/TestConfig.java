2ae72131ebd37402d9ea07de4ae607aed108e255
김민수 MINSOO KIM <alstn113@gmail.com>
2024-06-03 10:42:55 +0900

    refactor: 'allow-bean-definition-overriding: true' 제거

--- src/test/java/roomescape/support/config/TestConfig.java
+++ src/test/java/roomescape/support/config/TestConfig.java
@@ -6,13 +6,15 @@ import java.time.ZoneId;
 import java.time.ZoneOffset;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import roomescape.support.extension.DatabaseCleaner;
 
 @TestConfiguration
 public class TestConfig {
 
     @Bean
-    public Clock clock() {
+    @Primary
+    public Clock testClock() {
         Instant fixedInstant = Instant.parse("2024-04-08T00:00:00Z");
         ZoneId zone = ZoneOffset.UTC;
 
