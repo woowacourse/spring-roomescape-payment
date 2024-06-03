05ec3d516410136474723d6427da170b6ff6dbb2
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2024-06-03 19:08:13 +0900

    refactor: 결제 요청 오류 메시지 로그 출력

--- src/main/java/roomescape/reservation/service/TossPaymentClient.java
+++ src/main/java/roomescape/reservation/service/TossPaymentClient.java
@@ -1,17 +1,23 @@
 package roomescape.reservation.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 import roomescape.global.exception.IllegalRequestException;
 import roomescape.global.exception.InternalServerException;
+import roomescape.global.exception.PaymentErrorResponse;
 import roomescape.reservation.dto.PaymentConfirmRequest;
 
 @Component
 public class TossPaymentClient implements PaymentClient {
     private static final String CONFIRM_URI = "/v1/payments/confirm";
     private final RestClient restClient;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final Logger log = LoggerFactory.getLogger(getClass());
 
     public TossPaymentClient(RestClient.Builder restClientBuilder) {
         this.restClient = restClientBuilder.build();
@@ -24,6 +30,9 @@ public class TossPaymentClient implements PaymentClient {
                 .body(paymentConfirmRequest)
                 .retrieve()
                 .onStatus(HttpStatusCode::is4xxClientError, (req, res) -> {
+                    PaymentErrorResponse errorResponse = objectMapper.readValue(res.getBody(),
+                            PaymentErrorResponse.class);
+                    log.error("결제 요청 문제 발생: {}", errorResponse.getMessage());
                     throw new IllegalRequestException("결제 요청에 대한 문제가 있습니다.");
                 })
                 .onStatus(HttpStatusCode::is5xxServerError, (req, res) -> {
