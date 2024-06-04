19c7f405c31121465c421888b825130ef391880b
Seokmyung Ham <gkatjraud1@gmail.com>
2024-06-04 17:03:36 +0900

    test: ReservationTimeJpaRepository 테스트 추가

--- src/main/java/roomescape/domain/reservationdetail/ReservationTime.java
+++ src/main/java/roomescape/domain/reservationdetail/ReservationTime.java
@@ -7,6 +7,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import java.time.LocalTime;
 import java.util.List;
+import java.util.Objects;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -30,6 +31,23 @@ public class ReservationTime {
 
     public ReservationTime(String startAt) {
         this(LocalTime.parse(startAt));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ReservationTime that = (ReservationTime) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
     }
 
     public boolean isAlreadyBooked(List<ReservationTime> reservedTimes) {
--- src/main/java/roomescape/infrastructure/repository/ReservationTimeJpaRepository.java
+++ src/main/java/roomescape/infrastructure/repository/ReservationTimeJpaRepository.java
@@ -4,12 +4,14 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.Repository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import roomescape.domain.reservationdetail.ReservationTime;
 
-public interface ReservationTimeJpaRepository extends Repository<ReservationTime, Long> {
+@Repository
+public interface ReservationTimeJpaRepository extends JpaRepository<ReservationTime, Long> {
 
     ReservationTime save(ReservationTime time);
 
