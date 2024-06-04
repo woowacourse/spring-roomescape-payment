a390b6a6cc373417afa7674ee427b253e26fd19c
gyungchan Jo <namejgc@naver.com>
2024-06-05 01:26:21 +0900

    refactor: RestClient 설정 변경

--- src/main/java/roomescape/config/RestClientConfig.java
+++ src/main/java/roomescape/config/RestClientConfig.java
@@ -1,14 +1,12 @@
 package roomescape.config;
 
-import org.apache.hc.client5.http.config.RequestConfig;
-import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
-import org.apache.hc.client5.http.impl.classic.HttpClients;
-import org.apache.hc.core5.util.Timeout;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
+
+import java.time.Duration;
 
 @Configuration
 public class RestClientConfig {
@@ -17,19 +15,13 @@ public class RestClientConfig {
 
     @Bean
     public RestClient restClient() {
-        RequestConfig requestConfig = RequestConfig.custom()
-                .setConnectTimeout(Timeout.ofSeconds(5))
-                .setConnectionRequestTimeout(Timeout.ofSeconds(5))
-                .setResponseTimeout(Timeout.ofSeconds(5))
-                .build();
-
-        CloseableHttpClient httpClient = HttpClients.custom()
-                .setDefaultRequestConfig(requestConfig)
-                .build();
+        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        requestFactory.setConnectTimeout(Duration.ofSeconds(3));
+        requestFactory.setReadTimeout(Duration.ofSeconds(10));
 
         return RestClient.builder()
                 .baseUrl(url)
-                .requestFactory(new HttpComponentsClientHttpRequestFactory(httpClient))
+                .requestFactory(requestFactory)
                 .build();
     }
 }
