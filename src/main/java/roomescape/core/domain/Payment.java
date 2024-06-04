da0d329e59b4e4c2d79a585b9f8ef111d7505001
hangillee <skfcb10@naver.com>
2024-06-04 18:21:27 +0900

    feat: 결제 정보를 담은 도메인을 예약으로부터 분리

---
+++ src/main/java/roomescape/core/domain/Payment.java
@@ -0,0 +1,82 @@
+package roomescape.core.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "reservation_id", nullable = false)
+    private Reservation reservation;
+
+    @NotNull(message = "paymentKey는 비어있을 수 없습니다.")
+    private String paymentKey;
+
+    @NotNull(message = "orderId는 비어있을 수 없습니다.")
+    private String orderId;
+
+    @Min(0)
+    @Max(Integer.MAX_VALUE)
+    @NotNull(message = "결제 금액은 비어있을 수 없습니다.")
+    private Integer amount;
+
+    @Enumerated(EnumType.STRING)
+    @NotNull(message = "결제 상태는 비어있을 수 없습니다.")
+    private PaymentStatus status;
+
+    public Payment() {
+    }
+
+    public Payment(final Reservation reservation, final String paymentKey, final String orderId, final Integer amount,
+                   final PaymentStatus status) {
+        this(null, reservation, paymentKey, orderId, amount, status);
+    }
+
+    public Payment(final Long id, final Reservation reservation, final String paymentKey, final String orderId,
+                   final Integer amount, final PaymentStatus status) {
+        this.id = id;
+        this.reservation = reservation;
+        this.paymentKey = paymentKey;
+        this.orderId = orderId;
+        this.amount = amount;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Reservation getReservation() {
+        return reservation;
+    }
+
+    public @NotNull(message = "paymentKey는 비어있을 수 없습니다.") String getPaymentKey() {
+        return paymentKey;
+    }
+
+    public @NotNull(message = "orderId는 비어있을 수 없습니다.") String getOrderId() {
+        return orderId;
+    }
+
+    public @Min(0) @Max(Integer.MAX_VALUE) @NotNull(message = "결제 금액은 비어있을 수 없습니다.") Integer getAmount() {
+        return amount;
+    }
+
+    public @NotNull(message = "결제 상태는 비어있을 수 없습니다.") PaymentStatus getStatus() {
+        return status;
+    }
+}
