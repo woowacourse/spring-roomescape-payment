af2f14c7c8493ea82ec6c5e87e02f07bdd4a296d
김민수 MINSOO KIM <alstn113@gmail.com>
2024-06-04 17:56:04 +0900

    feat: RestDocs 의존성 추가

---
+++ src/test/java/roomescape/documentation/AbstractDocumentTest.java
@@ -0,0 +1,38 @@
+package roomescape.documentation;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@AutoConfigureRestDocs
+@ExtendWith(RestDocumentationExtension.class)
+@ActiveProfiles("test")
+public abstract class AbstractDocumentTest {
+
+    protected MockMvc mockMvc;
+
+    protected ObjectMapper objectMapper;
+
+    @BeforeEach
+    public void setUp(
+            WebApplicationContext webApplicationContext,
+            RestDocumentationContextProvider restDocumentation
+    ) {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .apply(documentationConfiguration(restDocumentation)
+                        .operationPreprocessors()
+                        .withRequestDefaults(prettyPrint())
+                        .withResponseDefaults(prettyPrint()))
+                .build();
+    }
+}
