d639b55ee230cd3828300262b0d96230e8c165fa
yoonjuho <dbswn990@naver.com>
2024-06-04 16:10:07 +0900

    refactor: 객체 생성 로직 빈 주입으로 대체

--- src/main/java/roomescape/payment/api/TossPaymentClient.java
+++ src/main/java/roomescape/payment/api/TossPaymentClient.java
@@ -8,8 +8,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import roomescape.payment.config.PaymentClientResponseErrorHandler;
 import roomescape.payment.dto.PaymentRequest;
 import roomescape.payment.dto.PaymentResponse;
@@ -23,18 +21,18 @@ public class TossPaymentClient implements PaymentClient {
     private static final Logger log = LoggerFactory.getLogger(TossPaymentClient.class);
     private final String encodedSecretKey;
     private final RestClient restClient;
-    private final ObjectMapper objectMapper;
+    private final PaymentClientResponseErrorHandler paymentClientResponseErrorHandler;
 
     public TossPaymentClient(@Value("${security.toss.payment.secret-key}") String secretKey,
                              @Value("${security.toss.payment.url}") String paymentUrl,
                              RestClient.Builder restClientBuilder,
-                             ObjectMapper objectMapper) {
+                             PaymentClientResponseErrorHandler paymentClientResponseErrorHandler) {
         this.encodedSecretKey = Base64.getEncoder()
                 .encodeToString((secretKey + DELIMITER).getBytes());
         this.restClient = restClientBuilder
                 .baseUrl(paymentUrl)
                 .build();
-        this.objectMapper = objectMapper;
+        this.paymentClientResponseErrorHandler = paymentClientResponseErrorHandler;
     }
 
     @Override
@@ -45,7 +43,7 @@ public class TossPaymentClient implements PaymentClient {
                 .header(AUTH_HEADER, AUTH_METHOD + encodedSecretKey)
                 .body(paymentRequest)
                 .retrieve()
-                .onStatus(new PaymentClientResponseErrorHandler(objectMapper))
+                .onStatus(paymentClientResponseErrorHandler)
                 .body(PaymentResponse.class);
         log.info("URI: {}, Method: {}, ResponseBody:{} ", APPROVE_PAYMENT_URI, "POST", paymentResponse);
         return paymentResponse;
