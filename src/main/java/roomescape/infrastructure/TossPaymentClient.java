345a0e4f64dae9770ce5d9c00d61f55dc55b4a98
mzeong <mjeong@sookmyung.ac.kr>
2024-06-04 15:22:58 +0900

    refactor: 토스 결제 uri 환경 변수로 관리

--- src/main/java/roomescape/infrastructure/TossPaymentClient.java
+++ src/main/java/roomescape/infrastructure/TossPaymentClient.java
@@ -1,5 +1,6 @@
 package roomescape.infrastructure;
 
+import java.util.Base64;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
@@ -8,29 +9,31 @@ import roomescape.payment.PaymentClient;
 import roomescape.payment.TossPaymentClientErrorHandler;
 import roomescape.service.dto.request.PaymentRequest;
 
-import java.util.Base64;
-
 @Component
 public class TossPaymentClient implements PaymentClient {
-    private static final String URI = "https://api.tosspayments.com/v1/payments/confirm";
+
     private static final String ENCODING_FORMAT = "%s:";
 
     private final RestClient restClient;
     private final TossPaymentClientErrorHandler tossPaymentClientErrorHandler;
+    private final String uri;
     private final String encodedSecretKey;
 
     public TossPaymentClient(RestClient restClient,
                              TossPaymentClientErrorHandler tossPaymentClientErrorHandler,
+                             @Value("${toss.url}") String uri,
                              @Value("${toss.secret-key}") String secretKey) {
         this.restClient = restClient;
         this.tossPaymentClientErrorHandler = tossPaymentClientErrorHandler;
-        this.encodedSecretKey = Base64.getEncoder().encodeToString(String.format(ENCODING_FORMAT, secretKey).getBytes());
+        this.uri = uri;
+        this.encodedSecretKey = Base64.getEncoder()
+                .encodeToString(String.format(ENCODING_FORMAT, secretKey).getBytes());
     }
 
     @Override
     public void confirm(PaymentRequest paymentRequest) {
         restClient.post()
-                .uri(URI)
+                .uri(uri)
                 .header("Authorization", "Basic " + encodedSecretKey)
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(paymentRequest)
