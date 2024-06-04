2d02872e114488e5d8090682e522dbdfe7d55bfa
fram1998 <koust6u@pusan.ac.kr>
2024-06-04 13:41:51 +0900

    test: 예약 생성 테스트 작성

--- src/test/java/roomescape/config/ControllerConfig.java
+++ src/test/java/roomescape/config/ControllerConfig.java
@@ -4,6 +4,7 @@ import static org.mockito.Mockito.mock;
 
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+
 import roomescape.auth.TokenProvider;
 
 @TestConfiguration
--- src/test/java/roomescape/reservation/controller/ReservationApiControllerTest.java
+++ src/test/java/roomescape/reservation/controller/ReservationApiControllerTest.java
@@ -1,21 +1,41 @@
 package roomescape.reservation.controller;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 import jakarta.servlet.http.Cookie;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
-import org.springframework.http.MediaType;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import roomescape.client.PaymentClient;
 import roomescape.config.ControllerConfig;
+import roomescape.member.dto.MemberResponse;
+import roomescape.reservation.dto.request.ReservationCreateRequest;
+import roomescape.reservation.dto.response.PaymentResponse;
+import roomescape.reservation.dto.response.ReservationResponse;
+import roomescape.reservation.dto.response.ThemeResponse;
+import roomescape.reservation.dto.response.TimeResponse;
 import roomescape.reservation.service.ReservationService;
 
 @WebMvcTest(ReservationApiController.class)
@@ -39,7 +59,7 @@ class ReservationApiControllerTest {
     void findAll() throws Exception {
         mockMvc.perform(get("/reservations")
                         .cookie(new Cookie("token", "cookieValue"))
-                        .accept(MediaType.APPLICATION_JSON))
+                        .accept(APPLICATION_JSON))
                 .andExpect(status().isOk());
     }
 
@@ -48,7 +68,7 @@ class ReservationApiControllerTest {
     void myReservationsTest() throws Exception {
         mockMvc.perform(get("/reservations/me")
                         .cookie(new Cookie("token", "cookieValue"))
-                        .accept(MediaType.APPLICATION_JSON))
+                        .accept(APPLICATION_JSON))
                 .andExpect(status().isOk());
     }
 
@@ -57,7 +77,7 @@ class ReservationApiControllerTest {
     void deleteReservationRequestTest() throws Exception {
         mockMvc.perform(delete("/reservations/{id}", 1L)
                         .cookie(new Cookie("token", "cookieValue"))
-                        .accept(MediaType.APPLICATION_JSON))
+                        .accept(APPLICATION_JSON))
                 .andExpect(status().isNoContent());
     }
 
@@ -66,7 +86,35 @@ class ReservationApiControllerTest {
     void findWaitingTest() throws Exception {
         mockMvc.perform(get("/reservations/waiting")
                         .cookie(new Cookie("token", "cookieValue"))
-                        .accept(MediaType.APPLICATION_JSON))
+                        .accept(APPLICATION_JSON))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("예약 대기를 성공적으로 생성하면 201 응답을 받는다.")
+    void createMemberReservationTest() throws Exception {
+        // given
+        TimeResponse time = new TimeResponse(1L, LocalTime.of(1, 0));
+        ThemeResponse theme = new ThemeResponse(1L, "n", "d", "t");
+        LocalDate date = LocalDate.of(2023, 1, 1);
+        MemberResponse name = new MemberResponse(1L, "name");
+        ReservationCreateRequest request = new ReservationCreateRequest(date, theme.id(), time.id(),
+                "", "", 1000L, "");
+        ReservationResponse response = new ReservationResponse(1L, name, date, theme, time);
+
+        Mockito.when(reservationService.save(any(), any()))
+                .thenReturn(1L);
+        Mockito.when(paymentClient.paymentReservation(any(), any()))
+                .thenReturn(new ResponseEntity<>(new PaymentResponse(""), HttpStatus.OK));
+        Mockito.when(reservationService.findById(anyLong()))
+                .thenReturn(response);
+
+        // when & then
+        mockMvc.perform(post("/reservations")
+                        .cookie(new Cookie("token", "value"))
+                        .contentType(APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andDo(print())
+                .andExpect(content().json(objectMapper.writeValueAsString(response)));
     }
 }
