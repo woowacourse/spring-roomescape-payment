f0c3a416264ade6e7613979a3b4320ce7f8ecc58
fram1998 <koust6u@pusan.ac.kr>
2024-06-04 18:16:11 +0900

    feat: 서드 파티 api 오류 상태코드 500번으로 재정의

--- src/main/java/roomescape/exception/ExceptionApiController.java
+++ src/main/java/roomescape/exception/ExceptionApiController.java
@@ -4,7 +4,6 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -39,6 +38,6 @@ public class ExceptionApiController {
     public ResponseEntity<ExceptionInfo> paymentExHandler(PaymentException paymentException) {
         ExceptionInfo exceptionInfo = new ExceptionInfo(paymentException.getMessage());
 
-        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(exceptionInfo);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(exceptionInfo);
     }
 }
