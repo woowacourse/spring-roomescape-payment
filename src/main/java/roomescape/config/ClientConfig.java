0b874c0d209c1816c450bb712eeab4978b4878a2
yoonseo choi <choiyoonseo02@gmail.com>
2024-06-04 14:31:31 +0900

    refactor: payment bean config에서 등록, 중복 url 분리

--- src/main/java/roomescape/config/ClientConfig.java
+++ src/main/java/roomescape/config/ClientConfig.java
@@ -3,11 +3,20 @@ package roomescape.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.client.RestClient.Builder;
+import roomescape.infrastructure.PaymentClient;
 
 @Configuration
 public class ClientConfig {
     @Bean
-    public RestClient getRestClient() {
-        return RestClient.builder().build();
+    public PaymentClient getPaymentClient(Builder builder) {
+        return new PaymentClient(builder
+                .baseUrl("https://api.tosspayments.com/")
+                .build());
+    }
+
+    @Bean
+    public Builder restClient() {
+        return RestClient.builder();
     }
 }
--- src/main/java/roomescape/infrastructure/PaymentClient.java
+++ src/main/java/roomescape/infrastructure/PaymentClient.java
@@ -3,13 +3,11 @@ package roomescape.infrastructure;
 import java.util.Map;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 import roomescape.core.dto.auth.PaymentAuthorizationResponse;
 import roomescape.core.dto.payment.TossPaymentRequest;
 import roomescape.core.dto.payment.TossPaymentResponse;
 
-@Component
 public class PaymentClient {
     private final RestClient restClient;
 
@@ -20,7 +18,7 @@ public class PaymentClient {
     public TossPaymentResponse approvePayment(final TossPaymentRequest tossPaymentRequest,
                                               final PaymentAuthorizationResponse paymentAuthorizationResponse) {
         return restClient.post()
-                .uri("https://api.tosspayments.com/v1/payments/confirm")
+                .uri("v1/payments/confirm")
                 .header("Authorization", paymentAuthorizationResponse.getPaymentAuthorization())
                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .body(tossPaymentRequest)
@@ -32,7 +30,7 @@ public class PaymentClient {
     public TossPaymentResponse refundPayment(final TossPaymentResponse tossPaymentResponse,
                                              final PaymentAuthorizationResponse paymentAuthorizationResponse) {
         return restClient.post()
-                .uri("https://api.tosspayments.com/v1/payments/" + tossPaymentResponse.getPaymentKey() + "/cancel")
+                .uri("v1/payments/" + tossPaymentResponse.getPaymentKey() + "/cancel")
                 .header("Authorization", paymentAuthorizationResponse.getPaymentAuthorization())
                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .body(Map.of("cancelReason", "고객 변심"))
