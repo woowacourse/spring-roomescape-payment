67f27714063b7af5900c00770ab2683ffd7f3e68
fromitive <fromitive@gmail.com>
2024-06-04 13:46:13 +0900

    fix: eol 문제 수정

--- src/main/java/roomescape/service/httpclient/TossPaymentRestClient.java
+++ src/main/java/roomescape/service/httpclient/TossPaymentRestClient.java
@@ -1,17 +1,16 @@
 package roomescape.service.httpclient;
 
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 import roomescape.controller.request.PaymentRequest;
 
 public class TossPaymentRestClient extends TossPaymentClient {
 
+    private final RestClient restClient;
+
     public TossPaymentRestClient(final RestClient restClient) {
         this.restClient = restClient;
     }
-
-    private final RestClient restClient;
 
     @Override
     protected void request(final PaymentRequest paymentRequest) {
