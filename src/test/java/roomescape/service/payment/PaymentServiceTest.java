f5bc3ade39761146cc7e51f97a06ee0d4d08009a
HyungUk Ryu <hw0603@naver.com>
2024-06-05 03:17:44 +0900

    test(PaymentServiceTest): 결제 서비스 테스트 코드 추가

---
+++ src/test/java/roomescape/service/payment/PaymentServiceTest.java
@@ -0,0 +1,116 @@
+package roomescape.service.payment;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.math.BigDecimal;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import roomescape.Fixture;
+import roomescape.domain.payment.Payment;
+import roomescape.domain.payment.PaymentRepository;
+import roomescape.domain.payment.PaymentType;
+import roomescape.exception.PaymentException;
+import roomescape.service.ServiceTestBase;
+import roomescape.service.payment.dto.PaymentRequest;
+
+class PaymentServiceTest extends ServiceTestBase {
+    @Autowired
+    private PaymentService paymentService;
+    @Autowired
+    private PaymentRepository paymentRepository;
+
+    @DisplayName("관리자 결제의 경우 저장된 결제 금액은 0원이다.")
+    @Test
+    void paymentAmountOfAdminIsZero() {
+        // given
+        PaymentRequest paymentRequest = new PaymentRequest(
+                Fixture.TEST_PAYMENT_KEY, Fixture.TEST_ORDER_ID,
+                BigDecimal.ZERO, PaymentType.ADMIN
+        );
+
+        // when
+        Payment confirmed = paymentService.confirm(paymentRequest);
+
+        // then
+        long paymentId = confirmed.getId();
+        assertThat(paymentRepository.findById(paymentId).orElseThrow().getAmount().intValue()).isZero();
+    }
+
+    @DisplayName("관리자 결제 정보를 DB에 저장한다.")
+    @Test
+    void saveAdminPayment() {
+        // given
+        PaymentRequest paymentRequest = new PaymentRequest(
+                Fixture.TEST_PAYMENT_KEY, Fixture.TEST_ORDER_ID,
+                BigDecimal.ZERO, PaymentType.ADMIN
+        );
+
+        // when
+        Payment confirmed = paymentService.confirm(paymentRequest);
+
+        // then
+        long paymentId = confirmed.getId();
+        assertThat(paymentRepository.findById(paymentId)).isPresent();
+    }
+
+    @DisplayName("결제 정보를 DB에 저장한다.")
+    @ParameterizedTest
+    @ValueSource(longs = {1000, 10000})
+    void savePayment(long amount) {
+        // given
+        PaymentRequest paymentRequest = new PaymentRequest(
+                Fixture.TEST_PAYMENT_KEY, Fixture.TEST_ORDER_ID,
+                BigDecimal.valueOf(amount), PaymentType.NORMAL
+        );
+
+        // when
+        Payment confirmed = paymentService.confirm(paymentRequest);
+
+        // then
+        long paymentId = confirmed.getId();
+        long actual = paymentRepository.findById(paymentId).orElseThrow().getAmount().longValue();
+        long expected = (int) amount;
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @DisplayName("결제를 취소한다.")
+    @Test
+    void cancelPayment() {
+        // given
+        PaymentRequest paymentRequest = new PaymentRequest(
+                Fixture.TEST_PAYMENT_KEY, Fixture.TEST_ORDER_ID,
+                BigDecimal.TEN, PaymentType.NORMAL
+        );
+        Payment saved = paymentService.confirm(paymentRequest);
+
+        // when
+        paymentService.cancel(saved);
+
+        // then
+        long paymentId = saved.getId();
+        assertThat(paymentRepository.findById(paymentId)).isNotPresent();
+    }
+
+    @DisplayName("결제 취소 중 오류가 발생하면 결제정보가 삭제되지 않는다.")
+    @Test
+    void cannotCancelWhenExceptionThrown() {
+        // given
+        PaymentRequest paymentRequest = new PaymentRequest(
+                Fixture.CANCEL_ERROR_KEY, Fixture.TEST_ORDER_ID,
+                BigDecimal.TEN, PaymentType.NORMAL
+        );
+        Payment saved = paymentService.confirm(paymentRequest);
+
+        // when
+        try {
+            paymentService.cancel(saved);
+        } catch (PaymentException ignored) {
+        }
+
+        // then
+        assertThat(paymentRepository.findById(saved.getId())).isPresent();
+    }
+}
