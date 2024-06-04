1cb9d9e2e7fcc99e28ab7f25df45c8de58e1dec5
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-06-04 23:13:02 +0900

    style(/src): import 문 최적화

--- src/main/java/roomescape/payment/dto/request/ConfirmPaymentRequest.java
+++ src/main/java/roomescape/payment/dto/request/ConfirmPaymentRequest.java
@@ -1,8 +1,6 @@
 package roomescape.payment.dto.request;
 
-import roomescape.payment.model.Payment;
 import roomescape.reservation.dto.request.CreateMyReservationRequest;
-import roomescape.reservation.model.Reservation;
 
 public record ConfirmPaymentRequest(String paymentKey,
                                     String orderId,
