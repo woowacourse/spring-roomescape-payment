f8e7cb133050265fca9a5c0197a7eabaea635651
hangillee <skfcb10@naver.com>
2024-06-05 14:01:13 +0900

    refactor: 내 예약 목록에서 예약 대기일 경우에만 취소 버튼 표시하도록 변경

--- src/main/resources/static/js/reservation-mine.js
+++ src/main/resources/static/js/reservation-mine.js
@@ -31,7 +31,7 @@ function render(data) {
     /*
     TODO: [3단계] 예약 대기 기능 - 예약 대기 취소 기능 구현 후 활성화
      */
-    if (status !== '예약') { // 예약 대기 상태일 때 예약 대기 취소 버튼 추가하는 코드, 상태 값은 변경 가능
+    if (status === '예약 대기') { // 예약 대기 상태일 때 예약 대기 취소 버튼 추가하는 코드, 상태 값은 변경 가능
       const cancelCell = row.cells[4];
       const cancelButton = document.createElement('button');
       cancelButton.textContent = '취소';
