d0e2d9c9db64cf3da932f9f262e44abd83894919
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-06-05 14:27:28 +0900

    feat: 금액 상한 검증 추가

--- src/test/java/roomescape/global/entity/PriceTest.java
+++ src/test/java/roomescape/global/entity/PriceTest.java
@@ -14,7 +14,7 @@ class PriceTest {
 
     @DisplayName("형식에 맞지 않은 가격 생성 시, 예외가 발생한다.")
     @ParameterizedTest
-    @ValueSource(doubles = {-1000.0, -1.1})
+    @ValueSource(doubles = {-1000.0, -1.1, 210000000000000000L})
     void invalidPrice(Double invalidPrice) {
         //given & when & then
         BigDecimal bigDecimal = BigDecimal.valueOf(0);
