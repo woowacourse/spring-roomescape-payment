dd7130b38bf317899ac27a288b3afd34d4c83d1f
BurningFalls <sherlocky31415@gmail.com>
2024-06-05 09:28:43 +0900

    refactor: RestClientException handling 추가
    Signed-off-by: BurningFalls <sherlocky31415@gmail.com>

--- src/main/java/roomescape/payment/TossPaymentClient.java
+++ src/main/java/roomescape/payment/TossPaymentClient.java
@@ -1,12 +1,15 @@
 package roomescape.payment;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.client.RestClientException;
 import roomescape.payment.dto.PaymentRequest;
 
+import java.net.SocketTimeoutException;
 import java.util.Base64;
 
 public class TossPaymentClient implements PaymentClient{
@@ -28,13 +31,20 @@ public class TossPaymentClient implements PaymentClient{
     @Override
     public ResponseEntity<Void> postPayment(final PaymentRequest paymentRequest) {
         final String secret = "Basic " + Base64.getEncoder().encodeToString((secretKey+password).getBytes());
-        
-        return restClient.post()
-                .uri("/v1/payments/confirm")
-                .header("Authorization", secret)
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(paymentRequest)
-                .retrieve()
-                .toBodilessEntity();
+
+        try {
+            return restClient.post()
+                    .uri("/v1/payments/confirm")
+                    .header("Authorization", secret)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(paymentRequest)
+                    .retrieve()
+                    .toBodilessEntity();
+        } catch (RestClientException e) {
+            if (e.getCause() instanceof SocketTimeoutException) {
+                return ResponseEntity.status(HttpStatus.REQUEST_TIMEOUT).build();
+            }
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 }
