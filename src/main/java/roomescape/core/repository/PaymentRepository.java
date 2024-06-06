fa23aa1bb5b33e52edab2848809a222c9b4179eb
hangillee <skfcb10@naver.com>
2024-06-07 02:44:48 +0900

    refactor: Payment 엔티티 조회 메소드 Optional 추가

--- src/main/java/roomescape/core/repository/PaymentRepository.java
+++ src/main/java/roomescape/core/repository/PaymentRepository.java
@@ -1,11 +1,12 @@
 package roomescape.core.repository;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import roomescape.core.domain.Payment;
 import roomescape.core.domain.Reservation;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
-    Payment findByReservation(Reservation reservation);
+    Optional<Payment> findByReservation(Reservation reservation);
 
     boolean existsByReservation(Reservation reservation);
 }
