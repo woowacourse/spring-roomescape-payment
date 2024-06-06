504cd971354d976e4f0d69a8db97a8750510a64d
Minjoo <m0522inju@gmail.com>
2024-06-07 03:58:34 +0900

    refactor: 불필요한 TODO 삭제

--- src/main/resources/static/js/reservation-mine.js
+++ src/main/resources/static/js/reservation-mine.js
@@ -1,8 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-    /*
-    TODO: [2단계] 내 예약 목록 조회 기능
-          endpoint 설정
-     */
     fetch('/reservations/mine') // 내 예약 목록 조회 API 호출
         .then(response => {
             if (response.status === 200) return response.json();
@@ -18,11 +14,6 @@ function render(data) {
 
     data.forEach(item => {
         const row = tableBody.insertRow();
-
-        /*
-        TODO: [2단계] 내 예약 목록 조회 기능
-              response 명세에 맞춰 값 설정
-         */
         const theme = item.theme;
         const date = item.date;
         const time = item.time;
@@ -33,9 +24,6 @@ function render(data) {
         row.insertCell(2).textContent = time;
         row.insertCell(3).textContent = status;
 
-    /*
-    TODO: [3단계] 예약 대기 기능 - 예약 대기 취소 기능 구현 후 활성화
-     */
     if (status !== '예약') { // 예약 대기 상태일 때 예약 대기 취소 버튼 추가하는 코드, 상태 값은 변경 가능
       const cancelCell = row.insertCell(4);
       const cancelButton = document.createElement('button');
@@ -54,9 +42,6 @@ function render(data) {
 }
 
 function requestDeleteWaiting(id) {
-    /*
-    TODO: [3단계] 예약 대기 기능 - 예약 대기 취소 API 호출
-     */
     const endpoint = `/waitings/`;
     return fetch(endpoint + id, {
         method: 'DELETE'
