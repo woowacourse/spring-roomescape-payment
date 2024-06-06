4f4746c89a8083ab336ed028383b76a5d1792298
hangillee <skfcb10@naver.com>
2024-06-07 02:43:34 +0900

    chore: 테스트용 Fake 객체에 기능 추가

--- src/test/java/roomescape/utils/FakePaymentClient.java
+++ src/test/java/roomescape/utils/FakePaymentClient.java
@@ -2,12 +2,17 @@ package roomescape.utils;
 
 import org.springframework.context.annotation.Profile;
 import roomescape.core.dto.payment.PaymentConfirmResponse;
-import roomescape.core.dto.reservation.ReservationPaymentRequest;
+import roomescape.core.dto.payment.PaymentRequest;
 import roomescape.infrastructure.PaymentClient;
 
 @Profile("test")
 public class FakePaymentClient implements PaymentClient {
-    public PaymentConfirmResponse getPaymentConfirmResponse(final ReservationPaymentRequest memberRequest) {
-        return new PaymentConfirmResponse(1000, "orderId", "paymentKey");
+    @Override
+    public PaymentConfirmResponse getPaymentConfirmResponse(final PaymentRequest memberRequest) {
+        return new PaymentConfirmResponse(1L, 1000, "orderId", "paymentKey");
+    }
+
+    @Override
+    public void getPaymentCancelResponse(final String paymentKey) {
     }
 }
