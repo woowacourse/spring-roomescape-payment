9d7e06ba90e1faf7be9c118e088a27aeefe08e4d
hangillee <skfcb10@naver.com>
2024-06-07 02:44:13 +0900

    refactor: 결제 승인 응답 정보에 id 포함

--- src/main/java/roomescape/core/dto/payment/PaymentConfirmResponse.java
+++ src/main/java/roomescape/core/dto/payment/PaymentConfirmResponse.java
@@ -1,17 +1,24 @@
 package roomescape.core.dto.payment;
 
 public class PaymentConfirmResponse {
+    private Long id;
     private Integer totalAmount;
     private String orderId;
     private String paymentKey;
 
-    public PaymentConfirmResponse(final Integer totalAmount, final String orderId, final String paymentKey) {
+    public PaymentConfirmResponse() {
+    }
+
+    public PaymentConfirmResponse(final Long id, final Integer totalAmount, final String orderId,
+                                  final String paymentKey) {
+        this.id = id;
         this.totalAmount = totalAmount;
         this.orderId = orderId;
         this.paymentKey = paymentKey;
     }
 
-    public PaymentConfirmResponse() {
+    public Long getId() {
+        return id;
     }
 
     public Integer getTotalAmount() {
