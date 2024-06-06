13cfd8de1c05ea00164a48bc4af50fd561519330
hyun <osh123chm@gmail.com>
2024-06-06 21:24:57 +0900

    feat: API 문서화 기본 설정 추가

--- src/test/java/roomescape/config/IntegrationTest.java
+++ src/test/java/roomescape/config/IntegrationTest.java
@@ -1,19 +1,27 @@
 package roomescape.config;
 
+import static org.springframework.restdocs.restassured.RestAssuredRestDocumentation.documentationConfiguration;
+
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
 import roomescape.auth.domain.Role;
 import roomescape.auth.jwt.JwtTokenProvider;
 import roomescape.member.domain.Member;
 import roomescape.member.domain.MemberName;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@ExtendWith(RestDocumentationExtension.class)
 public abstract class IntegrationTest {
 
     @LocalServerPort
@@ -28,9 +36,13 @@ public abstract class IntegrationTest {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
+    protected RequestSpecification spec;
+
     @BeforeEach
-    void setPort() {
+    void setUp(RestDocumentationContextProvider restDocumentation) {
         RestAssured.port = port;
+        this.spec = new RequestSpecBuilder().addFilter(documentationConfiguration(restDocumentation))
+                .build();
     }
 
     @AfterEach
