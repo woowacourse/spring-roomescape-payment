43dae91e1cee82a7a0328776a17bece764e0593c
eun-byeol <joeunbyeol98@gmail.com>
2024-06-07 01:27:29 +0900

    feat: paymentKey 칼럼 unique 설정

--- src/main/java/roomescape/payment/domain/Payment.java
+++ src/main/java/roomescape/payment/domain/Payment.java
@@ -17,7 +17,7 @@ public class Payment {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String paymentKey;
 
     @Column(nullable = false)
