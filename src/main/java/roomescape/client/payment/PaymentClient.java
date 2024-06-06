f8979a0eb85a2b791fae3a85e3adfd5241908ad0
lemone <lejinhy@gmail.com>
2024-06-07 01:22:54 +0900

    feat(ClientConfig, PaymentClient): 사용자 요청 바디, 요청 url 등 로깅 내역 구체화
    Co-authored-by: lemone <lejinhy@gmail.com>

--- src/main/java/roomescape/client/payment/PaymentClient.java
+++ src/main/java/roomescape/client/payment/PaymentClient.java
@@ -5,7 +5,9 @@ import static roomescape.exception.model.PaymentConfirmExceptionCode.FAILED_PAYM
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import org.slf4j.Logger;
 import org.springframework.http.HttpStatusCode;
@@ -14,8 +16,8 @@ import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestClientResponseException;
 import roomescape.client.payment.dto.PaymentConfirmToTossDto;
-import roomescape.exception.TossPaymentExceptionResponse;
 import roomescape.exception.PaymentConfirmException;
+import roomescape.exception.TossPaymentExceptionResponse;
 import roomescape.exception.global.GlobalExceptionCode;
 import roomescape.util.LoggerUtil;
 
@@ -52,25 +54,37 @@ public class PaymentClient {
                     })
                     .toBodilessEntity();
         } catch (RestClientResponseException e) {
-            log.error("토스 결제 응답 에러 message: {}, body: {}", e.getMessage(), e.getResponseBodyAsString(),
+            log.error("[토스 결제 api 실패] message: {}, body: {}", e.getMessage(), e.getResponseBodyAsString(),
                     e.getCause());
             throw new PaymentConfirmException(GlobalExceptionCode.HTTP_RESPONSE_DATA_INVALID);
         } catch (Exception e) {
-            log.error("토스 결제 에러 message: {}", e.getMessage(), e.getCause());
+            log.error("[토스 결제 실패] message: {}", e.getMessage(), e.getCause());
             throw new PaymentConfirmException(GlobalExceptionCode.INTERNAL_SERVER_ERROR);
         }
     }
 
-    private void throwConvertedCustomException(ClientHttpResponse response) throws IOException {
+    private void throwConvertedCustomException(ClientHttpResponse response) {
         try {
-            TossPaymentExceptionResponse paymentException = objectMapper.readValue(response.getBody(), TossPaymentExceptionResponse.class);
+            ByteArrayInputStream responseBody = convertResponseBody(response);
+            TossPaymentExceptionResponse paymentException =
+                    objectMapper.readValue(responseBody, TossPaymentExceptionResponse.class);
             throw new PaymentConfirmException(
                     paymentException.code() + paymentException.message(),
                     FAILED_PAYMENT_INTERNAL_SYSTEM_PROCESSING
             );
         } catch (JsonProcessingException e) {
             throw new PaymentConfirmException(GlobalExceptionCode.JSON_DATA_INVALID);
+        } catch (IOException e) {
+            throw new PaymentConfirmException(GlobalExceptionCode.HTTP_RESPONSE_DATA_INVALID);
         }
+    }
+
+    private ByteArrayInputStream convertResponseBody(ClientHttpResponse response) throws IOException {
+        String responseBody = new String(response.getBody().readAllBytes(), StandardCharsets.UTF_8);
+        log.error("[토스 api 예외 응답 객체] body: {}", responseBody);
+
+        byte[] responseBodyBytes = responseBody.getBytes(StandardCharsets.UTF_8);
+        return new ByteArrayInputStream(responseBodyBytes);
     }
 
     public String getEncodedSecretKey() {
--- src/main/java/roomescape/config/ClientConfig.java
+++ src/main/java/roomescape/config/ClientConfig.java
@@ -1,18 +1,27 @@
 package roomescape.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestClientCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import roomescape.client.payment.PaymentClient;
+import roomescape.util.LoggerUtil;
 
 @Configuration
 public class ClientConfig {
 
     private static final String TOSS_PAYMENT_CONFIRM_URL = "https://api.tosspayments.com";
+
+    private static final Logger log = LoggerUtil.getLogger(ClientConfig.class);
 
     @Bean
     public RestClient restClient(RestClient.Builder builder) {
@@ -29,10 +38,31 @@ public class ClientConfig {
     public RestClientCustomizer restClientCustomizer() {
         return restClientBuilder -> restClientBuilder
                 .requestFactory(getClientHttpRequestFactory())
-                .baseUrl(TOSS_PAYMENT_CONFIRM_URL);
+                .baseUrl(TOSS_PAYMENT_CONFIRM_URL)
+                .requestInterceptor(
+                        (request, body, execution) -> {
+                            logRequest(request, body);
+                            ClientHttpResponse response = execution.execute(request, body);
+                            logResponse(response);
+                            return response;
+                        })
+                .build();
     }
 
     private SimpleClientHttpRequestFactory getClientHttpRequestFactory() {
         return new SimpleClientHttpRequestFactory();
     }
+
+    private void logRequest(HttpRequest request, byte[] body) {
+        log.error("[토스 api 요청 정보] HTTP method: {}, URL: {}", request.getMethod(), request.getURI());
+        if (request.getMethod().equals(HttpMethod.POST)) {
+            log.error("[토스 api 사용자 요청] body: {}", new String(body, StandardCharsets.UTF_8));
+        }
+    }
+
+    private void logResponse(ClientHttpResponse response) throws IOException {
+        log.error("[토스 api 예외 상태코드] code: {}", response.getStatusCode());
+    }
 }
+
+
