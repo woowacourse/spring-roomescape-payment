57943d5425518f7eff4d0270d053c5203691202f
donghoony <aru0504@naver.com>
2024-06-06 18:30:01 +0900

    feat: Rest Docs 빌드 추가 / Acceptance에서 설정

--- src/test/java/roomescape/acceptance/AcceptanceTest.java
+++ src/test/java/roomescape/acceptance/AcceptanceTest.java
@@ -1,12 +1,26 @@
 package roomescape.acceptance;
 
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.modifyHeaders;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.modifyUris;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.restassured.RestAssuredRestDocumentation.documentationConfiguration;
+
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Import;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.operation.preprocess.HeadersModifyingOperationPreprocessor;
+import org.springframework.restdocs.operation.preprocess.UriModifyingOperationPreprocessor;
+import org.springframework.restdocs.restassured.RestAssuredRestDocumentationConfigurer;
 import roomescape.application.config.TestConfig;
 import roomescape.support.DatabaseCleanerExtension;
 
@@ -14,8 +28,8 @@ import roomescape.support.DatabaseCleanerExtension;
         classes = TestConfig.class,
         webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Import(AcceptanceFixture.class)
-@ExtendWith(DatabaseCleanerExtension.class)
-public abstract class AcceptanceTest {
+@ExtendWith({DatabaseCleanerExtension.class, RestDocumentationExtension.class})
+abstract class AcceptanceTest {
 
     @LocalServerPort
     private int port;
@@ -23,8 +37,32 @@ public abstract class AcceptanceTest {
     @Autowired
     protected AcceptanceFixture fixture;
 
+    private RequestSpecification spec;
+
     @BeforeEach
-    void setPort() {
+    void setPortAndDocs(RestDocumentationContextProvider provider) {
+        UriModifyingOperationPreprocessor uriModifier = modifyUris().scheme("http").host("woowa.hoony.me").port(8080);
+        HeadersModifyingOperationPreprocessor requestHeaderModifier = modifyHeaders()
+                .remove("Content-Length");
+        HeadersModifyingOperationPreprocessor responseHeaderModifier = modifyHeaders()
+                .remove("Date")
+                .remove("Keep-Alive")
+                .remove("Connection")
+                .remove("Transfer-Encoding")
+                .remove("Content-Length")
+                .set(CONTENT_TYPE, APPLICATION_JSON_VALUE);
+
         RestAssured.port = port;
+        RestAssuredRestDocumentationConfigurer configurer = documentationConfiguration(provider);
+        configurer.operationPreprocessors()
+                .withRequestDefaults(prettyPrint(), uriModifier, requestHeaderModifier)
+                .withResponseDefaults(prettyPrint(), responseHeaderModifier);
+        spec = new RequestSpecBuilder()
+                .addFilter(configurer)
+                .build();
+    }
+
+    protected RequestSpecification givenWithSpec() {
+        return RestAssured.given(spec);
     }
 }
