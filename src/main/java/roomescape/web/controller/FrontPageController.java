02a20a7fc4b91228d19762534718e0ac58cf240b
hyeonjilee <skylar12200@gmail.com>
2024-06-06 14:43:11 +0900

    fix: aws 페이지 오류 수정

--- src/main/java/roomescape/web/controller/FrontPageController.java
+++ src/main/java/roomescape/web/controller/FrontPageController.java
@@ -6,18 +6,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 class FrontPageController {
 
-    @GetMapping("/reservation")
+    @GetMapping("reservation")
     public String reservation(){
-        return "/reservation";
+        return "reservation";
     }
 
-    @GetMapping("/reservation-mine")
+    @GetMapping("reservation-mine")
     public String reservationMine() {
-        return "/reservation-mine";
+        return "reservation-mine";
     }
 
-    @GetMapping("/login")
+    @GetMapping("login")
     public String login() {
-        return "/login";
+        return "login";
     }
 }
