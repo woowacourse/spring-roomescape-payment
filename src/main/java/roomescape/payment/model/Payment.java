43b2d1868bdf56bd2a25aee65b851972c86f3b75
KimSangHyeok <tkdgur0906@naver.com>
2024-06-07 16:07:41 +0900

    feat(data.sql): 결제 더미 데이터 추가

--- src/main/java/roomescape/payment/model/Payment.java
+++ src/main/java/roomescape/payment/model/Payment.java
@@ -4,11 +4,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import roomescape.common.model.BaseEntity;
 
 import java.util.Objects;
 
 @Entity
-public class Payment {
+public class Payment extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
