f9ee731f0aaea2cc414ced8bc44ca9427bfcc137
pricelees <priceelees@gmail.com>
2024-06-07 17:38:35 +0900

    feat; ReservationRequest에 PaymentRequest를 얻어내는 메서드 추가

--- src/main/java/roomescape/reservation/dto/request/ReservationRequest.java
+++ src/main/java/roomescape/reservation/dto/request/ReservationRequest.java
@@ -2,23 +2,22 @@ package roomescape.reservation.dto.request;
 
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
-import roomescape.member.domain.Member;
-import roomescape.reservation.domain.Reservation;
-import roomescape.reservation.domain.ReservationStatus;
-import roomescape.reservation.domain.ReservationTime;
-import roomescape.theme.domain.Theme;
+import roomescape.payment.PaymentRequest;
 
 public record ReservationRequest(
-
-        @NotNull(message = "예약 날짜는 null일 수 없습니다.")
-        LocalDate date,
-        @NotNull(message = "예약 요청의 timeId는 null일 수 없습니다.")
-        Long timeId,
-        @NotNull(message = "예약 요청의 themeId는 null일 수 없습니다.")
-        Long themeId,
-        String paymentKey,
-        String orderId,
-        String amount,
-        String paymentType
+    @NotNull(message = "예약 날짜는 null일 수 없습니다.")
+    LocalDate date,
+    @NotNull(message = "예약 요청의 timeId는 null일 수 없습니다.")
+    Long timeId,
+    @NotNull(message = "예약 요청의 themeId는 null일 수 없습니다.")
+    Long themeId,
+    String paymentKey,
+    String orderId,
+    String amount,
+    String paymentType
 ) {
+
+    public PaymentRequest getPaymentRequest() {
+        return new PaymentRequest(paymentKey, orderId, amount, paymentType);
+    }
 }
