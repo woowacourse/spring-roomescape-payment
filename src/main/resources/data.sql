a4d64ab7eddcd99c964f36db332db6a1b8aab7aa
linirini <2001yerin@naver.com>
2024-06-07 16:30:59 +0900

    fix: 내 예약 조회 시 결제 정보 조회 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,6 +4,13 @@ INSERT INTO theme(name, description, thumbnail) VALUES ('테마3', '설명3', 'h
 
 INSERT INTO reservation_time(start_at) VALUES ('10:00');
 
+INSERT INTO payment(amount, approved_at, requested_at, order_id, payment_key) VALUES (1000, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'orderId', 'paymentKey');
+INSERT INTO payment(amount, approved_at, requested_at, order_id, payment_key) VALUES (1000, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'orderId', 'paymentKey');
+INSERT INTO payment(amount, approved_at, requested_at, order_id, payment_key) VALUES (1000, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'orderId', 'paymentKey');
+INSERT INTO payment(amount, approved_at, requested_at, order_id, payment_key) VALUES (1000, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'orderId', 'paymentKey');
+INSERT INTO payment(amount, approved_at, requested_at, order_id, payment_key) VALUES (1000, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'orderId', 'paymentKey');
+INSERT INTO payment(amount, approved_at, requested_at, order_id, payment_key) VALUES (1000, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'orderId', 'paymentKey');
+
 INSERT INTO member(name, email, password, role) VALUES('어드민', 'admin@email.com', 'admin123', 'ADMIN');
 INSERT INTO member(name, email, password, role) VALUES('리니', 'lini@email.com', 'lini123', 'GUEST');
 INSERT INTO member(name, email, password, role) VALUES('피케이', 'pk@email.com', 'pkpk123', 'GUEST');
@@ -17,11 +24,11 @@ INSERT INTO reservation_detail(date, time_id, theme_id) VALUES (DATEADD('DAY', -
 INSERT INTO reservation_detail(date, time_id, theme_id) VALUES (DATEADD('DAY', -5, CURRENT_DATE),1, 1);
 INSERT INTO reservation_detail(date, time_id, theme_id) VALUES (DATEADD('DAY', -6, CURRENT_DATE),1, 1);
 
-INSERT INTO reservation(detail_id, member_id, status, created_at) VALUES (1, 1, 'RESERVED', DATEADD('DAY', -1, CURRENT_DATE));
-INSERT INTO reservation(detail_id, member_id, status, created_at) VALUES (3, 2, 'RESERVED', DATEADD('DAY', -1, CURRENT_DATE));
-INSERT INTO reservation(detail_id, member_id, status, created_at) VALUES (5, 3, 'RESERVED', DATEADD('DAY', -1, CURRENT_DATE));
-INSERT INTO reservation(detail_id, member_id, status, created_at) VALUES (4, 3, 'RESERVED', DATEADD('DAY', -1, CURRENT_DATE));
-INSERT INTO reservation(detail_id, member_id, status, created_at) VALUES (2, 3, 'RESERVED', DATEADD('DAY', -2, CURRENT_DATE));
-INSERT INTO reservation(detail_id, member_id, status, created_at) VALUES (6, 4, 'RESERVED', DATEADD('DAY', -1, CURRENT_DATE));
+INSERT INTO reservation(detail_id, member_id, payment_id, status, created_at) VALUES (1, 1, 1, 'RESERVED', DATEADD('DAY', -1, CURRENT_DATE));
+INSERT INTO reservation(detail_id, member_id, payment_id, status, created_at) VALUES (3, 2, 2, 'RESERVED', DATEADD('DAY', -1, CURRENT_DATE));
+INSERT INTO reservation(detail_id, member_id, payment_id, status, created_at) VALUES (5, 3, 3, 'RESERVED', DATEADD('DAY', -1, CURRENT_DATE));
+INSERT INTO reservation(detail_id, member_id, payment_id, status, created_at) VALUES (4, 3, 4, 'RESERVED', DATEADD('DAY', -1, CURRENT_DATE));
+INSERT INTO reservation(detail_id, member_id, payment_id, status, created_at) VALUES (2, 3, 5, 'RESERVED', DATEADD('DAY', -2, CURRENT_DATE));
+INSERT INTO reservation(detail_id, member_id, payment_id, status, created_at) VALUES (6, 4, 6, 'RESERVED', DATEADD('DAY', -1, CURRENT_DATE));
 INSERT INTO reservation(detail_id, member_id, status, created_at) VALUES (2, 4, 'WAITING', DATEADD('DAY', -1, CURRENT_DATE));
 
