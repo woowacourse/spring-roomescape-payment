39ca23f20324ed5c2cc6c5d8ec8d5afe5c0e1561
masonkimseoul <masonkimseoul@gmail.com>
2024-06-07 17:09:19 +0900

    refactor: 내 예약 조회 클라이언트 코드에 paymentKey, amount 추가

--- src/main/resources/static/js/reservation-mine.js
+++ src/main/resources/static/js/reservation-mine.js
@@ -36,16 +36,46 @@ function render(data) {
         /*
         TODO: [3단계] 예약 대기 기능 - 예약 대기 취소 기능 구현 후 활성화
          */
-
         const cancelCell = row.insertCell(4);
         const cancelButton = document.createElement('button');
-        cancelButton.textContent = '취소';
         cancelButton.className = 'btn btn-danger';
-        cancelButton.onclick = function () {
-            requestDelete(item.reservationId).then(() => window.location.reload());
-        };
-        cancelCell.appendChild(cancelButton);
 
+        if (status === '결제 대기') {
+            const payCell = row.insertCell(5);
+            const payButton = document.createElement('button');
+            payButton.className = 'btn btn-danger';
+
+            payButton.textContent = '결제';
+            payButton.onclick = function () {
+                //requestDelete(item.reservationId).then(() => window.location.reload());
+                //결제 메서드 연결
+            };
+            payCell.appendChild(payButton);
+        }
+
+        if (status !== '예약') {
+            cancelButton.textContent = '대기 취소';
+            cancelButton.onclick = function () {
+                requestDelete(item.reservationId).then(() => window.location.reload());
+            };
+            cancelCell.appendChild(cancelButton);
+
+        }
+        else{
+                cancelButton.textContent = '예약 취소';
+                cancelButton.onclick = function () {
+                    requestDelete(item.reservationId).then(() => window.location.reload());
+                };
+                cancelCell.appendChild(cancelButton);
+
+                /*
+                TODO: [미션4 - 2단계] 내 예약 목록 조회 시,
+                    예약 완료 상태일 때 결제 정보를 함께 보여주기
+                    결제 정보 필드명은 자신의 response 에 맞게 변경하기
+                */
+                row.insertCell(6).textContent = item.paymentKey;
+                row.insertCell(7).textContent = item.amount;
+        }
     });
 }
 
