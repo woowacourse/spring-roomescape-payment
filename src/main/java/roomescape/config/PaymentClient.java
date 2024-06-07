dcdf76a8f2f48e0d941936ce5e882aa093da22ed
gyungchan Jo <namejgc@naver.com>
2024-06-07 16:28:14 +0900

    refactor: 결제 승인하는 기능 수정

--- src/main/java/roomescape/config/PaymentClient.java
+++ src/main/java/roomescape/config/PaymentClient.java
@@ -1,69 +1,39 @@
 package roomescape.config;
 
-import java.util.Base64;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import roomescape.dto.PaymentRequest;
 import roomescape.dto.PaymentResponse;
-import roomescape.exception.ExceptionType;
-import roomescape.exception.RoomescapeException;
+
+import java.util.Base64;
 
 @Component
 public class PaymentClient {
-
     private static final Logger log = LoggerFactory.getLogger(PaymentClient.class);
+    private static final String PAYMENT_CONFIRM_URI = "/v1/payments/confirm";
+
     private final String secretKey;
     private final RestClient restClient;
-    private final ObjectMapper objectMapper;
 
-    public PaymentClient(@Value("${security.payment.secret-key}") String secretKey, RestClient restClient, ObjectMapper objectMapper) {
+    public PaymentClient(@Value("${security.payment.secret-key}") String secretKey, RestClient restClient) {
         this.secretKey = secretKey;
         this.restClient = restClient;
-        this.objectMapper = objectMapper;
     }
 
-    public void approve(PaymentRequest paymentRequest) {
-        String uri = "/v1/payments/confirm";
-        String basic = getAuthorization();
-        restClient.post()
-                .uri(uri)
-                .header("Authorization", "Basic " + basic)
-                .body(paymentRequest)
+    public PaymentResponse approve(PaymentRequest request) {
+        PaymentResponse response = restClient.post()
+                .uri(PAYMENT_CONFIRM_URI)
+                .header("Authorization", "Basic " + getAuthorization())
+                .body(request)
                 .retrieve()
                 .onStatus(new PaymentClientResponseErrorHandler())
-                .toBodilessEntity();
-        logging(uri, "POST", paymentRequest);
-    }
+                .body(PaymentResponse.class);
 
-    public PaymentResponse readPayment(String paymentKey) {
-        String basic = getAuthorization();
-        String response = restClient.get()
-                .uri("v1/payments/{paymentKey}", paymentKey)
-                .header("Authorization", "Basic " + basic)
-                .retrieve()
-                .onStatus(new PaymentClientResponseErrorHandler())
-                .body(String.class);
-        try {
-            JsonNode rootNode = objectMapper.readTree(response);
-            PaymentResponse paymentResponse = objectMapper.treeToValue(rootNode, PaymentResponse.class);
-            logging("v1/payments/" + paymentKey, "GET", paymentResponse);
-            return paymentResponse;
-        } catch (JsonProcessingException e) {
-            throw new RoomescapeException(ExceptionType.INVALID_PARSE_FORMAT);
-        }
-    }
-
-    private void logging(String uri, String httpMethod, Object body) {
-        log.info("URI: {}, Method: {}, Body:{} ", uri, httpMethod, body);
+        log.info("URI: {}, Method: {}, request: {} response: {}", PAYMENT_CONFIRM_URI, "POST", request, response);
+        return response;
     }
 
     private String getAuthorization() {
