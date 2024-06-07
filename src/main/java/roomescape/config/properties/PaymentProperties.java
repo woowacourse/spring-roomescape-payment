ef8c55e6671ee274e544085856283b3fff29a61a
Eugene Jang <kyunellroll@gmail.com>
2024-06-08 00:36:58 +0900

    refactor(TossPaymentProperties): Properties에 타임아웃도 추가
    - 타임아웃 추가에 따른 변경
    - RestClientConfigMaker 정상적으로 생성 테스트

--- src/main/java/roomescape/config/properties/PaymentProperties.java
+++ src/main/java/roomescape/config/properties/PaymentProperties.java
@@ -2,10 +2,15 @@ package roomescape.config.properties;
 
 import roomescape.paymenthistory.domain.PaymentUrl;
 import roomescape.paymenthistory.domain.SecretKey;
+import roomescape.paymenthistory.domain.TimeOut;
 
 public interface PaymentProperties {
 
     SecretKey getSecretKey();
 
     PaymentUrl getPaymentUrl();
+
+    TimeOut getConnectTimeOut();
+
+    TimeOut getReadTimeOut();
 }
--- src/main/java/roomescape/config/properties/TossPaymentProperties.java
+++ src/main/java/roomescape/config/properties/TossPaymentProperties.java
@@ -4,17 +4,22 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.bind.ConstructorBinding;
 import roomescape.paymenthistory.domain.PaymentUrl;
 import roomescape.paymenthistory.domain.SecretKey;
+import roomescape.paymenthistory.domain.TimeOut;
 
 @ConfigurationProperties(prefix = "payment.toss")
 public class TossPaymentProperties implements PaymentProperties {
 
     private final SecretKey secretKey;
     private final PaymentUrl paymentUrl;
+    private final TimeOut connectTimeOut;
+    private final TimeOut readTimeOut;
 
     @ConstructorBinding
-    public TossPaymentProperties(String secretKey, String paymentUrl) {
+    public TossPaymentProperties(String secretKey, String paymentUrl, String connectTimeOut, String readTimeOut) {
         this.secretKey = new SecretKey(secretKey);
         this.paymentUrl = new PaymentUrl(paymentUrl);
+        this.connectTimeOut = new TimeOut(connectTimeOut);
+        this.readTimeOut = new TimeOut(readTimeOut);
     }
 
     @Override
@@ -25,5 +30,15 @@ public class TossPaymentProperties implements PaymentProperties {
     @Override
     public PaymentUrl getPaymentUrl() {
         return paymentUrl;
+    }
+
+    @Override
+    public TimeOut getConnectTimeOut() {
+        return connectTimeOut;
+    }
+
+    @Override
+    public TimeOut getReadTimeOut() {
+        return readTimeOut;
     }
 }
