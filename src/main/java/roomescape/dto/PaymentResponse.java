895ad4fc250c7a5013411bb5b0b8259158076c1e
gyungchan Jo <namejgc@naver.com>
2024-06-07 16:11:36 +0900

    feat: 결제 정보 Entity 생성

--- src/main/java/roomescape/dto/PaymentResponse.java
+++ src/main/java/roomescape/dto/PaymentResponse.java
@@ -1,6 +1,7 @@
 package roomescape.dto;
 
-public record PaymentResponse(String orderName,
+public record PaymentResponse(String paymentKey,
+                              String orderName,
                               String requestedAt,
                               String approvedAt,
                               String currency,
