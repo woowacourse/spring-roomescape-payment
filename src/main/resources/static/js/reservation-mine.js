dbb2cf1b57aec58ae72e46cc443b893d8ded29ce
masonkimseoul <masonkimseoul@gmail.com>
2024-06-09 02:56:33 +0900

    fix: 클라이언트 코드 table index 오류 해결

--- src/main/resources/static/js/reservation-mine.js
+++ src/main/resources/static/js/reservation-mine.js
@@ -40,18 +40,10 @@ function render(data) {
         const cancelButton = document.createElement('button');
         cancelButton.className = 'btn btn-danger';
 
-        if (status === '결제 대기') {
-            const payCell = row.insertCell(5);
-            const payButton = document.createElement('button');
-            payButton.className = 'btn btn-danger';
+        const payCell = row.insertCell(5);
+        const payButton = document.createElement('button');
+        payButton.className = 'btn btn-danger';
 
-            payButton.textContent = '결제';
-            payButton.onclick = function () {
-                //requestDelete(item.reservationId).then(() => window.location.reload());
-                //결제 메서드 연결
-            };
-            payCell.appendChild(payButton);
-        }
 
         if (status !== '예약') {
             cancelButton.textContent = '대기 취소';
@@ -60,21 +52,29 @@ function render(data) {
             };
             cancelCell.appendChild(cancelButton);
 
+            if (status === '결제 대기') {
+                payButton.textContent = '결제';
+                payButton.onclick = function () {
+                    //requestDelete(item.reservationId).then(() => window.location.reload());
+                    //결제 메서드 연결
+                };
+                payCell.appendChild(payButton);
+            }
         }
         else{
-                cancelButton.textContent = '예약 취소';
-                cancelButton.onclick = function () {
-                    requestDelete(item.reservationId).then(() => window.location.reload());
-                };
-                cancelCell.appendChild(cancelButton);
+            cancelButton.textContent = '예약 취소';
+            cancelButton.onclick = function () {
+                requestDelete(item.reservationId).then(() => window.location.reload());
+            };
+            cancelCell.appendChild(cancelButton);
 
                 /*
                 TODO: [미션4 - 2단계] 내 예약 목록 조회 시,
                     예약 완료 상태일 때 결제 정보를 함께 보여주기
                     결제 정보 필드명은 자신의 response 에 맞게 변경하기
                 */
-                row.insertCell(6).textContent = item.paymentKey;
-                row.insertCell(7).textContent = item.amount;
+            row.insertCell(6).textContent = item.paymentKey;
+            row.insertCell(7).textContent = item.amount;
         }
     });
 }
