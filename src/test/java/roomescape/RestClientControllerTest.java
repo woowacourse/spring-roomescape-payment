12bf6b8daf3dce3389629425205abb815ab9e3f4
ikjo39 <chi4321@naver.com>
2024-06-08 22:22:02 +0900

    test: REST Docs를 위한 컨트롤러 설정 추상 클래스 추가

---
+++ src/test/java/roomescape/RestClientControllerTest.java
@@ -0,0 +1,44 @@
+package roomescape;
+
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.restassured.RestAssuredRestDocumentation.documentationConfiguration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@ExtendWith(RestDocumentationExtension.class)
+@Sql(value = "classpath:test-data.sql", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+public abstract class RestClientControllerTest {
+
+    public RequestSpecification spec;
+
+    @LocalServerPort
+    public int serverPort;
+
+    @Autowired
+    public ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp(final RestDocumentationContextProvider restDocumentation) {
+        RestAssured.port = serverPort;
+        this.spec = new RequestSpecBuilder()
+                .addFilter(documentationConfiguration(restDocumentation)
+                        .operationPreprocessors()
+                        .withRequestDefaults(prettyPrint())
+                        .withResponseDefaults(prettyPrint()))
+                .build();
+    }
+}
