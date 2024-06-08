72078a321d9e3f3fc3ea3cc4d4c8e59227a17c2e
yechop <channetattoo@gmail.com>
2024-06-08 18:39:43 +0900

    refactor: log level 변경

--- src/main/java/roomescape/exception/RoomescapeExceptionHandler.java
+++ src/main/java/roomescape/exception/RoomescapeExceptionHandler.java
@@ -1,8 +1,5 @@
 package roomescape.exception;
 
-import static roomescape.exception.ExceptionType.INVALID_DATE_TIME_FORMAT;
-import static roomescape.exception.ExceptionType.NO_QUERY_PARAMETER;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +9,9 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import roomescape.dto.ErrorResponse;
 
+import static roomescape.exception.ExceptionType.INVALID_DATE_TIME_FORMAT;
+import static roomescape.exception.ExceptionType.NO_QUERY_PARAMETER;
+
 @ControllerAdvice
 public class RoomescapeExceptionHandler {
 
@@ -19,14 +19,14 @@ public class RoomescapeExceptionHandler {
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<ErrorResponse> handle(HttpMessageNotReadableException e) {
-        logger.error(e.getMessage(), e);
+        logger.debug(e.getMessage(), e);
         return ResponseEntity.status(INVALID_DATE_TIME_FORMAT.getStatus())
                 .body(new ErrorResponse(INVALID_DATE_TIME_FORMAT.getMessage()));
     }
 
     @ExceptionHandler(RoomescapeException.class)
     public ResponseEntity<ErrorResponse> handle(RoomescapeException e) {
-        logger.error(e.getMessage(), e);
+        logger.debug(e.getMessage(), e);
         return ResponseEntity
                 .status(e.getHttpStatus())
                 .body(new ErrorResponse(e.getMessage()));
@@ -34,21 +34,21 @@ public class RoomescapeExceptionHandler {
 
     @ExceptionHandler(MissingServletRequestParameterException.class)
     public ResponseEntity<ErrorResponse> handle(MissingServletRequestParameterException e) {
-        logger.error(e.getMessage(), e);
+        logger.debug(e.getMessage(), e);
         return ResponseEntity.status(NO_QUERY_PARAMETER.getStatus())
                 .body(new ErrorResponse(NO_QUERY_PARAMETER.getMessage()));
     }
 
     @ExceptionHandler(PaymentException.class)
     public ResponseEntity<ErrorResponse> handle(PaymentException e) {
-        logger.error(e.getMessage(), e);
+        logger.debug(e.getMessage(), e);
         return ResponseEntity.internalServerError()
                 .body(new ErrorResponse(e.getErrorMessage()));
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handle(Exception e) {
-        logger.error(e.getMessage(), e);
+        logger.debug(e.getMessage(), e);
         return ResponseEntity.status(500)
                 .body(new ErrorResponse("예상치 못한 오류입니다. 서버 관계자에게 문의하세요."));
     }
