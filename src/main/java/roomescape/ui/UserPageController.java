f67e5f31815315ee84b5845b42f001e7028458fc
le2sky <leehaneul990623@gmail.com>
2024-06-08 15:24:54 +0900

    fix: 템플릿 경로 수정

--- src/main/java/roomescape/ui/UserPageController.java
+++ src/main/java/roomescape/ui/UserPageController.java
@@ -7,21 +7,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class UserPageController {
     @GetMapping
     public String mainPage() {
-        return "/index";
+        return "index";
     }
 
     @GetMapping("/reservation")
     public String reservationPage() {
-        return "/reservation";
+        return "reservation";
     }
 
     @GetMapping("/login")
     public String loginPage() {
-        return "/login";
+        return "login";
     }
 
     @GetMapping("/reservation-mine")
     public String myReservationPage() {
-        return "/reservation-mine";
+        return "reservation-mine";
     }
 }
