36434e6b917cb0eac371775868dcb2b7ebe77e4e
pricelees <priceelees@gmail.com>
2024-06-08 13:33:32 +0900

    feat: PaymentKey를 이용하여 Payment 정보를 불러오는 기능 추가

--- src/main/java/roomescape/payment/PaymentRepository.java
+++ src/main/java/roomescape/payment/PaymentRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
 
     Optional<Payment> findByReservationId(Long reservationId);
+
+    Optional<Payment> findByPaymentKey(String paymentKey);
 }
