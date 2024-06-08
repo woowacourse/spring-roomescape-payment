8561f5e949d369bebcd52286dfd35017f3f0e088
nayonsoso <panda0329@naver.com>
2024-06-08 14:03:35 +0900

    refactor: 로그 레벨 변경
    - 결제 관련 예외가 아니라면 ERROR 가 아니라 WARN 레벨을 사용하도록

--- src/main/java/roomescape/infra/GlobalExceptionHandler.java
+++ src/main/java/roomescape/infra/GlobalExceptionHandler.java
@@ -18,7 +18,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(RoomEscapeException.class)
     public ResponseEntity<String> handleCustomRoomEscapeException(RoomEscapeException exception) {
-        logger.error(exception.getMessage(), exception);
+        logger.warn(exception.getMessage(), exception);
         return ResponseEntity
                 .status(exception.getHttpStatus())
                 .body(exception.getMessage());
@@ -35,14 +35,14 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public String handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException exception) {
-        logger.error(exception.getMessage(), exception);
+        logger.warn(exception.getMessage(), exception);
         return GlobalExceptionCode.METHOD_ARGUMENT_TYPE_INVALID.getMessage();
     }
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public String handleInternalServerError(Exception exception) {
-        logger.error(exception.getMessage(), exception);
+        logger.warn(exception.getMessage(), exception);
         return GlobalExceptionCode.INTERNAL_SERVER_ERROR.getMessage();
     }
 }
