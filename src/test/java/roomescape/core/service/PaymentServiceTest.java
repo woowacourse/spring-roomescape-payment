ae72db3b11b33c10a16785325dc102cb26642115
hangillee <skfcb10@naver.com>
2024-06-08 16:39:33 +0900

    test: Payment 서비스 계층 통합 테스트 작성

---
+++ src/test/java/roomescape/core/service/PaymentServiceTest.java
@@ -0,0 +1,54 @@
+package roomescape.core.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import roomescape.core.dto.member.LoginMember;
+import roomescape.core.dto.payment.PaymentConfirmResponse;
+import roomescape.core.dto.payment.PaymentRequest;
+import roomescape.utils.DatabaseCleaner;
+import roomescape.utils.TestFixture;
+
+@ServiceTest
+class PaymentServiceTest {
+    @Autowired
+    private PaymentService paymentService;
+
+    @Autowired
+    private TestFixture testFixture;
+
+    @Autowired
+    private DatabaseCleaner databaseCleaner;
+
+    @BeforeEach
+    void setUp() {
+        databaseCleaner.executeTruncate();
+        testFixture.initTestData();
+    }
+
+    @Test
+    @DisplayName("결제를 승인할 수 있다.")
+    void confirmPayment() {
+        final PaymentRequest request = new PaymentRequest(1L, 1000, "orderId", "paymentKey");
+        final LoginMember loginMember = new LoginMember(1L);
+
+        final PaymentConfirmResponse response = paymentService.confirmPayment(request, loginMember);
+
+        assertAll(
+                () -> assertThat(response.getTotalAmount()).isEqualTo(1000),
+                () -> assertThat(response.getOrderId()).isEqualTo("orderId"),
+                () -> assertThat(response.getPaymentKey()).isEqualTo("paymentKey")
+        );
+    }
+
+    @Test
+    @DisplayName("결제를 취소할 수 있다.")
+    void cancelPayment() {
+        assertDoesNotThrow(() -> paymentService.cancel(1L));
+    }
+}
