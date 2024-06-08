ed843086bdedc30cc31c0c3361b46e3b642faaaf
J-I-H-O <jeongjiho0731@gmail.com>
2024-06-08 22:29:21 +0900

    docs: RestDocs 초기 설정 및 인기 테마 조회 API 문서화

---
+++ src/test/java/roomescape/document/ThemeRestDocsTest.java
@@ -0,0 +1,60 @@
+package roomescape.document;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import roomescape.controller.ThemeController;
+import roomescape.document.config.RestDocsSupport;
+import roomescape.dto.ThemeResponse;
+import roomescape.fixture.ThemeFixture;
+import roomescape.service.ThemeService;
+
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.queryParameters;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(ThemeController.class)
+public class ThemeRestDocsTest extends RestDocsSupport {
+
+    @MockBean
+    private ThemeService themeService;
+
+    @Test
+    public void findAndOrderByPopularity() throws Exception {
+        List<ThemeResponse> response = List.of(
+                ThemeResponse.from(ThemeFixture.DEFAULT_THEME),
+                ThemeResponse.from(ThemeFixture.DEFAULT_THEME)
+        );
+        given(themeService.findAndOrderByPopularity(any(), any(), anyInt()))
+                .willReturn(response);
+
+        mockMvc.perform(get("/themes/ranking")
+                        .param("start", "2024-06-01")
+                        .param("end", "2024-06-07")
+                        .param("count", "10")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(restDocs.document(
+                        queryParameters(
+                                parameterWithName("start").description("검색 시작 날짜"),
+                                parameterWithName("end").description("검색 종료 날짜"),
+                                parameterWithName("count").description("검색할 테마의 개수")
+                        ),
+                        responseFields(
+                                fieldWithPath("[].id").description("테마 id"),
+                                fieldWithPath("[].name").description("테마 이름"),
+                                fieldWithPath("[].description").description("테마 설명"),
+                                fieldWithPath("[].thumbnail").description("테마 썸네일 URL")
+                        )
+                ));
+    }
+}
---
+++ src/test/java/roomescape/document/config/RestDocsConfiguration.java
@@ -0,0 +1,22 @@
+package roomescape.document.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
+
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+
+@TestConfiguration
+public class RestDocsConfiguration {
+
+    @Bean
+    public RestDocumentationResultHandler restDocumentationResultHandler() {
+        return MockMvcRestDocumentation.document("{class-name}/{method-name}",
+                preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint())
+        );
+    }
+}
