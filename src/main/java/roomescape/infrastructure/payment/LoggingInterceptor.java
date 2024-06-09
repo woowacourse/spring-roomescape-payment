f6d06d21aa076183e4d568913a1381086c098cf4
Nam Gi Beom <rlqja0523@naver.com>
2024-06-09 17:06:04 +0900

    feat: RestClient logging interceptor 추가

---
+++ src/main/java/roomescape/infrastructure/payment/LoggingInterceptor.java
@@ -0,0 +1,20 @@
+package roomescape.infrastructure.payment;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
+
+import java.io.IOException;
+
+public class LoggingInterceptor implements ClientHttpRequestInterceptor {
+    private final Logger logger = LoggerFactory.getLogger(LoggingInterceptor.class);
+
+    @Override
+    public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
+        logger.info("Payment Request: {} {} {}", request.getMethod(), request.getURI(), new String(body));
+        return execution.execute(request, body);
+    }
+}
