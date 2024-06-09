9bf1ef270ee381dd8d80e70e8a0c8f1bd3165a4d
reddevilmidzy <midzy0228@gmail.com>
2024-06-09 22:52:48 +0900

    feat: 테마 썸네일 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,10 +6,10 @@ VALUES ('15:00'),
        ('19:00');
 
 INSERT INTO THEME(NAME, DESCRIPTION, THUMBNAIL)
-VALUES ('봄', '설명1', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg'),
-       ('여름', '설명2', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg'),
-       ('가을', '설명3', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg'),
-       ('겨울', '설명4', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg');
+VALUES ('봄', '설명1', 'https://raw.githubusercontent.com/reddevilmidzy/spring-roomescape-payment/step2/png/%EA%B3%A0%EB%82%9C%EC%97%AD%EA%B2%BD%EA%B7%B9%EB%B3%B5%EA%B8%B0.jpg'),
+       ('여름', '설명2', 'https://raw.githubusercontent.com/reddevilmidzy/spring-roomescape-payment/step2/png/%EB%82%B4%EA%B0%80%EA%B1%8D%ED%95%98%EC%A7%80.jpg'),
+       ('가을', '설명3', 'https://raw.githubusercontent.com/reddevilmidzy/spring-roomescape-payment/step2/png/%EC%9A%B8%EC%96%B4%EB%8F%84%EB%8F%BC.jpg'),
+       ('겨울', '설명4', 'https://raw.githubusercontent.com/reddevilmidzy/spring-roomescape-payment/step2/png/%EC%9D%B4%EC%A0%9C%EB%8A%94%EB%8D%94%EC%9D%B4%EC%83%81.jpg');
 
 INSERT INTO MEMBER(NAME, EMAIL, PASSWORD, ROLE)
 VALUES ('레디', 'redddy@gmail.com', '3e2b2d79b2f6f90ba2f3ae18a90ae990b149b2fdbc208f55ac9b763b4dad0a16', 'ADMIN'),
