41a6b46c7a3fb355f7f377f6a5ea711e0ad01156
SungKyum Kim <sungkyum1@naver.com>
2024-06-10 03:27:12 +0900

    test: 자동 예약 변경 기능 삭제로 인한 불필요 테스트 삭제

--- src/test/java/roomescape/service/reservation/ReservationCommonServiceTest.java
+++ src/test/java/roomescape/service/reservation/ReservationCommonServiceTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.jdbc.Sql;
-import roomescape.domain.dto.ReservationWithRank;
 import roomescape.domain.reservation.Reservation;
 import roomescape.domain.reservation.ReservationRepository;
 import roomescape.domain.reservation.ReservationStatus;
@@ -80,25 +79,6 @@ class ReservationCommonServiceTest extends ReservationServiceTest {
 
         //then
         assertThat(reservationCommonService.findAll()).isEmpty();
-    }
-
-    @DisplayName("관리자가 예약을 삭제하고, 예약 대기가 있다면 가장 우선순위가 높은 예약 대기를 예약으로 전환한다.")
-    @Test
-    void deleteThenUpdateReservation() {
-        //given
-        Reservation reservation = new Reservation(admin, reservationDetail, ReservationStatus.RESERVED);
-        Reservation reservation2 = new Reservation(member, reservationDetail, ReservationStatus.WAITING);
-        Reservation reservation3 = new Reservation(anotherMember, reservationDetail, ReservationStatus.WAITING);
-        reservationRepository.save(reservation);
-        reservationRepository.save(reservation2);
-        reservationRepository.save(reservation3);
-
-        //when
-        reservationCommonService.deleteById(reservation.getId());
-
-        //then
-        List<ReservationWithRank> reservations = reservationRepository.findWithRankingByMemberId(member.getId());
-        assertThat(reservations.get(0).getReservation().isReserved()).isTrue();
     }
 
     @DisplayName("관리자가 과거 예약을 삭제하려고 하면 예외가 발생한다.")
