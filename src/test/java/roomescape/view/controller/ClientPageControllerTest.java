c09f26dc5333b0122d5be211c40ef9c4cc7bdc88
pricelees <priceelees@gmail.com>
2024-06-09 18:11:50 +0900

    test: 회원 페이지 접속 테스트 추가

--- src/test/java/roomescape/view/controller/ClientPageControllerTest.java
+++ src/test/java/roomescape/view/controller/ClientPageControllerTest.java
@@ -33,4 +33,14 @@ class ClientPageControllerTest {
                 .then().log().all()
                 .statusCode(200);
     }
+
+    @Test
+    @DisplayName("/reservation-mine 으로 GET 요청을 보내면 방탈출 예약 페이지와 200 OK 를 받는다.")
+    void getMyReservationPage() {
+        RestAssured.given().log().all()
+                .port(port)
+                .when().get("/reservation-mine")
+                .then().log().all()
+                .statusCode(200);
+    }
 }
