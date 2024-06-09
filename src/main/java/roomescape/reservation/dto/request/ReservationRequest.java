e3d803d09a3ebb78e36e5592150e4697950da104
pricelees <priceelees@gmail.com>
2024-06-10 05:47:58 +0900

    refactor: 날짜 파라미터 입력시 조건 추가

--- src/main/java/roomescape/reservation/dto/request/ReservationRequest.java
+++ src/main/java/roomescape/reservation/dto/request/ReservationRequest.java
@@ -9,10 +9,10 @@ import roomescape.payment.dto.request.PaymentRequest;
 @Schema(name = "회원의 예약 저장 요청", description = "회원의 예약 요청시 사용됩니다.")
 public record ReservationRequest(
         @NotNull(message = "예약 날짜는 null일 수 없습니다.")
-        @Schema(description = "예약 날짜. yyyy-MM-dd 형식으로 입력해야 합니다.", type = "string", example = "2022-12-31")
+        @Schema(description = "예약 날짜. 지난 날짜는 지정할 수 없으며, yyyy-MM-dd 형식으로 입력해야 합니다.", type = "string", example = "2022-12-31")
         LocalDate date,
         @NotNull(message = "예약 요청의 timeId는 null일 수 없습니다.")
-        @Schema(description = "예약 시간 ID", example = "1")
+        @Schema(description = "예약 시간 ID.", example = "1")
         Long timeId,
         @NotNull(message = "예약 요청의 themeId는 null일 수 없습니다.")
         @Schema(description = "테마 ID", example = "1")
--- src/main/java/roomescape/reservation/dto/request/WaitingRequest.java
+++ src/main/java/roomescape/reservation/dto/request/WaitingRequest.java
@@ -7,7 +7,7 @@ import java.time.LocalDate;
 @Schema(name = "예약 대기 저장 요청", description = "회원의 예약 대기 요청시 사용됩니다.")
 public record WaitingRequest(
         @NotNull(message = "예약 날짜는 null일 수 없습니다.")
-        @Schema(description = "예약 날짜. yyyy-MM-dd 형식으로 입력해야 합니다.", type = "string", example = "2022-12-31")
+        @Schema(description = "예약 날짜. 지난 날짜는 지정할 수 없으며, yyyy-MM-dd 형식으로 입력해야 합니다.", type = "string", example = "2022-12-31")
         LocalDate date,
         @NotNull(message = "예약 요청의 timeId는 null일 수 없습니다.")
         @Schema(description = "예약 시간 ID", example = "1")
