6476bce3bdd1ac994ee64e48368322ff75e075a6
pricelees <priceelees@gmail.com>
2024-06-10 03:19:26 +0900

    refactor: 회원 예약 조회 및 예약 페이지 접근시의 로그인 체크 추가

--- src/main/java/roomescape/view/controller/ClientPageController.java
+++ src/main/java/roomescape/view/controller/ClientPageController.java
@@ -2,6 +2,7 @@ package roomescape.view.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import roomescape.system.auth.annotation.LoginRequired;
 
 @Controller
 public class ClientPageController {
@@ -11,11 +12,13 @@ public class ClientPageController {
         return "index";
     }
 
+    @LoginRequired
     @GetMapping("/reservation")
     public String showReservationPage() {
         return "reservation";
     }
 
+    @LoginRequired
     @GetMapping("/reservation-mine")
     public String showReservationMinePage() {
         return "/reservation-mine";
