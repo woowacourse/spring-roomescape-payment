2f1c1e98f5c9949d7c75ed2af48c8fa30e19bd1a
Nam Gi Beom <rlqja0523@naver.com>
2024-06-10 05:12:39 +0900

    test: base documentation test 추가

---
+++ src/test/java/roomescape/documentation/BaseDocumentationTest.java
@@ -0,0 +1,55 @@
+package roomescape.documentation;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
+import jakarta.servlet.http.Cookie;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import roomescape.support.FakeAuthArgumentResolver;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+
+@ExtendWith(RestDocumentationExtension.class)
+abstract class BaseDocumentationTest {
+
+    protected final ObjectMapper objectMapper;
+    protected final Cookie adminCookie = new Cookie("token", "admin-token");
+    protected final Cookie userCookie = new Cookie("token", "user-token");
+    protected MockMvc mockMvc;
+
+    BaseDocumentationTest() {
+        JavaTimeModule module = new JavaTimeModule();
+        module.addSerializer(LocalDate.class, new LocalDateSerializer(DateTimeFormatter.ISO_LOCAL_DATE));
+        module.addSerializer(LocalTime.class, new LocalTimeSerializer(DateTimeFormatter.ISO_LOCAL_TIME));
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(module);
+        this.objectMapper = objectMapper;
+    }
+
+    @BeforeEach
+    void setUp(RestDocumentationContextProvider restDocumentation) {
+        this.mockMvc = MockMvcBuilders.standaloneSetup(controller())
+                .apply(documentationConfiguration(restDocumentation)
+                        .operationPreprocessors()
+                        .withRequestDefaults(prettyPrint())
+                        .withResponseDefaults(prettyPrint())
+                )
+                .setMessageConverters(new MappingJackson2HttpMessageConverter(objectMapper))
+                .setCustomArgumentResolvers(new FakeAuthArgumentResolver())
+                .build();
+    }
+
+    abstract Object controller();
+}
