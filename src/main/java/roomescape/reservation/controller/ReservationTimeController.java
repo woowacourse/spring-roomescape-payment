2d074ac3ccf3450d48002528f0f2423983b18aaf
pricelees <priceelees@gmail.com>
2024-06-10 04:38:57 +0900

    feat: 예약 시간 API 문서 작성을 위한 코드 추가

--- src/main/java/roomescape/reservation/controller/ReservationTimeController.java
+++ src/main/java/roomescape/reservation/controller/ReservationTimeController.java
@@ -1,5 +1,12 @@
 package roomescape.reservation.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
@@ -20,9 +27,12 @@ import roomescape.reservation.dto.response.ReservationTimeResponse;
 import roomescape.reservation.dto.response.ReservationTimesResponse;
 import roomescape.reservation.service.ReservationTimeService;
 import roomescape.system.auth.annotation.Admin;
-import roomescape.system.dto.response.ApiResponse;
+import roomescape.system.auth.annotation.LoginRequired;
+import roomescape.system.dto.response.ErrorResponse;
+import roomescape.system.dto.response.RoomEscapeApiResponse;
 
 @RestController
+@Tag(name = "4. 예약 시간 API", description = "예약 시간을 조회 / 추가 / 삭제할 때 사용합니다.")
 public class ReservationTimeController {
 
     private final ReservationTimeService reservationTimeService;
@@ -34,39 +44,67 @@ public class ReservationTimeController {
     @Admin
     @GetMapping("/times")
     @ResponseStatus(HttpStatus.OK)
-    public ApiResponse<ReservationTimesResponse> getAllTimes() {
-        return ApiResponse.success(reservationTimeService.findAllTimes());
+    @Operation(summary = "모든 시간 조회", description = "모든 예약 시간을 조회합니다.", tags = "관리자 로그인이 필요한 API")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "성공", useReturnTypeSchema = true)
+    })
+    public RoomEscapeApiResponse<ReservationTimesResponse> getAllTimes() {
+        return RoomEscapeApiResponse.success(reservationTimeService.findAllTimes());
     }
 
     @Admin
     @PostMapping("/times")
     @ResponseStatus(HttpStatus.CREATED)
-    public ApiResponse<ReservationTimeResponse> saveTime(
+    @Operation(summary = "시간 추가", description = "새로운 예약 시간을 추가합니다.", tags = "관리자 로그인이 필요한 API")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "성공", useReturnTypeSchema = true),
+            @ApiResponse(responseCode = "409", description = "같은 시간을 추가할 수 없습니다.",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    })
+    public RoomEscapeApiResponse<ReservationTimeResponse> saveTime(
             @Valid @RequestBody ReservationTimeRequest reservationTimeRequest,
             HttpServletResponse response
     ) {
         ReservationTimeResponse reservationTimeResponse = reservationTimeService.addTime(reservationTimeRequest);
         response.setHeader(HttpHeaders.LOCATION, "/times/" + reservationTimeResponse.id());
 
-        return ApiResponse.success(reservationTimeResponse);
+        return RoomEscapeApiResponse.success(reservationTimeResponse);
     }
 
     @Admin
     @DeleteMapping("/times/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public ApiResponse<Void> removeTime(
-            @NotNull(message = "timeId는 null 또는 공백일 수 없습니다.") @PathVariable Long id
+    @Operation(summary = "시간 삭제", description = "특정 예약 시간을 삭제합니다.", tags = "관리자 로그인이 필요한 API")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "성공", useReturnTypeSchema = true),
+            @ApiResponse(responseCode = "409", description = "예약된 시간은 삭제할 수 없습니다.",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    })
+    public RoomEscapeApiResponse<Void> removeTime(
+            @NotNull(message = "timeId는 null 또는 공백일 수 없습니다.") @PathVariable @Parameter(description = "삭제하고자 하는 시간의 ID값") Long id
     ) {
         reservationTimeService.removeTimeById(id);
 
-        return ApiResponse.success();
+        return RoomEscapeApiResponse.success();
     }
 
+    @LoginRequired
     @GetMapping("/times/filter")
     @ResponseStatus(HttpStatus.OK)
-    public ApiResponse<ReservationTimeInfosResponse> findAllAvailableReservationTimes(
-            @NotNull(message = "날짜는 null일 수 없습니다.") @RequestParam LocalDate date,
-            @NotNull(message = "themeId는 null일 수 없습니다.") @RequestParam Long themeId) {
-        return ApiResponse.success(reservationTimeService.findAllAvailableTimesByDateAndTheme(date, themeId));
+    @Operation(summary = "예약 가능한 시간 조회", description = "특정 날짜와 테마에 대한 예약 가능한 시간을 조회합니다.", tags = "로그인이 필요한 API")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "성공", useReturnTypeSchema = true)
+    })
+    public RoomEscapeApiResponse<ReservationTimeInfosResponse> findAllAvailableReservationTimes(
+            @NotNull(message = "날짜는 null일 수 없습니다.")
+            @RequestParam
+            @Parameter(description = "yyyy-MM-dd 형식으로 입력해주세요.", example = "2024-06-10")
+            LocalDate date,
+            @NotNull(message = "themeId는 null일 수 없습니다.")
+            @RequestParam
+            @Parameter(description = "조회할 테마의 ID를 입력해주세요.", example = "1")
+            Long themeId
+    ) {
+        return RoomEscapeApiResponse.success(reservationTimeService.findAllAvailableTimesByDateAndTheme(date, themeId));
     }
 }
