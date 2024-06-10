28711f373da5a7251d2c79b06ad2ae5d07345565
ikjo39 <chi4321@naver.com>
2024-06-10 14:44:12 +0900

    fix: 유저 로그인 인증 필터 추가

--- src/main/java/roomescape/config/WebConfig.java
+++ src/main/java/roomescape/config/WebConfig.java
@@ -27,19 +27,19 @@ public class WebConfig implements WebMvcConfigurer {
         resolvers.add(new AuthenticatedMemberArgumentResolver(authService));
     }
 
-//    @Override
-//    public void addInterceptors(final InterceptorRegistry registry) {
-//        registry.addInterceptor(new LoginCheckInterceptor(tokenProvider))
-//                .order(1)
-//                .addPathPatterns("/**")
-//                .excludePathPatterns(
-//                        "/css/**", "/js/**", "/images/**", "/fonts/**", "/*.ico",
-//                        "/image/default-profile.png", "/index.html",
-//                        "/", "/popular-themes", "/login", "/signup", "/members"
-//                );
-//
-//        registry.addInterceptor(new AdminCheckInterceptor(tokenProvider))
-//                .order(2)
-//                .addPathPatterns("/admin/**");
-//    }
+    @Override
+    public void addInterceptors(final InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginCheckInterceptor(tokenProvider))
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/css/**", "/js/**", "/images/**", "/fonts/**", "/*.ico",
+                        "/image/default-profile.png", "/index.html",
+                        "/", "/popular-themes", "/login", "/signup", "/members"
+                );
+
+        registry.addInterceptor(new AdminCheckInterceptor(tokenProvider))
+                .order(2)
+                .addPathPatterns("/admin/**");
+    }
 }
