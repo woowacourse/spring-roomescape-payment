26f384c4927b330b3d01a0e0207a4d1ac1d37b55
masonkimseoul <masonkimseoul@gmail.com>
2024-06-10 09:55:13 +0900

    feat: 결제 버튼 클릭 시 Toss 결제 모달 연결 기능 구현

--- src/main/resources/static/js/reservation-mine.js
+++ src/main/resources/static/js/reservation-mine.js
@@ -42,8 +42,7 @@ function render(data) {
 
         const payCell = row.insertCell(5);
         const payButton = document.createElement('button');
-        payButton.className = 'btn btn-danger';
-
+        payButton.className = 'btn btn-primary';
 
         if (status !== '예약') {
             cancelButton.textContent = '대기 취소';
@@ -55,8 +54,7 @@ function render(data) {
             if (status === '결제대기') {
                 payButton.textContent = '결제';
                 payButton.onclick = function () {
-                    //requestDelete(item.reservationId).then(() => window.location.reload());
-                    //결제 메서드 연결
+                    showTossPaymentModal(item);
                 };
                 payCell.appendChild(payButton);
             }
@@ -76,6 +74,71 @@ function render(data) {
             row.insertCell(6).textContent = item.paymentKey;
             row.insertCell(7).textContent = item.amount;
         }
+    });
+}
+
+function showTossPaymentModal(item) {
+
+    $('#payment-modal').modal('show');
+
+    const paymentAmount = 20000;
+    const widgetClientKey = "test_gck_docs_Ovk5rk1EwkEbP0W43n07xlzm";
+    const paymentWidget = PaymentWidget(widgetClientKey, PaymentWidget.ANONYMOUS);
+
+    paymentWidget.renderPaymentMethods(
+        "#payment-method",
+        {value: paymentAmount},
+        {variantKey: "DEFAULT"}
+    );
+
+    document.getElementById('reserve-button').onclick = function () {
+        const generateRandomString = () => window.btoa(Math.random()).slice(0, 20);
+        const orderIdPrefix = "ROOMESCAPE_";
+        paymentWidget.requestPayment({
+            orderId: orderIdPrefix + generateRandomString(),
+            orderName: "테스트 방탈출 예약 결제 1건",
+            amount: paymentAmount,
+        }).then(function (data) {
+            console.debug(data);
+            fetchReservationPayment(data, {
+                reservationId: item.reservationId
+            });
+        }).catch(function (error) {
+            alert(error.code + " :" + error.message + "/ orderId : " + error.orderId);
+        });
+    };
+}
+
+async function fetchReservationPayment(paymentData, reservationData) {
+    const reservationPaymentRequest = {
+        reservationId: reservationData.reservationId,
+        paymentKey: paymentData.paymentKey,
+        orderId: paymentData.orderId,
+        amount: paymentData.amount,
+        paymentType: paymentData.paymentType,
+    }
+
+    const reservationURL = "/reservations/payment";
+    fetch(reservationURL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(reservationPaymentRequest),
+    }).then(response => {
+        if (!response.ok) {
+            return response.json().then(errorBody => {
+                console.error("예약 결제 실패 : " + JSON.stringify(errorBody));
+                window.alert("예약 결제 실패 메시지 : " + errorBody.message);
+            });
+        } else {
+            response.json().then(successBody => {
+                console.log("예약 결제 성공 : " + JSON.stringify(successBody));
+                window.location.reload();
+            });
+        }
+    }).catch(error => {
+        console.error(error.message);
     });
 }
 
