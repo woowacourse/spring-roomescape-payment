f5b69ef593b610fdc2788a23b03a1aa2cddd43bd
unifolio0 <boho82ha@gmail.com>
2024-06-10 16:10:52 +0900

    refactor: AbstractExceptionHandler 삭제

--- src/main/java/roomescape/exception/GlobalExceptionHandler.java
+++ src/main/java/roomescape/exception/GlobalExceptionHandler.java
@@ -1,14 +1,19 @@
 package roomescape.exception;
 
 import java.time.format.DateTimeParseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import roomescape.RoomescapeApplication;
 import roomescape.dto.response.reservation.PaymentExceptionResponse;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler extends AbstractExceptionHandler {
+public class GlobalExceptionHandler {
+    private final Logger log = LoggerFactory.getLogger(RoomescapeApplication.class);
+
     @ExceptionHandler
     public ResponseEntity<RoomescapeExceptionResponse> roomescapeExceptionHandler(RoomescapeException exception) {
         logError(exception);
@@ -35,5 +40,9 @@ public class GlobalExceptionHandler extends AbstractExceptionHandler {
         logError(exception);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body("서버 내부에서 에러가 발생했습니다.");
+    }
+
+    private void logError(Exception exception) {
+        log.error("Error occur {}", exception.toString());
     }
 }
