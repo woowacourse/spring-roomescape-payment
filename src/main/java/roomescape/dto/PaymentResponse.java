dcb58af0501e5db2d80a58d661561a8b7d083d5f
J-I-H-O <jeongjiho0731@gmail.com>
2024-06-10 13:23:18 +0900

    feat: 결제 API 문서화
    Co-authored-by: J-I-H-O <jeongjiho0731@gmail.com>

--- src/main/java/roomescape/dto/PaymentResponse.java
+++ src/main/java/roomescape/dto/PaymentResponse.java
@@ -6,10 +6,9 @@ import roomescape.domain.Payment;
 public record PaymentResponse(
         long id,
         String paymentKey,
-        String orderId,
-        BigDecimal totalAmount
+        BigDecimal amount
 ) {
     public static PaymentResponse from(Payment payment) {
-        return new PaymentResponse(payment.getId(), payment.getPaymentKey(), payment.getOrderId(), payment.getAmount());
+        return new PaymentResponse(payment.getId(), payment.getPaymentKey(), payment.getAmount());
     }
 }
