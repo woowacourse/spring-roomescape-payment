3dda5afac114c6c971c68a853e8368169ada67b8
nhlee98 <e5slnh9876@gmail.com>
2024-06-10 18:39:47 +0900

    refactor(LoggingInterceptor): 예외에 대한 로깅은 GlobalExceptionHandler에서 처리하도록 변경

--- src/main/java/roomescape/advice/GlobalExceptionHandler.java
+++ src/main/java/roomescape/advice/GlobalExceptionHandler.java
@@ -1,6 +1,9 @@
 package roomescape.advice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,8 +17,12 @@ public class GlobalExceptionHandler {
     private static final String UNEXPECTED_EXCEPTION_ERROR_MESSAGE = "예상치 못한 예외가 발생했습니다. 관리자에게 문의하세요.";
     private static final String DATA_INTEGRITY_VIOLATION_EXCEPTION_ERROR_MESSAGE = "제약 조건에 어긋난 요청입니다.";
 
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
     @ExceptionHandler(RoomEscapeException.class)
     public ResponseEntity<ProblemDetail> handleRoomEscapeException(RoomEscapeException e) {
+        logging(e, HttpStatus.valueOf(e.getStatus()));
+
         return ResponseEntity.status(e.getStatus())
                 .body(e.getProblemDetail());
     }
@@ -24,6 +31,8 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ProblemDetail> handleNullPointerException(NullPointerException e) {
         ProblemDetail problemDetail = createProblemDetail(
                 NULL_POINTER_EXCEPTION_ERROR_MESSAGE, ExceptionTitle.ILLEGAL_USER_REQUEST);
+        logging(e, HttpStatus.valueOf(problemDetail.getStatus()));
+
         return ResponseEntity.status(problemDetail.getStatus())
                 .body(problemDetail);
     }
@@ -32,6 +41,8 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ProblemDetail> handleDataIntegrityViolationException(DataIntegrityViolationException e) {
         ProblemDetail problemDetail = createProblemDetail(
                 DATA_INTEGRITY_VIOLATION_EXCEPTION_ERROR_MESSAGE, ExceptionTitle.ILLEGAL_USER_REQUEST);
+        logging(e, HttpStatus.valueOf(problemDetail.getStatus()));
+
         return ResponseEntity.status(problemDetail.getStatus())
                 .body(problemDetail);
     }
@@ -40,6 +51,8 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ProblemDetail> handleUnexpectedException(Exception e) {
         ProblemDetail problemDetail = createProblemDetail(
                 UNEXPECTED_EXCEPTION_ERROR_MESSAGE, ExceptionTitle.INTERNAL_SERVER_ERROR);
+        logging(e, HttpStatus.valueOf(problemDetail.getStatus()));
+
         return ResponseEntity.status(problemDetail.getStatus())
                 .body(problemDetail);
     }
@@ -49,5 +62,13 @@ public class GlobalExceptionHandler {
         problemDetail.setTitle(title.getTitle());
 
         return problemDetail;
+    }
+
+    private void logging(Exception e, HttpStatus status) {
+        if (status.is5xxServerError()) {
+            logger.error(e.getMessage(), e);
+            return;
+        }
+        logger.warn(e.getMessage(), e);
     }
 }
--- src/main/java/roomescape/config/handler/LoggingInterceptor.java
+++ src/main/java/roomescape/config/handler/LoggingInterceptor.java
@@ -4,11 +4,13 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.mvc.ParameterizableViewController;
 import org.springframework.web.servlet.resource.ResourceHttpRequestHandler;
 import org.springframework.web.util.ContentCachingRequestWrapper;
 import org.springframework.web.util.ContentCachingResponseWrapper;
@@ -19,12 +21,17 @@ public class LoggingInterceptor implements HandlerInterceptor {
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
     @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception e) throws Exception {
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception e) {
         ContentCachingRequestWrapper cachingRequest = (ContentCachingRequestWrapper) request;
         ContentCachingResponseWrapper cachingResponse = (ContentCachingResponseWrapper) response;
 
-        if (e != null) {
-            logError(cachingRequest, cachingResponse, e);
+        if (HttpStatus.valueOf(response.getStatus()).is5xxServerError()) {
+            logError(cachingRequest, cachingResponse);
+            return;
+        }
+
+        if (HttpStatus.valueOf(response.getStatus()).is4xxClientError()) {
+            logWarn(cachingRequest, cachingResponse);
             return;
         }
 
@@ -35,8 +42,20 @@ public class LoggingInterceptor implements HandlerInterceptor {
         logInfo(cachingRequest, cachingResponse);
     }
 
+    private void logError(ContentCachingRequestWrapper request, ContentCachingResponseWrapper response) {
+        logger.error("Request {} {} {}",
+                request.getMethod(), request.getRequestURI(), new String(request.getContentAsByteArray()));
+        logger.error("Response {} {}", response.getStatus(), new String(response.getContentAsByteArray()));
+    }
+
+    private void logWarn(ContentCachingRequestWrapper request, ContentCachingResponseWrapper response) {
+        logger.warn("Request {} {} {}",
+                request.getMethod(), request.getRequestURI(), new String(request.getContentAsByteArray()));
+        logger.warn("Response {} {}", response.getStatus(), new String(response.getContentAsByteArray()));
+    }
+
     private boolean isStaticHandler(Object handler) {
-        if (handler instanceof ResourceHttpRequestHandler) {
+        if (handler instanceof ResourceHttpRequestHandler || handler instanceof ParameterizableViewController) {
             return true;
         }
 
@@ -54,13 +73,6 @@ public class LoggingInterceptor implements HandlerInterceptor {
 
     private boolean hasNotResponseBodyAnnotation(HandlerMethod handlerMethod) {
         return !handlerMethod.hasMethodAnnotation(ResponseBody.class);
-    }
-
-    private void logError(ContentCachingRequestWrapper request, ContentCachingResponseWrapper response, Exception e) {
-        logger.error(e.getMessage(), e);
-        logger.error("Request {} {} {}",
-                request.getMethod(), request.getRequestURI(), new String(request.getContentAsByteArray()));
-        logger.error("Response {} {}", response.getStatus(), response.getContentAsByteArray());
     }
 
     private void logInfo(ContentCachingRequestWrapper request, ContentCachingResponseWrapper response) {
