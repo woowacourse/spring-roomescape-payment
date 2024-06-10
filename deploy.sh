067f2af7c36f13ccac42a22d5816c106adb46401
libienz <leekh10195@gmail.com>
2024-06-10 21:34:55 +0900

    fix: 배포 스크립트에 기존에 운용되고 있는 어플리케이션 종료 로직 추가

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,15 @@ echo "> 프로젝트 Build 시작"
 echo "> JAR 위치로 이동"
 cd build/libs
 
+echo "> 현재 실행 중인 프로세스 확인"
+EXISTING_PROCESS_ID=$(lsof -ti tcp:8080)
+
+if [ -n "$EXISTING_PROCESS_ID" ]; then
+    echo "> 기존에 8080 포트를 사용 중인 프로세스를 종료합니다."
+    kill -15 $EXISTING_PROCESS_ID
+    sleep 5
+fi
+
 echo "> 애플리케이션 실행"
 nohup java -jar spring-roomescape-payment-0.0.1-SNAPSHOT.jar &
 
