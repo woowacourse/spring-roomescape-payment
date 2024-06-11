9d4c3e372c7ed330a1f22f39dfd91ad9a63caae2
nayonsoso <panda0329@naver.com>
2024-06-11 22:32:08 +0900

    refactor: 예약 시간과 관련된 API 허용 범위 수정
    - 예약 시간 생성, 삭제는 관리자만 가능

--- src/main/java/roomescape/reservationtime/controller/ReservationTimeController.java
+++ src/main/java/roomescape/reservationtime/controller/ReservationTimeController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import roomescape.reservationtime.dto.ReservationTimeRequest;
 import roomescape.reservationtime.dto.ReservationTimeResponse;
@@ -16,7 +15,6 @@ import roomescape.reservationtime.service.ReservationTimeService;
 import java.util.List;
 
 @RestController
-@RequestMapping("/times")
 public class ReservationTimeController implements ReservationTimeControllerSwagger {
 
     private final ReservationTimeService reservationTimeService;
@@ -26,7 +24,7 @@ public class ReservationTimeController implements ReservationTimeControllerSwagg
     }
 
     @Override
-    @PostMapping
+    @PostMapping("/admin/times")
     public ResponseEntity<ReservationTimeResponse> reservationTimeSave(
             @RequestBody ReservationTimeRequest reservationTimeRequest) {
         return ResponseEntity
@@ -35,13 +33,13 @@ public class ReservationTimeController implements ReservationTimeControllerSwagg
     }
 
     @Override
-    @GetMapping
+    @GetMapping("/times")
     public List<ReservationTimeResponse> reservationTimesList() {
         return reservationTimeService.findReservationTimes();
     }
 
     @Override
-    @DeleteMapping("/{reservationTimeId}")
+    @DeleteMapping("/admin/times/{reservationTimeId}")
     public ResponseEntity<Void> reservationTimeRemove(@PathVariable long reservationTimeId) {
         reservationTimeService.removeReservationTime(reservationTimeId);
         return ResponseEntity.noContent().build();
