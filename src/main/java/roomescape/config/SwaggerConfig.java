758c889c8ee9e0fc98cbd043498dccf51fc209a7
HyungUk Ryu <hw0603@naver.com>
2024-06-11 13:49:58 +0900

    refactor(SwaggerConfig): API 버전 정보를 yml에서 가져오도록 변경

--- src/main/java/roomescape/config/SwaggerConfig.java
+++ src/main/java/roomescape/config/SwaggerConfig.java
@@ -7,12 +7,16 @@ import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.security.SecurityScheme.In;
 import io.swagger.v3.oas.models.security.SecurityScheme.Type;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import roomescape.auth.AuthConstants;
 
 @Configuration
 public class SwaggerConfig {
+    @Value("${apiVersion}")
+    private static String apiVersion;
+
     @Bean
     public OpenAPI openAPI() {
         Components cookieAuthComponent = new Components()
@@ -34,6 +38,6 @@ public class SwaggerConfig {
         return new Info()
                 .title("RoomEscape API")
                 .description("방탈출 예약 서비스 API 입니다.")
-                .version("0.0.1");
+                .version(apiVersion);
     }
 }
