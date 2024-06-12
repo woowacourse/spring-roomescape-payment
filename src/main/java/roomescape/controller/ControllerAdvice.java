181319de28dce213db8b23d995c3425cded94e5a
takoyakimchi <c4nyou@gmail.com>
2024-06-12 14:18:18 +0900

    feat: exception 발생 시 로그 출력

--- src/main/java/roomescape/controller/ControllerAdvice.java
+++ src/main/java/roomescape/controller/ControllerAdvice.java
@@ -1,5 +1,6 @@
 package roomescape.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -13,36 +14,41 @@ import roomescape.global.exception.PaymentException;
 import roomescape.global.exception.RoomescapeException;
 
 @RestControllerAdvice
+@Slf4j
 public class ControllerAdvice {
 
     @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<ErrorMessageResponse> handleRuntimeException() {
+    public ResponseEntity<ErrorMessageResponse> handleRuntimeException(RuntimeException e) {
+        log.error("Unexpected error occured: ", e);
         ErrorMessageResponse errorMessageResponse = new ErrorMessageResponse("서버 내부 에러가 발생하였습니다.");
         return ResponseEntity.internalServerError().body(errorMessageResponse);
     }
 
     @ExceptionHandler(RoomescapeException.class)
     public ResponseEntity<ErrorMessageResponse> handleRoomescapeException(RoomescapeException e) {
+        log.warn("Exception(normal): {}", e.getMessage());
         ErrorMessageResponse response = new ErrorMessageResponse(e.getMessage());
         return ResponseEntity.badRequest().body(response);
     }
 
     @ExceptionHandler(PaymentException.class)
     public ResponseEntity<ErrorMessageResponse> handlePaymentException(PaymentException e) {
+        log.warn("Exception(pay): {}", e.getMessage());
         ErrorMessageResponse response = new ErrorMessageResponse(e.getMessage());
         return ResponseEntity.badRequest().body(response);
     }
 
     @ExceptionHandler(AuthorizationException.class)
     public ResponseEntity<ErrorMessageResponse> handleAuthorizationException(AuthorizationException e) {
+        log.warn("Exception(auth): {}", e.getMessage());
         ErrorMessageResponse response = new ErrorMessageResponse(e.getMessage());
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorMessageResponse> handleMethodArgumentNotValidException(
-        MethodArgumentNotValidException e) {
-
+        MethodArgumentNotValidException e
+    ) {
         BindingResult bindingResult = e.getBindingResult();
 
         StringBuilder builder = new StringBuilder();
@@ -53,6 +59,7 @@ public class ControllerAdvice {
             builder.append(fieldError.getDefaultMessage());
         }
 
+        log.warn("Exception(arg): {}", e.getMessage());
         ErrorMessageResponse response = new ErrorMessageResponse(builder.toString());
         return ResponseEntity.badRequest().body(response);
     }
