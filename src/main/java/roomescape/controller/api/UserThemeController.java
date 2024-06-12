96c4d8f71fed2fa9eb925f1f74295978ee3c7ac4
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-06-12 17:30:54 +0900

    refactor(UserThemeApiDocs): 문서에 필요한 annotation interface로 구현

--- src/main/java/roomescape/controller/api/UserThemeController.java
+++ src/main/java/roomescape/controller/api/UserThemeController.java
@@ -7,29 +7,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
+import roomescape.controller.api.docs.UserThemeApiDocs;
 import roomescape.controller.dto.response.ThemeResponse;
 import roomescape.service.ThemeService;
 
-@Tag(name = "UserTheme", description = "사용자도 확인 가능한 방탈출 테마 관련 API")
 @RestController
 @RequestMapping("/themes")
-public class UserThemeController {
+public class UserThemeController implements UserThemeApiDocs {
     private final ThemeService themeService;
 
     public UserThemeController(ThemeService themeService) {
         this.themeService = themeService;
     }
 
-    @Operation(summary = "모든 테마 조회", description = "모든 방탈출 테마를 조회할 수 있다.")
     @GetMapping
     public ResponseEntity<List<ThemeResponse>> findAll() {
         List<ThemeResponse> response = themeService.findAll();
         return ResponseEntity.ok().body(response);
     }
 
-    @Operation(summary = "인기 테마 조회", description = "최근 일주일 간 인기 있었던 테마 10개를 조회할 수 있다.")
     @GetMapping("/trending")
     public ResponseEntity<List<ThemeResponse>> findPopular() {
         List<ThemeResponse> response = themeService.findPopular();
---
+++ src/main/java/roomescape/controller/api/docs/UserThemeApiDocs.java
@@ -0,0 +1,45 @@
+package roomescape.controller.api.docs;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import roomescape.controller.dto.response.ErrorMessageResponse;
+import roomescape.controller.dto.response.ThemeResponse;
+
+@Tag(name = "UserTheme", description = "사용자도 확인 가능한 방탈출 테마 관련 API")
+public interface UserThemeApiDocs {
+    @Operation(summary = "모든 테마 조회", description = "모든 방탈출 테마를 조회할 수 있다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "조회 성공",
+                    content = {@Content(
+                            mediaType = "application/json", schema = @Schema(implementation = ThemeResponse.class))}),
+            @ApiResponse(responseCode = "400", description = "요청 정보 문제로 실패",
+                    content = {@Content(
+                            mediaType = "application/json", schema = @Schema(implementation = ErrorMessageResponse.class))}),
+            @ApiResponse(responseCode = "500", description = "서버 내부 문제로 실패",
+                    content = {@Content(
+                            mediaType = "application/json", schema = @Schema(implementation = ErrorMessageResponse.class))}),
+    })
+    ResponseEntity<List<ThemeResponse>> findAll();
+
+    @Operation(summary = "인기 테마 조회", description = "최근 일주일 간 인기 있었던 테마 10개를 조회할 수 있다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "조회 성공",
+                    content = {@Content(
+                            mediaType = "application/json", schema = @Schema(implementation = ThemeResponse.class))}),
+            @ApiResponse(responseCode = "400", description = "요청 정보 문제로 실패",
+                    content = {@Content(
+                            mediaType = "application/json", schema = @Schema(implementation = ErrorMessageResponse.class))}),
+            @ApiResponse(responseCode = "500", description = "서버 내부 문제로 실패",
+                    content = {@Content(
+                            mediaType = "application/json", schema = @Schema(implementation = ErrorMessageResponse.class))}),
+    })
+    ResponseEntity<List<ThemeResponse>> findPopular();
+}
