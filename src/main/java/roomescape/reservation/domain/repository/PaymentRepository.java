9dd60f761f4362b9042d5f749c2a9ff23b5235e9
HoeSeong123 <ghltjd3@gmail.com>
2024-06-12 14:47:22 +0900

    refactor : 불필요한 라인 제거

--- src/main/java/roomescape/reservation/domain/repository/PaymentRepository.java
+++ src/main/java/roomescape/reservation/domain/repository/PaymentRepository.java
@@ -4,6 +4,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import roomescape.reservation.domain.Payment;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
-
-
 }
