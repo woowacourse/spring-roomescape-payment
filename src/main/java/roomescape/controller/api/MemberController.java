34f66b03618a6ffa23d19f4f73945d705fdd7a56
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-06-12 17:25:50 +0900

    refactor(MemberApiDocs): 문서에 필요한 annotation interface로 구현

--- src/main/java/roomescape/controller/api/MemberController.java
+++ src/main/java/roomescape/controller/api/MemberController.java
@@ -7,22 +7,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
+import roomescape.controller.api.docs.MemberApiDocs;
 import roomescape.controller.dto.response.MemberResponse;
 import roomescape.service.MemberService;
 
-@Tag(name = "Member", description = "사용자 관련 API")
 @RestController
 @RequestMapping("/members")
-public class MemberController {
+public class MemberController implements MemberApiDocs {
     private final MemberService memberService;
 
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
 
-    @Operation(summary = "모든 사용자 조회", description = "모든 사용자 정보를 조회할 수 있다.")
     @GetMapping
     public ResponseEntity<List<MemberResponse>> findAll() {
         List<MemberResponse> response = memberService.findAll();
