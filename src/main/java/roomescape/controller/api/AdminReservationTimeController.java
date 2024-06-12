2162c5d19b9a736a1ac56f7e4f436f22ac3addf5
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-06-12 17:02:08 +0900

    refactor(AdminReservationTimeApiDocs): 문서에 필요한 annotation interface로 구현

--- src/main/java/roomescape/controller/api/AdminReservationTimeController.java
+++ src/main/java/roomescape/controller/api/AdminReservationTimeController.java
@@ -14,30 +14,26 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
+import roomescape.controller.api.docs.AdminReservationTimeApiDocs;
 import roomescape.controller.dto.request.CreateTimeRequest;
 import roomescape.controller.dto.response.TimeResponse;
 import roomescape.service.ReservationTimeService;
 
-@Tag(name = "AdminReservationTime", description = "관리자만 접근할 수 있는 방탈출 예약 가능 시간 관련 API")
 @RestController
 @RequestMapping("/admin/times")
-public class AdminReservationTimeController {
+public class AdminReservationTimeController implements AdminReservationTimeApiDocs {
     private final ReservationTimeService reservationTimeService;
 
     public AdminReservationTimeController(ReservationTimeService reservationTimeService) {
         this.reservationTimeService = reservationTimeService;
     }
 
-    @Operation(summary = "모든 시간 조회", description = "모든 예약 가능 시간을 조회할 수 있다.")
     @GetMapping
     public ResponseEntity<List<TimeResponse>> findAll() {
         List<TimeResponse> response = reservationTimeService.findAll();
         return ResponseEntity.ok(response);
     }
 
-    @Operation(summary = "시간 생성", description = "예약 가능 시간을 생성할 수 있다.")
     @PostMapping
     public ResponseEntity<TimeResponse> save(@Valid @RequestBody CreateTimeRequest request) {
         TimeResponse response = reservationTimeService.save(request.startAt());
@@ -45,7 +41,6 @@ public class AdminReservationTimeController {
                 .body(response);
     }
 
-    @Operation(summary = "시간 삭제", description = "예약 가능 시간을 삭제할 수 있다.")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         reservationTimeService.delete(id);
