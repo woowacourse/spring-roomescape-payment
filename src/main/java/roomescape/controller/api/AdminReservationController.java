404d5562d0d09d1cf0a182c3d516242539fa3783
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-06-12 16:32:22 +0900

    feat(AdminReservationController): request, response 정보 연동 및 responseCode 가 나타내는 의미 설명 추가

--- src/main/java/roomescape/controller/api/AdminReservationController.java
+++ src/main/java/roomescape/controller/api/AdminReservationController.java
@@ -15,10 +15,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import roomescape.controller.dto.request.CreateReservationRequest;
 import roomescape.controller.dto.request.SearchReservationFilterRequest;
+import roomescape.controller.dto.response.ErrorMessageResponse;
 import roomescape.controller.dto.response.ReservationResponse;
+import roomescape.global.exception.RoomescapeException;
 import roomescape.service.AdminReservationService;
 
 @Tag(name = "AdminReservation", description = "관리자 예약 관련 API")
@@ -32,6 +38,14 @@ public class AdminReservationController {
     }
 
     @Operation(summary = "모든 예약 조회", description = "모든 사용자의 예약을 조회할 수 있다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "조회 성공",
+                    content = {@Content(
+                            mediaType = "application/json", schema = @Schema(implementation = ReservationResponse.class))}),
+            @ApiResponse(responseCode = "500", description = "서버 내부 문제로 실패",
+                    content = {@Content(
+                            mediaType = "application/json", schema = @Schema(implementation = ErrorMessageResponse.class))}),
+    })
     @GetMapping
     public ResponseEntity<List<ReservationResponse>> findAll() {
         List<ReservationResponse> response = adminReservationService.findAllReserved();
@@ -39,6 +53,17 @@ public class AdminReservationController {
     }
 
     @Operation(summary = "특정 예약 조회", description = "해당 기간에 해당 테마, 멤버인 예약들을 조회할 수 있다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "조회 성공",
+                    content = {@Content(
+                            mediaType = "application/json", schema = @Schema(implementation = ReservationResponse.class))}),
+            @ApiResponse(responseCode = "400", description = "요청 정보 문제로 실패",
+                    content = {@Content(
+                            mediaType = "application/json", schema = @Schema(implementation = ErrorMessageResponse.class))}),
+            @ApiResponse(responseCode = "500", description = "서버 내부 문제로 실패",
+                    content = {@Content(
+                            mediaType = "application/json", schema = @Schema(implementation = ErrorMessageResponse.class))}),
+    })
     @GetMapping("/search")
     public ResponseEntity<List<ReservationResponse>> find(SearchReservationFilterRequest request) {
         List<ReservationResponse> response = adminReservationService.findAllByFilter(request);
@@ -46,6 +71,14 @@ public class AdminReservationController {
     }
 
     @Operation(summary = "모든 예약 대기 조회", description = "모든 사용자의 예약 대기을 조회할 수 있다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "조회 성공",
+                    content = {@Content(
+                            mediaType = "application/json", schema = @Schema(implementation = ReservationResponse.class))}),
+            @ApiResponse(responseCode = "500", description = "서버 내부 문제로 실패",
+                    content = {@Content(
+                            mediaType = "application/json", schema = @Schema(implementation = ErrorMessageResponse.class))}),
+    })
     @GetMapping("/standby")
     public ResponseEntity<List<ReservationResponse>> findAllStandby() {
         List<ReservationResponse> response = adminReservationService.findAllStandby();
@@ -53,6 +86,17 @@ public class AdminReservationController {
     }
 
     @Operation(summary = "예약 생성", description = "예약을 생성할 수 있다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "생성 성공",
+                    content = {@Content(
+                            mediaType = "application/json", schema = @Schema(implementation = ReservationResponse.class))}),
+            @ApiResponse(responseCode = "400", description = "요청 정보 문제로 실패",
+                    content = {@Content(
+                            mediaType = "application/json", schema = @Schema(implementation = ErrorMessageResponse.class))}),
+            @ApiResponse(responseCode = "500", description = "서버 내부 문제로 실패",
+                    content = {@Content(
+                            mediaType = "application/json", schema = @Schema(implementation = ErrorMessageResponse.class))}),
+    })
     @PostMapping
     public ResponseEntity<ReservationResponse> save(@Valid @RequestBody CreateReservationRequest request) {
         ReservationResponse response = adminReservationService.reserve(request);
@@ -62,6 +106,16 @@ public class AdminReservationController {
     }
 
     @Operation(summary = "예약 삭제", description = "예약을 삭제할 수 있다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "삭제 성공",
+                    content = {@Content(mediaType = "application/json")}),
+            @ApiResponse(responseCode = "400", description = "요청 정보 문제로 실패",
+                    content = {@Content(
+                            mediaType = "application/json", schema = @Schema(implementation = ErrorMessageResponse.class))}),
+            @ApiResponse(responseCode = "500", description = "서버 내부 문제로 실패",
+                    content = {@Content(
+                            mediaType = "application/json", schema = @Schema(implementation = ErrorMessageResponse.class))}),
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         adminReservationService.deleteById(id);
@@ -69,6 +123,16 @@ public class AdminReservationController {
     }
 
     @Operation(summary = "예약 대기 삭제", description = "예약 대기를 삭제할 수 있다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "삭제 성공",
+                    content = {@Content(mediaType = "application/json")}),
+            @ApiResponse(responseCode = "400", description = "요청 정보 문제로 실패",
+                    content = {@Content(
+                            mediaType = "application/json", schema = @Schema(implementation = ErrorMessageResponse.class))}),
+            @ApiResponse(responseCode = "500", description = "서버 내부 문제로 실패",
+                    content = {@Content(
+                            mediaType = "application/json", schema = @Schema(implementation = ErrorMessageResponse.class))}),
+    })
     @DeleteMapping("/standby/{id}")
     public ResponseEntity<Void> deleteStandby(@PathVariable Long id) {
         adminReservationService.deleteStandby(id);
