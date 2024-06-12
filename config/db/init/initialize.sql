8eca32fd21f8e849977ea069b6b84874500e1141
fromitive <fromitive@gmail.com>
2024-06-12 13:49:47 +0900

    refactor: erd, sql 스키마 수정

--- config/db/init/initialize.sql
+++ config/db/init/initialize.sql
@@ -2,54 +2,64 @@ CREATE DATABASE roomescape DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE roomescape;
 
 CREATE TABLE member (
-        id BIGINT AUTO_INCREMENT,
-        email VARCHAR(255),
-        name VARCHAR(255),
-        password VARCHAR(255),
-        role enum ('MEMBER', 'ADMIN'),
-        PRIMARY KEY (id)
+                        id BIGINT AUTO_INCREMENT,
+                        email VARCHAR(255),
+                        name VARCHAR(255),
+                        password VARCHAR(255),
+                        role ENUM ('MEMBER', 'ADMIN'),
+                        PRIMARY KEY (id)
 );
 
 CREATE TABLE theme (
-       id BIGINT AUTO_INCREMENT,
-       description VARCHAR(255),
-       name VARCHAR(255),
-       thumbnail VARCHAR(255),
-       PRIMARY KEY (id)
+                       id BIGINT AUTO_INCREMENT,
+                       description VARCHAR(255),
+                       name VARCHAR(255),
+                       thumbnail VARCHAR(255),
+                       PRIMARY KEY (id)
 );
 
 CREATE TABLE reservation_time (
-      start_at TIME(6),
-      id BIGINT AUTO_INCREMENT,
-      PRIMARY KEY (id)
+                                  id BIGINT AUTO_INCREMENT,
+                                  start_at TIME,
+                                  PRIMARY KEY (id)
 );
 
 CREATE TABLE reservation (
-     date DATE,
-     id BIGINT AUTO_INCREMENT,
-     member_id BIGINT,
-     theme_id BIGINT,
-     time_id BIGINT,
-     total_amount BIGINT DEFAULT 0,
-     payment_key VARCHAR(255) DEFAULT '',
-     PRIMARY KEY (id),
-     UNIQUE (date, time_id, theme_id),
-     FOREIGN KEY (member_id) REFERENCES member(id),
-     FOREIGN KEY (theme_id) REFERENCES theme(id),
-     FOREIGN KEY (time_id) REFERENCES reservation_time(id)
+                             id BIGINT AUTO_INCREMENT,
+                             date DATE,
+                             member_id BIGINT,
+                             theme_id BIGINT,
+                             time_id BIGINT,
+                             PRIMARY KEY (id),
+                             UNIQUE (date, time_id, theme_id),
+                             FOREIGN KEY (member_id) REFERENCES member(id),
+                             FOREIGN KEY (theme_id) REFERENCES theme(id),
+                             FOREIGN KEY (time_id) REFERENCES reservation_time(id)
+);
+
+CREATE TABLE payment (
+                         id BIGINT AUTO_INCREMENT,
+                         payment_key VARCHAR(255),
+                         amount DECIMAL(15, 2),
+                         reservation_id BIGINT,
+                         PRIMARY KEY (id),
+                         FOREIGN KEY (reservation_id) REFERENCES reservation(id)
 );
 
 CREATE TABLE waiting (
-     date DATE,
-     id BIGINT AUTO_INCREMENT,
-     member_id BIGINT,
-     theme_id BIGINT,
-     time_id BIGINT,
-     PRIMARY KEY (id),
-     FOREIGN KEY (member_id) REFERENCES member(id),
-     FOREIGN KEY (theme_id) REFERENCES theme(id),
-     FOREIGN KEY (time_id) REFERENCES reservation_time(id)
+                         id BIGINT AUTO_INCREMENT,
+                         date DATE,
+                         member_id BIGINT,
+                         theme_id BIGINT,
+                         time_id BIGINT,
+                         PRIMARY KEY (id),
+                         FOREIGN KEY (member_id) REFERENCES member(id),
+                         FOREIGN KEY (theme_id) REFERENCES theme(id),
+                         FOREIGN KEY (time_id) REFERENCES reservation_time(id)
 );
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON roomescape.* TO 'user'@'%';
+FLUSH PRIVILEGES;
 
 INSERT INTO reservation_time (start_at) VALUES ('10:00');
 INSERT INTO reservation_time (start_at) VALUES ('11:00');
@@ -80,8 +90,6 @@ INSERT INTO reservation (date, time_id, theme_id, member_id) VALUES ('2024-05-08
 INSERT INTO reservation (date, time_id, theme_id, member_id) VALUES ('2024-05-09', 4, 2, 1);
 INSERT INTO reservation (date, time_id, theme_id, member_id) VALUES ('2024-05-09', 3, 2, 1);
 INSERT INTO reservation (date, time_id, theme_id, member_id) VALUES ('2024-05-09', 1, 3, 2);
-
-
 
 GRANT SELECT,INSERT,UPDATE,DELETE ON roomescape.* TO 'user'@'%';
 
