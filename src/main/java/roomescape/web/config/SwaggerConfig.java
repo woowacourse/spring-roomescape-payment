1441a41bd7527405899b79413350ed2f65d92fa5
KIMGYUTAE <kiimwf0819@gmail.com>
2024-06-12 17:16:42 +0900

    refactor: api 문서 버전 properties 설정 파일을 통해 관리하도록 변경

--- src/main/java/roomescape/web/config/SwaggerConfig.java
+++ src/main/java/roomescape/web/config/SwaggerConfig.java
@@ -2,11 +2,18 @@ package roomescape.web.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
+    private final String version;
+
+    public SwaggerConfig(@Value("${springdoc.openapi.docs.version}") String version) {
+        this.version = version;
+    }
 
     @Bean
     public OpenAPI openAPI() {
@@ -14,6 +21,6 @@ public class SwaggerConfig {
                 .info(new Info()
                         .title("방탈출 예약 API")
                         .description("방탈출 예약 및 대기를 하는 API 입니다.")
-                        .version("1.0.0"));
+                        .version(version));
     }
 }
