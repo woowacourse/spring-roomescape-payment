917ba1d3ffa483960925680fb0bb758201703d1b
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-06-12 17:11:39 +0900

    refactor(AdminThemeApiDocs): 문서에 필요한 annotation interface로 구현

--- src/main/java/roomescape/controller/api/AdminThemeController.java
+++ src/main/java/roomescape/controller/api/AdminThemeController.java
@@ -13,22 +13,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
+import roomescape.controller.api.docs.AdminThemeApiDocs;
 import roomescape.controller.dto.request.CreateThemeRequest;
 import roomescape.controller.dto.response.ThemeResponse;
 import roomescape.service.ThemeService;
 
-@Tag(name = "AdminTheme", description = "관리자만 접근할 수 있는 방탈출 테마 관련 API")
 @RestController
 @RequestMapping("/admin/themes")
-public class AdminThemeController {
+public class AdminThemeController implements AdminThemeApiDocs {
     private final ThemeService themeService;
 
     public AdminThemeController(ThemeService themeService) {
         this.themeService = themeService;
     }
 
-    @Operation(summary = "테마 생성", description = "테마를 생성할 수 있다.")
     @PostMapping
     public ResponseEntity<ThemeResponse> save(@Valid @RequestBody CreateThemeRequest request) {
         ThemeResponse response = themeService.save(request);
@@ -36,7 +34,6 @@ public class AdminThemeController {
                 .body(response);
     }
 
-    @Operation(summary = "테마 삭제", description = "테마를 삭제할 수 있다.")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         themeService.delete(id);
