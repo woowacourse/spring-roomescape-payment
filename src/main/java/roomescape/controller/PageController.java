aa407fd50d19fb30a472231520cba2af90557a99
SCY <helenason@naver.com>
2024-06-13 15:45:55 +0900

    fix(yml): 브라우저 페이지 파일 prefix 수정

--- src/main/java/roomescape/controller/PageController.java
+++ src/main/java/roomescape/controller/PageController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PageController {
 
-    @GetMapping("")
+    @GetMapping
     public String home() {
         return "index";
     }
