0ec6e8848de48ee14eadf814f4a048c44b0345e5
gyungchan Jo <namejgc@naver.com>
2024-06-13 19:46:46 +0900

    style: TODO 제거

--- src/main/resources/static/js/reservation-mine.js
+++ src/main/resources/static/js/reservation-mine.js
@@ -1,8 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-    /*
-    TODO: [2단계] 내 예약 목록 조회 기능
-          endpoint 설정
-     */
     fetch('/member/reservation') // 내 예약 목록 조회 API 호출
         .then(response => {
             if (response.status === 200) return response.json();
@@ -19,10 +15,6 @@ function render(data) {
     data.forEach(item => {
         const row = tableBody.insertRow();
 
-        /*
-        TODO: [2단계] 내 예약 목록 조회 기능
-              response 명세에 맞춰 값 설정
-         */
         const theme = item.theme;
         const date = item.date;
         const time = item.time;
@@ -33,9 +25,6 @@ function render(data) {
         row.insertCell(2).textContent = time;
         row.insertCell(3).textContent = status;
 
-        /*
-        TODO: [3단계] 예약 대기 기능 - 예약 대기 취소 기능 구현 후 활성화
-         */
         if (status !== '예약') { // 예약 대기 상태일 때 예약 대기 취소 버튼 추가하는 코드, 상태 값은 변경 가능
             const cancelCell = row.insertCell(4);
             const cancelButton = document.createElement('button');
@@ -46,11 +35,6 @@ function render(data) {
             };
             cancelCell.appendChild(cancelButton);
         } else { // 예약 완료 상태일 때
-            /*
-            TODO: [미션4 - 2단계] 내 예약 목록 조회 시,
-             예약 완료 상태일 때 결제 정보를 함께 보여주기
-             결제 정보 필드명은 자신의 response 에 맞게 변경하기
-            */
             row.insertCell(4).textContent = '';
             row.insertCell(5).textContent = item.paymentKey;
             row.insertCell(6).textContent = item.amount;
@@ -59,9 +43,6 @@ function render(data) {
 }
 
 function requestDeleteWaiting(id) {
-    /*
-    TODO: [3단계] 예약 대기 기능 - 예약 대기 취소 API 호출
-     */
     const endpoint = `/reservations-waiting/${id}`;
     return fetch(endpoint, {
         method: 'DELETE'
--- src/main/resources/static/js/waiting.js
+++ src/main/resources/static/js/waiting.js
@@ -1,8 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-    /*
-    TODO: [4단계] 예약 대기 관리 기능
-          예약 대기 목록 조회 endpoint 설정
-     */
     fetch('reservations-waiting') // 내 예약 목록 조회 API 호출
         .then(response => {
             if (response.status === 200) return response.json();
@@ -19,10 +15,6 @@ function render(data) {
     data.forEach(item => {
         const row = tableBody.insertRow();
 
-        /*
-        TODO: [4단계] 예약 대기 관리 기능
-              예약 대기 목록 조회 response 명세에 맞춰 값 설정
-         */
         const id = item.id;
         const waitingNumber = item.waitingNumber;
         const name = item.member;
@@ -39,10 +31,6 @@ function render(data) {
 
         const actionCell = row.insertCell(row.cells.length);
 
-        /*
-        TODO: [4단계] 예약 대기 관리 기능
-              예약 대기 승인/거절 버튼이 필요한 경우 활성화하여 사용
-         */
         // actionCell.appendChild(createActionButton('승인', 'btn-primary', approve));
         actionCell.appendChild(createActionButton('거절', 'btn-danger', deny));
     });
@@ -52,10 +40,6 @@ function approve(event) {
     const row = event.target.closest('tr');
     const id = row.cells[0].textContent;
 
-    /*
-    TODO: [4단계] 예약 대기 목록 관리 기능
-          예약 대기 승인 API 호출
-     */
     const endpoint = '' + id;
     return fetch(endpoint, {
         method: ''
@@ -69,10 +53,6 @@ function deny(event) {
     const row = event.target.closest('tr');
     const id = row.cells[0].textContent;
 
-    /*
-    TODO: [4단계] 예약 대기 목록 관리 기능
-          예약 대기 거절 API 호출
-     */
     const endpoint = `/admin/reservations-waiting/${id}`;
     return fetch(endpoint, {
         method: 'DELETE'
