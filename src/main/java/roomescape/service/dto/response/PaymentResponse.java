f5d44bbdd135d2a21568c17456aad047114fdc41
jinwoo22 <jenny010925@naver.com>
2024-06-13 16:24:41 +0900

    refactor: PaymentResponse 레코드로 변환

--- src/main/java/roomescape/service/dto/response/PaymentResponse.java
+++ src/main/java/roomescape/service/dto/response/PaymentResponse.java
@@ -5,34 +5,12 @@ import roomescape.domain.reservation.Payment;
 import roomescape.domain.reservation.Reservation;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class PaymentResponse {
-
-    private String paymentKey;
-    private String orderId;
-    private String totalAmount;
-
-    protected PaymentResponse() {
-    }
-
-    public PaymentResponse(String paymentKey, String orderId, String totalAmount) {
-        this.paymentKey = paymentKey;
-        this.orderId = orderId;
-        this.totalAmount = totalAmount;
-    }
-
-    public void setPaymentKey(String paymentKey) {
-        this.paymentKey = paymentKey;
-    }
-
-    public void setOrderId(String orderId) {
-        this.orderId = orderId;
-    }
-
-    public void setTotalAmount(String totalAmount) {
-        this.totalAmount = totalAmount;
-    }
-
+public record PaymentResponse(
+        String paymentKey,
+        String orderId,
+        Integer totalAmount
+) {
     public Payment toPayment(Reservation reservation) {
-        return new Payment(reservation, paymentKey, orderId, Integer.valueOf(totalAmount));
+        return new Payment(reservation, paymentKey, orderId, totalAmount);
     }
 }
