390f383c22c167b22c1b8f0e570e667a16a4f60c
Minjoo <m0522inju@gmail.com>
2024-06-14 09:54:36 +0900

    feat: 쉘 스크립트 수정

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,7 @@ if [ -z "$PROJECT_PATH" ] || [ -z "$JAR_DIR" ] || [ -z "$PROJECT_NAME" ]; then
 fi
 
 cd $PROJECT_PATH
+git reset --hard HEAD
 
 echo "$(date +"%Y-%m-%d %H:%M:%S"): Github에서 최신 코드를 가져옵니다."
 git pull origin $PROJECT_BRANCH || { echo "$(date +"%Y-%m-%d %H:%M:%S"): git pull에 실패했습니다."; exit 1; }
@@ -27,10 +28,10 @@ else
   echo "$(date +"%Y-%m-%d %H:%M:%S"): 현재 구동 중인 어플리케이션이 없습니다."
 fi
 
-JAR_NAME=$(ls -t /$PROJECT_PATH/$JAR_DIR/*.jar | head -n 1)
+JAR_NAME=$(ls -t $PROJECT_PATH/$JAR_DIR/*.jar | head -n 1)
 if [ -n "$JAR_NAME" ]; then
   echo "$(date +"%Y-%m-%d %H:%M:%S"): 어플리케이션을 실행합니다. JAR 파일: $JAR_NAME"
-  nohup java -jar /$PROJECT_PATH/$JAR_DIR/$JAR_NAME >> $PROJECT_PATH/nohup.out 2>&1 &
+  nohup java -jar $PROJECT_PATH/$JAR_DIR/$JAR_NAME >> $PROJECT_PATH/nohup.out 2>&1 &
   echo "$(date +"%Y-%m-%d %H:%M:%S"): 어플리케이션 실행이 완료되었습니다."
 else
   echo "$(date +"%Y-%m-%d %H:%M:%S"): 실행할 JAR 파일을 찾을 수 없습니다."
