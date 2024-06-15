1283a1a7d7e0ff414f460d25e7369506a86166ed
Seokmyung Ham <gkatjraud1@gmail.com>
2024-06-15 23:54:07 +0900

    refactor: @ParameterizedTest로 결제 금액 음수 값 테스트 개선

--- src/test/java/roomescape/domain/payment/PaymentTest.java
+++ src/test/java/roomescape/domain/payment/PaymentTest.java
@@ -7,6 +7,8 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class PaymentTest {
 
@@ -19,13 +21,14 @@ class PaymentTest {
                 .isThrownBy(() -> new Payment(BigDecimal.valueOf(0.1), "paymentKey", "orderId", dateTime, dateTime));
     }
 
+    @ParameterizedTest
     @DisplayName("결제 금액이 0이하면 예외를 발생시킨다.")
-    @Test
-    void throw_exception_when_amount_less_than_0() {
+    @ValueSource(doubles = {-1000, -0.1, 0})
+    void throw_exception_when_amount_less_than_0(double amount) {
         String dateTime = LocalDateTime.now().toString();
 
         assertThatThrownBy(
-                () -> new Payment(BigDecimal.valueOf(0), "paymentKey", "orderId", dateTime, dateTime)
+                () -> new Payment(BigDecimal.valueOf(amount), "paymentKey", "orderId", dateTime, dateTime)
         );
     }
 }
