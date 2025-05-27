81d4d64cb02e2ef99a075ef2701bd8682663b332
Firedrago95 <yonghwa95@naver.com>
2025-05-27 21:48:30 +0900

    feat: 예약 결제 요청 예외처리 로직 추가
    co-authored-by: yeonnhuu <yeonnhuu@gmail.com>

---
+++ src/main/java/roomescape/reservation/exception/PaymentRequestException.java
@@ -0,0 +1,7 @@
+package roomescape.reservation.exception;
+
+public class PaymentRequestException extends RuntimeException {
+    public PaymentRequestException(String message) {
+        super(message);
+    }
+}
--- src/main/java/roomescape/reservation/service/PaymentService.java
+++ src/main/java/roomescape/reservation/service/PaymentService.java
@@ -1,11 +1,14 @@
 package roomescape.reservation.service;
 
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
+import roomescape.reservation.exception.PaymentRequestException;
 import roomescape.reservation.presentation.dto.PaymentRequest;
 import roomescape.reservation.presentation.dto.PaymentResponse;
 
@@ -29,6 +32,10 @@ public class PaymentService {
                 .header("Authorization", "Basic " + encodeSecretKey())
                 .body(paymentRequest)
                 .retrieve()
+                .onStatus(status -> status.is4xxClientError() || status.is5xxServerError(),
+                    (request, response) -> {
+                        throw new PaymentRequestException(response.getStatusCode() + "");
+                })
                 .body(PaymentResponse.class);
     }
 
