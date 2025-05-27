0dc4c3983dca734a536e9d1c5ff53d0d61d9ee43
Chaeyoung714 <202102760@hufs.ac.kr>
2025-05-27 17:23:48 +0900

    feat: 결제 승인 관련 예외 객체 추가

---
+++ src/main/java/roomescape/exception/PaymentConfirmServerException.java
@@ -0,0 +1,8 @@
+package roomescape.exception;
+
+public class PaymentConfirmServerException extends RuntimeException {
+
+    public PaymentConfirmServerException(String message) {
+        super(message);
+    }
+}
