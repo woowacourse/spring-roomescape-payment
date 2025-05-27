3fe28e0ead037599c35c8170a86f50718305fbf3
eoehd1ek <eoehd2ek@gmail.com>
2025-05-27 17:13:44 +0900

    feat(ReservationRequest): 결제 정보 추가

--- src/main/java/roomescape/reservation/dto/request/ReservationCreateRequest.java
+++ src/main/java/roomescape/reservation/dto/request/ReservationCreateRequest.java
@@ -6,6 +6,10 @@ import java.time.LocalDate;
 public record ReservationCreateRequest(
         @NotNull LocalDate date,
         @NotNull Long timeId,
-        @NotNull Long themeId
+        @NotNull Long themeId,
+        @NotNull String paymentKey,
+        @NotNull String orderId,
+        @NotNull Long amount,
+        @NotNull String paymentType
 ) {
 }
