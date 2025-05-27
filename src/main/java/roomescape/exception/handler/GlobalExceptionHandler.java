6a1db59d9be6b7ea50ab5e8f5f2e45a8e074ee21
ChoiDongjun <adong0808@naver.com>
2025-05-27 16:53:09 +0900

    feat: 예외 발생 시 로그 추가

--- src/main/java/roomescape/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/roomescape/exception/handler/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package roomescape.exception.handler;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -13,9 +15,12 @@ import roomescape.exception.dto.ErrorResponse;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler(BadRequestException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleBadRequestException(BadRequestException ex) {
+        log.error(ex.getMessage());
         return new ErrorResponse(
                 ex.getMessage(),
                 HttpStatus.BAD_REQUEST
@@ -25,6 +30,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(NotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handleNotFoundException(NotFoundException ex) {
+        log.error(ex.getMessage());
         return new ErrorResponse(
                 ex.getMessage(),
                 HttpStatus.NOT_FOUND
@@ -34,6 +40,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(UnauthorizationException.class)
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     public ErrorResponse handleAuthorizationException(UnauthorizationException ex) {
+        log.error(ex.getMessage());
         return new ErrorResponse(
                 ex.getMessage(),
                 HttpStatus.UNAUTHORIZED
@@ -43,6 +50,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(AccessDeniedException.class)
     @ResponseStatus(HttpStatus.FORBIDDEN)
     public ErrorResponse handleAccessDeniedException(AccessDeniedException ex) {
+        log.error(ex.getMessage());
         return new ErrorResponse(
                 ex.getMessage(),
                 HttpStatus.FORBIDDEN
@@ -52,6 +60,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse handleDefault(Exception ex) {
+        log.error(ex.getMessage());
         return new ErrorResponse(
                 ex.getMessage(),
                 HttpStatus.INTERNAL_SERVER_ERROR);
