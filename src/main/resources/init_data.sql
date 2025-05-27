fb8bc45684bce9cc65a98b1ce7985e04823de70e
PraiseBak <60121346+PraiseBak@users.noreply.github.com>
2025-05-27 16:50:03 +0900

    fix: data.sql과 불일치하던 문제 수정

--- src/main/resources/init_data.sql
+++ src/main/resources/init_data.sql
@@ -1,17 +1,17 @@
-INSERT INTO member(role, name, email, password, created_date, last_modified_date)
+INSERT INTO member(role, name, email, password, created_at, updated_at)
 VALUES ('GENERAL', 'member1', 'member1@email.com', 'qwer1234!', '2025-05-23 17:37:43.488281', '2025-05-23 17:37:43.488281'),
        ('GENERAL', 'member2', 'member2@email.com', 'qwer1234!', '2025-05-23 17:37:43.488281', '2025-05-23 17:37:43.488281'),
        ('GENERAL', 'member3', 'member3@email.com', 'qwer1234!', '2025-05-23 17:37:43.488281', '2025-05-23 17:37:43.488281'),
        ('GENERAL', 'member4', 'member4@email.com', 'qwer1234!', '2025-05-23 17:37:43.488281', '2025-05-23 17:37:43.488281'),
        ('ADMIN', 'admin', 'admin@email.com', 'qwer1234!', '2025-05-23 17:37:43.488281', '2025-05-23 17:37:43.488281');
 
-INSERT INTO reservation_time(start_at, created_date, last_modified_date)
+INSERT INTO reservation_time(start_at, created_at, updated_at)
 VALUES ('10:00', '2025-05-23 18:37:43.488281', '2025-05-23 18:37:43.488281'),
        ('11:00', '2025-05-23 18:37:43.488281', '2025-05-23 18:37:43.488281'),
        ('12:00', '2025-05-23 18:37:43.488281', '2025-05-23 18:37:43.488281'),
        ('13:00', '2025-05-23 18:37:43.488281', '2025-05-23 18:37:43.488281');
 
-INSERT INTO theme(name, description, thumbnail, created_date, last_modified_date)
+INSERT INTO theme(name, description, thumbnail, created_at, updated_at)
 VALUES ('Theme 1', '설명1',
         'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQqlkX2ISwyii-yHkQmp-Ad0hsfekERx2RNEa_RFNrr25BDWEAxHRgghcPid7ckxbLngE&usqp=CAU',
         '2025-05-23 18:37:43.488281', '2025-05-23 18:37:43.488281'),
@@ -25,7 +25,7 @@ VALUES ('Theme 1', '설명1',
         'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQqlkX2ISwyii-yHkQmp-Ad0hsfekERx2RNEa_RFNrr25BDWEAxHRgghcPid7ckxbLngE&usqp=CAU',
         '2025-05-23 18:37:43.488281', '2025-05-23 18:37:43.488281');
 
-INSERT INTO reservation(date, reservation_time_id, theme_id, member_id, created_date, last_modified_date)
+INSERT INTO reservation(date, reservation_time_id, theme_id, member_id, created_at, updated_at)
 VALUES
 ('2025-05-10', 1, 1, 1, '2025-05-23 19:37:43.488281', '2025-05-23 19:37:43.488281'),
 
