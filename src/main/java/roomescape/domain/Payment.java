d152eca7e61f5ab82b0889576e8dba739d02d047
CheChe903 <cheche903@naver.com>
2025-05-27 15:07:59 +0900

    feat(Payment): 결제 도메인 생성

---
+++ src/main/java/roomescape/domain/Payment.java
@@ -0,0 +1,44 @@
+package roomescape.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.util.Objects;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String paymentKey;
+
+    private String orderId;
+
+    public static Payment create(String paymentKey, String orderId) {
+        return new Payment(null, paymentKey, orderId);
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final Payment payment = (Payment) o;
+        return Objects.equals(id, payment.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
