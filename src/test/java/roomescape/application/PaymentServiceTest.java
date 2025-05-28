ef69a608ec21297b9f1850ccb5972c153735eaa6
CheChe903 <cheche903@naver.com>
2025-05-28 17:18:11 +0900

    test(PaymentService): 결졔 성공 및 실패 테스트 구현

--- src/test/java/roomescape/application/PaymentServiceTest.java
+++ src/test/java/roomescape/application/PaymentServiceTest.java
@@ -1,10 +1,14 @@
 package roomescape.application;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
@@ -12,10 +16,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import roomescape.application.dto.PaymentProcessRequest;
-import roomescape.infrastructure.thirdparty.PaymentRestClient;
+import roomescape.application.exception.PaymentException;
 import roomescape.domain.Payment;
 import roomescape.infrastructure.repository.PaymentRepository;
+import roomescape.infrastructure.thirdparty.PaymentRestClient;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 @ExtendWith(MockitoExtension.class)
@@ -27,20 +34,73 @@ public class PaymentServiceTest {
     @Mock
     private PaymentRepository paymentRepository;
 
+    @Mock
+    private ObjectMapper objectMapper;
+
     @InjectMocks
     private PaymentService paymentService;
 
     @Test
-    void 결제를_진행한다() {
+    void 결제를_진행한다() throws JsonProcessingException {
         PaymentProcessRequest paymentProcessRequest = new PaymentProcessRequest("test", "test", "1000");
         Payment payment = new Payment(null, "test", "test");
         Payment savedPayment = new Payment(1L, "test", "test");
+        String paymentString = """
+                {
+                    "paymentKey": "test",
+                    "orderId": "test"
+                }
+                """;
+        ResponseEntity<String> responseEntity = new ResponseEntity<>(paymentString, HttpStatus.OK);
 
-        when(paymentRestClient.getPayment(paymentProcessRequest)).thenReturn(payment);
+        JsonNode jsonNode = new ObjectMapper().readTree(paymentString);
+
+        when(objectMapper.readTree(paymentString)).thenReturn(jsonNode);
+        when(paymentRestClient.getPaymentResponse(paymentProcessRequest)).thenReturn(responseEntity);
         when(paymentRepository.save(payment)).thenReturn(savedPayment);
 
         Payment resultPayment = paymentService.process(paymentProcessRequest);
         assertThat(resultPayment).isEqualTo(savedPayment);
-        verify(paymentRestClient, times(1)).getPayment(paymentProcessRequest);
+        verify(paymentRestClient, times(1)).getPaymentResponse(paymentProcessRequest);
+    }
+
+    @Test
+    void 사용자_에러를_반환한다() throws JsonProcessingException {
+        PaymentProcessRequest paymentProcessRequest = new PaymentProcessRequest("test", "test", "1000");
+        String paymentString = """
+                {
+                    "code": "test",
+                    "message": "test"
+                }
+                """;
+        ResponseEntity<String> responseEntity = new ResponseEntity<>(paymentString, HttpStatus.BAD_REQUEST);
+
+        JsonNode jsonNode = new ObjectMapper().readTree(paymentString);
+
+        when(objectMapper.readTree(paymentString)).thenReturn(jsonNode);
+        when(paymentRestClient.getPaymentResponse(paymentProcessRequest)).thenReturn(responseEntity);
+
+        assertThatThrownBy(() -> paymentService.process(paymentProcessRequest))
+                .isInstanceOf(PaymentException.class);
+    }
+
+    @Test
+    void 서버_에러를_반환한다() throws JsonProcessingException {
+        PaymentProcessRequest paymentProcessRequest = new PaymentProcessRequest("test", "test", "1000");
+        String paymentString = """
+                {
+                    "code": "test",
+                    "message": "test"
+                }
+                """;
+        ResponseEntity<String> responseEntity = new ResponseEntity<>(paymentString, HttpStatus.INTERNAL_SERVER_ERROR);
+
+        JsonNode jsonNode = new ObjectMapper().readTree(paymentString);
+
+        when(objectMapper.readTree(paymentString)).thenReturn(jsonNode);
+        when(paymentRestClient.getPaymentResponse(paymentProcessRequest)).thenReturn(responseEntity);
+
+        assertThatThrownBy(() -> paymentService.process(paymentProcessRequest))
+                .isInstanceOf(PaymentException.class);
     }
 }
