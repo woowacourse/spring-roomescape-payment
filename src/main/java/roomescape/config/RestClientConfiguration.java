bf31bc5c36d0c68a63b144aadcf48e1fc88572c4
seaniiio <cnc4750@naver.com>
2025-05-28 21:38:23 +0900

    feat: RestClient timeout 설정

--- src/main/java/roomescape/config/RestClientConfiguration.java
+++ src/main/java/roomescape/config/RestClientConfiguration.java
@@ -2,6 +2,7 @@ package roomescape.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
 @Configuration
@@ -9,6 +10,15 @@ public class RestClientConfiguration {
 
     @Bean
     public RestClient restClient() {
-        return RestClient.builder().build();
+        return RestClient.builder()
+                .requestFactory(getSimpleClientHttpRequestFactory())
+                .build();
+    }
+
+    private SimpleClientHttpRequestFactory getSimpleClientHttpRequestFactory() {
+        var factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(3000);
+        factory.setReadTimeout(30000);
+        return factory;
     }
 }
--- src/main/java/roomescape/payment/TossPaymentClient.java
+++ src/main/java/roomescape/payment/TossPaymentClient.java
@@ -1,15 +1,18 @@
 package roomescape.payment;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestClient;
 import roomescape.exception.custom.reason.payment.PaymentException;
 import roomescape.payment.dto.PaymentConfirmRequest;
 
 import java.util.Base64;
 
+@Slf4j
 @Component
 @AllArgsConstructor
 public class TossPaymentClient implements PaymentClient {
@@ -23,14 +26,19 @@ public class TossPaymentClient implements PaymentClient {
 
     @Override
     public void confirm(final PaymentConfirmRequest request) {
-        ResponseEntity<Void> response = restClient.post()
-                .uri(URL_PREFIX + "/confirm")
-                .header("Authorization", "Basic " + ENCODED_SECRET_KEY)
-                .body(request)
-                .retrieve()
-                .onStatus(tossPaymentConfirmErrorHandler)
-                .toBodilessEntity();
-        if (response.getStatusCode() != HttpStatus.OK) {
+        try {
+            ResponseEntity<Void> response = restClient.post()
+                    .uri(URL_PREFIX + "/confirm")
+                    .header("Authorization", "Basic " + ENCODED_SECRET_KEY)
+                    .body(request)
+                    .retrieve()
+                    .onStatus(tossPaymentConfirmErrorHandler)
+                    .toBodilessEntity();
+            if (response.getStatusCode() != HttpStatus.OK) {
+                throw new PaymentException("결제 승인에 실패하였습니다.");
+            }
+        } catch (ResourceAccessException e) {
+            log.warn("리소스 접근 에러 ", e);
             throw new PaymentException("결제 승인에 실패하였습니다.");
         }
     }
