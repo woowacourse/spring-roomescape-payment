01ee9ceed6a436d6cf22cede445939145cb69284
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2025-05-28 20:19:10 +0900

    refactor: 결제 정보 임시 저장을 DB가 아닌 세션으로 로직 변경

--- src/main/java/roomescape/payment/domain/PrePaymentRepository.java
+++
@@ -1,9 +0,0 @@
-package roomescape.payment.domain;
-
-import java.util.Optional;
-
-public interface PrePaymentRepository {
-    Optional<PrePayment> findByOrderId(String orderId);
-
-    void save(PrePayment prePayment);
-}
--- src/main/java/roomescape/payment/infrastructure/exception/TossPaymentApprovalFailedException.java
+++
@@ -1,9 +0,0 @@
-package roomescape.payment.infrastructure.exception;
-
-import org.springframework.http.HttpStatus;
-
-public class TossPaymentApprovalFailedException extends TossException {
-    public TossPaymentApprovalFailedException(HttpStatus status, String message) {
-        super(status, message);
-    }
-}
