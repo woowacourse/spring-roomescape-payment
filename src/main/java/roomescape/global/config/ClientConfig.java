926949c215912d6f5b3f943c171c54b6d4c0a5a1
sonjh919 <kingjh1125@gmail.com>
2025-05-28 15:01:11 +0900

    refactor(payment): 외부 API 응답 지연에 대한 timeout 설정 추가

--- src/main/java/roomescape/global/config/ClientConfig.java
+++ src/main/java/roomescape/global/config/ClientConfig.java
@@ -2,6 +2,7 @@ package roomescape.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import roomescape.payment.application.service.PaymentClient;
 import roomescape.payment.infrastructure.TossPaymentClient;
@@ -11,6 +12,12 @@ public class ClientConfig {
 
     @Bean
     public PaymentClient paymentClient() {
-        return new TossPaymentClient(RestClient.builder().baseUrl("https://api.tosspayments.com/v1").build());
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+        factory.setConnectTimeout(10_000); // ms
+        factory.setReadTimeout(10_000);
+
+        return new TossPaymentClient(RestClient.builder()
+                .requestFactory(new HttpComponentsClientHttpRequestFactory())
+                .baseUrl("https://api.tosspayments.com/v1").build());
     }
 }
