750ceb5bb65d8784ce356812b51799dc380f9edf
Yehyeok Bang <qkddpgur318@gmail.com>
2025-05-28 14:54:12 +0900

    refactor: 결제 승인 기능 가독성 개선
    Co-authored-by: m-a-king <9gle3492@gmail.com>

--- src/main/java/roomescape/application/payment/TossPaymentClient.java
+++ src/main/java/roomescape/application/payment/TossPaymentClient.java
@@ -3,52 +3,72 @@ package roomescape.application.payment;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatusCode;
-import org.springframework.stereotype.Repository;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 import roomescape.application.payment.dto.PaymentCommand;
 import roomescape.infrastructure.error.exception.PaymentException;
 
-@Repository
+@Component
 public class TossPaymentClient {
 
-    private static final String SECRET_KEY = "test_gsk_docs_OaPz8L5KdmQXkzRz3y47BMw6" + ":";
+    private static final String TOSS_PAYMENT_SERVER_URL = "https://api.tosspayments.com/v1/payments";
+    private static final String SECRET_KEY = "test_gsk_docs_OaPz8L5KdmQXkzRz3y47BMw6:";
+    private static final String CONFIRM_URI = "/confirm";
+    private static final String AUTH_SCHEME = "Basic ";
+
     private static final Logger log = LoggerFactory.getLogger(TossPaymentClient.class);
 
     private final RestClient restClient;
+    private final ObjectMapper objectMapper;
 
-    public TossPaymentClient() {
+    public TossPaymentClient(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
         this.restClient = RestClient.builder()
-                .baseUrl("https://api.tosspayments.com/v1/payments")
+                .baseUrl(TOSS_PAYMENT_SERVER_URL)
                 .build();
     }
 
     public void approve(PaymentCommand command) {
-        String encodedKey = Base64.getEncoder().encodeToString(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
         restClient.post()
-                .uri("/confirm")
-                .header("Authorization", "Basic " + encodedKey)
+                .uri(CONFIRM_URI)
+                .header(HttpHeaders.AUTHORIZATION, createAuthorizationHeader())
                 .body(command)
                 .retrieve()
-                .onStatus(HttpStatusCode::is4xxClientError, (request, response) -> {
-                    ObjectMapper mapper = new ObjectMapper();
-                    try {
-                        JsonNode node = mapper.readTree(response.getBody());
-                        if (node.has("message")) {
-                            log.warn("code: {} message: {}", node.get("code").asText(), node.get("code").asText());
-                            throw new PaymentException(node.get("message").asText());
-                        }
-                    } catch (JsonProcessingException ignored) {
-                    }
-                    throw new PaymentException("페이먼츠 예외");
-                })
-                .onStatus(HttpStatusCode::is5xxServerError, (request, response) -> {
-                    throw new PaymentException("토스 서버 예외");
-                })
-                .body(String.class);
+                .onStatus(HttpStatusCode::is4xxClientError, this::handle4xxError)
+                .onStatus(HttpStatusCode::is5xxServerError, this::handle5xxError)
+                .toBodilessEntity();
+    }
+
+    private String createAuthorizationHeader() {
+        String encoded = Base64.getEncoder().encodeToString(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
+        return AUTH_SCHEME + encoded;
+    }
+
+    private void handle4xxError(HttpRequest httpRequest, ClientHttpResponse clientHttpResponse) {
+        try {
+            JsonNode node = objectMapper.readTree(clientHttpResponse.getBody());
+            String code = node.path("code").asText();
+            String message = node.path("message").asText("토스 결제 실패 관리자에게 문의하세요.");
+            log.warn("결제 승인 실패 - code: {}, message: {}", code, message);
+            throw new PaymentException(message);
+        } catch (JsonProcessingException e) {
+            log.warn("토스 페이먼트 응답 형식 에러", e);
+            throw new PaymentException("토스 결제 실패 관리자에게 문의하세요.");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void handle5xxError(HttpRequest httpRequest, ClientHttpResponse clientHttpResponse) {
+        throw new PaymentException("결제 서버 오류, 잠시 후 다시 시도해주세요.");
     }
 }
