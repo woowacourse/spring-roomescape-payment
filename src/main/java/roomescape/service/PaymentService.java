f26168e676b926e4548ab32d51d8756b12912f10
ppparkta <pppppark2@gmail.com>
2025-05-28 18:55:33 +0900

    feat: 결제 정보 생성 기능 추가
    Co-authored-by: goohong <goohong.chung@gmail.com>

--- src/main/java/roomescape/service/PaymentService.java
+++ src/main/java/roomescape/service/PaymentService.java
@@ -1,13 +1,31 @@
 package roomescape.service;
 
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.service.annotation.PostExchange;
-import roomescape.dto.payment.PaymentConfirmRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import roomescape.domain.Payment;
+import roomescape.domain.Reservation;
 import roomescape.dto.payment.PaymentConfirmResponse;
+import roomescape.exception.NotFoundException;
+import roomescape.repository.PaymentRepository;
+import roomescape.repository.ReservationRepository;
 
-public interface PaymentService {
-    @PostExchange("/payments/confirm")
-    PaymentConfirmResponse confirm(@RequestHeader("Authorization") String authorization,
-                                   @RequestBody PaymentConfirmRequest request);
+@Service
+public class PaymentService {
+
+    private final PaymentRepository paymentRepository;
+    private final ReservationRepository reservationRepository;
+
+    public PaymentService(PaymentRepository paymentRepository, ReservationRepository reservationRepository) {
+        this.paymentRepository = paymentRepository;
+        this.reservationRepository = reservationRepository;
+    }
+
+    @Transactional
+    public void createPayment(PaymentConfirmResponse paymentConfirm, Long reservationId) {
+        Reservation reservation = reservationRepository.findById(reservationId)
+                .orElseThrow(() -> new NotFoundException(""));
+        Payment payment = Payment.createWithoutId(paymentConfirm.orderId(), paymentConfirm.paymentKey(),
+                paymentConfirm.totalAmount(), reservation);
+        paymentRepository.save(payment);
+    }
 }
