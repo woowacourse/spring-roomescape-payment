356f41152419040741bd28cc6e8fca9f7da8e21c
황재웅 <wodnd0131@naver.com>
2025-05-28 13:51:37 +0900

    feat: 클라이언트 코드 수정

--- src/main/resources/static/js/user-reservation.js
+++ src/main/resources/static/js/user-reservation.js
@@ -184,7 +184,7 @@ function onReservationButtonClick(event, paymentWidget) {
         */
         // TOSS 결제 위젯 Javascript SDK 연동 방식 중 'Promise로 처리하기'를 적용함
         // https://docs.tosspayments.com/reference/widget-sdk#promise%EB%A1%9C-%EC%B2%98%EB%A6%AC%ED%95%98%EA%B8%B0
-        const orderIdPrefix = "LALA_";
+        const orderIdPrefix = "LALA-";
 
         paymentWidget.requestPayment({
             orderId: orderIdPrefix + generateRandomString(),
@@ -196,7 +196,7 @@ function onReservationButtonClick(event, paymentWidget) {
         }).catch(function (error) {
             // TOSS 에러 처리: 에러 목록을 확인하세요
             // https://docs.tosspayments.com/reference/error-codes#failurl 로-전달되는-에러
-            alert(error.code + " :" + error.message); //+ "/ orderId : " + err.orderId
+            alert(error.code + " :" + error.message);
         });
     } else {
         alert("Please select a date, theme, and time before making a reservation.");
@@ -232,11 +232,13 @@ async function fetchReservationPayment(paymentData, reservationData) {
         if (!response.ok) {
             return response.json().then(errorBody => {
                 console.error("예약 결제 실패 : " + JSON.stringify(errorBody));
-                window.alert("예약 결제 실패 메시지");
+                window.alert(errorBody.status + " " + JSON.stringify(errorBody.detail));
             });
         } else {
             response.json().then(successBody => {
                 console.log("예약 결제 성공 : " + JSON.stringify(successBody));
+                window.alert("예약 결제 성공 ");
+
                 window.location.reload();
             });
         }
