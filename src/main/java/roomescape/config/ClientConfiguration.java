a826d2a210009f21d96773a8a797ba45b98d17c5
jhan0121 <jhan0121@gmail.com>
2025-05-28 14:19:55 +0900

    feat: 결졔 API 연결 timeout 시간 설정 추가

--- src/main/java/roomescape/config/ClientConfiguration.java
+++ src/main/java/roomescape/config/ClientConfiguration.java
@@ -2,12 +2,21 @@ package roomescape.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
 @Configuration
 public class ClientConfiguration {
     @Bean
     public RestClient initializeRestClient() {
-        return RestClient.builder().baseUrl("https://api.tosspayments.com").build();
+
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+        factory.setConnectTimeout(5_000);
+        factory.setReadTimeout(10_000);
+
+        return RestClient.builder()
+                .baseUrl("https://api.tosspayments.com")
+                .requestFactory(factory)
+                .build();
     }
 }
