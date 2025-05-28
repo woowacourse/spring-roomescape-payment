59e7a4cfdaa4f4275af1bd10504d94a94240e445
byesol <bsj5967@naver.com>
2025-05-28 15:52:10 +0900

    refactor : 예약과 예약 대기 분리

--- src/main/java/roomescape/reservation/service/ReservationFacadeService.java
+++ src/main/java/roomescape/reservation/service/ReservationFacadeService.java
@@ -9,12 +9,14 @@ import org.springframework.stereotype.Service;
 import roomescape.global.auth.dto.UserInfo;
 import roomescape.member.domain.Member;
 import roomescape.member.service.MemberService;
+import roomescape.payment.service.PaymentService;
 import roomescape.reservation.domain.Reservation;
 import roomescape.reservation.domain.ReservationInfo;
 import roomescape.reservation.domain.Waiting;
 import roomescape.reservation.dto.response.MyReservationResponse;
 import roomescape.reservation.dto.response.ReservationResponse;
 import roomescape.reservation.dto.response.WaitingWithRank;
+import roomescape.reservation.exception.ReservationAlreadyExistsException;
 import roomescape.reservationtime.domain.ReservationTime;
 import roomescape.reservationtime.service.ReservationTimeService;
 import roomescape.theme.domain.Theme;
@@ -28,17 +30,20 @@ public class ReservationFacadeService {
     private final MemberService memberService;
     private final ThemeService themeService;
     private final ReservationTimeService reservationTimeService;
+    private final PaymentService paymentService;
 
     public ReservationFacadeService(final ReservationService reservationService,
                                     final WaitingService waitingService,
-                                    MemberService memberService,
-                                    ThemeService themeService,
-                                    ReservationTimeService reservationTimeService) {
+                                    final MemberService memberService,
+                                    final ThemeService themeService,
+                                    final ReservationTimeService reservationTimeService,
+                                    final PaymentService paymentService) {
         this.reservationService = reservationService;
         this.waitingService = waitingService;
         this.memberService = memberService;
         this.themeService = themeService;
         this.reservationTimeService = reservationTimeService;
+        this.paymentService = paymentService;
     }
 
     public List<MyReservationResponse> findMyReservations(final UserInfo userInfo) {
@@ -52,7 +57,7 @@ public class ReservationFacadeService {
 
     public ReservationResponse createForAdmin(final LocalDate date, final Long timeId, final Long themeId,
                                               final Long memberId) {
-        if (!reservationService.isReservationExists(date, timeId, themeId)) {
+        if (reservationService.isReservationExists(date, timeId, themeId)) {
             return createReservation(date, timeId, themeId, memberId);
         }
         return createWaiting(date, timeId, themeId, memberId);
@@ -62,10 +67,11 @@ public class ReservationFacadeService {
                                       final Long memberId, final String paymentKey, final String orderId,
                                       final Integer amount, final String paymentType) {
 
-        if (!reservationService.isReservationExists(date, timeId, themeId)) {
+        if (reservationService.isReservationExists(date, timeId, themeId)) {
+            paymentService.payment(paymentKey, orderId, amount, paymentType);
             return createReservation(date, timeId, themeId, memberId);
         }
-        return createWaiting(date, timeId, themeId, memberId);
+        throw new ReservationAlreadyExistsException("이미 예약이 존재합니다.");
     }
 
     private ReservationResponse createReservation(final LocalDate date, final Long timeId, final Long themeId,
@@ -81,8 +87,9 @@ public class ReservationFacadeService {
         return ReservationResponse.of(newReservation);
     }
 
-    private ReservationResponse createWaiting(final LocalDate date, final Long timeId, final Long themeId,
-                                              final Long memberId) {
+    public ReservationResponse createWaiting(final LocalDate date, final Long timeId, final Long themeId,
+                                             final Long memberId) {
+        validateAlreadyExistReservation(date, timeId, themeId);
         ReservationTime time = reservationTimeService.findReservationTime(timeId);
         Theme theme = themeService.findTheme(themeId);
         Member member = memberService.findUserByMemberId(memberId);
@@ -91,6 +98,12 @@ public class ReservationFacadeService {
         Waiting newWaiting = waitingService.save(
                 Waiting.createUpcomingReservationWithUnassignedId(member, turn + 1, reservationInfo));
         return ReservationResponse.of(newWaiting);
+    }
+
+    private void validateAlreadyExistReservation(final LocalDate date, final Long timeId, final Long themeId) {
+        if (reservationService.isReservationExists(date, timeId, themeId)) {
+            throw new ReservationAlreadyExistsException("이미 예약이 존재합니다.");
+        }
     }
 
     public void deleteReservation(final Long reservationId) {
