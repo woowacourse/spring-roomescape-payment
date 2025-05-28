7bfcc5da831e07806414091c4d5b8f1d1c8c4036
jeyong <joon6093@naver.com>
2025-05-28 20:44:33 +0900

    chore: 헤더에 대해 상수로 처리하도록 변경

--- src/main/java/roomescape/payment/application/TossPaymentGatewayClient.java
+++ src/main/java/roomescape/payment/application/TossPaymentGatewayClient.java
@@ -1,6 +1,9 @@
 package roomescape.payment.application;
 
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
@@ -20,14 +23,15 @@ public class TossPaymentGatewayClient {
     public TossPaymentGatewayClient() {
         this.restClient = RestClient.builder()
             .baseUrl(BASE_URL)
-            .defaultHeader("Authorization", encodeSecretKey("test_gsk_docs_OaPz8L5KdmQXkzRz3y47BMw6"))
-            .defaultHeader("Content-Type", "application/json")
+            .defaultHeader(AUTHORIZATION, encodeSecretKey("test_gsk_docs_OaPz8L5KdmQXkzRz3y47BMw6"))
+            .defaultHeader(CONTENT_TYPE, APPLICATION_JSON_VALUE)
             .build();
     }
 
     private String encodeSecretKey(final String secretKey) {
         final String raw = secretKey + ":";
-        final String encoded = Base64.getEncoder().encodeToString(raw.getBytes(StandardCharsets.UTF_8));
+        final String encoded = Base64.getEncoder()
+            .encodeToString(raw.getBytes(StandardCharsets.UTF_8));
         return "Basic " + encoded;
     }
 
