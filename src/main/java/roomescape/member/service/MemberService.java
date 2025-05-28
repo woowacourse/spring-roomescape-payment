72aa66ecc8e0f32196d302bea1a20f253d5c31cb
ryan-dia <cjfdnjs158@naver.com>
2025-05-28 10:00:28 +0900

    feat: @Transactional 애너테이션 추가
    Co-authored-by: yuyoungrhee <bbclara@naver.com>

--- src/main/java/roomescape/member/service/MemberService.java
+++ src/main/java/roomescape/member/service/MemberService.java
@@ -3,6 +3,7 @@ package roomescape.member.service;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import roomescape.member.domain.Member;
 import roomescape.member.domain.MemberRole;
 import roomescape.member.domain.Password;
@@ -16,6 +17,7 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    @Transactional
     public void save(final MemberRequest memberRequest) {
         if (memberRepository.existsByEmail(memberRequest.email())) {
             throw new IllegalArgumentException("이미 가입된 이메일입니다. email=" + memberRequest.email());
