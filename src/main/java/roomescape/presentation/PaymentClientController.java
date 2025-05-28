6b07b4973785a71e5c80dc562e77a6a871c01955
kwonkeonhyeong <rjsgud19566@gmail.com>
2025-05-28 20:52:05 +0900

    fix: PaymentClientController.java @Component 애노테이션 적용

--- src/main/java/roomescape/presentation/PaymentClientController.java
+++ src/main/java/roomescape/presentation/PaymentClientController.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.client.RestClient;
 import roomescape.domain.PaymentInfo;
 import roomescape.dto.PaymentRequest;
@@ -13,6 +15,7 @@ import roomescape.exception.PaymentException;
 import java.util.Arrays;
 import java.util.List;
 
+@Component
 public class PaymentClientController {
 
     private static final List<String> CODES = List.of("INVALID_API_KEY", "UNAUTHORIZED_KEY", "INCORRECT_BASIC_AUTH_FORMAT");
