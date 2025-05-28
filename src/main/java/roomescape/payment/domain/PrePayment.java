a037bf55d384737dfd4f9fe2b7fecb081806cecf
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2025-05-28 12:22:16 +0900

    feat: 결제 요청 전 결제 정보 임시저장하는 API 구현

---
+++ src/main/java/roomescape/payment/domain/PrePayment.java
@@ -0,0 +1,37 @@
+package roomescape.payment.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.math.BigDecimal;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class PrePayment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(nullable = false, unique = true)
+    String orderId;
+
+    @Column(nullable = false)
+    BigDecimal amount;
+
+    public PrePayment(String orderId, BigDecimal amount) {
+        this.orderId = orderId;
+        this.amount = amount;
+    }
+
+    //TODO: 네이밍 고민  (2025-05-28, 수, 11:55)
+    public boolean isSameAmount(BigDecimal amount) {
+        return this.amount.equals(amount);
+    }
+}
