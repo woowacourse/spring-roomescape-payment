bef12cb8ed9ae3eb569ce5ac17f9dfedf464943b
Mint <audwl03071@gmail.com>
2025-05-28 20:19:59 +0900

    refactor: PaymentService로부터 PaymentClient를 분리하여 캡슐화

---
+++ src/main/java/roomescape/common/config/PaymentClientConfig.java
@@ -0,0 +1,31 @@
+package roomescape.common.config;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+import roomescape.common.properties.PaymentClientProperties;
+import roomescape.payment.exception.handler.PaymentApproveExceptionHandler;
+
+@Configuration
+@EnableConfigurationProperties(PaymentClientProperties.class)
+public class PaymentClientConfig {
+
+    private final PaymentClientProperties paymentClientProperties;
+
+    public PaymentClientConfig(final PaymentClientProperties paymentClientProperties) {
+        this.paymentClientProperties = paymentClientProperties;
+    }
+
+    @Bean
+    public RestClient restClient() {
+        return RestClient.builder()
+                .baseUrl(paymentClientProperties.getBaseUrl())
+                .build();
+    }
+
+    @Bean
+    PaymentApproveExceptionHandler paymentApproveExceptionHandler() {
+        return new PaymentApproveExceptionHandler();
+    }
+}
--- src/main/java/roomescape/payment/application/PaymentService.java
+++ src/main/java/roomescape/payment/application/PaymentService.java
@@ -1,40 +1,20 @@
 package roomescape.payment.application;
 
-import java.util.Base64;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestClient;
-import roomescape.payment.exception.handler.PaymentApproveExceptionHandler;
+import roomescape.payment.application.client.PaymentClient;
 import roomescape.payment.presentation.dto.request.PaymentApproveRequest;
 import roomescape.payment.presentation.dto.response.PaymentApproveResponse;
 
 @Service
 public class PaymentService {
 
-    private static final String SECRET_KEY = "test_gsk_docs_OaPz8L5KdmQXkzRz3y47BMw6";
-    private static final String PAYMENTS_CONFIRM_API = "/v1/payments/confirm";
-    private static final String AUTHORIZATION = "Authorization";
-    private static final String BASIC = "Basic ";
-    private static final String COLON = ":";
+    private final PaymentClient paymentClient;
 
-    private final RestClient restClient;
-    private final PaymentApproveExceptionHandler paymentApproveExceptionHandler;
-
-    public PaymentService(final RestClient restClient, final PaymentApproveExceptionHandler paymentApproveExceptionHandler) {
-        this.restClient = restClient;
-        this.paymentApproveExceptionHandler = paymentApproveExceptionHandler;
+    public PaymentService(final PaymentClient paymentClient) {
+        this.paymentClient = paymentClient;
     }
 
     public PaymentApproveResponse approvePayment(final PaymentApproveRequest paymentApproveRequest) {
-        return restClient.post()
-                .uri(PAYMENTS_CONFIRM_API)
-                .header(AUTHORIZATION, BASIC + toBase64(SECRET_KEY + COLON))
-                .body(paymentApproveRequest)
-                .retrieve()
-                .onStatus(paymentApproveExceptionHandler)
-                .body(PaymentApproveResponse.class);
-    }
-
-    private String toBase64(String rawText) {
-        return Base64.getEncoder().encodeToString(rawText.getBytes());
+        return paymentClient.approvePayment(paymentApproveRequest);
     }
 }
