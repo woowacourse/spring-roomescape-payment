161310aeb66217895e120663cfda042a03de88db
dltmddus <tmddus4671@gmail.com>
2025-05-29 16:51:34 +0900

    feat: 타임아웃 설정 기능 추가

--- src/main/java/roomescape/common/config/TossPaymentClientConfig.java
+++ src/main/java/roomescape/common/config/TossPaymentClientConfig.java
@@ -4,6 +4,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
 @Configuration
@@ -13,8 +14,13 @@ public class TossPaymentClientConfig {
         String secretKey = "test_gsk_docs_OaPz8L5KdmQXkzRz3y47BMw6";
         String encodedAuth = "Basic " + Base64.getEncoder().encodeToString((secretKey + ":").getBytes(StandardCharsets.UTF_8));
 
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(3000);
+        factory.setReadTimeout(3000);
+
         return RestClient.builder()
                 .baseUrl("https://api.tosspayments.com/v1")
+                .requestFactory(factory)
                 .defaultHeader("Authorization", encodedAuth)
                 .defaultHeader("Content-Type", "application/json")
                 .build();
