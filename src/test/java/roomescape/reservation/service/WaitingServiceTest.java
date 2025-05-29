14101ae9d29860048e04bc3be755bbe05f178436
ykmxxi <ykmxxi97@gmail.com>
2025-05-29 16:11:44 +0900

    refactor: 패키지 구조 변경

--- src/test/java/roomescape/reservation/service/WaitingServiceTest.java
+++ src/test/java/roomescape/reservation/service/WaitingServiceTest.java
@@ -22,15 +22,16 @@ import roomescape.member.domain.Member;
 import roomescape.member.domain.Role;
 import roomescape.member.repository.MemberRepository;
 import roomescape.reservation.domain.Reservation;
-import roomescape.reservation.domain.ReservationTime;
-import roomescape.reservation.domain.Theme;
-import roomescape.reservation.domain.Waiting;
+import roomescape.reservation.time.domain.ReservationTime;
+import roomescape.theme.domain.Theme;
+import roomescape.reservation.waiting.domain.Waiting;
 import roomescape.reservation.dto.request.WaitingCreateRequest;
 import roomescape.reservation.dto.response.WaitingResponse;
 import roomescape.reservation.repository.ReservationRepository;
-import roomescape.reservation.repository.ReservationTimeRepository;
-import roomescape.reservation.repository.ThemeRepository;
-import roomescape.reservation.repository.WaitingRepository;
+import roomescape.reservation.time.repository.ReservationTimeRepository;
+import roomescape.theme.repository.ThemeRepository;
+import roomescape.reservation.waiting.repository.WaitingRepository;
+import roomescape.reservation.waiting.service.WaitingService;
 
 @ActiveProfiles("test")
 @DataJpaTest
