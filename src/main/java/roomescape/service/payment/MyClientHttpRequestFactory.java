cae7000d93bf2862de92d4b2a3d3f1f14988c4d2
Dompoo <dlckdrms0517@gmail.com>
2025-05-29 16:29:41 +0900

    fix: 타임아웃을 1초씩으로 설정

--- src/main/java/roomescape/service/payment/MyClientHttpRequestFactory.java
+++ src/main/java/roomescape/service/payment/MyClientHttpRequestFactory.java
@@ -3,11 +3,13 @@ package roomescape.service.payment;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
 
+import java.time.Duration;
+
 @Component
 public class MyClientHttpRequestFactory extends SimpleClientHttpRequestFactory {
 
     public MyClientHttpRequestFactory() {
-        super.setConnectTimeout(1);
-        super.setReadTimeout(1);
+        super.setConnectTimeout(Duration.ofSeconds(1));
+        super.setReadTimeout(Duration.ofSeconds(1));
     }
 }
