503ff8ec9153efe016bbd7584f86893c88545584
jeyong <joon6093@naver.com>
2025-05-29 17:25:47 +0900

    chore: 불필요한 주석 제거

--- src/main/java/roomescape/payment/application/dto/TossConfirmResponse.java
+++ src/main/java/roomescape/payment/application/dto/TossConfirmResponse.java
@@ -2,11 +2,13 @@ package roomescape.payment.application.dto;
 
 public record TossConfirmResponse(
     String paymentKey,
-    String orderId, // amount <- 요청
+    String orderId,
     EasyPayInfo easyPay
 ) {
+
     public record EasyPayInfo(
         Long amount
     ) {
+
     }
 }
