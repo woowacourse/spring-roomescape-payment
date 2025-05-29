35b5c7fe70efe2ac78e91034f8bd906ab96412bd
2jin2.1031 <2jin2.1031@gmail.com>
2025-05-29 15:06:46 +0900

    chore: API baseUrl properties에 추가

--- src/main/java/roomescape/global/config/TossRestClientConfig.java
+++ src/main/java/roomescape/global/config/TossRestClientConfig.java
@@ -1,31 +1,32 @@
 package roomescape.global.config;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
 
-import java.nio.charset.StandardCharsets;
-import java.util.Base64;
-
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
-
 
 @Configuration
-public class RestClientConfig {
+public class TossRestClientConfig {
 
     private final String secretKey;
+    private final String baseUrl;
 
-    public RestClientConfig(@Value("${toss.payment.secret-key}") String secretKey) {
+    public TossRestClientConfig(@Value("${toss.payment.secret-key}") String secretKey, @Value("${toss.payment.base-url}") String baseUrl) {
         this.secretKey = secretKey;
+        this.baseUrl = baseUrl;
     }
 
     @Bean
-    public RestClient tossPayRestClient() {
+    public RestClient restClient() {
         String basicAuthValue = encodeBasicAuth(secretKey);
 
         return RestClient.builder()
-                .baseUrl("https://api.tosspayments.com")
+                .baseUrl(baseUrl)
                 .defaultHeader("Authorization", basicAuthValue)
                 .defaultHeader("Content-Type", APPLICATION_JSON_VALUE)
                 .build();
