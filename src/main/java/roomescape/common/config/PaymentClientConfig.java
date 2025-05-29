3e006c8ad7e6c0368bcf5390ffb1f629ce731a1f
Mint <audwl03071@gmail.com>
2025-05-29 15:57:10 +0900

    refactor: 불필요한 @Autowired 제거

--- src/main/java/roomescape/common/config/PaymentClientConfig.java
+++ src/main/java/roomescape/common/config/PaymentClientConfig.java
@@ -1,6 +1,5 @@
 package roomescape.common.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
