bafe7555cf3fb1c2398d126e4b18693b4da4be00
kimdev <kim__dev@hufs.ac.kr>
2025-05-30 17:44:58 +0900

    refactor: config 메서드 접근제어자 public 추가

--- src/main/java/roomescape/common/config/PaymentClientConfig.java
+++ src/main/java/roomescape/common/config/PaymentClientConfig.java
@@ -25,7 +25,7 @@ public class PaymentClientConfig {
     }
 
     @Bean
-    PaymentApproveExceptionHandler paymentApproveExceptionHandler() {
+    public PaymentApproveExceptionHandler paymentApproveExceptionHandler() {
         return new PaymentApproveExceptionHandler();
     }
 }
