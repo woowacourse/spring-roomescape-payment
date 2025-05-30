1397c5f272640feb52254e16a4027edf913fdce8
kimdev <kim__dev@hufs.ac.kr>
2025-05-30 23:01:31 +0900

    test: PaymentClient 예외 테스트 추가

--- src/test/java/roomescape/payment/application/client/PaymentClientTest.java
+++ src/test/java/roomescape/payment/application/client/PaymentClientTest.java
@@ -1,18 +1,23 @@
 package roomescape.payment.application.client;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withStatus;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 
+import org.assertj.core.api.Assertions;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.client.MockRestServiceServer;
 import roomescape.common.config.PaymentClientConfig;
 import roomescape.common.properties.PaymentClientProperties;
+import roomescape.payment.exception.PaymentApproveException;
 import roomescape.payment.presentation.dto.request.PaymentApproveRequest;
 import roomescape.payment.presentation.dto.response.PaymentApproveResponse;
 
@@ -81,6 +86,7 @@ class PaymentClientTest {
     @Autowired
     private PaymentClientProperties paymentClientProperties;
 
+
     @BeforeEach
     void setUp() {
         String baseUrl = paymentClientProperties.getBaseUrl();
@@ -104,5 +110,19 @@ class PaymentClientTest {
             softAssertions.assertThat(response.orderId()).isEqualTo(ORDER_ID);
             softAssertions.assertThat(response.totalAmount()).isEqualTo(50000);
         });
+    }
+
+    @Test
+    void 잘못된_시크릿_키로_요청할_경우_예외가_발생한다() {
+        // Given
+        PaymentApproveRequest request = new PaymentApproveRequest("INVALID", ORDER_ID,
+                50_000L);
+        mockServer.reset();
+        mockServer.expect(requestTo(paymentClientProperties.getBaseUrl() + paymentClientProperties.getConfirmApi()))
+                .andRespond(withStatus(HttpStatus.BAD_REQUEST));
+
+        // When & Then
+        assertThatThrownBy(() -> paymentClient.approvePayment(request))
+                .isInstanceOf(PaymentApproveException.class);
     }
 }
