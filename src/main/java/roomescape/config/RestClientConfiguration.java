00efe8ebd441d3b9d5abd56e3124f250c69612ee
taek2222 <oot309@daum.net>
2025-05-31 00:33:13 +0900

    feat: RestClient 요청 설정 기능 추가
    - 요청 연결 시간 제한 설정 (2초)
    - 요청 읽기 시간 제한 설정 (3초)
    - ClientHttpRequestFactory 구현을 통한 RestClient 커스터마이징

--- src/main/java/roomescape/config/RestClientConfiguration.java
+++ src/main/java/roomescape/config/RestClientConfiguration.java
@@ -1,8 +1,12 @@
 package roomescape.config;
 
+import java.time.Duration;
+import org.springframework.boot.http.client.ClientHttpRequestFactoryBuilder;
+import org.springframework.boot.http.client.ClientHttpRequestFactorySettings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
 @Configuration
@@ -11,6 +15,21 @@ public class RestClientConfiguration {
 
     @Bean
     public RestClient restClient() {
-        return RestClient.builder().build();
+        ClientHttpRequestFactory requestFactory = createRequestFactory(createRequestSettings());
+        return RestClient.builder().
+                requestFactory(requestFactory)
+                .build();
+    }
+
+    private ClientHttpRequestFactory createRequestFactory(ClientHttpRequestFactorySettings settings) {
+        return ClientHttpRequestFactoryBuilder
+                .simple()
+                .build(settings);
+    }
+
+    private ClientHttpRequestFactorySettings createRequestSettings() {
+        return ClientHttpRequestFactorySettings.defaults()
+                .withConnectTimeout(Duration.ofSeconds(2))
+                .withReadTimeout(Duration.ofSeconds(3));
     }
 }
