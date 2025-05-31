be2397c8a688ae663b5a9045a17c5da282e1b7ff
rladmstn <oz33177351@gmail.com>
2025-05-31 16:25:03 +0900

    refactor: TODO 해결 및 주석 처리 제거

--- src/main/resources/static/js/user-reservation.js
+++ src/main/resources/static/js/user-reservation.js
@@ -13,9 +13,6 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    // ------  결제위젯 초기화 ------
-    // @docs https://docs.tosspayments.com/reference/widget-sdk#sdk-설치-및-초기화
-    // @docs https://docs.tosspayments.com/reference/widget-sdk#renderpaymentmethods선택자-결제-금액-옵션
     const paymentAmount = 2000;
     const widgetClientKey = "test_gck_docs_Ovk5rk1EwkEbP0W43n07xlzm";
     const paymentWidget = PaymentWidget(widgetClientKey, PaymentWidget.ANONYMOUS);
@@ -169,23 +166,16 @@ function onReservationButtonClick(event, paymentWidget) {
 
         const generateRandomString = () =>
             window.btoa(Math.random()).slice(0, 20);
-        /*
-        TODO: [1단계]
-              - orderIdPrefix 를 자신만의 prefix로 변경
-        */
-        // TOSS 결제 위젯 Javascript SDK 연동 방식 중 'Promise로 처리하기'를 적용함
-        // https://docs.tosspayments.com/reference/widget-sdk#promise%EB%A1%9C-%EC%B2%98%EB%A6%AC%ED%95%98%EA%B8%B0
-        const orderIdPrefix = "WTEST";
+
+        const orderIdPrefix = "TEST";
         paymentWidget.requestPayment({
             orderId: orderIdPrefix + generateRandomString(),
             orderName: "테스트 방탈출 예약 결제 1건",
-            amount: 500000, // TODO: 어디감?
+            amount: 2000,
         }).then(function (data) {
             console.debug(data);
             fetchReservationPayment(data, reservationData);
         }).catch(function (error) {
-            // TOSS 에러 처리: 에러 목록을 확인하세요
-            // https://docs.tosspayments.com/reference/error-codes#failurl 로-전달되는-에러
             alert(error.code + " :" + error.message + "/ orderId : " + err.orderId);
         });
     } else {
@@ -194,12 +184,7 @@ function onReservationButtonClick(event, paymentWidget) {
 }
 
 async function fetchReservationPayment(paymentData, reservationData) {
-    /*
-    TODO: [1단계]
-        - 자신의 예약 API request에 맞게 reservationPaymentRequest 필드명 수정
-        - 내 서버 URL에 맞게 reservationURL 변경
-        - 예약 결제 실패 시, 사용자가 실패 사유를 알 수 있도록 alert 에서 에러 메시지 수정
-    */
+
     const reservationPaymentRequest = {
         date: reservationData.date,
         themeId: reservationData.themeId,
