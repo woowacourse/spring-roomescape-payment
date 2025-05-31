ae14ec9a432ccaf604a53e0391e154dad2fcf158
KimDaehyeon <daehyeon3351@gmail.com>
2025-05-31 20:56:35 +0900

    refactor: 토스 결제 요청 API 예외 핸들러에서 IOException 을 전파시키지 않도록 변경

--- src/main/java/roomescape/payment/processor/toss/TossPaymentProcessorErrorHandler.java
+++ src/main/java/roomescape/payment/processor/toss/TossPaymentProcessorErrorHandler.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.net.URI;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.web.client.ResponseErrorHandler;
@@ -18,15 +19,32 @@ public class TossPaymentProcessorErrorHandler implements ResponseErrorHandler {
     }
 
     @Override
-    public boolean hasError(final ClientHttpResponse response) throws IOException {
-        final HttpStatusCode statusCode = response.getStatusCode();
-        return statusCode.is4xxClientError() || statusCode.is5xxServerError();
+    public boolean hasError(final ClientHttpResponse response) {
+        try {
+            final HttpStatusCode statusCode = response.getStatusCode();
+            return statusCode.is4xxClientError() || statusCode.is5xxServerError();
+        } catch (final IOException e) {
+            // 로깅 적용 전 임시 sout
+            System.out.println("토스 결제 처리 중 오류가 발생했습니다. " + e.getMessage());
+            return true;
+        }
     }
 
     @Override
-    public void handleError(final URI url, final HttpMethod method, final ClientHttpResponse response)
-            throws IOException {
-        final TossPaymentConfirmError error = objectMapper.readValue(response.getBody(), TossPaymentConfirmError.class);
-        throw new TossPaymentException(response.getStatusCode(), error.message());
+    public void handleError(final URI url, final HttpMethod method, final ClientHttpResponse response) {
+        try {
+            final TossPaymentConfirmError error =
+                objectMapper.readValue(response.getBody(), TossPaymentConfirmError.class);
+            throw new TossPaymentException(response.getStatusCode(), error.message());
+        } catch (final IOException e) {
+
+            // 로깅 적용 전 임시 sout
+            System.out.println("토스 결제 처리 중 오류가 발생했습니다. " + e.getMessage());
+
+            throw new TossPaymentException(
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                "토스 결제 처리 중 오류가 발생했습니다. "
+            );
+        }
     }
 }
