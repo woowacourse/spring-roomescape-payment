ce125e032a97b99bfdc991961c9a9bbf31277d86
rladmstn <oz33177351@gmail.com>
2025-05-31 17:41:42 +0900

    refactor: base url 및 타임아웃 상수화

--- src/main/java/roomescape/client/config/ClientConfig.java
+++ src/main/java/roomescape/client/config/ClientConfig.java
@@ -11,20 +11,25 @@ import roomescape.client.TossPaymentsClient;
 @Configuration
 public class ClientConfig {
 
+    private static final String TOSS_PAYMENTS_BASE_URL = "https://api.tosspayments.com/v1/payments/";
+
+    private static final int CONNECTION_TIMEOUT = 3000;
+    private static final int READ_TIMEOUT = 5000;
+
     private final String secretKey;
 
-    public ClientConfig(@Value("${toss.payments.secret-key}") String secretKey) {
+    public ClientConfig(@Value("${toss.payments.secret-key}") final String secretKey) {
         this.secretKey = secretKey;
     }
 
     @Bean
     public TossPaymentsClient tossRestClient() {
         final SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
-        requestFactory.setConnectTimeout(3000);
-        requestFactory.setReadTimeout(5000);
+        requestFactory.setConnectTimeout(CONNECTION_TIMEOUT);
+        requestFactory.setReadTimeout(READ_TIMEOUT);
         return new TossPaymentsClient(
                 RestClient.builder()
-                        .baseUrl("https://api.tosspayments.com/v1/payments/")
+                        .baseUrl(TOSS_PAYMENTS_BASE_URL)
                         .requestFactory(requestFactory)
                         .defaultHeader("Authorization", getBasicAuthorizationValue())
                         .build()
