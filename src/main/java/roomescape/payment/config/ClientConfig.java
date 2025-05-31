a92b702b5899081aade5dca589e986106be52459
AppleMint98 <applemint9821@gmail.com>
2025-05-31 15:57:58 +0900

    refactor: 인코딩 기능 유틸클래스로 분리

--- src/main/java/roomescape/payment/config/ClientConfig.java
+++ src/main/java/roomescape/payment/config/ClientConfig.java
@@ -1,7 +1,7 @@
 package roomescape.payment.config;
 
-import java.nio.charset.StandardCharsets;
-import java.util.Base64;
+import static roomescape.common.util.EncodingUtil.encodeBase64;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,14 +15,10 @@ public class ClientConfig {
 
     @Bean
     public RestClient tossPaymentRestClient() {
-        Base64.Encoder encoder = Base64.getEncoder();
-        byte[] encodedBytes = encoder.encode((secretKey + ":").getBytes(StandardCharsets.UTF_8));
-        String authorizations = "Basic " + new String(encodedBytes);
-
         return RestClient.builder()
                 .baseUrl("https://api.tosspayments.com")
                 .defaultHeader("Content-Type", "application/json")
-                .defaultHeader("Authorization", authorizations)
+                .defaultHeader("Authorization", "Basic " + encodeBase64((secretKey + ":")))
                 .build();
     }
 }
