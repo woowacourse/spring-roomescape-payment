baa7ddf07c32c69d86901e8753cb0a7b1e8e1f92
BWY <quantumjump10@gmail.com>
2025-05-31 13:24:57 +0900

    style: schema.sql 줄맞춤

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,20 +1,20 @@
 CREATE TABLE IF NOT EXISTS reservation_time (
-                                                id BIGINT NOT NULL AUTO_INCREMENT,
-                                                start_at VARCHAR(255) NOT NULL,
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    start_at VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
     );
 
 CREATE TABLE IF NOT EXISTS theme (
-                                     id BIGINT NOT NULL AUTO_INCREMENT,
-                                     name VARCHAR(255) NOT NULL,
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
     description VARCHAR(255) NOT NULL,
     thumbnail VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
     );
 
 CREATE TABLE IF NOT EXISTS users (
-                                     id BIGINT NOT NULL AUTO_INCREMENT,
-                                     role VARCHAR(255) NOT NULL,
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    role VARCHAR(255) NOT NULL,
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
@@ -22,8 +22,8 @@ CREATE TABLE IF NOT EXISTS users (
     );
 
 CREATE TABLE IF NOT EXISTS reservation (
-                                           id BIGINT NOT NULL AUTO_INCREMENT,
-                                           date VARCHAR(255) NOT NULL,
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    date VARCHAR(255) NOT NULL,
     status VARCHAR(255) NOT NULL,
     reservation_time_id BIGINT,
     theme_id BIGINT,
