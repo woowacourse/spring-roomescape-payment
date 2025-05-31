5e54e040087aa1bf16f0d8aaca5d8e7ee101640f
Minuring <mw9505@gmail.com>
2025-05-31 13:46:37 +0900

    test: 결제 실패 예외 테스트

---
+++ src/test/java/roomescape/exception/PaymentFailedExceptionTest.java
@@ -0,0 +1,26 @@
+package roomescape.exception;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import roomescape.domain.payment.PaymentFailCode;
+import roomescape.domain.payment.PaymentFailure;
+
+class PaymentFailedExceptionTest {
+
+    @DisplayName("예외 발생 원인이 클라이언트에게 있는 지 알 수 있다.")
+    @ParameterizedTest
+    @CsvSource({
+        "CONDITION_NOT_SATISFIED,true",
+        "INVALID_AUTH_CREDENTIALS,false",
+        "EXTERNAL_PROCESSING,false"
+    })
+    void causedByClient(final PaymentFailCode code, final boolean expected) {
+        var paymentFailure = new PaymentFailure(code, "결제 실패");
+        var exception = new PaymentFailedException(paymentFailure);
+
+        assertThat(exception.causedByClient()).isEqualTo(expected);
+    }
+}
