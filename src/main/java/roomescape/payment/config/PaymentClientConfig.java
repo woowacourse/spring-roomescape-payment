faaaa121026b7105d8bcfc988082632c8c984a61
soeun2537 <soeun2537@gmail.com>
2025-06-01 01:52:03 +0900

    refactor: timeout 설정

--- src/main/java/roomescape/payment/config/PaymentClientConfig.java
+++ src/main/java/roomescape/payment/config/PaymentClientConfig.java
@@ -3,10 +3,12 @@ package roomescape.payment.config;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 import java.nio.charset.StandardCharsets;
+import java.time.Duration;
 import java.util.Base64;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
 @Configuration
@@ -21,7 +23,12 @@ public class PaymentClientConfig {
                 .encodeToString((tossSecretKey + ":").getBytes(StandardCharsets.UTF_8));
         String authorizationHeader = "Basic " + base64EncodedKey;
 
+        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        requestFactory.setConnectTimeout(Duration.ofSeconds(3));
+        requestFactory.setReadTimeout(Duration.ofSeconds(5));
+
         return RestClient.builder()
+                .requestFactory(requestFactory)
                 .baseUrl("https://api.tosspayments.com/v1/payments/confirm")
                 .defaultHeader("Authorization", authorizationHeader)
                 .defaultHeader("Content-Type", APPLICATION_JSON_VALUE)
