d683645d9380576c5a5647c9ba8862f7e980c169
7i.w__k <tizmfns1218@Naver.com>
2025-05-31 17:19:38 +0900

    refactor: 타임아웃에 대한 재시도 추가

--- src/main/java/roomescape/payment/toss/service/TossPaymentService.java
+++ src/main/java/roomescape/payment/toss/service/TossPaymentService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.ResourceAccessException;
 import roomescape.payment.exception.PaymentTemporaryException;
 import roomescape.payment.toss.dto.TossPaymentRequest;
 import roomescape.payment.toss.dto.TossPaymentResponse;
@@ -14,12 +15,14 @@ public class TossPaymentService {
 
     private final TossPaymentClient tossPaymentClient;
 
-    @Retryable(retryFor = {PaymentTemporaryException.class}, maxAttempts = 3, backoff = @Backoff(delay = 500))
+    @Retryable(retryFor = {PaymentTemporaryException.class, ResourceAccessException.class},
+            maxAttempts = 2, backoff = @Backoff(delay = 500))
     public TossPaymentResponse confirmPayment(TossPaymentRequest paymentRequest) {
         return tossPaymentClient.getPaymentConfirm(paymentRequest);
     }
 
-    @Retryable(retryFor = {PaymentTemporaryException.class}, maxAttempts = 3, backoff = @Backoff(delay = 500))
+    @Retryable(retryFor = {PaymentTemporaryException.class, ResourceAccessException.class},
+            maxAttempts = 2, backoff = @Backoff(delay = 500))
     public TossPaymentResponse getPayment(TossPaymentRequest paymentRequest) {
         return tossPaymentClient.getPaymentConfirm(paymentRequest);
     }
