e78f2c1df3406d5510e6eef8b077ad6ca469c2c9
eoehd1ek <eoehd2ek@gmail.com>
2025-05-31 19:56:53 +0900

    refactor: RestClient 타임아웃 추가

--- src/main/java/roomescape/payment/config/TossPaymentRestClientConfig.java
+++ src/main/java/roomescape/payment/config/TossPaymentRestClientConfig.java
@@ -7,6 +7,7 @@ import java.util.Base64;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
 @Configuration
@@ -14,6 +15,12 @@ public class TossPaymentRestClientConfig {
 
     @Value("${toss.confirm.secret-key}")
     private String tossSecretKey;
+
+    @Value("${restclient.connection-timeout:3000}")
+    private int connectionTimeout;
+
+    @Value("${restclient.read-timeout:5000}")
+    private int readTimeout;
 
     @Bean
     public RestClient tossPaymentRestClient() {
@@ -25,6 +32,14 @@ public class TossPaymentRestClientConfig {
                 .baseUrl("https://api.tosspayments.com/v1/payments/confirm")
                 .defaultHeader("Authorization", authorizationHeader)
                 .defaultHeader("Content-Type", APPLICATION_JSON_VALUE)
+                .requestFactory(simpleClientHttpRequestFactory())
                 .build();
+    }
+
+    private SimpleClientHttpRequestFactory simpleClientHttpRequestFactory() {
+        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        requestFactory.setConnectTimeout(connectionTimeout);
+        requestFactory.setReadTimeout(readTimeout);
+        return requestFactory;
     }
 }
