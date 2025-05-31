f4fc1fcee97a84fea483939acfb6a943a2cdeaf1
eoehd1ek <eoehd2ek@gmail.com>
2025-05-31 17:40:33 +0900

    refactor(ReservationTimeRepositoryTest): 테스트 코드 포매팅

--- src/test/java/roomescape/reservation/unit/repository/ReservationTimeRepositoryTest.java
+++ src/test/java/roomescape/reservation/unit/repository/ReservationTimeRepositoryTest.java
@@ -35,37 +35,40 @@ class ReservationTimeRepositoryTest {
     @DisplayName("날짜와 테마 ID로 모든 예약되어 있는 시간을 조회할 수 있다.")
     void findAllReservedTimeByDateAndThemeId() {
         // given
-        var reservationTime1 = new ReservationTime(LocalTime.of(10, 0));
-        var reservationTime2 = new ReservationTime(LocalTime.of(11, 0));
-        var theme1 = new Theme("테마1", "설명1", "썸네일1");
-        var theme2 = new Theme("테마2", "설명2", "썸네일2");
-        var member1 = new Member("미소", "miso@email.com", "miso", RoleType.USER);
-        var member2 = new Member("훌라", "hula@email.com", "hula", RoleType.USER);
-        var date = LocalDate.now().plusDays(1);
-        var payment1 = new Payment("any", "any", 1L, "any");
-        var payment2 = new Payment("any", "any", 1L, "any");
-        var payment3 = new Payment("any", "any", 1L, "any");
-        var reservation1 = new Reservation(date, reservationTime1, theme1, member1, payment1);
-        var reservation2 = new Reservation(date, reservationTime2, theme2, member2, payment2);
-        var reservation3 = new Reservation(date, reservationTime2, theme1, member2, payment3);
+        ReservationTime reservationTime1 = new ReservationTime(LocalTime.of(10, 0));
         entityManager.persist(reservationTime1);
+        ReservationTime reservationTime2 = new ReservationTime(LocalTime.of(11, 0));
         entityManager.persist(reservationTime2);
+
+        Theme theme1 = new Theme("테마1", "설명1", "썸네일1");
         entityManager.persist(theme1);
+        Theme theme2 = new Theme("테마2", "설명2", "썸네일2");
         entityManager.persist(theme2);
+
+        Member member1 = new Member("미소", "miso@email.com", "miso", RoleType.USER);
         entityManager.persist(member1);
+        Member member2 = new Member("훌라", "hula@email.com", "hula", RoleType.USER);
         entityManager.persist(member2);
+
+        Payment payment1 = new Payment("paymentKey0001", "TESTOrder0001", 1000L, "NORMAL");
         entityManager.persist(payment1);
+        Payment payment2 = new Payment("paymentKey0002", "TESTOrder0002", 1000L, "NORMAL");
         entityManager.persist(payment2);
+        Payment payment3 = new Payment("paymentKey0003", "TESTOrder0003", 1000L, "NORMAL");
         entityManager.persist(payment3);
+
+        LocalDate date = LocalDate.now().plusDays(1);
+
+        Reservation reservation1 = new Reservation(date, reservationTime1, theme1, member1, payment1);
         entityManager.persist(reservation1);
+        Reservation reservation2 = new Reservation(date, reservationTime2, theme2, member2, payment2);
         entityManager.persist(reservation2);
+        Reservation reservation3 = new Reservation(date, reservationTime2, theme1, member2, payment3);
         entityManager.persist(reservation3);
 
         // when
-        List<ReservationTime> reservationTimes = reservationTimeRepository.findAllReservedTimeByDateAndThemeId(
-                date,
-                1L
-        );
+        List<ReservationTime> reservationTimes =
+                reservationTimeRepository.findAllReservedTimeByDateAndThemeId(date, 1L);
 
         // then
         assertAll(
