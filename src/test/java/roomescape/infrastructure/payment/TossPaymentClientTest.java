3a338dd0058c1d58f7e3480aca23832a88e3f874
junhaa <2171326@hansung.ac.kr>
2025-05-31 11:24:07 +0900

    test: 토스 결제 승인 API 예외 발생 테스트 추가

--- src/test/java/roomescape/infrastructure/payment/TossPaymentClientTest.java
+++ src/test/java/roomescape/infrastructure/payment/TossPaymentClientTest.java
@@ -1,13 +1,17 @@
 package roomescape.infrastructure.payment;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withStatus;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
@@ -21,6 +25,7 @@ import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.web.client.RestClient;
 import roomescape.application.request.PaymentInfo;
 import roomescape.application.response.PaymentResponse;
+import roomescape.exception.PaymentException;
 import roomescape.infrastructure.payment.toss.TossPaymentClient;
 import roomescape.infrastructure.payment.toss.TossRestClientProperties;
 
@@ -58,6 +63,27 @@ class TossPaymentClientTest {
         assertAll(() -> assertThat(result.paymentKey()).isEqualTo(paymentInfo.paymentKey()),
                 () -> assertThat(result.orderId()).isEqualTo(paymentInfo.orderId()),
                 () -> assertThat(result.amount()).isEqualTo(paymentInfo.amount()));
+    }
+
+    @ParameterizedTest
+    @EnumSource(PaymentErrorCode.class)
+    @DisplayName("결제 API 호출 시 에러가 발생하면 PaymentException 예외가 발생한다.")
+    void confirmPayment_WhenExceptionThrown(PaymentErrorCode errorCode) {
+        // given
+        PaymentInfo paymentInfo = new PaymentInfo("paymentKey", "ROOM_ESCAPE_test_order_id", 1000);
+
+        server.expect(requestTo("https://api.tosspayments.com/v1/payments/confirm")).andExpect(method(HttpMethod.POST))
+                .andRespond(withStatus(errorCode.getStatusCode()).contentType(MediaType.APPLICATION_JSON)
+                        .body(String.format("""
+                                {
+                                    "code": "%s",
+                                    "message": "%s"
+                                }
+                                """, errorCode.name(), errorCode.getMessage())));
+
+        // when & then
+        assertThatThrownBy(() -> paymentClient.confirmPayment(paymentInfo)).isInstanceOf(PaymentException.class)
+                .matches(exception -> ((PaymentException) exception).getErrorCode() == errorCode);
     }
 
     @TestConfiguration
