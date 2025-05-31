86785a3dbd37b12d175931a1b4b0a53240a4ddf7
eoehd1ek <eoehd2ek@gmail.com>
2025-05-31 18:03:03 +0900

    refactor: 테스트 경계 값 사용하도록 변경

--- src/test/java/roomescape/reservation/unit/repository/ReservationTimeRepositoryTest.java
+++ src/test/java/roomescape/reservation/unit/repository/ReservationTimeRepositoryTest.java
@@ -39,32 +39,26 @@ class ReservationTimeRepositoryTest {
         entityManager.persist(reservationTime1);
         ReservationTime reservationTime2 = new ReservationTime(LocalTime.of(11, 0));
         entityManager.persist(reservationTime2);
+        ReservationTime reservationTime3 = new ReservationTime(LocalTime.of(12, 0));
+        entityManager.persist(reservationTime3);
 
         Theme theme1 = new Theme("테마1", "설명1", "썸네일1");
         entityManager.persist(theme1);
-        Theme theme2 = new Theme("테마2", "설명2", "썸네일2");
-        entityManager.persist(theme2);
 
         Member member1 = new Member("미소", "miso@email.com", "miso", RoleType.USER);
         entityManager.persist(member1);
-        Member member2 = new Member("훌라", "hula@email.com", "hula", RoleType.USER);
-        entityManager.persist(member2);
 
         Payment payment1 = new Payment("paymentKey0001", "TESTOrder0001", 1000L, "NORMAL");
         entityManager.persist(payment1);
         Payment payment2 = new Payment("paymentKey0002", "TESTOrder0002", 1000L, "NORMAL");
         entityManager.persist(payment2);
-        Payment payment3 = new Payment("paymentKey0003", "TESTOrder0003", 1000L, "NORMAL");
-        entityManager.persist(payment3);
 
         LocalDate date = LocalDate.now().plusDays(1);
 
         Reservation reservation1 = new Reservation(date, reservationTime1, theme1, member1, payment1);
         entityManager.persist(reservation1);
-        Reservation reservation2 = new Reservation(date, reservationTime2, theme2, member2, payment2);
+        Reservation reservation2 = new Reservation(date, reservationTime2, theme1, member1, payment2);
         entityManager.persist(reservation2);
-        Reservation reservation3 = new Reservation(date, reservationTime2, theme1, member2, payment3);
-        entityManager.persist(reservation3);
 
         // when
         List<ReservationTime> reservationTimes =
