feb6a58d0569257a468d16cb818bff0d640eef42
geonwoo <kysub99@naver.com>
2025-05-31 20:02:06 +0900

    refactor: 사용하지 않는 메서드 제거

--- src/main/java/roomescape/dto/response/PaymentErrorResponse.java
+++ src/main/java/roomescape/dto/response/PaymentErrorResponse.java
@@ -2,9 +2,5 @@ package roomescape.dto.response;
 
 public record PaymentErrorResponse(
         String code,
-        String message
-) {
-    public static PaymentErrorResponse from(String code, String message) {
-        return new PaymentErrorResponse(code, message);
-    }
+        String message) {
 }
