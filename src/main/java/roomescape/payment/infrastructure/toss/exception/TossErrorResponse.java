ef071c4787e90bd5a7ac3af0039debab8d1ef2bf
LJM\eueo8 <eueo8259@gmail.com>
2025-06-01 17:06:34 +0900

    refactor: 토스 에러 문서 주석 추가

--- src/main/java/roomescape/payment/infrastructure/toss/exception/TossErrorResponse.java
+++ src/main/java/roomescape/payment/infrastructure/toss/exception/TossErrorResponse.java
@@ -4,6 +4,7 @@ import java.util.Set;
 
 public record TossErrorResponse(String code, String message) {
     public boolean hasNonUserFacingMessage() {
+        //https://docs.tosspayments.com/reference/error-codes#%EA%B2%B0%EC%A0%9C-%EC%8A%B9%EC%9D%B8
         return Set.of(
                 "INVALID_API_KEY",
                 "UNAUTHORIZED_KEY",
