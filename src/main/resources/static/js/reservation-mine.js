0fc0652d4e33a20c82334f2f476822087e0214a3
Dompoo <dlckdrms0517@gmail.com>
2025-06-01 18:17:03 +0900

    fix: 예약 상태 비교 로직이 작동하지 않던 문제 수정

--- src/main/resources/static/js/reservation-mine.js
+++ src/main/resources/static/js/reservation-mine.js
@@ -21,6 +21,8 @@ function render(data) {
         const date = item.date;
         const time = item.time;
         const status = item.status;
+        const paymentKey = item.paymentKey;
+        const amount = item.amount;
 
         row.insertCell(0).textContent = theme;
         row.insertCell(1).textContent = date;
@@ -31,7 +33,7 @@ function render(data) {
             row.insertCell(3).textContent = "예약 확정";
         }
 
-        if (status !== '예약') { // 예약 대기 상태일 때 예약 대기 취소 버튼 추가하는 코드, 상태 값은 변경 가능
+        if (status !== '예약 확정') { // 예약 대기 상태일 때 예약 대기 취소 버튼 추가하는 코드, 상태 값은 변경 가능
             const cancelCell = row.insertCell(4);
             const cancelButton = document.createElement('button');
             cancelButton.textContent = '취소';
@@ -42,8 +44,8 @@ function render(data) {
             cancelCell.appendChild(cancelButton);
         } else { // 예약 완료 상태일 때
             row.insertCell(4).textContent = '';
-            row.insertCell(5).textContent = item.paymentKey;
-            row.insertCell(6).textContent = item.amount;
+            row.insertCell(5).textContent = paymentKey;
+            row.insertCell(6).textContent = amount;
         }
     });
 }
