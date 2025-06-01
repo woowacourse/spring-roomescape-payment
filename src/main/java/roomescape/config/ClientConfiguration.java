f6970b5c7c718b9655c40b53bd1675af461aab7d
geonwoo <kysub99@naver.com>
2025-06-02 02:53:54 +0900

    refactor: URL 및 타임아웃 설정 분리

--- src/main/java/roomescape/config/ClientConfiguration.java
+++ src/main/java/roomescape/config/ClientConfiguration.java
@@ -2,6 +2,7 @@ package roomescape.config;
 
 import java.time.Duration;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
@@ -9,19 +10,28 @@ import org.springframework.web.client.RestClient;
 
 @Configuration
 public class ClientConfiguration {
+    @Value("${toss.payment.base-url}")
+    private String baseUrl;
+
+    @Value("${toss.payment.connect-timeout}")
+    private int connectTimeout;
+
+    @Value("${toss.payment.read-timeout}")
+    private int readTimeout;
+
     @Bean
     @Qualifier("tossClient")
     public RestClient tossClient(RestClient.Builder builder) {
         return builder
-                .baseUrl("https://api.tosspayments.com")
+                .baseUrl(baseUrl)
                 .requestFactory(getRequestFactory())
                 .build();
     }
 
     public SimpleClientHttpRequestFactory getRequestFactory() {
         SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
-        requestFactory.setConnectTimeout(Duration.ofSeconds(5));
-        requestFactory.setReadTimeout(Duration.ofSeconds(10));
+        requestFactory.setConnectTimeout(Duration.ofSeconds(connectTimeout));
+        requestFactory.setReadTimeout(Duration.ofSeconds(readTimeout));
         return requestFactory;
     }
 }
