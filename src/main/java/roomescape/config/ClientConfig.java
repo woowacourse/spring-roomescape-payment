9e2250b03979f7f1dd4cb29ced3586ce7597c86f
yush <yush2202@gmail.com>
2025-06-02 00:11:58 +0900

    Timeout 시간 변경

--- src/main/java/roomescape/config/ClientConfig.java
+++ src/main/java/roomescape/config/ClientConfig.java
@@ -9,16 +9,24 @@ import org.springframework.web.client.RestClient;
 @Configuration
 public class ClientConfig {
 
+    private static final int READ_TIMEOUT = 60_000;
+    private static final int CONNECT_TIMEOUT = 5_000;
+    private static final String baseUrl = "https://api.tosspayments.com";
+    private static final String secretKey = "test_gsk_docs_OaPz8L5KdmQXkzRz3y47BMw6:";
+
     @Bean
     public RestClient paymentClient() {
         HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
-        factory.setConnectionRequestTimeout(5_000);
-        factory.setReadTimeout(5_000);
+        factory.setConnectTimeout(CONNECT_TIMEOUT);
+        factory.setReadTimeout(READ_TIMEOUT);
+
+        System.out.println(baseUrl);
+        System.out.println(secretKey);
 
         return RestClient.builder()
-                .baseUrl("https://api.tosspayments.com")
+                .baseUrl(baseUrl)
                 .defaultHeader("Authorization", String.format("%s %s", "Basic", Base64.getEncoder()
-                        .encodeToString("test_gsk_docs_OaPz8L5KdmQXkzRz3y47BMw6:".getBytes())))
+                        .encodeToString(secretKey.getBytes())))
                 .requestFactory(factory)
                 .build();
     }
