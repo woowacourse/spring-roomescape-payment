c4321a05f97758cd4c2ad1b5de9083ca7f445156
kjungw <sagakkb@gmail.com>
2025-06-01 21:40:43 +0900

    refactor: 환경 분리

--- src/main/java/roomescape/configuration/ClientConfiguration.java
+++ src/main/java/roomescape/configuration/ClientConfiguration.java
@@ -4,12 +4,14 @@ import java.time.Duration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import roomescape.utility.payment.PaymentClient;
 import roomescape.utility.payment.TossPaymentClient;
 
 @Configuration
+@Profile("product")
 public class ClientConfiguration {
 
     @Bean
