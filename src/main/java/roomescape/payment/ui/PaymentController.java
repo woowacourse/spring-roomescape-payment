3638fa422155a231f319161a13a20096b0cd3822
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2025-06-02 00:49:53 +0900

    refactor: PaymentController에 기본 경로 설정

--- src/main/java/roomescape/payment/ui/PaymentController.java
+++ src/main/java/roomescape/payment/ui/PaymentController.java
@@ -6,12 +6,14 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import roomescape.common.response.ApiResponse;
 import roomescape.common.session.SessionManager;
 import roomescape.payment.application.dto.PrePaymentRequest;
 
 @RestController
+@RequestMapping("payments")
 @AllArgsConstructor
 public class PaymentController {
     private final SessionManager sessionManager;
