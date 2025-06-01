b4bbeddc8636fab29cfc13190c17a8d4d37ff7f0
DEV_WOONG <ywcsuwon@gmail.com>
2025-06-01 22:02:55 +0900

    test: 멤버의 예약 실패 테스트 추가

--- src/test/java/roomescape/reservation/service/CreateReservationWithPaymentTest.java
+++ src/test/java/roomescape/reservation/service/CreateReservationWithPaymentTest.java
@@ -2,6 +2,7 @@ package roomescape.reservation.service;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,6 +13,8 @@ import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import roomescape.auth.service.dto.LoginMember;
+import roomescape.common.exception.DuplicatedException;
+import roomescape.common.exception.EntityNotFoundException;
 import roomescape.member.domain.Member;
 import roomescape.member.domain.Role;
 import roomescape.payment.infraStructure.PaymentClientSelector;
@@ -88,5 +91,115 @@ public class CreateReservationWithPaymentTest {
             softly.assertThat(result.time()).isEqualTo(new ReservationTimeResponse(time.getId(), time.getStartAt()));
             softly.assertThat(result.theme()).isEqualTo(new ThemeResponse(theme.getId(), theme.getName(), theme.getDescription(), theme.getThumbnail()));
         });
+    }
+
+    @DisplayName("멤버가 중복 예약을 시도하면 실패한다.")
+    @Test
+    void failCreateMembersPaymentAndReservation() {
+        // given
+        ConfirmPaymentRequest paymentRequest = new ConfirmPaymentRequest("paymentKey", "1234", 1000);
+        ConfirmPaymentResponse paymentResponse = new ConfirmPaymentResponse(1000, "paymentKey", null);
+        Mockito.when(mockTossPaymentClient.postConfirmPayment(paymentRequest)).thenReturn(paymentResponse);
+
+        LocalDate date = now.plusDays(1).toLocalDate();
+        ReservationWithPaymentRequest reservationWithPaymentRequest = new ReservationWithPaymentRequest(
+                date,
+                time.getId(),
+                theme.getId(),
+                paymentResponse.paymentKey(),
+                paymentRequest.orderId(),
+                1000,
+                "TOSS"
+        );
+
+        // when
+        ReservationResponse result = reservationService.createWithPayment(reservationWithPaymentRequest, LoginMember.of(member));
+
+        //then
+        Assertions.assertThatThrownBy(
+                () -> reservationService.createWithPayment(reservationWithPaymentRequest, LoginMember.of(member)))
+                .isInstanceOf(DuplicatedException.class)
+                .hasMessage("중복되는 예약이 존재합니다.");
+    }
+
+    @DisplayName("멤버가 존재하지 않는 예약 아이디로로 예약을 시도하면 실패한다.")
+    @Test
+    void failCreateMembersPaymentAndReservation2() {
+        // given
+        ConfirmPaymentRequest paymentRequest = new ConfirmPaymentRequest("paymentKey", "1234", 1000);
+        ConfirmPaymentResponse paymentResponse = new ConfirmPaymentResponse(1000, "paymentKey", null);
+        Mockito.when(mockTossPaymentClient.postConfirmPayment(paymentRequest)).thenReturn(paymentResponse);
+
+        LocalDate date = now.plusDays(1).toLocalDate();
+        ReservationWithPaymentRequest reservationWithPaymentRequest = new ReservationWithPaymentRequest(
+                date,
+                0L,
+                theme.getId(),
+                paymentResponse.paymentKey(),
+                paymentRequest.orderId(),
+                1000,
+                "TOSS"
+        );
+
+        //when
+        //then
+        Assertions.assertThatThrownBy(
+                        () -> reservationService.createWithPayment(reservationWithPaymentRequest, LoginMember.of(member)))
+                .isInstanceOf(EntityNotFoundException.class)
+                .hasMessage("존재하지 않는 예약 가능 시간입니다.");
+    }
+
+    @DisplayName("멤버가 존재하지 않는 테마 아이디로 예약을 시도하면 실패한다.")
+    @Test
+    void failCreateMembersPaymentAndReservation3() {
+        // given
+        ConfirmPaymentRequest paymentRequest = new ConfirmPaymentRequest("paymentKey", "1234", 1000);
+        ConfirmPaymentResponse paymentResponse = new ConfirmPaymentResponse(1000, "paymentKey", null);
+        Mockito.when(mockTossPaymentClient.postConfirmPayment(paymentRequest)).thenReturn(paymentResponse);
+
+        LocalDate date = now.plusDays(1).toLocalDate();
+        ReservationWithPaymentRequest reservationWithPaymentRequest = new ReservationWithPaymentRequest(
+                date,
+                time.getId(),
+                0L,
+                paymentResponse.paymentKey(),
+                paymentRequest.orderId(),
+                1000,
+                "TOSS"
+        );
+
+        // when
+        //then
+        Assertions.assertThatThrownBy(
+                        () -> reservationService.createWithPayment(reservationWithPaymentRequest, LoginMember.of(member)))
+                .isInstanceOf(EntityNotFoundException.class)
+                .hasMessage("존재하지 않는 테마입니다.");
+    }
+
+    @DisplayName("멤버가 지난 날짜로 예약을 시도하면 실패한다.")
+    @Test
+    void failCreateMembersPaymentAndReservation4() {
+        // given
+        ConfirmPaymentRequest paymentRequest = new ConfirmPaymentRequest("paymentKey", "1234", 1000);
+        ConfirmPaymentResponse paymentResponse = new ConfirmPaymentResponse(1000, "paymentKey", null);
+        Mockito.when(mockTossPaymentClient.postConfirmPayment(paymentRequest)).thenReturn(paymentResponse);
+
+        LocalDate date = now.minusDays(1).toLocalDate();
+        ReservationWithPaymentRequest reservationWithPaymentRequest = new ReservationWithPaymentRequest(
+                date,
+                time.getId(),
+                theme.getId(),
+                paymentResponse.paymentKey(),
+                paymentRequest.orderId(),
+                1000,
+                "TOSS"
+        );
+
+        // when
+        //then
+        Assertions.assertThatThrownBy(
+                        () -> reservationService.createWithPayment(reservationWithPaymentRequest, LoginMember.of(member)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("과거 날짜의 예약은 생성할 수 없습니다.");
     }
 }
