05136de0a3058afd31b662e6be402f7805d43f4d
DEV_WOONG <ywcsuwon@gmail.com>
2025-06-01 17:10:39 +0900

    refactor: toss payment 연결 경로 yml 파일에 추가, @Value 어노테이션을 통해 경로 가져오기 추가

--- src/main/java/roomescape/config/ClientConfig.java
+++ src/main/java/roomescape/config/ClientConfig.java
@@ -1,5 +1,6 @@
 package roomescape.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
@@ -7,10 +8,14 @@ import roomescape.payment.service.ReservationPaymentClient;
 
 @Configuration
 public class ClientConfig {
+
+    @Value("${payment.toos.base-url}")
+    private String tossPaymentBaseUrl;
+
     @Bean
     public ReservationPaymentClient getReservationPaymentClient() {
         return new ReservationPaymentClient(
-                RestClient.builder().baseUrl("https://api.tosspayments.com/v1/payments/").build()
+                RestClient.builder().baseUrl(tossPaymentBaseUrl).build()
         );
     }
 }
