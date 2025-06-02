bac1128963a08789d3c1757cc008c9bb8fb3048f
jhpark1227 <junhyeok0246@naver.com>
2025-06-02 23:05:59 +0900

    refactor: 불필요한 코드 제거

--- src/main/java/roomescape/infrastructure/payment/dto/PaymentApproveErrorResponse.java
+++ src/main/java/roomescape/infrastructure/payment/dto/PaymentApproveErrorResponse.java
@@ -1,21 +1,4 @@
 package roomescape.infrastructure.payment.dto;
 
-import lombok.Getter;
-
-@Getter
-public class PaymentApproveErrorResponse extends RuntimeException {
-
-    private final String code;
-    private final String message;
-
-    public PaymentApproveErrorResponse(String code, String message) {
-        super(message);
-        this.code = code;
-        this.message = message;
-    }
-
-    @Override
-    public String getMessage() {
-        return message;
-    }
+public record PaymentApproveErrorResponse(String code, String message) {
 }
