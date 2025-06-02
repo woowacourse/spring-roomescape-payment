ed788c90315762d27320e45ad81643793cb70b4d
YuyoungRhee <bbclara@naver.com>
2025-06-02 16:22:28 +0900

    feat: 프론트 수정 - 내 예약 페이지에 결제 정보 추가

--- src/main/resources/static/js/reservation-mine.js
+++ src/main/resources/static/js/reservation-mine.js
@@ -39,8 +39,15 @@ function render(data) {
                 requestDeleteWaiting(item.reservationId).then(() => window.location.reload());
             };
             cancelCell.appendChild(cancelButton);
-        } else { // 예약 완료 상태일 때
+        } else { // 예약 완료 상태일 때상
+            /*
+          TODO: [미션4 - 2단계] 내 예약 목록 조회 시,
+           예약 완료 상태일 때 결제 정보를 함께 보여주기Add commentMore actions
+           결제 정보 필드명은 자신의 response 에 맞게 변경하기
+           */
             row.insertCell(4).textContent = '';
+            row.insertCell(5).textContent = item.paymentKey;
+            row.insertCell(6).textContent = item.amount;
         }
     });
 }
