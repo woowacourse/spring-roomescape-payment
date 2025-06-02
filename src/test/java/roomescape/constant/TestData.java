ffd237189f03ec32a9c5c09e6c1c3b4893c8fc79
taek2222 <oot309@daum.net>
2025-06-02 19:19:04 +0900

    refactor: TestData 상수에 final 키워드 추가

--- src/test/java/roomescape/constant/TestData.java
+++ src/test/java/roomescape/constant/TestData.java
@@ -5,8 +5,6 @@ public final class TestData {
     private TestData() {
     }
 
-    public static int MEMBER_COUNT = 2;
-    public static int RESERVATION_COUNT = 2;
-    public static int RESERVATION_TIME_COUNT = 2;
-    public static int THEME_COUNT = 2;
+    public static final int RESERVATION_COUNT = 2;
+    public static final int RESERVATION_TIME_COUNT = 2;
 }
