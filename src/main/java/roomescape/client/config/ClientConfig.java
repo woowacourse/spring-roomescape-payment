b91411da046a7dc21cafe343ed108e5827be7d94
yesjuhee <juhee0924@g.skku.edu>
2025-06-02 22:02:41 +0900

    refactor: `HttpHeaders` 상수 사용으로 코드 가독성 개선

--- src/main/java/roomescape/client/config/ClientConfig.java
+++ src/main/java/roomescape/client/config/ClientConfig.java
@@ -5,6 +5,7 @@ import java.util.Base64;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import roomescape.client.TossPaymentsClient;
@@ -33,7 +34,7 @@ public class ClientConfig {
                 RestClient.builder()
                         .baseUrl(baseUrl)
                         .requestFactory(requestFactory)
-                        .defaultHeader("Authorization", getBasicAuthorizationValue())
+                        .defaultHeader(HttpHeaders.AUTHORIZATION, getBasicAuthorizationValue())
                         .build(),
                 objectMapper
         );
