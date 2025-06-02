a53e905083720937b47f84c7483f903322c8f73b
rladmstn <oz33177351@gmail.com>
2025-06-02 17:12:18 +0900

    chore: data.sql 결제 데이터 추가 및 예약 데이터 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -42,38 +42,69 @@ INSERT INTO member(name, email, password, role)
 VALUES ('어드민', 'admin@gmail.com', '1234', 'ADMIN');
 
 
-INSERT INTO reservation(date, member_id, time_id, theme_id)
-VALUES (DATEADD('DAY', -1, CURRENT_DATE), 1, 1, 11);
-INSERT INTO reservation(date, member_id, time_id, theme_id)
-VALUES (DATEADD('DAY', -1, CURRENT_DATE), 2, 2, 11);
-INSERT INTO reservation(date, member_id, time_id, theme_id)
-VALUES (DATEADD('DAY', -1, CURRENT_DATE), 3, 3, 11);
+-- Payment 테이블 데이터 삽입 (payment_key는 'pay_001' ~ 'pay_014'로 구성)
+INSERT INTO payment(payment_key, amount)
+VALUES ('pay_001', 10000);
+INSERT INTO payment(payment_key, amount)
+VALUES ('pay_002', 11000);
+INSERT INTO payment(payment_key, amount)
+VALUES ('pay_003', 12000);
+INSERT INTO payment(payment_key, amount)
+VALUES ('pay_004', 9000);
+INSERT INTO payment(payment_key, amount)
+VALUES ('pay_005', 9500);
+INSERT INTO payment(payment_key, amount)
+VALUES ('pay_006', 8000);
+INSERT INTO payment(payment_key, amount)
+VALUES ('pay_007', 8500);
+INSERT INTO payment(payment_key, amount)
+VALUES ('pay_008', 8700);
+INSERT INTO payment(payment_key, amount)
+VALUES ('pay_009', 8900);
+INSERT INTO payment(payment_key, amount)
+VALUES ('pay_010', 9100);
+INSERT INTO payment(payment_key, amount)
+VALUES ('pay_011', 9300);
+INSERT INTO payment(payment_key, amount)
+VALUES ('pay_012', 9700);
+INSERT INTO payment(payment_key, amount)
+VALUES ('pay_013', 9900);
+INSERT INTO payment(payment_key, amount)
+VALUES ('pay_014', 10200);
 
-INSERT INTO reservation(date, member_id, time_id, theme_id)
-VALUES (DATEADD('DAY', -2, CURRENT_DATE), 1, 4, 9);
-INSERT INTO reservation(date, member_id, time_id, theme_id)
-VALUES (DATEADD('DAY', -2, CURRENT_DATE), 2, 5, 9);
+INSERT INTO reservation(date, member_id, time_id, theme_id, payment_key)
+VALUES (DATEADD('DAY', -1, CURRENT_DATE), 1, 1, 11, 'pay_001');
+INSERT INTO reservation(date, member_id, time_id, theme_id, payment_key)
+VALUES (DATEADD('DAY', -1, CURRENT_DATE), 2, 2, 11, 'pay_002');
+INSERT INTO reservation(date, member_id, time_id, theme_id, payment_key)
+VALUES (DATEADD('DAY', -1, CURRENT_DATE), 3, 3, 11, 'pay_003');
 
-INSERT INTO reservation(date, member_id, time_id, theme_id)
-VALUES (DATEADD('DAY', -3, CURRENT_DATE), 1, 1, 1);
-INSERT INTO reservation(date, member_id, time_id, theme_id)
-VALUES (DATEADD('DAY', -3, CURRENT_DATE), 2, 2, 2);
-INSERT INTO reservation(date, member_id, time_id, theme_id)
-VALUES (DATEADD('DAY', -3, CURRENT_DATE), 3, 3, 3);
-INSERT INTO reservation(date, member_id, time_id, theme_id)
-VALUES (DATEADD('DAY', -4, CURRENT_DATE), 1, 4, 4);
-INSERT INTO reservation(date, member_id, time_id, theme_id)
-VALUES (DATEADD('DAY', -4, CURRENT_DATE), 2, 5, 5);
-INSERT INTO reservation(date, member_id, time_id, theme_id)
-VALUES (DATEADD('DAY', -4, CURRENT_DATE), 3, 1, 6);
-INSERT INTO reservation(date, member_id, time_id, theme_id)
-VALUES (DATEADD('DAY', -5, CURRENT_DATE), 1, 2, 7);
-INSERT INTO reservation(date, member_id, time_id, theme_id)
-VALUES (DATEADD('DAY', -5, CURRENT_DATE), 2, 3, 8);
-INSERT INTO reservation(date, member_id, time_id, theme_id)
-VALUES (DATEADD('DAY', -8, CURRENT_DATE), 3, 4, 10);
-INSERT INTO reservation(date, member_id, time_id, theme_id)
-VALUES (DATEADD('DAY', -8, CURRENT_DATE), 1, 5, 10);
+INSERT INTO reservation(date, member_id, time_id, theme_id, payment_key)
+VALUES (DATEADD('DAY', -2, CURRENT_DATE), 1, 4, 9, 'pay_004');
+INSERT INTO reservation(date, member_id, time_id, theme_id, payment_key)
+VALUES (DATEADD('DAY', -2, CURRENT_DATE), 2, 5, 9, 'pay_005');
+
+INSERT INTO reservation(date, member_id, time_id, theme_id, payment_key)
+VALUES (DATEADD('DAY', -3, CURRENT_DATE), 1, 1, 1, 'pay_006');
+INSERT INTO reservation(date, member_id, time_id, theme_id, payment_key)
+VALUES (DATEADD('DAY', -3, CURRENT_DATE), 2, 2, 2, 'pay_007');
+INSERT INTO reservation(date, member_id, time_id, theme_id, payment_key)
+VALUES (DATEADD('DAY', -3, CURRENT_DATE), 3, 3, 3, 'pay_008');
+
+INSERT INTO reservation(date, member_id, time_id, theme_id, payment_key)
+VALUES (DATEADD('DAY', -4, CURRENT_DATE), 1, 4, 4, 'pay_009');
+INSERT INTO reservation(date, member_id, time_id, theme_id, payment_key)
+VALUES (DATEADD('DAY', -4, CURRENT_DATE), 2, 5, 5, 'pay_010');
+INSERT INTO reservation(date, member_id, time_id, theme_id, payment_key)
+VALUES (DATEADD('DAY', -4, CURRENT_DATE), 3, 1, 6, 'pay_011');
+
+INSERT INTO reservation(date, member_id, time_id, theme_id, payment_key)
+VALUES (DATEADD('DAY', -5, CURRENT_DATE), 1, 2, 7, 'pay_012');
+INSERT INTO reservation(date, member_id, time_id, theme_id, payment_key)
+VALUES (DATEADD('DAY', -5, CURRENT_DATE), 2, 3, 8, 'pay_013');
+
+INSERT INTO reservation(date, member_id, time_id, theme_id, payment_key)
+VALUES (DATEADD('DAY', -8, CURRENT_DATE), 3, 4, 10, 'pay_014');
 
 INSERT INTO waiting(member_id, reservation_id, created_at)
 VALUES (2, 1, '2025-05-01 20:41:04.077864'); -- 1 번째 예약대기
