36b9248751748d3602e9224148ec78902b298de3
taek2222 <oot309@daum.net>
2025-06-02 19:18:04 +0900

    refactor: 사용되지 않는 기본 생성자 제거

--- src/main/java/roomescape/exception/PaymentClientException.java
+++ src/main/java/roomescape/exception/PaymentClientException.java
@@ -2,9 +2,6 @@ package roomescape.exception;
 
 public class PaymentClientException extends RuntimeException {
 
-    public PaymentClientException() {
-    }
-
     public PaymentClientException(String message) {
         super(message);
     }
