9d44f24c1b14d70a5f8e810a961d3503087192e6
2jin2.1031 <2jin2.1031@gmail.com>
2025-06-02 22:20:01 +0900

    refactor: 500대 에러까지 사용자 정의 예외로 처리

--- src/main/java/roomescape/payment/TossRestClient.java
+++ src/main/java/roomescape/payment/TossRestClient.java
@@ -1,14 +1,12 @@
 package roomescape.payment;
 
+import java.nio.charset.StandardCharsets;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 import roomescape.payment.domain.TossPayment;
 import roomescape.payment.domain.dto.PaymentRequestDto;
 import roomescape.payment.exception.InvalidPaymentException;
-
-import java.nio.charset.StandardCharsets;
 
 @Component
 public class TossRestClient {
@@ -24,7 +22,7 @@ public class TossRestClient {
                 .uri("/v1/payments/confirm")
                 .body(requestDto)
                 .retrieve()
-                .onStatus(HttpStatusCode::is4xxClientError, (req, res) -> {
+                .onStatus(statusCode -> statusCode.is4xxClientError() || statusCode.is5xxServerError(), (req, res) -> {
                     String body = new String(res.getBody().readAllBytes(), StandardCharsets.UTF_8);
                     HttpStatus httpStatus = HttpStatus.valueOf(res.getStatusCode().value());
                     throw new InvalidPaymentException(body, httpStatus);
