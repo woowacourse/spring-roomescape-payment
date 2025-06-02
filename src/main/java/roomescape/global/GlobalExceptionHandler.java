eb8ce59f4c1c4c1a93c2fe0992e64489bf5fb146
Dompoo <dlckdrms0517@gmail.com>
2025-06-02 21:45:49 +0900

    feat: 예외 응답 전에 로깅 추가

--- src/main/java/roomescape/global/GlobalExceptionHandler.java
+++ src/main/java/roomescape/global/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package roomescape.global;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
@@ -10,25 +11,30 @@ import javax.naming.AuthenticationException;
 import java.util.NoSuchElementException;
 
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> handleIllegalArgumentException(final IllegalArgumentException e) {
+        log.warn("handled IllegalArgumentException: {}", e.getMessage(), e);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 
     @ExceptionHandler(NoSuchElementException.class)
     public ResponseEntity<String> handleNoSuchElementException(final NoSuchElementException e) {
+        log.warn("handled NoSuchException: {}", e.getMessage(), e);
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
     }
 
     @ExceptionHandler(AuthenticationException.class)
     public String handleAuthenticationException(final AuthenticationException e) {
+        log.warn("handled AuthenticationException: {}", e.getMessage(), e);
         return "login";
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ProblemDetail> handleException(final Exception e) {
+        log.warn("handled Exception: {}", e.getMessage(), e);
         return ResponseEntity.badRequest().body(ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, e.getMessage()));
     }
 }
