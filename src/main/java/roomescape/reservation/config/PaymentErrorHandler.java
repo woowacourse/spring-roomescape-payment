662d6a718dcde4bc3377ca1af8fa8ac18be76cdf
taek2222 <oot309@daum.net>
2025-06-02 22:42:51 +0900

    refactor: PaymentErrorHandler에서 ObjectMapper 인스턴스 변경

--- src/main/java/roomescape/reservation/config/PaymentErrorHandler.java
+++ src/main/java/roomescape/reservation/config/PaymentErrorHandler.java
@@ -17,12 +17,7 @@ import roomescape.exception.PaymentClientException;
 public class PaymentErrorHandler implements ResponseErrorHandler {
 
     private static final String SERVER_ERROR_MESSAGE = "서버의 문제가 발생했습니다.";
-
-    private final ObjectMapper objectMapper;
-
-    public PaymentErrorHandler(ObjectMapper objectMapper) {
-        this.objectMapper = objectMapper;
-    }
+    private static final ObjectMapper MAPPER = new ObjectMapper();
 
     @Override
     public boolean hasError(ClientHttpResponse response) throws IOException {
@@ -37,7 +32,7 @@ public class PaymentErrorHandler implements ResponseErrorHandler {
             log.error("Payment API 에러 발생 - URL: {}, Method: {}, Status: {}, Response: {}",
                     url, method, response.getStatusCode(), responseBody);
 
-            Response errorResponse = objectMapper.readValue(responseBody, Response.class);
+            Response errorResponse = MAPPER.readValue(responseBody, Response.class);
             throw new PaymentClientException(errorResponse.message());
         } catch (JsonProcessingException e) {
             log.error("Payment API Json 에러 - URL: {}, Method: {}, Status: {}",
