f9c780b83f8227a5dd8462c704919b08d109ed2b
Minuring <mw9505@gmail.com>
2025-06-03 14:23:09 +0900

    refactor: Base64 Encoder 상수 선언

--- src/main/java/roomescape/infrastructure/EncodeUtil.java
+++ src/main/java/roomescape/infrastructure/EncodeUtil.java
@@ -2,12 +2,14 @@ package roomescape.infrastructure;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
+import java.util.Base64.Encoder;
 
 public class EncodeUtil {
 
+    private static final Encoder BASE_64_ENCODER = Base64.getEncoder();
+
     public static String encodeBase64(final String string) {
-        var base64Encoder = Base64.getEncoder();
-        var encodedBytes = base64Encoder.encode(string.getBytes(StandardCharsets.UTF_8));
+        var encodedBytes = BASE_64_ENCODER.encode(string.getBytes(StandardCharsets.UTF_8));
         return new String(encodedBytes);
     }
 }
