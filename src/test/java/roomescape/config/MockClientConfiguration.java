303f110e183a0196066ecff53aaec3e3280b1532
taek2222 <oot309@daum.net>
2025-06-03 12:13:21 +0900

    test: 결제 오류 핸들러 클래스 구조 개선 및 테스트 추가

--- src/test/java/roomescape/config/MockClientConfiguration.java
+++ src/test/java/roomescape/config/MockClientConfiguration.java
@@ -5,9 +5,20 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.web.client.RestClient;
+import roomescape.reservation.config.TossClientErrorHandler;
+import roomescape.reservation.config.TossServerErrorHandler;
 
 @Configuration
 public class MockClientConfiguration {
+
+    private final TossServerErrorHandler tossServerErrorHandler;
+    private final TossClientErrorHandler tossClientErrorHandler;
+
+    public MockClientConfiguration(TossServerErrorHandler tossServerErrorHandler,
+                                   TossClientErrorHandler tossClientErrorHandler) {
+        this.tossServerErrorHandler = tossServerErrorHandler;
+        this.tossClientErrorHandler = tossClientErrorHandler;
+    }
 
     @Bean
     public MockServerRestClientCustomizer mockServerRestClientCustomizer() {
@@ -20,6 +31,9 @@ public class MockClientConfiguration {
         customizer.customize(builder);
         return builder
                 .baseUrl("https://api.tosspayments.com/v1/payments")
+                .defaultStatusHandler(tossServerErrorHandler)
+                .defaultStatusHandler(tossClientErrorHandler)
+
                 .build();
     }
 
