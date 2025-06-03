a2823d62f37e5e63a6649f3b7eba06cc46b1e7dd
jhpark1227 <junhyeok0246@naver.com>
2025-06-03 19:38:36 +0900

    chore: js 수정

--- src/main/resources/static/js/ranking.js
+++ src/main/resources/static/js/ranking.js
@@ -16,7 +16,7 @@ function render(data) {
         const description = theme.description;
 
         const htmlContent = `
-            <img class="mr-3 img-thumbnail" src="${thumbnail}" alt="${name}">
+            <img class="mr-3 img-thumbnail" src="${thumbnail}" alt="${name}" style="width: 235px; height: 309px;">
             <div class="media-body">
                 <h5 class="mt-0 mb-1">${name}</h5>
                 ${description}
