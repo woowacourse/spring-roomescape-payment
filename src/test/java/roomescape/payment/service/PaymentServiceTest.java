0de888a847b80efe9bbc1bbcc62b31af60cf82ff
2jin2.1031 <2jin2.1031@gmail.com>
2025-06-03 20:35:09 +0900

    test: 불필요한 @SpringBootTest 제거 및 공통 필드 분리

--- src/test/java/roomescape/payment/service/PaymentServiceTest.java
+++ src/test/java/roomescape/payment/service/PaymentServiceTest.java
@@ -6,21 +6,19 @@ import static org.mockito.Mockito.when;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.http.HttpStatus;
+import roomescape.payment.global.domain.Payment;
 import roomescape.payment.global.domain.dto.PaymentRequestDto;
 import roomescape.payment.global.exception.InvalidPaymentException;
 import roomescape.payment.toss.TossPaymentRestClient;
-import roomescape.payment.global.domain.Payment;
 import roomescape.payment.toss.service.TossPaymentService;
 
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class PaymentServiceTest {
+
+    private final TossPaymentRestClient mockRestClient = mock(TossPaymentRestClient.class);
 
     @Test
     void 결제_승인_요청시_200_OK() {
-        TossPaymentRestClient mockRestClient = mock(TossPaymentRestClient.class);
         PaymentRequestDto dto = new PaymentRequestDto("paymentKey", "orderId", 1000, "NORMAL");
 
         Payment dummyPayment = new Payment("paymentKey", "orderId", 1000, "NORMAL");
