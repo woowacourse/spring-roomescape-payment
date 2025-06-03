789841bf380e450b8394ff59939844e767e7ed5b
BWY <quantumjump10@gmail.com>
2025-06-03 16:30:41 +0900

    feat: PaymentProperties 추가

---
+++ src/main/java/roomescape/payment/config/PaymentProperties.java
@@ -0,0 +1,25 @@
+package roomescape.payment.config;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter
+@AllArgsConstructor
+@ConfigurationProperties(prefix = "payment")
+public class PaymentProperties {
+
+    private final Map<String, Vendor> properties = new HashMap<>();
+
+    @Getter
+    @AllArgsConstructor
+    public static class Vendor {
+        private String secretKey;
+        private String baseUrl;
+    }
+}
+
+
