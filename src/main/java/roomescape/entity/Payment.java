5fd5799ee6c779681a8506c2ddf2b78f461a871a
geonwoo <kysub99@naver.com>
2025-06-04 18:03:04 +0900

    feat: 결제 도메인 추가

---
+++ src/main/java/roomescape/entity/Payment.java
@@ -0,0 +1,45 @@
+package roomescape.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "payment_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String orderId;
+
+    @Column(nullable = false)
+    private int amount;
+
+    @Column(nullable = false)
+    private String paymentKey;
+
+    @Column(nullable = false)
+    private String paymentType;
+
+    @OneToOne
+    @JoinColumn(name = "reservation_id")
+    private Reservation reservation;
+
+    public Payment() {
+    }
+
+    public Payment(String orderId, int amount, String paymentKey, String paymentType, Reservation reservation) {
+        this.orderId = orderId;
+        this.amount = amount;
+        this.paymentKey = paymentKey;
+        this.paymentType = paymentType;
+        this.reservation = reservation;
+    }
+}
