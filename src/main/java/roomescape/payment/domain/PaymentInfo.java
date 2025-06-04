d05099d1b9c524a71cc0f7d6989adc04276e0f23
supernovaMK <minkikim2002@gmail.com>
2025-06-04 20:45:27 +0900

    feat: 기존 PaymentInfo entity로 변경

--- src/main/java/roomescape/payment/domain/PaymentInfo.java
+++ src/main/java/roomescape/payment/domain/PaymentInfo.java
@@ -1,17 +1,38 @@
 package roomescape.payment.domain;
 
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import lombok.Getter;
+import roomescape.reservation.domain.Reservation;
 
 @Getter
+@Entity
 public class PaymentInfo {
-    private final String paymentKey;
-    private final String orderId;
-    private final long amount;
 
-    public PaymentInfo(String paymentKey, String orderId, long amount) {
+    @Id
+    private String paymentKey;
+
+    private String orderId;
+
+    private long amount;
+
+    @OneToOne
+    @JoinColumn(name = "reservation_id", nullable = true, unique = true)
+    private Reservation reservation;
+
+    protected PaymentInfo() {
+    }
+
+    public PaymentInfo(String paymentKey, String orderId, long amount, Reservation reservation) {
         this.paymentKey = paymentKey;
         this.orderId = orderId;
         this.amount = amount;
+        this.reservation = reservation;
+    }
+
+    public void disconnectReservation() {
+        this.reservation = null;
     }
 }
