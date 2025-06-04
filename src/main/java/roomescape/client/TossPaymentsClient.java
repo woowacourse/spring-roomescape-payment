455c2bdeca719a06344c0f4fb1c1b205a59928e8
yesjuhee <juhee0924@g.skku.edu>
2025-06-04 16:00:38 +0900

    feat: 내 예약 조회 페이지 클라이언트 코드 수정

--- src/main/java/roomescape/client/TossPaymentsClient.java
+++ src/main/java/roomescape/client/TossPaymentsClient.java
@@ -3,7 +3,6 @@ package roomescape.client;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.http.client.ClientHttpResponse;
