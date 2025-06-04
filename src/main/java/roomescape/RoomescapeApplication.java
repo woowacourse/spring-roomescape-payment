62d6f0c66077ef60c45295e6ba397865006f00b1
Dompoo <dlckdrms0517@gmail.com>
2025-06-05 00:50:05 +0900

    docs: 서버 정보 추가

--- src/main/java/roomescape/RoomescapeApplication.java
+++ src/main/java/roomescape/RoomescapeApplication.java
@@ -1,8 +1,21 @@
 package roomescape;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "방탈출 예약 및 결제 API",
+                version = "v1.0.0"
+        ),
+        servers = {
+                @Server(url = "http://localhost:8080", description = "로컬 개발 서버"),
+                @Server(url = "https://13.209.85.78:8080", description = "개발 환경 서버"),
+        }
+)
 @SpringBootApplication
 public class RoomescapeApplication {
     public static void main(String[] args) {
