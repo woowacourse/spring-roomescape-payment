7286dab8a2757b1b0ffa872b556001d040ba2a04
jhpark1227 <junhyeok0246@naver.com>
2025-06-04 19:20:03 +0900

    test: 결제 생성 API 테스트 작성

---
+++ src/test/java/roomescape/api/PaymentApiTest.java
@@ -0,0 +1,51 @@
+package roomescape.api;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import roomescape.business.model.entity.Payment;
+import roomescape.business.model.vo.PaymentStatus;
+import roomescape.infrastructure.PaymentRepository;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class PaymentApiTest {
+
+    @Autowired
+    private PaymentRepository paymentRepository;
+
+    @Test
+    void 결제를_생성한다() {
+        // given
+        Map<String, Object> body = Map.of(
+                "orderId", "orderId1",
+                "amount", 1000L
+        );
+        // when & then
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(body)
+                .when().post("/payments")
+                .then().log().all()
+                .statusCode(201);
+    }
+
+    @Test
+    void 중복_결제_생성이면_400에러가_발생한다() {
+        // given
+        paymentRepository.save(Payment.restore("id", "orderId1", "paymentKey", 1000L, PaymentStatus.IN_PROGRESS, null));
+        Map<String, Object> body = Map.of(
+                "orderId", "orderId1",
+                "amount", 1000L
+        );
+        // when & then
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(body)
+                .when().post("/payments")
+                .then().log().all()
+                .statusCode(400);
+    }
+}
