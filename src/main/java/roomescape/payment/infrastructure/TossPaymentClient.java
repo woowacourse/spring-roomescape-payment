11be1fa57d619fdb46635d5c3508924d258cabe7
julie <7jujubuilds@gmail.com>
2025-06-04 16:53:52 +0900

    refactor(payment): 외부 API가 반환하는 오류 중 사용자가 원인이 아닌 경우에 로깅 진행

--- src/main/java/roomescape/payment/infrastructure/TossPaymentClient.java
+++ src/main/java/roomescape/payment/infrastructure/TossPaymentClient.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.web.client.RestClient;
@@ -14,6 +16,8 @@ import roomescape.payment.infrastructure.dto.TossPaymentResponse;
 import roomescape.reservation.presentation.dto.ReservationRequest;
 
 public class TossPaymentClient implements PaymentClient {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TossPaymentClient.class);
 
     @Value("${payment.secret-key.test}")
     private String SECRET_KEY;
@@ -57,6 +61,7 @@ public class TossPaymentClient implements PaymentClient {
 
     private void handleErrorResponse(HttpStatusCode statusCode, TossPaymentErrorResponse errorResponse) {
         if (TossPaymentErrorMessage.contains(errorResponse.getCode())) {
+            LOGGER.warn(errorResponse.getMessage());
             throw new PaymentException(statusCode, "시스템 오류로 인해 결제가 실패하였습니다. 고객센터에 문의해주세요.");
         }
         throw new PaymentException(statusCode, errorResponse.getMessage());
