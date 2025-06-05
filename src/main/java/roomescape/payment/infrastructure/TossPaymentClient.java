ca2001c4fb5209048c71369e7ba776097182494b
MinJun Choi <mj043000@naver.com>
2025-06-05 22:00:35 +0900

    refactor: TossPaymentClient 예외 처리 로직 개선 및 HttpComponentsClientHttpRequestFactory 적용
    - 4xx 및 5xx 에러 처리 메서드 분리 및 추가 예외 처리 로직 적용
    - HttpComponentsClientHttpRequestFactory로 요청 팩토리 변경
    - 의존성에 Apache HttpClient 추가

--- src/main/java/roomescape/payment/infrastructure/TossPaymentClient.java
+++ src/main/java/roomescape/payment/infrastructure/TossPaymentClient.java
@@ -1,15 +1,18 @@
 package roomescape.payment.infrastructure;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 import roomescape.payment.application.PaymentClient;
+import roomescape.payment.application.PaymentException;
 import roomescape.payment.application.dto.PaymentRequest;
 import roomescape.payment.application.dto.PaymentResponse;
 import roomescape.payment.infrastructure.dto.TossPaymentResponse;
@@ -28,20 +31,28 @@ public class TossPaymentClient implements PaymentClient {
                 .uri("v1/payments/confirm")
                 .body(request)
                 .retrieve()
-                .onStatus(HttpStatusCode::isError,
-                        (req, res) -> {
-                            handleException(res);
-                        })
+                .onStatus(HttpStatusCode::is4xxClientError, this::handle4xxError)
+                .onStatus(HttpStatusCode::is5xxServerError, this::handle5xxError)
                 .body(TossPaymentResponse.class);
     }
 
-    private void handleException(final ClientHttpResponse res) throws IOException {
-        final String status = res.getStatusCode().toString();
-        final int code = Integer.parseInt(status.substring(0, 3));
-        if (code == 401) {
-            throw new TossPaymentException(HttpStatus.INTERNAL_SERVER_ERROR, "secret key가 유효하지 않습니다");
+    private void handle4xxError(HttpRequest httpRequest, ClientHttpResponse clientHttpResponse) {
+        try {
+            JsonNode node = objectMapper.readTree(clientHttpResponse.getBody());
+            String code = node.path("code").asText();
+            String message = node.path("message").asText();
+            if (code.equals("UNAUTHORIZED_KEY")) {
+                throw new PaymentException(HttpStatus.INTERNAL_SERVER_ERROR,
+                        "방탈출 예약 서비스 결제 시스템에서 오류가 발생했습니다. 잠시 후 다시 시도해주세요.");
+            }
+            throw new PaymentException(HttpStatus.BAD_REQUEST, message);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-        throw new TossPaymentException(HttpStatus.BAD_REQUEST, "유효하지 않은 결제방식입니다.");
+    }
+
+    private void handle5xxError(HttpRequest httpRequest, ClientHttpResponse clientHttpResponse) {
+        throw new PaymentException(HttpStatus.INTERNAL_SERVER_ERROR, "토스 결제 시스템에서 오류가 발생했습니다. 잠시 후 다시 시도해주세요.");
     }
 }
 
