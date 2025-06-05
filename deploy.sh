07befc13c27d0a2d73372c213cf0e0377f364aea
7i.w__k <tizmfns1218@Naver.com>
2025-06-05 11:22:11 +0900

    refactor: 도커 컴포즈 권한 처리

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ echo "==== 1. 최신 코드 가져오는 중... ===="
 git pull origin $BRANCH
 
 echo "==== 2. 도커 컴포즈 실행 (docker compose up -d) ===="
-docker-compose -f $DOCKER_COMPOSE_FILE up -d
+sudo docker-compose -f $DOCKER_COMPOSE_FILE up -d
 
 echo "==== 3. 실행 중인 Spring 애플리케이션 종료 (중복 방지) ===="
 pkill -f $JAR_NAME || true
