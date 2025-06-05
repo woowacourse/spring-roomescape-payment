d91419069f32140923efb9a6b633348c9e119263
jhan0121 <jhan0121@gmail.com>
2025-06-05 22:11:03 +0900

    feat: aop 기반 log 출력 기능 추가

---
+++ src/main/java/roomescape/global/LogAspect.java
@@ -0,0 +1,108 @@
+package roomescape.global;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import jakarta.servlet.http.HttpServletRequest;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+@Aspect
+@Component
+public class LogAspect {
+
+    private static final Logger log = LoggerFactory.getLogger(LogAspect.class);
+
+    @Pointcut("execution(* roomescape..*.*(..)) && !execution(* roomescape.global..*(..))")
+    public void all() {
+    }
+
+    @Pointcut("execution(* roomescape..*Controller.*(..))")
+    public void controller() {
+    }
+
+    @Pointcut("@annotation(org.springframework.web.bind.annotation.ExceptionHandler)")
+    public void exceptionHandler() {
+    }
+
+    @Around("all()")
+    public Object logging(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        try {
+            return joinPoint.proceed();
+        } finally {
+            long end = System.currentTimeMillis();
+            long runMilTime = end - start;
+            log.info("{} | time = {}ms", joinPoint.getSignature(), runMilTime);
+        }
+    }
+
+    @Around("controller()")
+    public Object loggingController(ProceedingJoinPoint joinPoint) throws Throwable {
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+
+        String controllerName = joinPoint.getSignature().getDeclaringType().getName();
+        String methodName = joinPoint.getSignature().getName();
+        Map<String, Object> params = new HashMap<>();
+
+        try {
+            String decodedURI = URLDecoder.decode(request.getRequestURI(), StandardCharsets.UTF_8);
+
+            params.put("controller", controllerName);
+            params.put("method", methodName);
+            params.put("params", getParams(request));
+            params.put("log_time", System.currentTimeMillis());
+            params.put("request_uri", decodedURI);
+            params.put("http_method", request.getMethod());
+        } catch (Exception e) {
+            log.error("LoggerAspect error", e);
+        }
+
+        log.info("[{}] {}", params.get("http_method"), params.get("request_uri"));
+        log.info("method: {}.{}", params.get("controller"), params.get("method"));
+        log.info("params: {}", params.get("params"));
+
+        return joinPoint.proceed();
+    }
+
+    @Around("exceptionHandler()")
+    public Object loggingException(ProceedingJoinPoint joinPoint) throws Throwable {
+        Exception exception = null;
+
+        for (Object arg : joinPoint.getArgs()) {
+            if (arg instanceof Exception) {
+                exception = (Exception) arg;
+                break;
+            }
+        }
+
+        if (exception != null) {
+            log.error("exception {} thrown message: {}", exception.getClass().getName(), exception.getMessage());
+        }
+
+        return joinPoint.proceed();
+    }
+
+    private ObjectNode getParams(HttpServletRequest request) {
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectNode jsonNode = mapper.createObjectNode();
+        Enumeration<String> params = request.getParameterNames();
+        while (params.hasMoreElements()) {
+            String param = params.nextElement();
+            String replaceParam = param.replaceAll("\\.", "-");
+            jsonNode.put(replaceParam, request.getParameter(param));
+        }
+        return jsonNode;
+    }
+}
