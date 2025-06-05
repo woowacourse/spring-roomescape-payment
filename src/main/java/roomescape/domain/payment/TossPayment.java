47dfeac25c04d6cce2639a75ffd52b4bfa94d014
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-06-05 23:54:02 +0900

    feat: 토스 페이먼츠 엔티티 추가

--- src/main/java/roomescape/domain/payment/TossPayment.java
+++ src/main/java/roomescape/domain/payment/TossPayment.java
@@ -1,12 +1,7 @@
 package roomescape.domain.payment;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import roomescape.domain.BaseEntity;
@@ -22,12 +17,31 @@ public class TossPayment extends BaseEntity {
 
     private long amount;
 
-    public TossPayment(
+    private PaymentStatus paymentStatus;
+
+    private TossPayment(
             final String paymentKey,
             final String orderId,
-            final long amount) {
+            final long amount,
+            final PaymentStatus paymentStatus) {
         this.paymentKey = paymentKey;
         this.orderId = orderId;
         this.amount = amount;
+        this.paymentStatus = paymentStatus;
+    }
+
+    public static TossPayment init(
+            final String paymentKey,
+            final String orderId,
+            final long amount) {
+        return new TossPayment(paymentKey, orderId, amount, PaymentStatus.PENDING);
+    }
+
+    public void approve() {
+        paymentStatus = PaymentStatus.APPROVED;
+    }
+
+    public void fail() {
+        paymentStatus = PaymentStatus.FAILED;
     }
 }
