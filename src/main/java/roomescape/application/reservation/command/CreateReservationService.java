4213775b61963f16860449bb9157026ba9856524
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-06-06 01:25:30 +0900

    feat: 예약 생성에서 결제 승인 로직 삭제

--- src/main/java/roomescape/application/reservation/command/CreateReservationService.java
+++ src/main/java/roomescape/application/reservation/command/CreateReservationService.java
@@ -25,14 +25,12 @@ import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
 public class CreateReservationService {
 
     private final ReservationRepository reservationRepository;
     private final ReservationTimeRepository reservationTimeRepository;
     private final ThemeRepository themeRepository;
     private final MemberRepository memberRepository;
-    private final TossPaymentService tossPaymentService;
     private final Clock clock;
 
     public Long reserve(final CreateReservationCommand command) {
@@ -44,11 +42,6 @@ public class CreateReservationService {
         reservation.validateReservable(LocalDateTime.now(clock));
         final Reservation savedReservation = reservationRepository.save(reservation);
         return savedReservation.getId();
-    }
-
-    public Long reserve(final CreateReservationWithPaymentCommand command) {
-        tossPaymentService.approve(command.toPaymentCommand());
-        return reserve(command.toCreateWithoutPaymentCommand());
     }
 
     private Member getMember(final Long memberId) {
@@ -66,8 +59,14 @@ public class CreateReservationService {
                 .orElseThrow(() -> new ThemeException("존재하지 않는 테마입니다."));
     }
 
-    private void validateDuplicateReservation(final LocalDate date, final ReservationTime time, final Theme theme) {
-        final boolean duplicated = reservationRepository.existsByDateAndTimeIdAndThemeId(date, time.getId(), theme.getId());
+    private void validateDuplicateReservation(
+            final LocalDate date,
+            final ReservationTime time,
+            final Theme theme) {
+        final boolean duplicated = reservationRepository.existsByDateAndTimeIdAndThemeId(
+                date,
+                time.getId(),
+                theme.getId());
         if (duplicated) {
             throw new ReservationException("날짜와 시간이 중복된 예약이 존재합니다.");
         }
