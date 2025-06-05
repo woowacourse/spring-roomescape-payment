1a23189f288e79a798baf6ce1e9b15680aac6a36
민경윤 <ruddbs3308@naver.com>
2025-06-05 18:54:07 +0900

    test: PaymentQueryUseCase 기능 검증

--- src/test/java/roomescape/payment/service/usecase/PaymentCommandUseCaseTest.java
+++ src/test/java/roomescape/payment/service/usecase/PaymentCommandUseCaseTest.java
@@ -2,7 +2,6 @@ package roomescape.payment.service.usecase;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import jakarta.persistence.EntityManager;
