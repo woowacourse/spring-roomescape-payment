15efd7539c63559abf1c43bfde11c67c394dd9c6
ChoiDongjun <adong0808@naver.com>
2025-06-06 16:54:35 +0900

    chore: 배포 스크립트 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,7 @@
 set -e
 
 # 🛠️ 배포에 사용할 변수들
-REMOTE_APP_DIR="/home/ec2-moru/spring-roomescape-payment"
+REMOTE_APP_DIR="~/spring-roomescape-payment"
 GIT_REPO="https://github.com/Choidongjun0830/spring-roomescape-payment"
 BRANCH="step2"
 JAR_NAME="spring-roomescape-payment.jar"
@@ -11,14 +11,10 @@ echo "📁 작업 디렉토리로 이동"
 mkdir -p ${REMOTE_APP_DIR}
 cd ${REMOTE_APP_DIR}
 
-if [ ! -d ".git" ]; then
-  echo "🔄 Git 저장소 clone 중..."
-  git clone -b ${BRANCH} ${GIT_REPO} .
-else
-  echo "🔄 Git pull로 최신화 중..."
-  git checkout ${BRANCH}
-  git pull origin ${BRANCH}
-fi
+echo "🔄 Git pull로 최신화 중..."
+git checkout ${BRANCH}
+git pull origin ${BRANCH}
+
 
 echo "🔨 빌드 시작..."
 ./gradlew bootJar
