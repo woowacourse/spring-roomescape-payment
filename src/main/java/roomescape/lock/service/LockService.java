192ef2b488a28d60c148d604cd49ff343f9d5770
ryan-dia <cjfdnjs158@naver.com>
2025-06-06 19:45:24 +0900

    refactor: lock service 변경

--- src/main/java/roomescape/lock/service/LockService.java
+++ src/main/java/roomescape/lock/service/LockService.java
@@ -1,12 +1,10 @@
 package roomescape.lock.service;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.LockModeType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
-import roomescape.lock.Lock;
+import roomescape.lock.LockEntity;
 import roomescape.lock.repository.LockRepository;
 
 @Service
@@ -14,14 +12,10 @@ import roomescape.lock.repository.LockRepository;
 public class LockService {
 
     private final LockRepository lockRepository;
-    private final EntityManager em;
 
     @Transactional(isolation = Isolation.REPEATABLE_READ)
     public void acquireLock(final String lockKey) {
-        if (!lockRepository.existsById(lockKey)) {
-            Lock newLock = new Lock(lockKey);
-            lockRepository.save(newLock);
-        }
-        em.find(Lock.class, lockKey, LockModeType.PESSIMISTIC_WRITE);
+        lockRepository.save(new LockEntity(lockKey));
+        lockRepository.findByKeyWithPessimisticWrite(lockKey);
     }
 }
