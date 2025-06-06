1071320a3041c6288e8ea06a04461a5e5979bda1
ryan-dia <cjfdnjs158@naver.com>
2025-06-06 19:45:24 +0900

    refactor: lock service 변경

--- src/main/java/roomescape/lock/LockEntity.java
+++ src/main/java/roomescape/lock/LockEntity.java
@@ -12,7 +12,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-public class Lock {
+public class LockEntity {
 
     @Id
     @Column(length = 100)
--- src/main/java/roomescape/lock/repository/LockRepository.java
+++ src/main/java/roomescape/lock/repository/LockRepository.java
@@ -1,7 +1,14 @@
 package roomescape.lock.repository;
 
+import jakarta.persistence.LockModeType;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import roomescape.lock.Lock;
+import org.springframework.data.jpa.repository.Query;
+import roomescape.lock.LockEntity;
 
-public interface LockRepository extends JpaRepository<Lock, String> {
+public interface LockRepository extends JpaRepository<LockEntity, String> {
+
+    @org.springframework.data.jpa.repository.Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select l from LockEntity l where l.lockKey = :key")
+    Optional<LockEntity> findByKeyWithPessimisticWrite(final String key);
 }
