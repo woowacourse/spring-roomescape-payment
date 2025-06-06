75f8bd75c541d8ead92ccea7ac2c8bc8ea3cefb1
minSsan <msp1125@hanyang.ac.kr>
2025-06-06 21:39:23 +0900

    chore: `@EnableRetry` 어노테이션 위치 변경 - Application -> Configuration 클래스로 이동

--- src/main/java/roomescape/config/PaymentRestClientConfig.java
+++ src/main/java/roomescape/config/PaymentRestClientConfig.java
@@ -5,10 +5,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.web.client.RestClient;
 import roomescape.common.AuthHeaderEncoder;
 
 @Configuration
+@EnableRetry
 public class PaymentRestClientConfig {
 
     @Value("${api.toss.url}")
