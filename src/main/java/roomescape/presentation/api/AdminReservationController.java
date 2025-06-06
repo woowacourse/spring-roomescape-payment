6cdf392208f54a1ba3af8d36d620d1f5d7bb9f18
ljhee92 <weilainib7@gmail.com>
2025-06-06 23:17:58 +0900

    feat: 관리자 관련 API 클래스 노출 숨김 처리

--- src/main/java/roomescape/presentation/api/AdminReservationController.java
+++ src/main/java/roomescape/presentation/api/AdminReservationController.java
@@ -1,5 +1,6 @@
 package roomescape.presentation.api;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +21,7 @@ import roomescape.presentation.dto.response.WaitingResponse;
 import java.time.LocalDate;
 import java.util.List;
 
+@Hidden
 @RestController
 @RequestMapping("/admin/reservations")
 public class AdminReservationController {
