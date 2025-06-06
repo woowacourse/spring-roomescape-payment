fad2cdfa7c613486378bc19d71a555583b91be70
taek2222 <oot309@daum.net>
2025-06-06 16:39:46 +0900

    refactor: UnauthorizedException 생성자 정리

--- src/main/java/roomescape/auth/exception/UnauthorizedException.java
+++ src/main/java/roomescape/auth/exception/UnauthorizedException.java
@@ -5,8 +5,4 @@ public class UnauthorizedException extends RuntimeException {
     public UnauthorizedException(final String message) {
         super(message);
     }
-
-    public UnauthorizedException(final String message, final Throwable cause) {
-        super(message, cause);
-    }
 }
