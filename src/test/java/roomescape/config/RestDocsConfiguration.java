06bc12789304ceace8b701ec340c067165425f12
7i.w__k <tizmfns1218@Naver.com>
2025-06-06 16:27:03 +0900

    feat: RestDocs 설정 추가

---
+++ src/test/java/roomescape/config/RestDocsConfiguration.java
@@ -0,0 +1,34 @@
+package roomescape.config;
+
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.modifyHeaders;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
+import org.springframework.restdocs.operation.preprocess.Preprocessors;
+
+@TestConfiguration
+public class RestDocsConfiguration {
+
+    @Bean
+    public RestDocumentationResultHandler write() {
+        return MockMvcRestDocumentation.document(
+                "{class-name}/{method-name}",
+                Preprocessors.preprocessRequest(
+                        modifyHeaders()
+                                .remove("Content-Length")
+                                .remove("Host"),
+                        Preprocessors.prettyPrint()),
+                Preprocessors.preprocessResponse(modifyHeaders()
+                                .remove("Content-Length")
+                                .remove("X-Content-Type-Options")
+                                .remove("X-XSS-Protection")
+                                .remove("Cache-Control")
+                                .remove("Pragma")
+                                .remove("Expires")
+                                .remove("X-Frame-Options"),
+                        Preprocessors.prettyPrint())
+        );
+    }
+}
