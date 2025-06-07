7010e023acfddce01ccdc8b0eddbbaf8307a6957
junhaa <2171326@hansung.ac.kr>
2025-06-07 17:03:25 +0900

    refactor: 클라이언트 에러 HTTPStatus 코드 변경

---
+++ src/main/java/roomescape/common/config/AopConfig.java
@@ -0,0 +1,11 @@
+package roomescape.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class AopConfig {
+    // AOP 설정을 활성화하는 빈 클래스
+    // @EnableAspectJAutoProxy 어노테이션은 Spring AOP를 활성화합니다.
+}
