5a657d6a2454dea6e8361df19f43facabef44164
seaniiio <cnc4750@naver.com>
2025-06-07 20:16:52 +0900

    feat: api 요청, 응답 로깅에 요청 Id 추가

--- src/main/java/roomescape/log/LoggingInterceptor.java
+++ src/main/java/roomescape/log/LoggingInterceptor.java
@@ -3,36 +3,48 @@ package roomescape.log;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.util.UUID;
 
 @Slf4j
 @Component
 public class LoggingInterceptor implements HandlerInterceptor {
 
     private static final String REQUEST_START_TIME_ATTRIBUTE = "requestStartTime";
+    private static final String REQUEST_ID_ATTRIBUTE = "requestId";
 
     @Override
     public boolean preHandle(final HttpServletRequest request, final HttpServletResponse response, final Object handler) throws Exception {
+        String requestId = UUID.randomUUID().toString().substring(0, 8);
+
+        request.setAttribute(REQUEST_ID_ATTRIBUTE, requestId);
         request.setAttribute(REQUEST_START_TIME_ATTRIBUTE, System.currentTimeMillis());
-        log.info(request.getMethod() + " " + request.getRequestURI() + " 요청");
+
+        MDC.put("requestId", requestId);
+
+        log.info("[{}] {} {} 요청", requestId, request.getMethod(), request.getRequestURI());
         return true;
     }
 
     @Override
     public void afterCompletion(final HttpServletRequest request, final HttpServletResponse response, final Object handler, final Exception ex) throws Exception {
-
         Long startTime = (Long) request.getAttribute(REQUEST_START_TIME_ATTRIBUTE);
+        String requestId = (String) request.getAttribute(REQUEST_ID_ATTRIBUTE);
         if (startTime != null) {
             long duration = System.currentTimeMillis() - startTime;
 
             if (ex != null) {
-                log.error(request.getMethod() + " " + request.getRequestURI() + " 처리 중 예외 발생: {}", ex.getMessage());
-                return;
+                log.error("[{}] {} {} 처리 중 예외 발생: {}, HTTP status: {}", requestId, request.getMethod(), request.getRequestURI(), ex.getMessage(), response.getStatus());
+            } else {
+                log.info("[{}] {} {} 처리 성공, 응답까지 소요 시간: {} ms, HTTP status: {}", requestId, request.getMethod(), request.getRequestURI(), duration, response.getStatus());
             }
-            log.info(request.getMethod() + " " + request.getRequestURI() + " 처리 성공, 응답까지 소요 시간: {} ms", duration);
-            return;
+        } else {
+            log.warn("[{}] {} {} 요청 시작 시간을 찾을 수 없음, HTTP status: {}", requestId, request.getMethod(), request.getRequestURI(), response.getStatus());
         }
-        log.warn(request.getMethod() + " " + request.getRequestURI() + " 처리 성공");
+
+        MDC.remove("requestId");
     }
 }
