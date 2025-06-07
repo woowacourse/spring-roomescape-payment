6d992bc6af4c6a9ee247711f0b745b9185f9c9e2
Mint <audwl03071@gmail.com>
2025-06-07 12:22:26 +0900

    feat: health check api 추가
    - spring actuator를 사용하여 health check 수행

---
+++ src/test/java/roomescape/e2e/InfraTest.java
@@ -0,0 +1,36 @@
+package roomescape.e2e;
+
+import static org.hamcrest.Matchers.equalTo;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.annotation.DirtiesContext;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+public class InfraTest {
+
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+    }
+
+    @Test
+    void healthCheckBasicOperation() {
+        RestAssured.given().log().ifValidationFails()
+                .when().get("/actuator/health")
+                .then().log().ifValidationFails()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("status", equalTo("UP"));
+    }
+}
