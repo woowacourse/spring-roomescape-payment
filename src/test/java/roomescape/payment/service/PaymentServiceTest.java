66399318ba733398a03a7b4261f5ef74f1274467
eoehd1ek <eoehd2ek@gmail.com>
2025-06-08 06:15:49 +0900

    refactor(PaymentServiceTest): 픽스처 사용

--- src/test/java/roomescape/payment/service/PaymentServiceTest.java
+++ src/test/java/roomescape/payment/service/PaymentServiceTest.java
@@ -8,6 +8,7 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 
+import fixture.PaymentFixture;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -25,13 +26,12 @@ import roomescape.payment.repository.PaymentRepository;
 @ExtendWith(MockitoExtension.class)
 class PaymentServiceTest {
 
-    private PaymentService paymentService;
-
     @Mock
     private PaymentRepository paymentRepository;
-
     @Mock
     private PaymentClient paymentClient;
+
+    private PaymentService paymentService;
 
     @BeforeEach
     void setUp() {
@@ -39,61 +39,62 @@ class PaymentServiceTest {
     }
 
     @Test
-    @DisplayName("결제 승인 API를 호출한다.")
+    @DisplayName("결제 승인 - 성공")
     void confirmPayment_Success() {
         // given
-        String paymentKey = "paymentKey";
-        String orderId = "orderId";
-        Long amount = 1000L;
-        given(paymentClient.requestPaymentConfirm(paymentKey, orderId, amount))
-                .willReturn(new PaymentConfirmResponse(paymentKey, orderId, amount, "NORMAL"));
+        Payment payment = PaymentFixture.createDefault();
+        given(paymentClient.requestPaymentConfirm(payment.getPaymentKey(), payment.getOrderId(), payment.getAmount()))
+                .willReturn(new PaymentConfirmResponse(
+                        payment.getPaymentKey(),
+                        payment.getOrderId(),
+                        payment.getAmount(),
+                        payment.getPaymentType()
+                ));
 
         // when
-        paymentService.confirmPayment(paymentKey, orderId, amount);
+        paymentService.confirmPayment(payment.getPaymentKey(), payment.getOrderId(), payment.getAmount());
 
         // then
         ArgumentCaptor<Payment> captor = ArgumentCaptor.forClass(Payment.class);
         verify(paymentRepository).save(captor.capture());
         Payment saved = captor.getValue();
         assertAll(
-                () -> assertThat(saved.getPaymentKey()).isEqualTo(paymentKey),
-                () -> assertThat(saved.getOrderId()).isEqualTo(orderId),
-                () -> assertThat(saved.getAmount()).isEqualTo(amount),
-                () -> assertThat(saved.getPaymentType()).isEqualTo("NORMAL")
+                () -> assertThat(saved.getPaymentKey()).isEqualTo(payment.getPaymentKey()),
+                () -> assertThat(saved.getOrderId()).isEqualTo(payment.getOrderId()),
+                () -> assertThat(saved.getAmount()).isEqualTo(payment.getAmount()),
+                () -> assertThat(saved.getPaymentType()).isEqualTo(payment.getPaymentType())
         );
     }
 
     @Test
-    @DisplayName("결제 승인 API를 호출해서 400 예외가 터진다.")
+    @DisplayName("결제 승인 - 400 예외 실패")
     void confirmPayment_ThrowsBadRequestException() {
         // given
-        String paymentKey = "paymentKey";
-        String orderId = "orderId";
-        Long amount = 1000L;
-        given(paymentClient.requestPaymentConfirm(paymentKey, orderId, amount))
+        Payment payment = PaymentFixture.createDefault();
+        given(paymentClient.requestPaymentConfirm(payment.getPaymentKey(), payment.getOrderId(), payment.getAmount()))
                 .willThrow(new BadRequestException("API 오류"));
 
         // when & then
         assertAll(
-                () -> assertThatThrownBy(() -> paymentService.confirmPayment(paymentKey, orderId, amount))
+                () -> assertThatThrownBy(() -> paymentService.confirmPayment(
+                        payment.getPaymentKey(), payment.getOrderId(), payment.getAmount()))
                         .isInstanceOf(BadRequestException.class),
                 () -> verify(paymentRepository, never()).save(any())
         );
     }
 
     @Test
-    @DisplayName("결제 승인 API를 호출해서 500 예외가 터진다.")
+    @DisplayName("결제 승인 - 500 예외 실패")
     void confirmPayment_ThrowsInternalServerError() {
         // given
-        String paymentKey = "paymentKey";
-        String orderId = "orderId";
-        Long amount = 1000L;
-        given(paymentClient.requestPaymentConfirm(paymentKey, orderId, amount))
+        Payment payment = PaymentFixture.createDefault();
+        given(paymentClient.requestPaymentConfirm(payment.getPaymentKey(), payment.getOrderId(), payment.getAmount()))
                 .willThrow(new ServerException("API 오류"));
 
         // when & then
         assertAll(
-                () -> assertThatThrownBy(() -> paymentService.confirmPayment(paymentKey, orderId, amount))
+                () -> assertThatThrownBy(() -> paymentService.confirmPayment(
+                        payment.getPaymentKey(), payment.getOrderId(), payment.getAmount()))
                         .isInstanceOf(ServerException.class),
                 () -> verify(paymentRepository, never()).save(any())
         );
