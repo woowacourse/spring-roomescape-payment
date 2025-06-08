213fd1b891ddf82f8913cb78b78abacfe0d07296
supernovaMK <minkikim2002@gmail.com>
2025-06-08 16:14:03 +0900

    refactor: PaymentInfo ID 필드 추가

--- src/main/java/roomescape/payment/domain/PaymentInfo.java
+++ src/main/java/roomescape/payment/domain/PaymentInfo.java
@@ -1,6 +1,8 @@
 package roomescape.payment.domain;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
@@ -12,6 +14,9 @@ import roomescape.reservation.domain.Reservation;
 public class PaymentInfo {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String paymentKey;
 
     private String orderId;
