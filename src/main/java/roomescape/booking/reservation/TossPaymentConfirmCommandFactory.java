4270154abd549c61bf31e346389947df998e618a
seaniiio <cnc4750@naver.com>
2025-06-08 19:42:46 +0900

    refactor: dto import 추가

--- src/main/java/roomescape/booking/reservation/TossPaymentConfirmCommandFactory.java
+++ src/main/java/roomescape/booking/reservation/TossPaymentConfirmCommandFactory.java
@@ -2,11 +2,12 @@ package roomescape.booking.reservation;
 
 import org.springframework.stereotype.Component;
 import roomescape.external.tosspayment.dto.TossPaymentConfirmCommand;
+import roomescape.reservationpayment.dto.ReservationPaymentRequest;
 
 @Component
 public class TossPaymentConfirmCommandFactory {
 
-    public TossPaymentConfirmCommand toPaymentConfirmCommand(roomescape.reservationpayment.dto.ReservationPaymentRequest request) {
+    public TossPaymentConfirmCommand toPaymentConfirmCommand(final ReservationPaymentRequest request) {
         return new TossPaymentConfirmCommand(request.orderId(), request.amount(), request.paymentKey());
     }
 }
