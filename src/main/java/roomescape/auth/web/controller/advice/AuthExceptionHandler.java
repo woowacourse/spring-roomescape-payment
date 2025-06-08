7f0dd08d2259ea0be966e4a123343734a54fb4b3
GiSeok Kang <ime.rokz@gmail.com>
2025-06-08 22:38:36 +0900

    feat: 예외 핸들러에 요청 정보 로깅 기능 추가
    - HttpServletRequest를 인자로 추가하여 예외 발생 시 요청 정보를 로깅
    - 각 ExceptionHandler 메서드에 traceId와 예외 정보 로깅 로직 추가
    - @Slf4j 애노테이션을 사용하여 로깅 기능 활성화

--- src/main/java/roomescape/auth/web/controller/advice/AuthExceptionHandler.java
+++ src/main/java/roomescape/auth/web/controller/advice/AuthExceptionHandler.java
@@ -3,6 +3,8 @@ package roomescape.auth.web.controller.advice;
 import static roomescape.auth.web.controller.response.AuthErrorCode.NOT_ADMIN;
 import static roomescape.auth.web.controller.response.AuthErrorCode.NOT_AUTHORIZED;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,24 +15,31 @@ import roomescape.auth.web.exception.TokenNotFoundException;
 import roomescape.global.response.ApiResponse;
 
 @RestControllerAdvice
+@Slf4j
 public class AuthExceptionHandler {
 
     @ExceptionHandler
-    public ResponseEntity<ApiResponse<Void>> handleNotAdminException(NotAdminException e) {
+    public ResponseEntity<ApiResponse<Void>> handleNotAdminException(HttpServletRequest request,
+                                                                     NotAdminException e) {
+        log.info("{} | {} {}", request.getAttribute("traceId"), e.getClass(), e.getMessage());
         return ResponseEntity
                 .status(HttpStatus.FORBIDDEN)
                 .body(ApiResponse.fail(NOT_ADMIN));
     }
 
     @ExceptionHandler
-    public ResponseEntity<ApiResponse<Void>> handleTokenNotFoundException(TokenNotFoundException e) {
+    public ResponseEntity<ApiResponse<Void>> handleTokenNotFoundException(HttpServletRequest request,
+                                                                          TokenNotFoundException e) {
+        log.info("{} | {} {}", request.getAttribute("traceId"), e.getClass(), e.getMessage());
         return ResponseEntity
                 .status(HttpStatus.UNAUTHORIZED)
                 .body(ApiResponse.fail(NOT_AUTHORIZED));
     }
 
     @ExceptionHandler(value = NotAuthorizationException.class)
-    public ResponseEntity<ApiResponse<Void>> handleNotAuthorizationException() {
+    public ResponseEntity<ApiResponse<Void>> handleNotAuthorizationException(HttpServletRequest request,
+                                                                             NotAuthorizationException e) {
+        log.info("{} | {} {}", request.getAttribute("traceId"), e.getClass(), e.getMessage());
         return ResponseEntity
                 .status(HttpStatus.UNAUTHORIZED)
                 .body(ApiResponse.fail(NOT_AUTHORIZED));
--- src/main/java/roomescape/reservation/controller/exception/ReservationExceptionHandler.java
+++ src/main/java/roomescape/reservation/controller/exception/ReservationExceptionHandler.java
@@ -5,6 +5,8 @@ import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
 import static roomescape.reservation.controller.response.ReservationErrorCode.ALREADY_RESERVATION;
 import static roomescape.reservation.controller.response.ReservationErrorCode.PAST_RESERVATION;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -14,24 +16,31 @@ import roomescape.reservation.exception.InvalidStatusTransitionException;
 import roomescape.reservation.exception.PastReservationException;
 
 @RestControllerAdvice
+@Slf4j
 public class ReservationExceptionHandler {
 
     @ExceptionHandler(PastReservationException.class)
-    public ResponseEntity<ApiResponse<Void>> handlePastReservationException() {
+    public ResponseEntity<ApiResponse<Void>> handlePastReservationException(HttpServletRequest request,
+                                                                            PastReservationException e) {
+        log.info("{} | {} {}", request.getAttribute("traceId"), e.getClass(), e.getMessage());
         return ResponseEntity
                 .status(UNPROCESSABLE_ENTITY)
                 .body(ApiResponse.fail(PAST_RESERVATION));
     }
 
     @ExceptionHandler(InAlreadyReservationException.class)
-    public ResponseEntity<ApiResponse<Void>> handleAlreadyReservationException() {
+    public ResponseEntity<ApiResponse<Void>> handleAlreadyReservationException(HttpServletRequest request,
+                                                                               InAlreadyReservationException e) {
+        log.info("{} | {} {}", request.getAttribute("traceId"), e.getClass(), e.getMessage());
         return ResponseEntity
                 .status(CONFLICT)
                 .body(ApiResponse.fail(ALREADY_RESERVATION));
     }
 
     @ExceptionHandler(InvalidStatusTransitionException.class)
-    public ResponseEntity<ApiResponse<Void>> handleInvalidStatusTransitionException() {
+    public ResponseEntity<ApiResponse<Void>> handleInvalidStatusTransitionException(HttpServletRequest request,
+                                                                                    InvalidStatusTransitionException e) {
+        log.info("{} | {} {}", request.getAttribute("traceId"), e.getClass(), e.getMessage());
         return ResponseEntity
                 .status(UNPROCESSABLE_ENTITY)
                 .body(ApiResponse.fail(ALREADY_RESERVATION));
