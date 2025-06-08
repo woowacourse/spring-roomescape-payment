eb740e8aef804e429c8810448ff45901614721c5
eunseongu <rndmstjs75@gmail.com>
2025-06-09 02:39:10 +0900

    refactor: 에러 응답에서 ZonedDateTime을 사용하도록 변경

--- src/main/java/roomescape/common/exception/dto/ErrorResponse.java
+++ src/main/java/roomescape/common/exception/dto/ErrorResponse.java
@@ -1,14 +1,14 @@
 package roomescape.common.exception.dto;
 
-import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 
 public record ErrorResponse(
         String code,
         String message,
-        LocalDateTime timestamp
+        ZonedDateTime timestamp
 ) {
 
     public ErrorResponse(final String code, final String message) {
-        this(code, message, LocalDateTime.now());
+        this(code, message, ZonedDateTime.now());
     }
 }
