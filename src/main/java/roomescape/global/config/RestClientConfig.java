a8ad726053c4120cafa7afcb7d4232065f05830c
HyeonsuLee <leehyeonsu4888@naver.com>
2025-06-09 00:53:23 +0900

    feat: 타임아웃 설정

--- src/main/java/roomescape/global/config/RestClientConfig.java
+++ src/main/java/roomescape/global/config/RestClientConfig.java
@@ -2,13 +2,20 @@ package roomescape.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
 @Configuration
 public class RestClientConfig {
 
     @Bean
-    public RestClient generateRestClient() {
-        return RestClient.builder().build();
+    public RestClient restClient() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(3_000);
+        factory.setReadTimeout(3_000);
+
+        return RestClient.builder()
+                .requestFactory(factory)
+                .build();
     }
 }
