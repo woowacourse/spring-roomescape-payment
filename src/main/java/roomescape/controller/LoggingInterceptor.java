8b164103667deba18722211d167ae02c65111e34
ppparkta <pppppark2@gmail.com>
2025-06-09 01:55:10 +0900

    refactor: 로깅 인터셉터 방식으로 변경

---
+++ src/main/java/roomescape/controller/LoggingInterceptor.java
@@ -0,0 +1,21 @@
+package roomescape.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class LoggingInterceptor implements HandlerInterceptor {
+
+    private static final Logger LOG = LoggerFactory.getLogger(LoggingInterceptor.class);
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+        LOG.info("Request: {} {}", request.getMethod(), request.getRequestURI());
+        return true;
+    }
+}
