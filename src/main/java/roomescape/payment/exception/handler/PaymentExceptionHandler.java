ce03fc59c6eef6a91b55773e4c8f798253431a01
Mint <audwl03071@gmail.com>
2025-06-08 12:22:40 +0900

    feat: 토스 에러 메세지를 커스텀 메세지로 변경

--- src/main/java/roomescape/payment/exception/handler/PaymentExceptionHandler.java
+++ src/main/java/roomescape/payment/exception/handler/PaymentExceptionHandler.java
@@ -19,7 +19,8 @@ import roomescape.payment.presentation.dto.response.TossErrorResponse;
 @Component
 public class PaymentExceptionHandler implements ResponseErrorHandler {
 
-    private static final String FORBIDDEN_EXCEPTION_DEFAULT_MESSAGE = "결제가 제한되었습니다. 다른 결제 방법을 시도해주세요.";
+    private static final String UNRECOVERABLE_EXCEPTION_MESSAGE = "이 결제는 처리할 수 없습니다. 고객센터에 문의해주세요.";
+    private static final String FORBIDDEN_EXCEPTION_MESSAGE = "결제가 제한되었습니다. 다른 결제 수단을 이용해주세요.";
     private static final String CLIENT_EXCEPTION_DEFAULT_MESSAGE = "입력하신 정보를 다시 확인해주세요.";
     private static final String SERVER_EXCEPTION_DEFAULT_MESSAGE = "일시적인 시스템 오류가 발생했습니다. 잠시 후 다시 시도해주세요.";
 
@@ -40,16 +41,15 @@ public class PaymentExceptionHandler implements ResponseErrorHandler {
             throws IOException {
         TossErrorResponse errorResponse = objectMapper.readValue(response.getBody(), TossErrorResponse.class);
         String code = errorResponse.code();
-        String message = errorResponse.message();
         TossUnrecoverableErrorCode errorCode = TossUnrecoverableErrorCode.fromCode(code);
         if (errorCode.isUnrecoverable()) {
-            throw new PaymentServerException(code, message);
+            throw new PaymentServerException(code, UNRECOVERABLE_EXCEPTION_MESSAGE);
         }
 
         HttpStatusCode statusCode = response.getStatusCode();
         if (statusCode.is4xxClientError()) {
             if (statusCode == HttpStatus.FORBIDDEN) {
-                throw new PaymentForbiddenException(code, getErrorMessage(code, FORBIDDEN_EXCEPTION_DEFAULT_MESSAGE));
+                throw new PaymentForbiddenException(code, getErrorMessage(code, FORBIDDEN_EXCEPTION_MESSAGE));
             }
             throw new PaymentClientException(code, getErrorMessage(code, CLIENT_EXCEPTION_DEFAULT_MESSAGE));
         }
