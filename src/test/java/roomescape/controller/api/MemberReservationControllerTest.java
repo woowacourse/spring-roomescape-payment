44918507072cf0b58d8026d85a960038f48de8a0
ppparkta <pppppark2@gmail.com>
2025-06-08 15:38:33 +0900

    refactor: context 초기화 방식 통일

--- src/test/java/roomescape/controller/api/MemberReservationControllerTest.java
+++ src/test/java/roomescape/controller/api/MemberReservationControllerTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import roomescape.domain.Payment;
 import roomescape.domain.Reservation;
 import roomescape.domain.ReservationTime;
@@ -25,7 +26,7 @@ import roomescape.repository.ThemeRepository;
 import roomescape.util.JwtTokenProvider;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD)
 class MemberReservationControllerTest {
 
     @Autowired
