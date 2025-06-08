467719b871c4a9d0d4637b497594a91293e63448
seaniiio <cnc4750@naver.com>
2025-06-08 17:16:02 +0900

    refactor: 패키지 구조 수정

--- src/test/java/roomescape/external/payment/TossPaymentClientTest.java
+++ src/test/java/roomescape/external/payment/TossPaymentClientTest.java
@@ -1,4 +1,4 @@
-package roomescape.payment;
+package roomescape.external.payment;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
--- src/test/java/roomescape/external/payment/TossPaymentConfirmErrorHandlerTest.java
+++ src/test/java/roomescape/external/payment/TossPaymentConfirmErrorHandlerTest.java
@@ -1,4 +1,4 @@
-package roomescape.payment;
+package roomescape.external.payment;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
