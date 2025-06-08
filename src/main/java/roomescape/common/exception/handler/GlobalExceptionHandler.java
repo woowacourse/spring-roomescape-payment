c33801fb6962b12d974536d087b348f17b17ff20
kimdev <kim__dev@hufs.ac.kr>
2025-06-08 17:30:31 +0900

    feat: 예외 발생 시 로깅

--- src/main/java/roomescape/common/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/roomescape/common/exception/handler/GlobalExceptionHandler.java
@@ -3,6 +3,8 @@ package roomescape.common.exception.handler;
 import java.time.format.DateTimeParseException;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -18,43 +20,57 @@ import roomescape.payment.exception.PaymentApproveException;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    private final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler(DateTimeParseException.class)
     public ResponseEntity<ErrorResponse> handleDateTimeParseException(DateTimeParseException e) {
+        logException(e);
         return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage()));
     }
 
     @ExceptionHandler(ValidationException.class)
     public ResponseEntity<ErrorResponse> handleValidationException(ValidationException e) {
+        logException(e);
         return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage()));
     }
 
     @ExceptionHandler(InUseException.class)
     public ResponseEntity<ErrorResponse> handleInUseException(InUseException e) {
+        logException(e);
         return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage()));
     }
 
     @ExceptionHandler(UnAuthorizedException.class)
     public ResponseEntity<ErrorResponse> handleUnAuthorizedException(UnAuthorizedException e) {
+        logException(e);
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new ErrorResponse(e.getMessage()));
     }
 
     @ExceptionHandler(ForbiddenException.class)
     public ResponseEntity<ErrorResponse> handleForbiddenException(ForbiddenException e) {
+        logException(e);
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body(new ErrorResponse(e.getMessage()));
     }
 
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ErrorResponse> handleNotFoundException(NotFoundException e) {
+        logException(e);
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse(e.getMessage()));
     }
 
     @ExceptionHandler(DuplicatedException.class)
     public ResponseEntity<ErrorResponse> handleDuplicatedException(DuplicatedException e) {
+        logException(e);
         return ResponseEntity.status(HttpStatus.CONFLICT).body(new ErrorResponse(e.getMessage()));
     }
 
     @ExceptionHandler(PaymentApproveException.class)
     public ResponseEntity<ErrorResponse> handlePaymentApproveException(PaymentApproveException e) {
+        logException(e);
         return ResponseEntity.status(e.getHttpStatusCode()).body(new ErrorResponse(e.getMessage()));
+    }
+
+    private void logException(Exception e) {
+        logger.warn("Exception occurred. ({})", e.toString());
     }
 }
