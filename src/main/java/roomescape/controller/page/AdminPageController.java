d8c03ad142aa2152f6abc1ba9f3c4e58174da70e
ppparkta <pppppark2@gmail.com>
2025-06-09 15:42:55 +0900

    refactor: 사용하지 않는 import 제거

--- src/main/java/roomescape/controller/page/AdminPageController.java
+++ src/main/java/roomescape/controller/page/AdminPageController.java
@@ -1,10 +1,5 @@
 package roomescape.controller.page;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
