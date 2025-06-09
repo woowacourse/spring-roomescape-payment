8873f3cfacc9b08e14c77116e8be382c17a7105d
kwonkeonhyeong <rjsgud19566@gmail.com>
2025-06-09 10:03:38 +0900

    chore: 예외 문구 오타 수정

--- src/main/java/roomescape/exception/PaymentNotFoundException.java
+++ src/main/java/roomescape/exception/PaymentNotFoundException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 
 public class PaymentNotFoundException extends CustomException {
 
-    private static final String MESSAGE = "걸제 내역이 존재하지 않습니다.";
+    private static final String MESSAGE = "결제 내역이 존재하지 않습니다.";
     private static final HttpStatus STATUS = HttpStatus.NOT_FOUND;
 
     public PaymentNotFoundException() {
