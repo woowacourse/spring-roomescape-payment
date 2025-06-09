fb237b2a719003561ba8070871132b0d9484f44f
kimdev <kim__dev@hufs.ac.kr>
2025-06-09 14:03:40 +0900

    refactor: profile 환경 변수 제거 및 RestClientCustomizer를 활용하여 RestClient 구성

--- src/main/java/roomescape/common/config/PaymentClientConfig.java
+++ src/main/java/roomescape/common/config/PaymentClientConfig.java
@@ -1,20 +1,22 @@
 package roomescape.common.config;
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.client.RestClientCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import roomescape.common.properties.PaymentClientProperties;
 import roomescape.payment.exception.handler.PaymentApproveExceptionHandler;
 
+import java.time.Duration;
+
 @Configuration
 @EnableConfigurationProperties(PaymentClientProperties.class)
 public class PaymentClientConfig {
 
-    public static final int REQUEST_CONNECT_TIMEOUT_MILLISECOND = 3_000;
-    public static final int REQUEST_READ_TIMEOUT_MILLISECOND = 5_000;
+    public static final int REQUEST_CONNECT_TIMEOUT_MINUTE = 3;
+    public static final int REQUEST_READ_TIMEOUT_MINUTE = 5;
     private final PaymentClientProperties paymentClientProperties;
 
     public PaymentClientConfig(final PaymentClientProperties paymentClientProperties) {
@@ -22,27 +24,23 @@ public class PaymentClientConfig {
     }
 
     @Bean
-    @Profile("test")
-    public RestClient restClientTest(RestClient.Builder restClientBuilder) {
-        return restClientBuilder
-                .baseUrl(paymentClientProperties.getBaseUrl())
-                .build();
+    public RestClientCustomizer restClientCustomizer() {
+        return restClientBuilder -> restClientBuilder
+                .requestFactory(generateTimeoutRequestFactory());
+    }
+
+    private SimpleClientHttpRequestFactory generateTimeoutRequestFactory() {
+        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        requestFactory.setConnectTimeout(Duration.ofMinutes(REQUEST_CONNECT_TIMEOUT_MINUTE));
+        requestFactory.setReadTimeout(Duration.ofMinutes(REQUEST_READ_TIMEOUT_MINUTE));
+        return requestFactory;
     }
 
     @Bean
-    @Profile("!test")
-    public RestClient restClient(RestClient.Builder restClientBuilder) {
+    public RestClient paymentRestClient(RestClient.Builder restClientBuilder) {
         return restClientBuilder
-                .requestFactory(generateRequestFactory())
                 .baseUrl(paymentClientProperties.getBaseUrl())
                 .build();
-    }
-
-    private SimpleClientHttpRequestFactory generateRequestFactory() {
-        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
-        requestFactory.setConnectTimeout(REQUEST_CONNECT_TIMEOUT_MILLISECOND);
-        requestFactory.setReadTimeout(REQUEST_READ_TIMEOUT_MILLISECOND);
-        return requestFactory;
     }
 
     @Bean
