0e036b7baa6acf224f393f239e68bf7e0d250a71
ChoiDongjun <adong0808@naver.com>
2025-06-09 20:58:08 +0900

    refactor: 로그 포맷 json 형식으로 구조화

--- src/main/java/roomescape/global/LoggingAspect.java
+++ src/main/java/roomescape/global/LoggingAspect.java
@@ -1,103 +1,67 @@
 package roomescape.global;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.*;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @Aspect
 @Component
 @Slf4j
 public class LoggingAspect {
 
+    private final ObjectMapper objectMapper;
+
+    public LoggingAspect(final ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
     private final ThreadLocal<Long> startTime = new ThreadLocal<>();
 
     @Pointcut("within(roomescape.controller.api..*)")
     public void controllerMethods() {}
 
-//    @Around("controllerMethods()")
-//    public Object aroundLog(ProceedingJoinPoint joinPoint) throws Throwable {
-//        long start = System.currentTimeMillis();
-//
-//        ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
-//        HttpServletRequest request = requestAttributes.getRequest();
-//        String method = request.getMethod();
-//        String requestURI = request.getRequestURI();
-//        String queryString = request.getQueryString();
-//        String fullUri = requestURI + (queryString == null ? "" : "?" + queryString);
-//        Object[] args = joinPoint.getArgs();
-//
-//        log.info("""
-//                [Request]
-//                Method: {},
-//                Uri: {},
-//                Args: {}
-//                """, method, fullUri, args);
-//
-//        try {
-//            Object result = joinPoint.proceed();
-//
-//            long elapsed = System.currentTimeMillis() - start;
-//            log.info("""
-//                    [Response]
-//                    Method: {},
-//                    Time: {} ms
-//                    Result: {}
-//                    """, method, elapsed, result);
-//            return result;
-//        } catch (Exception e) {
-//            long elapsed = System.currentTimeMillis() - start;
-//            log.error("""
-//                    [Error]
-//                    Method: {},
-//                    Time: {} ms,
-//                    Message: {}
-//                    """, method, elapsed, e.getMessage());
-//            throw e;
-//        }
-//    }
-
     @Before("controllerMethods()")
-    public void beforeLog(JoinPoint joinPoint) {
+    public void beforeLog(JoinPoint joinPoint) throws JsonProcessingException {
         startTime.set(System.currentTimeMillis());
         ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
         HttpServletRequest request = requestAttributes.getRequest();
-        String method = request.getMethod();
+
+        Map<String, Object> requestLogInfos = new LinkedHashMap<>();
+        requestLogInfos.put("Log Type", "Request");
+        requestLogInfos.put("Methods", request.getMethod());
         String requestURI = request.getRequestURI();
         String queryString = request.getQueryString();
         String fullUri = requestURI + (queryString == null ? "" : "?" + queryString);
-        Object[] args = joinPoint.getArgs();
+        requestLogInfos.put("Uri", fullUri);
+        requestLogInfos.put("Args", joinPoint.getArgs());
 
-        log.info("""
-                [Request]
-                Method: {},
-                Uri: {},
-                Args: {}
-                """, method, fullUri, args);
+        if (log.isInfoEnabled()) {
+            log.info(objectMapper.writeValueAsString(requestLogInfos));
+        }
     }
 
     @AfterReturning(pointcut = "controllerMethods()", returning = "result")
-    public void afterLog(JoinPoint joinPoint, Object result) {
+    public void afterLog(JoinPoint joinPoint, Object result) throws JsonProcessingException {
+        Map<String, Object> responseLogInfos = new LinkedHashMap<>();
+        responseLogInfos.put("Log Type", "Response");
+        responseLogInfos.put("Method", joinPoint.getSignature().getName());
         long elapsed = System.currentTimeMillis() - startTime.get();
-        log.info("""
-                [Response]
-                Method: {},
-                Time: {} ms
-                Result: {}
-                """, joinPoint.getSignature().getName(), elapsed, result);
-    }
-
-    @AfterThrowing(pointcut = "controllerMethods()", throwing = "ex")
-    public void afterThrowingLog(JoinPoint joinPoint, Throwable ex) {
-        long elapsed = System.currentTimeMillis() - startTime.get();
-        log.error("""
-                [Error]
-                Method: {},
-                Time: {} ms,
-                Message: {}
-                """, joinPoint.getSignature().getName(), elapsed, ex.getMessage());
+        responseLogInfos.put("Time", elapsed + "ms");
+        responseLogInfos.put("Result", result);
+        if (log.isInfoEnabled()) {
+            log.info(objectMapper.writeValueAsString(responseLogInfos));
+        }
     }
 }
