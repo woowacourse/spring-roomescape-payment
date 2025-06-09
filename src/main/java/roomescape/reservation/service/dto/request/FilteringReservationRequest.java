3c3587f9cc4bfdef21dcb863e9191a7c87f741c1
minSsan <msp1125@hanyang.ac.kr>
2025-06-09 15:44:40 +0900

    fix: Reservation 필터링 쿼리 파라미터 nullable 로 변경

--- src/main/java/roomescape/reservation/service/dto/request/FilteringReservationRequest.java
+++ src/main/java/roomescape/reservation/service/dto/request/FilteringReservationRequest.java
@@ -1,18 +1,20 @@
 package roomescape.reservation.service.dto.request;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.PastOrPresent;
 
 import java.time.LocalDate;
 
 public record FilteringReservationRequest(
-        @NotNull
+        @Nullable
         Long themeId,
-        @NotNull
+        @Nullable
         Long memberId,
+        @Nullable
         @Past
         LocalDate dateFrom,
+        @Nullable
         @PastOrPresent
         LocalDate dateTo
 ) {
