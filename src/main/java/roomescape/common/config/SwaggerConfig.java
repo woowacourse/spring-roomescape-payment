218b87027ac92f2d6c227646a446f8de3cf1ed09
MinJun Choi <mj043000@naver.com>
2025-06-10 22:43:45 +0900

    chore: Swagger API 버전 변경

--- src/main/java/roomescape/common/config/SwaggerConfig.java
+++ src/main/java/roomescape/common/config/SwaggerConfig.java
@@ -1,19 +1,45 @@
 package roomescape.common.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
+    @Value("${app.version}")
+    private String appVersion;
+
     @Bean
     public OpenAPI springShopOpenAPI() {
-        String title = "RoomEscape Application Swagger";
-        String description = "방탈출 서비스의 API 문서입니다.";
+        final String title = "RoomEscape Application Swagger";
+        final String description = "방탈출 서비스의 API 문서입니다.";
 
-        Info info = new Info().title(title).description(description).version("1.0.0");
-
-        return new OpenAPI().info(info);
+        return new OpenAPI()
+                .info(new Info()
+                        .title(title)
+                        .description(description)
+                        .version(appVersion)
+                        .contact(new Contact()
+                                .name("MJ")
+                                .email("mj@example.com"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("http://www.apache.org/licenses/LICENSE-2.0.html"))
+                )
+                .servers(List.of(
+                        new Server()
+                                .url("http://localhost:8080")
+                                .description("로컬 서버"),
+                        new Server()
+                                .url("http://3.36.117.160:8080")
+                                .description("프로덕션 서버")
+                ));
     }
 }
