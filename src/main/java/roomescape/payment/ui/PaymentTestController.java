1c7edb50cdd53575122fe626adfa4a3a6682ebd7
Yeongjae Kim <sees77770@gmail.com>
2025-06-11 01:52:48 +0900

    refactor: 테스트용 api 환경 설정

--- src/main/java/roomescape/payment/ui/PaymentTestController.java
+++ src/main/java/roomescape/payment/ui/PaymentTestController.java
@@ -1,23 +1,27 @@
 package roomescape.payment.ui;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import roomescape.payment.application.dto.PrePaymentValidRequest;
 
-@Tag(name = "결제", description = "결제 관련 API")
+@Profile("test")
 @RestController
-public class PaymentController {
+public class PaymentTestController {
 
     public static final String PRE_PAYMENT = "prePayment";
 
-    @Operation(summary = "선결제 정보 저장 API", description = "선결제 정보를 세션에 저장합니다. 결제 요청 전에 호출되어야 합니다.")
-    @PostMapping("/payments")
-    public ResponseEntity<Void> setPrePayment(@RequestBody final PrePaymentValidRequest request, HttpSession session) {
+    @Operation(summary = "테스트용 선결제 정보 저장 API", description = "테스트를 위해 선결제 정보를 세션에 저장합니다.")
+    @PostMapping("/test/pre-payment")
+    public ResponseEntity<Void> storePrePaymentInSession(
+            @RequestBody @Valid PrePaymentValidRequest request,
+            HttpSession session
+    ) {
         session.setAttribute(PRE_PAYMENT, request);
         return ResponseEntity.ok().build();
     }
