cd9dfeb27ea385e48e4a264aa3cb820eacf94613
wogus <ind07152@naver.com>
2025-06-10 20:22:40 +0900

    feat: 엔티티 아이디 전략 수정

--- src/main/java/roomescape/payment/domain/Payment.java
+++ src/main/java/roomescape/payment/domain/Payment.java
@@ -6,6 +6,7 @@ import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
@@ -15,7 +16,7 @@ import roomescape.reservation.domain.Reservation;
 public class Payment {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "payment_key", nullable = false)
