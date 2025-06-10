4994bd09e097d5950171d3593727461c30921b95
junhaa <2171326@hansung.ac.kr>
2025-06-10 23:16:45 +0900

    refactor: ThreadPoolTaskExecutor 설정 값 추가

--- src/main/java/roomescape/common/config/AsyncConfig.java
+++ src/main/java/roomescape/common/config/AsyncConfig.java
@@ -1,6 +1,7 @@
 package roomescape.common.config;
 
 import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -10,9 +11,20 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 @EnableAsync
 public class AsyncConfig implements AsyncConfigurer {
 
+    private static int CORE_POOL_SIZE= 2;
+    private static int MAX_POOL_SIZE = 4;
+    private static int QUEUE_CAPACITY = 50;
+    private static String THREAD_NAME_PREFIX = "async-task";
+
     @Override
     public Executor getAsyncExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(CORE_POOL_SIZE);
+        executor.setMaxPoolSize(MAX_POOL_SIZE);
+        executor.setQueueCapacity(QUEUE_CAPACITY);
+        executor.setThreadNamePrefix(THREAD_NAME_PREFIX);
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
         executor.setTaskDecorator(new MdcTaskDecorator());
         executor.initialize();
         return executor;
