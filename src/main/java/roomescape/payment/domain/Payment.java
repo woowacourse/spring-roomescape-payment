de240fdac4913f28fae458ceca771b4b3d2a3352
Yeongjae Kim <sees77770@gmail.com>
2025-06-11 01:39:27 +0900

    refactor: Payment 생성자 private으로 변경

--- src/main/java/roomescape/payment/domain/Payment.java
+++ src/main/java/roomescape/payment/domain/Payment.java
@@ -34,8 +34,14 @@ public class Payment {
     @Enumerated(value = EnumType.STRING)
     private PaymentStatus paymentStatus;
 
-    public Payment(final Long id, final String orderId, final String paymentKey, final BigDecimal amount,
-                   final Reservation reservation, final PaymentStatus paymentStatus) {
+    private Payment(
+            final Long id,
+            final String orderId,
+            final String paymentKey,
+            final BigDecimal amount,
+            final Reservation reservation,
+            final PaymentStatus paymentStatus
+    ) {
         this.id = id;
         this.orderId = orderId;
         this.paymentKey = paymentKey;
