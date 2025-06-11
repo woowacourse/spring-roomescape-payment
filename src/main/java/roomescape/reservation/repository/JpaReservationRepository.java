af7708ec122bce1b071d822f9ae193811d0db9ab
byesol <bsj5967@naver.com>
2025-06-11 22:49:40 +0900

    fix: entityGraph 설정

--- src/main/java/roomescape/reservation/repository/JpaReservationRepository.java
+++ src/main/java/roomescape/reservation/repository/JpaReservationRepository.java
@@ -70,5 +70,9 @@ public interface JpaReservationRepository extends ListCrudRepository<Reservation
             @Param("themeId") Long themeId
     );
 
+    @EntityGraph(
+            type = EntityGraph.EntityGraphType.FETCH,
+            attributePaths = {"info.time", "info.theme", "member"}
+    )
     List<Reservation> findByMemberId(Long memberId);
 }
