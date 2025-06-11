9564fb3852c9e96a8ae2962dd4378995900f4d5e
DongchannN <ldc991104@gmail.com>
2025-06-11 17:28:11 +0900

    test: 불필요한 테스트 코드 삭제

--- src/test/java/roomescape/RoomescapeApplicationTest.java
+++ src/test/java/roomescape/RoomescapeApplicationTest.java
@@ -1,40 +1,12 @@
 package roomescape;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@Disabled("필요 시 해제")
-@ActiveProfiles("default")
 @SpringBootTest
 class RoomescapeApplicationTest {
 
-        @Value("${toss.payments.base-url}")
-        private String baseUrl;
-
-        @Value("${toss.payments.widget-secret-key}")
-        private String secretKey;
-
         @Test
         void contextLoads() {
-        }
-
-        @Test
-        @DisplayName("Toss Payments 설정이 올바르게 로드되는지 확인")
-        void checkTossPaymentsConfiguration() {
-                assertThat(baseUrl)
-                        .isNotNull()
-                        .isNotBlank()
-                        .isEqualTo("https://api.tosspayments.com");
-
-                assertThat(secretKey)
-                        .isNotNull()
-                        .isNotBlank()
-                        .doesNotContain("${TOSS_SECRET_KEY}");
         }
 }
