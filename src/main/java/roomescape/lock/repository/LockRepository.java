c42067ae133596df30fa69859faa5b8e93c8fcbf
ryan-dia <cjfdnjs158@naver.com>
2025-06-12 17:46:45 +0900

    fix: LockJpaRepository 잘못 설정된 key 타입 변경

--- src/main/java/roomescape/lock/repository/LockRepository.java
+++ src/main/java/roomescape/lock/repository/LockRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import roomescape.lock.LockEntity;
 
-public interface LockRepository extends JpaRepository<LockEntity, String> {
+public interface LockRepository extends JpaRepository<LockEntity, Long> {
 
     @org.springframework.data.jpa.repository.Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("select l from LockEntity l where l.lockKey = :key")
