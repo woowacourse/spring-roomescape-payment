186fdf9d70cc61585cc5d1a8ccf6980dcb25ee6e
minSsan <msp1125@hanyang.ac.kr>
2025-06-12 17:40:29 +0900

    feat(Amount): 검증 로직 예외를 BadRequestException으로 변경

--- src/main/java/roomescape/payment/domain/vo/Amount.java
+++ src/main/java/roomescape/payment/domain/vo/Amount.java
@@ -1,13 +1,14 @@
 package roomescape.payment.domain.vo;
 
 import jakarta.persistence.Embeddable;
+import roomescape.common.exception.BadRequestException;
 
 @Embeddable
 public record Amount(Long value) {
 
     public Amount {
         if (value < 0) {
-            throw new IllegalArgumentException("금액은 음수가 될 수 없습니다.");
+            throw new BadRequestException("금액은 음수가 될 수 없습니다.");
         }
     }
 }
