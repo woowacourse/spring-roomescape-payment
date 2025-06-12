55839c4556bce622d66abdb25150090071e1bf11
kjyyjk <rlawnsdud920@gmail.com>
2025-06-12 15:05:26 +0900

    refactor: springdoc의 서버 url 설정 파일로 분리

--- src/main/java/roomescape/common/config/SpringDocsConfig.java
+++ src/main/java/roomescape/common/config/SpringDocsConfig.java
@@ -1,24 +1,40 @@
 package roomescape.common.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.info.Contact;
-import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-@OpenAPIDefinition(
-        info = @Info(title = "방탈출 API", version = "v1",
-                description = "우아한테크코스 7기 레벨2 방탈출 예약 관리 시스템",
-                contact = @Contact(name = "레오", email = "rlawnsdud920@gmail.com")),
-        servers = @Server(url = "http://3.35.134.88:8080/")
-)
 @SecurityScheme(
         name = "cookieAuth",
         type = SecuritySchemeType.APIKEY,
         in = SecuritySchemeIn.COOKIE,
         paramName = "access_token"
 )
+@Configuration
 public class SpringDocsConfig {
+
+    @Value(value = "${springdoc.server-url}")
+    private String url;
+
+    @Bean
+    public OpenAPI openAPI() {
+        Contact contact = new Contact()
+                .name("레오")
+                .email("rlawnsdud920@navr.com");
+        Info info = new Info()
+                .title("방탈출 API")
+                .version("v1")
+                .description("우아한테크코스 7기 레벨2 방탈출 예약 관리 시스템")
+                .contact(contact);
+        return new OpenAPI()
+                .info(info)
+                .addServersItem(new Server().url(url));
+    }
 }
